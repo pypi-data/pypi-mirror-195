a39c71317b522f42e147c13af0627b51bbca4cb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:16:17 +0000

    Add package Geode-MyModule/1.2.0/Geode_MyModule-1.2.0-cp39-cp39-manylinux2014_x86_64.whl

---
+++ packages/Geode-MyModule/Geode_MyModule-1.2.0-cp39-cp39-manylinux2014_x86_64.whl/geode_mymodule/mylib.py
@@ -0,0 +1,9 @@
+#
+# Copyright (c) 2019 - 2022 Geode-solutions. All rights reserved.
+#
+
+import opengeode
+from geode_common import core
+
+from .geode_mymodule_py_mylib import *
+MyModuleMyLib.initialize()
