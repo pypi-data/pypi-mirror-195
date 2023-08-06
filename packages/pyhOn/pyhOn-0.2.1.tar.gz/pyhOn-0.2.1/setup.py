256bb2eeeb0c5293be4d012277068d638eea501a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:24 +0000

    Add package pyhOn/0.2.1/pyhOn-0.2.1.tar.gz

---
+++ packages/pyhOn/pyhOn-0.2.1.tar.gz/pyhOn-0.2.1/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name="pyhOn",
+    version="0.2.1",
+    author="Andre Basche",
+    description="Control hOn devices with python",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url="https://github.com/Andre0512/pyh0n",
+    license="MIT",
+    platforms="any",
+    packages=find_packages(),
+    include_package_data=True,
+    python_requires=">=3.10",
+    install_requires=["aiohttp"],
+    entry_points={
+        'console_scripts': [
+            'pyhOn = pyhon.__main__:start',
+        ]
+    }
+)
