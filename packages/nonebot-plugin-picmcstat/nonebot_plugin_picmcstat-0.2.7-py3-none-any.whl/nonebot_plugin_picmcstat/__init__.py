d1a56ebb70eca028f2731b433cfe6fd873f3350a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:32:58 +0000

    Add package nonebot-plugin-picmcstat/0.2.7/nonebot_plugin_picmcstat-0.2.7-py3-none-any.whl

---
+++ packages/nonebot-plugin-picmcstat/nonebot_plugin_picmcstat-0.2.7-py3-none-any.whl/nonebot_plugin_picmcstat/__init__.py
@@ -0,0 +1,13 @@
+from nonebot.plugin import PluginMetadata, require
+
+require("nonebot_plugin_imageutils")
+
+from .__main__ import *  # noqa
+
+__plugin_meta__ = PluginMetadata(
+    name="PicMCStat",
+    description="将一个 Minecraft 服务器的 MOTD 信息绘制为一张图片",
+    usage="使用 motd 指令查看使用帮助",
+)
+
+__version__ = "0.2.7"
