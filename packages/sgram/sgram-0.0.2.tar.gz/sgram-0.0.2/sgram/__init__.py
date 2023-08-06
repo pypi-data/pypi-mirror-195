0ad3b204d171f856b5715194dd910515c312bc22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:21 +0000

    Add package sgram/0.0.2/sgram-0.0.2.tar.gz

---
+++ packages/sgram/sgram-0.0.2.tar.gz/sgram-0.0.2/sgram/__init__.py
@@ -0,0 +1,11 @@
+__version__ = "0.0.2"
+__license__ = "GNU Lesser General Public License v3.0 (LGPL-3.0)"
+__copyright__ = "Copyright (C) 2023-present Pr0fess0r-99 <https://github.com/Sgram-s>"
+
+def sgram_install():
+  print(
+      f'Successfully installled sgram {__version__}' + '\n'
+      f'Copyright under {__copyright__}'
+      f'License under installed {__license__}'
+  )
+
