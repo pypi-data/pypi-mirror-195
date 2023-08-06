5ce10200d953101f60275d3c31e0720693ef4fd1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:52 +0000

    Add package distributions-violkim-04mar2023/0.1/distributions-violkim-04mar2023-0.1.tar.gz

---
+++ packages/distributions-violkim-04mar2023/distributions-violkim-04mar2023-0.1.tar.gz/distributions-violkim-04mar2023-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions-violkim-04mar2023',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions-violkim-04mar2023'],
+      author = 'Chester Kim',
+      author_email = 'violkim@gmail.com',
+      zip_safe=False)
