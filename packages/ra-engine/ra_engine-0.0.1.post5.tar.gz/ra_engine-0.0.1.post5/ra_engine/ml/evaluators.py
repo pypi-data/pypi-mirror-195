a1cbedeb946e0138d014c9258eeadb0b69cb9e6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:13:10 +0000

    Add package ra-engine/0.0.1.post5/ra_engine-0.0.1.post5.tar.gz

---
+++ packages/ra-engine/ra_engine-0.0.1.post5.tar.gz/ra_engine-0.0.1.post5/ra_engine/ml/evaluators.py
@@ -0,0 +1,88 @@
+import requests
+from ra_engine.core.app import RAEApp
+from ra_engine.type_def.ml import TrainData, PredData, MLData, TSData
+import pandas as pd
+from ra_engine.type_def.results import Response
+
+
+class EvaluatorML:
+    def __init__(
+        self,
+        endpoint: str,
+        app: RAEApp,
+        train_df: pd.DataFrame,
+        pred_df: pd.DataFrame,
+        features: list,
+        targets: list,
+        train_config: dict = None,
+        pred_config: dict = None,
+    ):
+        self.rae_app: RAEApp = app
+        self.ml_data = MLData(
+            TrainData(train_df, features, targets, train_config),
+            PredData(pred_df, pred_config),
+        )
+        self._app = app.app()
+        self.endpoint = endpoint
+        if self._app is None:
+            raise ValueError("RAEApp is not initialized. Please run app.init() first.")
+        if self._app.result is None:
+            raise ValueError(
+                "Provided RAEApp is not authenticated properly. Please check your credentials."
+            )
+
+    def exec(self) -> Response:
+        self.response = requests.get(
+            self.rae_app.credentials.host + self.endpoint,
+            json=self.ml_data.as_dict(),
+            headers={
+                "Content-Type": "application/json",
+                "Authorization": f"Bearer {self._app.result['jwt']}",
+            },
+        )
+        return Response(self.response)
+
+    def inputs(self) -> dict:
+        return self.ml_data.as_dict()
+
+
+class EvaluatorTS:
+    def __init__(
+        self,
+        endpoint: str,
+        app: RAEApp,
+        train_df: pd.DataFrame,
+        dates_col: str,
+        target_col: str,
+        train_config: dict = None,
+        forcast_for: int = 1,
+    ):
+
+        self.rae_app: RAEApp = app
+        self.ts_data = TSData(
+            train_df, dates_col, target_col, train_config, forcast_for
+        )
+        self._app = app.app()
+        self.endpoint = endpoint
+        self.response = None
+        self._json = None
+        if self._app is None:
+            raise ValueError("RAEApp is not initialized. Please run app.init() first.")
+        if self._app.result is None:
+            raise ValueError(
+                "Provided RAEApp is not authenticated properly. Please check your credentials."
+            )
+
+    def exec(self) -> Response:
+        self.response = requests.get(
+            self.rae_app.credentials.host + self.endpoint,
+            json=self.ts_data.as_dict(),
+            headers={
+                "Content-Type": "application/json",
+                "Authorization": f"Bearer {self._app.result['jwt']}",
+            },
+        )
+        return Response(self.response)
+
+    def inputs(self) -> dict:
+        return self.ts_data.as_dict()
