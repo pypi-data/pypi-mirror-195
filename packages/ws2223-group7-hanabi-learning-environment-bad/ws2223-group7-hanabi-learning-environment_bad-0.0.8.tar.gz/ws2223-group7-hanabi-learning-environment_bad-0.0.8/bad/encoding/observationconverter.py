bd27033ab230f807facb388ff4a487804f756441
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:38:24 +0000

    Add package ws2223-group7-hanabi-learning-environment-bad/0.0.8/ws2223-group7-hanabi-learning-environment_bad-0.0.8.tar.gz

---
+++ packages/ws2223-group7-hanabi-learning-environment-bad/ws2223-group7-hanabi-learning-environment_bad-0.0.8.tar.gz/ws2223-group7-hanabi-learning-environment_bad-0.0.8/bad/encoding/observationconverter.py
@@ -0,0 +1,16 @@
+# pylint: disable=missing-module-docstring, wrong-import-position, too-few-public-methods
+import sys
+import os
+
+currentPath = os.path.dirname(os.path.realpath(__file__))
+parentPath = os.path.dirname(currentPath)
+sys.path.append(parentPath)
+
+from bad.encoding.observation import Observation
+
+class ObservationConverter:
+    ''' converting hanabi observation to vector observation '''
+
+    def convert(self, observation: dict) -> Observation:
+        ''' one hot encoding '''
+        return Observation(observation)
