963b769d5720a66cc4af86e2dcfe804636524d2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:58 +0000

    Add package datasette-ui-extras/0.31.4/datasette_ui_extras-0.31.4-py3-none-any.whl

---
+++ packages/datasette-ui-extras/datasette_ui_extras-0.31.4-py3-none-any.whl/datasette_ui_extras/column_stats_schema.py
@@ -0,0 +1,204 @@
+import sqlite3
+
+DUX_COLUMN_STATS = 'dux_column_stats'
+DUX_COLUMN_STATS_OPS = 'dux_column_stats_ops'
+DUX_COLUMN_STATS_VALUES = 'dux_column_stats_values'
+DUX_PENDING_ROWS = 'dux_pending_rows'
+DUX_IDS = 'dux_ids'
+
+# This table should have reasonable defaults so we can insert new rows
+# without having knowledge about what stats are being collected.
+CREATE_DUX_COLUMN_STATS = '''
+CREATE TABLE {}(
+  table_id integer not null references dux_ids(id),
+  column_id integer not null references dux_ids(id),
+  type text not null,
+  nullable boolean not null,
+  pk boolean not null,
+  min any,
+  max any,
+  count integer not null default 0, -- count(*)
+  nulls integer not null default 0, -- the output of COUNT(*) FILTER (WHERE TYPEOF(column) == 'null')
+  integers integer not null default 0, -- as above, but integer
+  reals integer not null default 0, -- as above, but real
+  texts integer not null default 0, -- as above, but text
+  blobs integer not null default 0, -- as above, but blob
+  json_strings integer not null default 0, -- sum(json_valid(column) and json_type(column) = 'text')
+  json_arrays integer not null default 0, -- sum(json_valid(column) and json_type(column) = 'array')
+  json_objects integer not null default 0, -- sum(json_valid(column) and json_type(column) = 'object')
+  texts_min_length integer default 0,
+  texts_max_length integer default 0,
+  texts_whitespace integer not null default 0, -- # of texts that have whitespace
+  texts_newline integer not null default 0, -- # of texts that have a newline
+  blobs_min_length integer default 0,
+  blobs_max_length integer default 0,
+  primary key (table_id, column_id)
+)
+'''.format(DUX_COLUMN_STATS).strip()
+
+CREATE_DUX_COLUMN_STATS_VALUES = '''
+CREATE TABLE {}(
+  table_id integer not null references dux_ids(id),
+  column_id integer not null references dux_ids(id),
+  value text,
+  hash blob not null,
+  count integer not null,
+  pks text not null, -- JSON array of pkeys that have this value
+  primary key (table_id, column_id, value, hash)
+)
+'''.format(DUX_COLUMN_STATS_VALUES).strip()
+
+CREATE_DUX_IDS = '''
+CREATE TABLE {}(
+  id integer primary key,
+  name text not null unique
+)
+'''.format(DUX_IDS).strip()
+
+CREATE_DUX_COLUMN_STATS_OPS = '''
+CREATE TABLE {}(
+  table_id integer REFERENCES dux_ids(id),
+  column_id integer REFERENCES dux_ids(id),
+  last_key text not null default '{{}}',
+  pending integer not null check (pending in (0, 1)),
+  updated_at text not null default '1970-01-01 00:00:00Z',
+  primary key (table_id, column_id)
+)
+'''.format(DUX_COLUMN_STATS_OPS).strip()
+
+CREATE_DUX_PENDING_ROWS = '''
+CREATE TABLE {}(
+  id integer primary key,
+  "table" text not null,
+  the_rowid integer, -- for rowid tables, store the rowid
+  old text,
+  new text,
+  timestamp integer not null default (strftime('%Y-%m-%d %H:%M:%fZ'))
+)
+'''.format(DUX_PENDING_ROWS).strip()
+
+table_schemas = {
+    DUX_PENDING_ROWS: CREATE_DUX_PENDING_ROWS,
+    DUX_IDS: CREATE_DUX_IDS,
+    DUX_COLUMN_STATS_OPS: CREATE_DUX_COLUMN_STATS_OPS,
+    DUX_COLUMN_STATS: CREATE_DUX_COLUMN_STATS,
+    DUX_COLUMN_STATS_VALUES: CREATE_DUX_COLUMN_STATS_VALUES,
+}
+
+def ensure_schema_and_triggers(conn):
+    # We take a very brute force approach: if any table has the wrong schema,
+    # we'll drop all the tables.
+    ensure_schema(conn)
+    ensure_triggers(conn)
+
+def ensure_triggers(conn):
+    tables = indexable_tables(conn)
+
+    actual_triggers = conn.execute("SELECT name, tbl_name, sql FROM sqlite_master WHERE type = 'trigger' AND name LIKE 'dux_stats_%'")
+    actual_triggers = [(row[0], row[1], row[2]) for row in actual_triggers]
+
+    # We install 3 triggers: dux_stats_insert_<table>, dux_stats_update_<table>, dux_stats_delete_<table>
+    #
+    # We compute the expected set of triggers, compare it to the actual, and drop/create as needed.
+    expected_triggers = []
+    for table in tables:
+        is_rowid_table = True
+
+        if list(conn.execute('SELECT * FROM pragma_index_info(?)', [table])):
+            is_rowid_table = False
+
+        for name, tbl_name, sql in get_stats_triggers(conn, table, is_rowid_table):
+            expected_triggers.append((name, tbl_name, sql))
+
+    for actual in actual_triggers:
+        if not actual in expected_triggers:
+            conn.execute('DROP TRIGGER "{}"'.format(actual[0]))
+
+    for expected in expected_triggers:
+        if not expected in actual_triggers:
+            conn.execute(expected[2])
+
+def get_stats_triggers(conn, table, is_rowid_table):
+    columns = indexable_columns(conn, table)
+
+    def generate_json_object(prefix):
+        rv = ['json_object(']
+
+        sep = ''
+        for column in columns:
+            rv.append("{sep}CASE WHEN typeof({prefix}.\"{column}\") == 'blob' THEN '{column}_blob' ELSE '{column}_literal' END, CASE WHEN typeof({prefix}.\"{column}\") == 'blob' THEN hex({prefix}.\"{column}\") ELSE {prefix}.\"{column}\" END".format(column=column, prefix=prefix, sep=sep))
+            sep = ', '
+
+        rv.append(')')
+
+        return ''.join(rv)
+
+    rv = []
+    for op in ['delete', 'insert', 'update']:
+        trigger_name = 'dux_stats_{}_{}'.format(op, table)
+        maybe_old = 'NULL'
+        maybe_new = 'NULL'
+
+        if op == 'delete' or op == 'update':
+            maybe_old = generate_json_object('old')
+
+        if op == 'insert' or op == 'update':
+            maybe_new = generate_json_object('new')
+
+        maybe_rowid = 'NULL'
+        if is_rowid_table and op == 'delete':
+            maybe_rowid = '"old".rowid'
+        elif is_rowid_table and op == 'insert':
+            maybe_rowid = '"new".rowid'
+        elif is_rowid_table and op == 'update':
+            maybe_rowid = '"old".rowid'
+
+        sql = '''
+CREATE TRIGGER "{trigger_name}" AFTER {op} ON "{table}" FOR EACH ROW
+BEGIN
+  INSERT INTO dux_pending_rows("table", "the_rowid", "old", "new") VALUES ('{table}', {maybe_rowid}, {maybe_old}, {maybe_new});
+END
+'''.format(trigger_name=trigger_name, op=op, table=table, maybe_rowid=maybe_rowid, maybe_old=maybe_old, maybe_new=maybe_new).strip()
+
+
+        rv.append((trigger_name, table, sql))
+    return rv
+
+def ensure_schema(conn):
+    def get_sql(table):
+        sql = conn.execute("SELECT sql FROM sqlite_master WHERE name = ?", [table]).fetchone()
+
+        if not sql:
+            return None
+
+        return sql[0]
+
+    all_ok = True
+    for k, v in table_schemas.items():
+        if get_sql(k) != v:
+            all_ok = False
+            break
+
+    if all_ok:
+        return
+
+    # Drop all the tables, in reverse order so fkeys don't cause us grief.
+    for table in reversed(list(table_schemas.keys())):
+        conn.execute('DROP TABLE IF EXISTS {}'.format(table))
+
+    for table, sql in table_schemas.items():
+        conn.execute(sql)
+
+        roundtrip = get_sql(table)
+        if roundtrip != sql:
+            raise Exception('failed to create sql for table {}\nexpected: {}\nactual: {}'.format(table, sql, roundtrip))
+
+# A table is indexable if:
+# - not a virtual table (eg fts_posts)
+# - not a suffix of a virtual table (eg, fts_posts_idx)
+# - not a dux_ table
+def indexable_tables(conn):
+    return [row[0] for row in conn.execute("select name from sqlite_master sm where type = 'table' and not sql like 'CREATE VIRTUAL TABLE%' and not name like 'dux_%' and not exists(select 1 from sqlite_master sm2 where type = 'table' and sql like 'CREATE VIRTUAL TABLE%' and sm.name like sm2.name || '_%')")]
+
+def indexable_columns(conn, table):
+    return [row[0] for row in conn.execute('select name from pragma_table_info(?)', [table])]
---
+++ packages/datasette-ui-extras/datasette_ui_extras-0.31.4-py3-none-any.whl/datasette_ui_extras/static/lazy-facets.js
@@ -0,0 +1,318 @@
+(function () {
+  function initialize() {
+    // Only run on the table page
+    if (!document.body.classList.contains('table'))
+      return;
+
+
+    renderFacetOptions();
+
+    const tableWrapper = document.querySelector('.table-wrapper');
+
+    // This is unexpected, but let's gracefully fail.
+    if (!tableWrapper) {
+      document.body.classList.add('lazy-facets-ready');
+      return;
+    }
+
+    const tableWrapperWrapper = document.createElement('div');
+    tableWrapperWrapper.classList.add('table-wrapper-wrapper');
+
+    const flexy = document.createElement('div');
+    flexy.classList.add('facet-table-wrapper');
+    const parent = tableWrapper.parentElement;
+    parent.insertBefore(flexy, tableWrapper);
+
+    let facetResults = document.querySelector('.facet-results');
+    if (!facetResults) {
+      if (__dux_facets.length === 0) {
+        document.body.classList.add('lazy-facets-ready');
+        return;
+      }
+
+      facetResults = document.createElement('div');
+      facetResults.classList.add('facet-results');
+    }
+
+    flexy.insertBefore(facetResults, null);
+    const spinnerContainer = document.createElement('div');
+    spinnerContainer.classList.add('spinner-container');
+    const spinner = document.createElement('div');
+    spinner.classList.add('spinner');
+    spinnerContainer.appendChild(spinner);
+    facetResults.appendChild(spinnerContainer);
+
+    flexy.appendChild(tableWrapperWrapper);
+
+    tableWrapperWrapper.appendChild(tableWrapper);
+
+    // We hide the body tag to avoid jank when we insert the flexbox.
+    // A better approach would be to ensure the HTML structure is suitable
+    // at render-time, but that might require fiddling with templates in
+    // a way that breaks extensibility.
+    document.body.classList.add('lazy-facets-ready');
+    fetchFacets(__dux_facets);
+  }
+
+  function configureCog(index) {
+    // Remove any existing custom facet options
+    const suggestions = __dux_facet_suggestions[index];
+    const dropdownMenuUl = document.querySelector('.dropdown-menu ul');
+    const dropdownFacet = document.querySelector('.dropdown-menu li:has(.dropdown-facet)');
+
+    const toRemove = [...dropdownMenuUl.querySelectorAll('.dux-facet')];
+    for (const kill of toRemove)
+      kill.remove();
+
+    // Add any needed facet options
+    // <li><a class="dropdown-facet" href="#">Facet by this</a></li>
+    for (const suggestion of suggestions) {
+      // {"label":"this","params":{"_facet":"parent_id"}
+      const li = document.createElement('li');
+      li.classList.add('dux-facet');
+      const a = document.createElement('a');
+      const url = new URL(window.location.href);
+      for (const [k, v] of Object.entries(suggestion.params)) {
+        url.searchParams.append(k, v);
+      }
+      a.setAttribute('href', url.toString());
+      a.textContent = 'Facet by ' + suggestion.label;
+      li.appendChild(a);
+      dropdownMenuUl.insertBefore(li, dropdownFacet);
+    }
+  }
+
+  function renderFacetOptions() {
+    const dropdownMenu = document.querySelector('.dropdown-menu');
+    const dropdownFacet = document.querySelector('.dropdown-menu .dropdown-facet');
+
+    if (!dropdownMenu || !dropdownFacet)
+      return;
+
+    const cogs = [...document.querySelectorAll('.rows-and-columns th svg')];
+    let index = 0;
+    for (const cog of cogs) {
+      const captured = index;
+      cog.addEventListener('click', () => configureCog(captured))
+
+      index += 1;
+    }
+  }
+
+  function urlWithReplacedArgs(href, obj) {
+    if (!href.startsWith('http:') && !href.startsWith('https:')) {
+      href = window.location.protocol + '//' + window.location.host + href;
+    }
+    const url = new URL(href);
+    for (const [k, v] of Object.entries(obj)) {
+      if (v === null)
+        url.searchParams.delete(k);
+      else {
+        url.searchParams.set(k, v);
+      }
+    }
+
+    return url.toString();
+  }
+
+  function fixupToggleUrl(toggle_url, oldQueryParams) {
+    if (!toggle_url.startsWith('http:') && !toggle_url.startsWith('https:')) {
+      toggle_url = window.location.protocol + '//' + window.location.host + toggle_url;
+    }
+    const url = new URL(toggle_url);
+    for (const [k, v] of Object.entries(oldQueryParams)) {
+      if (v === null)
+        url.searchParams.delete(k);
+      else {
+        url.searchParams.set(k, v);
+      }
+    }
+
+    url.pathname = url.pathname.replace('.json', '');
+    return url.toString();
+  }
+
+  async function fetchFacets(facets) {
+    const me = new URL(window.location.href);
+    // Facet counts should not depend on pagination location
+
+    // TODO: we should stash the old values for _next/_size/_nocount and restore them
+    //       on each toggle_url that comes back
+    const oldQueryParams = {
+      '_next': me.searchParams.get('_next'),
+      '_size': me.searchParams.get('_size'),
+      '_nocount': me.searchParams.get('_nocount'),
+      '_dux_facet': null,
+      '_dux_facet_column': null,
+    };
+    me.searchParams.delete('_next');
+    me.searchParams.set('_size', '0');
+    me.searchParams.set('_nocount', '1');
+    me.pathname += '.json';
+
+    // Start all the fetches concurrently
+    // CONSIDER: maybe we should batch these?
+    const promises = [];
+    for (const facet of facets) {
+      const { param, source, column } = facet;
+      me.searchParams.set('_dux_facet', param);
+      me.searchParams.set('_dux_facet_column', column);
+      const results = fetch(me.toString())
+        .then((response) => response.json())
+      promises.push(results);
+    }
+
+    // Resolve them serially, this lets us safely insert in the right order
+    let i = 0;
+    while(promises.length > 0) {
+      const facet = facets[i];
+      i++;
+      const first = await promises.shift();
+
+      // In theory, we can do:
+      // const facetInfo = first.facet_results[facet.column];
+      //
+      // In practice, I think there's a limitation when the user facets the same
+      // columns multiple ways. We always compute the full set for that column,
+      // resulting in facet_results having key names like column_name,
+      // column_name_2.
+      //
+      // We have to go digging in the entry to know that it was the result for us.
+      let type = facet.param;
+      type = type.replace(/^_facet_/, '');
+      if (type === '_facet')
+        type = 'column';
+
+      if (first.ok === false) {
+        renderFailedFacet(facet, first.error);
+        continue;
+      }
+      const facetInfo = Object.values(first.facet_results).find(x => x.type === type);
+
+      if (!facetInfo) {
+        // Timed out? Show an error to the user.
+        renderFailedFacet(facet, "Computing this facet timed out.");
+        continue;
+      }
+
+      facetInfo.toggle_url = fixupToggleUrl(facetInfo.toggle_url, oldQueryParams);
+      for (const facetValue of facetInfo.results) {
+        facetValue.toggle_url = fixupToggleUrl(facetValue.toggle_url, oldQueryParams);
+      }
+
+      renderFacet(facetInfo);
+    }
+
+    document.body.classList.add('facets-loaded');
+    createDataLists();
+  }
+
+  // createDataLists is from Datasette: https://github.com/simonw/datasette/blob/0b4a28691468b5c758df74fa1d72a823813c96bf/datasette/static/table.js
+  // It runs too early, before we've loaded our facets, so we call it ourselves.
+  function createDataLists() {
+    var facetResults = document.querySelectorAll(
+      ".facet-results [data-column]"
+    );
+    Array.from(facetResults).forEach(function (facetResult) {
+      // Use link text from all links in the facet result
+      var links = Array.from(
+        facetResult.querySelectorAll("li:not(.facet-truncated) a")
+      );
+      // Create a datalist element
+      var datalist = document.createElement("datalist");
+      datalist.id = "datalist-" + facetResult.dataset.column;
+      // Create an option element for each link text
+      links.forEach(function (link) {
+        var option = document.createElement("option");
+        option.label = link.innerText;
+        option.value = link.dataset.facetValue;
+        datalist.appendChild(option);
+      });
+      // Add the datalist to the facet result
+      facetResult.appendChild(datalist);
+    });
+  }
+
+  function renderFailedFacet(facet, error) {
+    const facetResults = document.querySelector('.facet-results');
+
+    const node = document.createElement('div');
+    node.classList.add('facet-info');
+    node.setAttribute('data-column', facet.column);
+
+    let toggleUrl = '';
+
+    // You can remove the facet if its params were in the URL
+    const url = new URL(window.location.href);
+
+    const params = [...url.searchParams.entries()].filter(x => !(x[0] === facet.param && x[1] === facet.column));
+    if (params.length != [...url.searchParams.entries()].length) {
+      for (const k of url.searchParams.keys())
+        url.searchParams.delete(k);
+      for (const [k, v] of params) {
+        url.searchParams.append(k, v);
+      }
+
+      toggleUrl = url.toString();
+    }
+
+    node.innerHTML = `
+<p class="facet-info-name">
+    <strong>${facet.column}</strong>
+    ${toggleUrl ? `<a href="${toggleUrl}" class="cross">&#x2716;</a>` : ''}
+    <ul class="tight-bullets">
+      <li class="facet-error">${error}</li>
+    </ul>
+</p>
+`;
+
+    facetResults.insertBefore(node, document.querySelector('.spinner-container'));
+
+  }
+
+  function renderFacet(facetInfo) {
+    const facetResults = document.querySelector('.facet-results');
+
+    // Borrowed from https://github.com/simonw/datasette/blob/3c352b7132ef09b829abb69a0da0ad00be5edef9/datasette/templates/_facet_results.html
+    // Some things are not implemented:
+    // - the css classes for db/table/column
+    // - ability to expand truncated facets
+    const node = document.createElement('div');
+    node.classList.add('facet-info');
+    node.setAttribute('data-column', facetInfo.name);
+
+    const listItems = [];
+    for (const facetValue of facetInfo.results) {
+      if (!facetValue.selected) {
+        listItems.push(`<li><a href="${facetValue.toggle_url}" data-facet-value="${facetValue.value}">${facetValue.label || '-'}</a> ${facetValue.count.toLocaleString()}</li>`);
+      } else {
+        listItems.push(`<li>${facetValue.label || "-" } &middot; ${facetValue.count.toLocaleString()} <a href="${facetValue.toggle_url }" class="cross">&#x2716;</a></li>`);
+      }
+    }
+
+    const askedForFacetSizeMax = window.location.search.indexOf('_facet_size=max') >= 0;
+    let maybeMax = '';
+    if (facetInfo.truncated) {
+      maybeMax = `
+<li class="facet-truncated">${ !askedForFacetSizeMax ? `<a href="${urlWithReplacedArgs(window.location.href, { _facet_size: 'max' })}">…</a>` : `…`}
+                    </li>
+      `;
+    }
+    node.innerHTML = `
+<p class="facet-info-name">
+    <strong>${facetInfo.name}${ facetInfo.type !== 'column' ? ` (${facetInfo.type})` : ''}
+        <span class="facet-info-total">${ facetInfo.truncated ? '&gt; ' : '' }${facetInfo.results.length}</span>
+    </strong>
+    ${facetInfo.hideable ? `<a href="${facetInfo.toggle_url}" class="cross">&#x2716;</a>` : ''}
+    <ul class="tight-bullets">
+    ${listItems.join('\n')}
+    ${maybeMax}
+    </ul>
+</p>
+`;
+
+    facetResults.insertBefore(node, document.querySelector('.spinner-container'));
+  }
+
+  addEventListener('DOMContentLoaded', initialize);
+})();
