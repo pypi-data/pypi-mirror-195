4e69cca0ffe83c455dca856d3278464b3aa5c52b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:34 +0000

    Add package ocr-general-method/0.0.9/ocr_general_method-0.0.9-py3-none-any.whl

---
+++ packages/ocr-general-method/ocr_general_method-0.0.9-py3-none-any.whl/ocr_general_method/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# @Time: 2023/3/2 10:58
+# @Author: zyq
+# @File: __init__.py.py
+# @Software: PyCharm
+
+from example import example
+from common import file_utils, http_utils
+
