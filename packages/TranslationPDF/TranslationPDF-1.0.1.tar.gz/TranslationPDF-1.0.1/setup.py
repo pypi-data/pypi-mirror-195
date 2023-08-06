3077cbd4f147c0011de44dc9500c83ff8f4376a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:10 +0000

    Add package TranslationPDF/1.0.1/TranslationPDF-1.0.1.tar.gz

---
+++ packages/TranslationPDF/TranslationPDF-1.0.1.tar.gz/TranslationPDF-1.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+with open("README.md", "r") as arq:
+    readme = arq.read()
+
+setup(name='TranslationPDF',
+    version='1.0.1',
+    license='MIT License',
+    author='Willians Silva',
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    author_email='09willians@gmail.com',
+    keywords='pdf translation',
+    description=u'TranslationPDF é um pacote para a tradução de PDF.',
+    packages=['PDF_read'],
+    install_requires=['pypdf2', 'googletrans==4.0.0rc1', 'fpdf2', 'pytesseract'])
