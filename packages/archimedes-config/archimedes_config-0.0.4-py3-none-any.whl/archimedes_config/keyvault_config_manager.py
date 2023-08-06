9a8f007517ed1c2981d8f77d0211f400d4300e00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:14:47 +0000

    Add package archimedes-config/0.0.4/archimedes_config-0.0.4-py3-none-any.whl

---
+++ packages/archimedes-config/archimedes_config-0.0.4-py3-none-any.whl/archimedes_config/keyvault_config_manager.py
@@ -0,0 +1,33 @@
+"""
+Module for configurations and az vault secrets
+"""
+
+import logging
+
+from archimedes_config.config_manager import ConfigManager
+
+try:
+    from archimedes_config.keyvault_client import AzureKeyVaultClient
+except ImportError as err:
+    AzureKeyVaultClient = None
+
+
+class KeyvaultConfigManager(ConfigManager):  # pylint:disable=too-few-public-methods
+    """Class for configurations and az vault secrets"""
+
+    def __init__(self):
+        super().__init__()
+        if AzureKeyVaultClient is None:
+            error_message = (
+                "Environment has not been setup to handle interactions with Key Vault."
+            )
+            logging.critical(error_message)
+            raise ImportError(error_message)
+
+    def set_default_key_from_key_vault(self):
+        """Set default key using secret saved in Azure Key Vault"""
+
+        vault_name = self.config["AZURE_KEYVAULT"]["VAULT_NAME"]["value"]
+        key_name = self.config["AZURE_KEYVAULT"]["VAULT_KEY"]["value"]
+
+        self.set_default_key(AzureKeyVaultClient(vault_name).get_secret(name=key_name))
