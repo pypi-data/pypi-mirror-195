2c4127c4ae6bc0a021795624776ea11e1ca93c21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:12 +0000

    Add package leetcode-alg/0.1.0.dev0/leetcode_alg-0.1.0.dev0.tar.gz

---
+++ packages/leetcode-alg/leetcode_alg-0.1.0.dev0.tar.gz/leetcode_alg-0.1.0.dev0/leetcode_alg/_ds/__init__.py
@@ -0,0 +1,3 @@
+from ._segment_tree import *
+from ._binary_indexed_tree import *
+from ._heap import *
---
+++ packages/leetcode-alg/leetcode_alg-0.1.0.dev0.tar.gz/leetcode_alg-0.1.0.dev0/tests/test_tools.py
@@ -0,0 +1,6 @@
+from leetcode_alg import *
+
+if __name__ == "__main__":
+    l = [1, 2, 3, 4, 5]
+    ll = to_linkedlist(l)
+    print(from_linkedlist(ll))
