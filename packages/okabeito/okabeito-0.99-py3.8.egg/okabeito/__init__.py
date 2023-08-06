419014bf138752d9d1be2345f12f1e502c191fca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:16:48 +0000

    Add package okabeito/0.99/okabeito-0.99-py3.8.egg

---
+++ packages/okabeito/okabeito-0.99-py3.8.egg/okabeito/__init__.py
@@ -0,0 +1,12 @@
+""" 
+A color palette that can be distinguished by people with most color deficiencies. 
+See: https://jfly.uni-koeln.de/color/ for more information.
+"""
+lightblue = "#56B4E9"
+yellow = "#F0E442"
+orange = "#E69F00"
+green = "#009E73"
+purple = "#CC79A7"
+red = "#D55E00"
+blue = "#0072B2"
+black = "#000000"
