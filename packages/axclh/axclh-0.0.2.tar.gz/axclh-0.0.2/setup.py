6a749541711b72b8ec6dae28e77b5b3874ee1693
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:15:26 +0000

    Add package axclh/0.0.2/axclh-0.0.2.tar.gz

---
+++ packages/axclh/axclh-0.0.2.tar.gz/axclh-0.0.2/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+with open('requirements.txt', 'r') as req_file:
+    REQUIREMNTS = req_file.readlines()
+
+setup(
+    name='axclh',
+    version='0.0.2',
+    packages=find_packages(),
+    install_requires=REQUIREMNTS,
+    entry_points={
+        'console_scripts': [
+            'ac=axclh.main:search'
+        ]
+    }
+)
