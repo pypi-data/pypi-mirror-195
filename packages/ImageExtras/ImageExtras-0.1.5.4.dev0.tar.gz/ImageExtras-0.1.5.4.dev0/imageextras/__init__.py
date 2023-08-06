b7b2b71266978f2c434b882fa74a78430782d773
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:22:45 +0000

    Add package ImageExtras/0.1.5.4.dev0/ImageExtras-0.1.5.4.dev0.tar.gz

---
+++ packages/ImageExtras/ImageExtras-0.1.5.4.dev0.tar.gz/ImageExtras-0.1.5.4.dev0/imageextras/__init__.py
@@ -0,0 +1,49 @@
+# Native imports
+from imageextras.src.utils import *
+
+
+# Provisional workaround while I figure out why the other method doesn't work
+try:
+    from imageextras.compiled.py310.win import dithering
+except:
+    
+    try:
+        from imageextras.compiled.py39.win import dithering
+    except:
+        
+        try:
+            from imageextras.compiled.py310.linux import dithering
+        except:    
+            from imageextras.compiled.py39.linux import dithering
+
+
+"""
+# Compiled imports
+import os
+import platform
+import sys
+
+system = platform.system()
+
+if (sys.version_info >= (3, 10)) and (sys.version_info < (3, 11)):
+    py_ver = "py310"
+elif (sys.version_info >= (3, 9)) and (sys.version_info < (3, 10)):
+    py_ver = "py39"
+else:
+    raise RuntimeError("Unsupported Python version")
+
+if system == "Windows":
+    os_choice = "win"
+elif system == "Linux":
+    os_choice = "linux"
+else:
+    raise OSError(f"Unsupported platform: {system}")
+
+module_names = ["dithering"] # modules to add
+
+for module_name in module_names:
+    module_dir = os.path.join("imageextras", "compiled", py_ver, os_choice)
+    sys.path.append(module_dir)
+    print(module_dir, module_name)
+    __import__(module_name)
+"""
---
+++ packages/ImageExtras/ImageExtras-0.1.5.4.dev0.tar.gz/ImageExtras-0.1.5.4.dev0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, Extension, find_packages
+import sys, os
+
+version = '0.1.5.4'
+
+setup(name='ImageExtras',
+      version=version,
+      description="Image Processing extra functions",
+      long_description="""\
+            Utilities and compiled algorithms to make Image Processing easier""",
+      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='image img dither luma',
+      author='Adrià Brú i Cortés',
+      author_email='adriabrucortes@gmail.com',
+      url='https://gitlab.com/adriabrucortes/imageextras',
+      license='GPL3',
+      packages = find_packages(), # get all directories with __init__.py in it
+      include_package_data=True,
+      zip_safe=True,
+      install_requires=[
+          'numpy'
+      ],
+      requires_python = ">=3.9, <3.11"
+      )
