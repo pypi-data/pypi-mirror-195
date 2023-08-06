07ed0b8a54ed27dccc820dc3bc0a622ee2b5b499
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:14:10 +0000

    Add package kwnlp-sql-parser/0.0.2/kwnlp_sql_parser-0.0.2.tar.gz

---
+++ packages/kwnlp-sql-parser/kwnlp_sql_parser-0.0.2.tar.gz/kwnlp_sql_parser-0.0.2/kwnlp_sql_parser/__init__.py
@@ -0,0 +1,7 @@
+# Copyright 2020-present Kensho Technologies, LLC.
+__version__ = "0.0.2"
+
+from kwnlp_sql_parser.wp_sql_dump import WikipediaSqlCsvDialect
+from kwnlp_sql_parser.wp_sql_dump import WikipediaSqlDump
+from kwnlp_sql_parser.wp_sql_patterns import WikipediaSqlColumn
+from kwnlp_sql_parser.wp_sql_patterns import WikipediaSqlRow
