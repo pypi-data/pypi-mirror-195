6e9b754aecc0956bd13f9e9a7396bd169df336ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:09:56 +0000

    Add package django-universal-uploader-widget/0.2.4/django-universal-uploader-widget-0.2.4.tar.gz

---
+++ packages/django-universal-uploader-widget/django-universal-uploader-widget-0.2.4.tar.gz/django-universal-uploader-widget-0.2.4/setup.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+import os
+from setuptools import setup, find_packages
+
+f = open(os.path.join(os.path.dirname(__file__), 'README.md'))
+readme = f.read()
+f.close()
+
+setup(
+    name='django-universal-uploader-widget',
+    version='0.2.4',
+    description='Simple Universal Uploader Widget for Django-Admin',
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    author='Sandro Salles',
+    author_email='sandro@snippet.com.br',
+    url='https://github.com/sandro-salles/django-universal-uploader-widget',
+    license='MIT',
+    packages=find_packages(
+        exclude=("universal_uploader_widget_demo.*",
+                 "universal_uploader_widget_demo", )
+    ),
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    keywords='django,admin,widget,universal,uploader',
+)
