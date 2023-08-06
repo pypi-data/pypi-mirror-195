bc071e618e595657cf4b6c97a5615a7aa85b95d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:07 +0000

    Add package okome-lib/1.0/okome-lib-1.0.tar.gz

---
+++ packages/okome-lib/okome-lib-1.0.tar.gz/okome-lib-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="okome-lib",
+    version="1.0",
+    install_requires=["requests"],  
+    description="m.kuku.lu wrapper",
+    long_description=open("README.txt").read().replace("\r", ""),
+)
