07b72121de1a31b248c6fbb537fcd47d228f61e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:10:07 +0000

    Add package pywas/0.1.1/pywas-0.1.1.tar.gz

---
+++ packages/pywas/pywas-0.1.1.tar.gz/pywas-0.1.1/pywas/parse/results.py
@@ -0,0 +1,25 @@
+import h5py
+from pydantic import FilePath
+from typing import TypedDict, List
+
+
+class ResultDict(TypedDict):
+    label: str
+    array: List[float]
+
+
+class H5pyStruct(TypedDict):
+    name: str
+    result: List[ResultDict]
+
+
+def write_result(result: ResultDict, result_file: FilePath):
+    """
+    Export a
+    :param result:
+    :param result_file:
+    :return:
+    """
+    with h5py.File(result_file, "w") as f:
+        for keys in result:
+            f[keys] = result[keys]
