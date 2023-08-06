37c85844c3b41c72974b3dd778ff3a58143dc0f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:12:23 +0000

    Add package django-remote-control-1/1.0.2/django-remote-control-1-1.0.2.tar.gz

---
+++ packages/django-remote-control-1/django-remote-control-1-1.0.2.tar.gz/django-remote-control-1-1.0.2/setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(
+    name = 'django-remote-control-1',
+    packages = ['remote_control'],
+    version = '1.0.2',
+    install_requires=[
+        'requests',
+    ],
+    description = 'Securely call commands in other django applications.',
+    author = 'Luke Sapan',
+    author_email = 'lukevsapan@gmail.com',
+    url = 'https://github.com/ramkasula2019/django-remote-control',
+    download_url = 'https://github.com/ramkasula2019/django-remote-control/archive/1.0.2.tar.gz',
+    keywords = ['django', 'remote' 'commands', 'rpc'],
+    classifiers = [],
+)
