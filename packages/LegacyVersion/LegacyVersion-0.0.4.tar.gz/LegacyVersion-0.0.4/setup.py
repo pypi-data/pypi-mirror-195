280c15e60aa8f7f82ab246296f5437463cc06005
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:43:33 +0000

    Add package LegacyVersion/0.0.4/LegacyVersion-0.0.4.tar.gz

---
+++ packages/LegacyVersion/LegacyVersion-0.0.4.tar.gz/LegacyVersion-0.0.4/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.0.4'
+DESCRIPTION = 'Versioning library for Python 3.11+'
+LONG_DESCRIPTION = 'Library implements version functionality for Python 3.11 due to official version being deprecated.'
+
+# Setting up
+setup(
+    name="LegacyVersion",
+    version=VERSION,
+    author="Stanislav Jochman",
+    author_email="<s.jochman@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python', 'LegacyVersion', '_BaseVersion'],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
