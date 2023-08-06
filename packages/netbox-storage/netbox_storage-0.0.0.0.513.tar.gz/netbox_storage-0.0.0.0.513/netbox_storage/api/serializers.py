bc96044b65856632ea018e9ff26a3038fd252a32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:15:34 +0000

    Add package netbox-storage/0.0.0.0.513/netbox_storage-0.0.0.0.513.tar.gz

---
+++ packages/netbox-storage/netbox_storage-0.0.0.0.513.tar.gz/netbox_storage-0.0.0.0.513/netbox_storage/api/serializers.py
@@ -0,0 +1,80 @@
+from rest_framework import serializers
+
+from netbox.api.serializers import NetBoxModelSerializer
+from netbox_storage.api.nested_serializers import NestedFilesystemSerializer, NestedDriveSerializer, \
+    NestedPartitionSerializer
+from netbox_storage.models import Drive, Filesystem, Partition, LinuxVolume, StorageConfiguration
+from virtualization.api.nested_serializers import NestedClusterSerializer, NestedVirtualMachineSerializer
+
+
+
+class FilesystemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Filesystem
+        fields = (
+            "id",
+            "filesystem",
+            "description",
+            "created",
+            "last_updated",
+            "custom_fields",
+        )
+
+
+class DriveSerializer(NetBoxModelSerializer):
+    cluster = NestedClusterSerializer(required=False, allow_null=True)
+    url = serializers.HyperlinkedIdentityField(view_name="plugins-api:netbox_storage-api:drive-detail")
+
+    class Meta:
+        model = Drive
+        fields = (
+            "id",
+            "url",
+            "display",
+            "size",
+            "cluster",
+            "identifier",
+            "description",
+            "created",
+            "last_updated",
+            "custom_fields",
+        )
+
+
+class PartitionSerializer(serializers.ModelSerializer):
+    url = serializers.HyperlinkedIdentityField(view_name='plugins-api:netbox_storage-api:partition-detail')
+    drive = NestedDriveSerializer(required=False, allow_null=True)
+
+    class Meta:
+        model = Partition
+        fields = (
+            "id",
+            "url",
+            "drive",
+            "device",
+            "size",
+            "type",
+            "description",
+            "created",
+            "last_updated",
+            "custom_fields",
+        )
+
+
+class LinuxVolumeSerializer(serializers.ModelSerializer):
+    fs = NestedFilesystemSerializer(required=False, allow_null=True)
+    partition = NestedPartitionSerializer(required=False, allow_null=True)
+
+    class Meta:
+        model = LinuxVolume
+        fields = (
+            "id",
+            "partition",
+            "fs",
+            "size",
+            "path",
+            "description",
+            "created",
+            "last_updated",
+            "custom_fields",
+        )
---
+++ packages/netbox-storage/netbox_storage-0.0.0.0.513.tar.gz/netbox_storage-0.0.0.0.513/netbox_storage/api/urls.py
@@ -0,0 +1,21 @@
+from netbox.api.routers import NetBoxRouter
+
+from netbox_storage.api.views import (
+    NetboxStorageRootView,
+    DriveViewSet,
+    FilesystemViewSet,
+    PartitionViewSet,
+    LinuxVolumeViewSet,
+    StorageConfigurationViewSet
+)
+
+router = NetBoxRouter()
+router.APIRootView = NetboxStorageRootView
+
+router.register("drive", DriveViewSet)
+router.register("filesystem", FilesystemViewSet)
+router.register("partition", PartitionViewSet)
+router.register("linuxvolume", LinuxVolumeViewSet)
+router.register("storageconfiguration", StorageConfigurationViewSet)
+
+urlpatterns = router.urls
