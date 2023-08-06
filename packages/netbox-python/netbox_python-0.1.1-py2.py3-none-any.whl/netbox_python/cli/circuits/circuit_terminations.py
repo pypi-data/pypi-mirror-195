85b0c4882c6f7876c9474842a7896f8705cc5c9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:40:31 +0000

    Add package netbox-python/0.1.1/netbox_python-0.1.1-py2.py3-none-any.whl

---
+++ packages/netbox-python/netbox_python-0.1.1-py2.py3-none-any.whl/netbox_python/cli/circuits/circuit_terminations.py
@@ -0,0 +1,42 @@
+from typing import List
+
+import pandas as pd
+import typer
+from cli.crud import BaseCrudCommand, ListFormat, TableFormat
+from netbox import NetBoxClient
+from tabulate import tabulate
+
+
+class CircuitTermination(BaseCrudCommand):
+    @property
+    def base_api(self):
+        return self.connection.circuits.circuit_terminations
+
+
+app = typer.Typer()
+
+
+@app.command()
+def list(
+    format: ListFormat = typer.Option(ListFormat.json, prompt=True),
+    columns: str = typer.Option("id"),
+    indent: int = typer.Option(4),
+    tablefmt: TableFormat = typer.Option(TableFormat.psql),
+):
+    CircuitTermination().list(format, columns, indent, tablefmt)
+
+
+def get():
+    CircuitTermination().get()
+
+
+def create():
+    CircuitTermination().create()
+
+
+def update():
+    CircuitTermination().update()
+
+
+def delete():
+    CircuitTermination().delete()
