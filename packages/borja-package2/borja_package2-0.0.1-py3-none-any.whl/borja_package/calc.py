f990d0e666729a58ab81e82c11b31f047304885f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:30:59 +0000

    Add package borja-package2/0.0.1/borja_package2-0.0.1-py3-none-any.whl

---
+++ packages/borja-package2/borja_package2-0.0.1-py3-none-any.whl/borja_package/calc.py
@@ -0,0 +1,10 @@
+from borja_package.const import PI
+
+
+def sum(num1: float, num2: float) -> float:
+    return num1 + num2
+
+
+def area_circle(radio: float) -> float:
+    return radio * radio * PI
+
