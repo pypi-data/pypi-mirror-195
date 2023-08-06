d14de4149a69320822f4ef4df0faefe7cb6fd1b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:25:50 +0000

    Add package addana/0.0.1/addana-0.0.1.tar.gz

---
+++ packages/addana/addana-0.0.1.tar.gz/addana-0.0.1/README.md
@@ -0,0 +1,21 @@
+# addana
+
+
+[![image](https://img.shields.io/pypi/v/addana.svg)](https://pypi.python.org/pypi/addana)
+[![image](https://img.shields.io/conda/vn/conda-forge/addana.svg)](https://anaconda.org/conda-forge/addana)
+
+
+**address**
+
+
+-   Free software: MIT license
+-   Documentation: https://advancehs.github.io/addana
+    
+
+## Features
+
+-   TODO
+
+## Credits
+
+This package was created with [Cookiecutter](https://github.com/cookiecutter/cookiecutter) and the [giswqs/pypackage](https://github.com/giswqs/pypackage) project template.
---
+++ packages/addana/addana-0.0.1.tar.gz/addana-0.0.1/tests/test_drawer.py
@@ -0,0 +1,27 @@
+import os
+import sys
+
+import pandas as pd
+
+sys.path.append('..')
+import addressparser2
+from addressparser2 import drawer
+
+origin_addr = pd.read_csv(os.path.join(os.path.dirname(__file__), 'addr.csv'))
+df = addressparser2.transform(origin_addr['原始地址'])
+
+
+def test_draw_locations():
+    """使用folium绘制热力图"""
+    drawer.draw_locations(df, "df.html")
+
+
+def test_echarts_draw():
+    """使用echarts绘制热力图"""
+    drawer.echarts_draw(df,
+                        "df_echarts.html", title="地域分布图", subtitle="location distribute")
+
+
+def test_echarts_cate_draw():
+    """使用echarts绘制分类散点图"""
+    drawer.echarts_cate_draw(df, df['省'], "df_echarts_cate.html")
