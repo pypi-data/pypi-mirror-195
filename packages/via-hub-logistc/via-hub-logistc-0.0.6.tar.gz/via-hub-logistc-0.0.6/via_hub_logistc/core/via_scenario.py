1ac3d3d8fc768f29634f9d9a74b8265a208c2eb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:35:46 +0000

    Add package via-hub-logistc/0.0.6/via-hub-logistc-0.0.6.tar.gz

---
+++ packages/via-hub-logistc/via-hub-logistc-0.0.6.tar.gz/via-hub-logistc-0.0.6/via_hub_logistc/core/via_scenario.py
@@ -0,0 +1,15 @@
+from robot.api.deco import keyword
+from robot.api import Error
+
+class scenarios():
+    ###Load id the scenarios ###
+    @keyword(name="get id scenario")
+    def get_id_scenario(self, lst_tags, tag_id):
+        try:
+            for tag in lst_tags:
+                if str(tag).startswith(tag_id.casefold()):
+                    return str(tag).upper()
+
+            return None
+        except Exception as e:
+            raise Error(e)
