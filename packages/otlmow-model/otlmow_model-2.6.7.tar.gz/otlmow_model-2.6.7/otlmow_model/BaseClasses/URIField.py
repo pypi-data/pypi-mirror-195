8cabcb5996792cdccfef76ef808fd66779e53bba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:35:37 +0000

    Add package otlmow-model/2.6.7/otlmow_model-2.6.7.tar.gz

---
+++ packages/otlmow-model/otlmow_model-2.6.7.tar.gz/otlmow_model-2.6.7/otlmow_model/BaseClasses/URIField.py
@@ -0,0 +1,28 @@
+from otlmow_model.BaseClasses.StringField import StringField
+import re
+
+class URIField(StringField):
+    """Een tekstwaarde die een verwijzing naar meer informatie van het element bevat volgens http://www.w3.org/2001/XMLSchema#anyURI ."""
+    naam = 'AnyURI'
+    objectUri = 'http://www.w3.org/2001/XMLSchema#anyURI'
+    definition = 'Een tekstwaarde die een verwijzing naar meer informatie van het element bevat volgens http://www.w3.org/2001/XMLSchema#anyURI.'
+    label = 'URI'
+    usagenote = 'https://www.w3.org/TR/xmlschema-2/#anyURI'
+
+    @staticmethod
+    def validate(value, attribuut):
+        if not StringField.validate(value, attribuut):
+            return False
+
+        regex = re.compile(
+            r'^(?:http|ftp)s?://'
+            r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|'  
+            r'localhost|'
+            r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'  
+            r'(?::\d+)?' 
+            r'(?:/?|[/?]\S+)$', re.IGNORECASE)  # django url validation regex
+        return re.match(regex, value) is not None
+
+    def __str__(self):
+        return StringField.__str__(self)
+
