3bfbc8ffc1198d42ce1557cb02ccb240e3c558eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:34 +0000

    Add package fastapi_amis_admin/0.5.0/fastapi_amis_admin-0.5.0.tar.gz

---
+++ packages/fastapi_amis_admin/fastapi_amis_admin-0.5.0.tar.gz/fastapi_amis_admin-0.5.0/fastapi_amis_admin/admin/__init__.py
@@ -0,0 +1,22 @@
+from .admin import (
+    AdminAction,
+    AdminApp,
+    BaseAdmin,
+    BaseAdminSite,
+    BaseFormAdmin,
+    BaseModelAdmin,
+    FormAction,
+    FormAdmin,
+    IframeAdmin,
+    LinkAdmin,
+    LinkModelForm,
+    ModelAction,
+    ModelAdmin,
+    PageAdmin,
+    PageSchemaAdmin,
+    RouterAdmin,
+    TemplateAdmin,
+)
+from .parser import AmisParser
+from .settings import Settings
+from .site import AdminSite, DocsAdmin, FileAdmin, HomeAdmin, ReDocsAdmin
