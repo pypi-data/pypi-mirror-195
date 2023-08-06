04a2bf8ed3413142210cc0239dce3468d25acdeb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:14 +0000

    Add package titania-nlp/0.1/titania_nlp-0.1-py3-none-any.whl

---
+++ packages/titania-nlp/titania_nlp-0.1-py3-none-any.whl/titania_nlp/titania_nlp.py
@@ -0,0 +1,9 @@
+import spacy
+
+def extract_entities(text):
+    nlp = spacy.load("en_core_web_sm")
+    doc = nlp(text)
+    entities = []
+    for ent in doc.ents:
+        entities.append((ent.text, ent.label_))
+    return entities
