f594f8f4a07b0b7a2a458abe411e2d078bc13e8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:07 +0000

    Add package mysqld-integration-test/0.0.13/mysqld_integration_test-0.0.13-py3-none-any.whl

---
+++ packages/mysqld-integration-test/mysqld_integration_test-0.0.13-py3-none-any.whl/mysqld_integration_test/helpers.py
@@ -0,0 +1,36 @@
+import os
+import re
+import socket
+
+BASEDIRS = ['/', '/usr', '/usr/local']
+
+
+class Utils():
+    @staticmethod
+    def find_program(name, subdirs):
+        for basedir in BASEDIRS:
+            for subdir in subdirs:
+                path = os.path.join(basedir, subdir, name)
+                if os.path.exists(path):
+                    return path
+        raise RuntimeError(f"Command not found: {name}")
+
+    @staticmethod
+    def get_unused_port():
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.bind(('localhost', 0))
+        _, port = sock.getsockname()
+        sock.close()
+
+        return port
+
+    @staticmethod
+    def parse_version(version_str):
+        version_info = (re.findall(r"Ver (\d+)\.([0-9.]+)\-(\w+)", version_str))
+        version_major = int(version_info[0][0])
+        version_minor = version_info[0][1]
+        version_variant = version_info[0][2].lower()
+        if version_major == 8:
+            version_variant = "mysql"
+
+        return (version_variant, version_major, version_minor)
