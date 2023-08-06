ccfead6b8a4f00495d17534e87b7c5b4afd1ccf3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:16 +0000

    Add package qbandas/1.0.3/qbandas-1.0.3-py3-none-any.whl

---
+++ packages/qbandas/qbandas-1.0.3-py3-none-any.whl/qbandas/__init__.py
@@ -0,0 +1,13 @@
+"""
+Integrates the popular data handling library Pandas and the QuickBase API.
+"""
+
+from .schema import (
+    add_args,
+    pull_schema,
+    set_args
+)
+
+from .upload import upload_records
+
+
