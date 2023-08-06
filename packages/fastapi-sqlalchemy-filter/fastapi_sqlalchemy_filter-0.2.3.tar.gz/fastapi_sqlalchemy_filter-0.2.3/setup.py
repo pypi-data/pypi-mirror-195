1a2316ef2408bb1a226760f803aab1c7cc4006eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:16 +0000

    Add package fastapi-sqlalchemy-filter/0.2.3/fastapi_sqlalchemy_filter-0.2.3.tar.gz

---
+++ packages/fastapi-sqlalchemy-filter/fastapi_sqlalchemy_filter-0.2.3.tar.gz/fastapi_sqlalchemy_filter-0.2.3/setup.py
@@ -0,0 +1,37 @@
+from distutils.core import setup
+
+with open("README.md", "r", encoding = "utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name = "fastapi_sqlalchemy_filter",
+    packages = ['fastapi_sqlalchemy_filter'],
+    version = "0.2.3",
+    author = "Warui",
+    author_email = "waruingugientp@gmail.com",
+    description = "A lite version of fastapi-filter for SQLAlchemy only",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    url = "https://github.com/waruingugi/fastapi-filter-sqlalchemy-lite/tree/sqlalchemy-filter-lite",
+    project_urls = {
+        "Bug Tracker": "https://github.com/waruingugi/fastapi-filter-sqlalchemy-lite/issues",
+    },
+    classifiers = [
+        "Natural Language :: English",
+        "Framework :: FastAPI",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3 :: Only",
+    ],
+    download_url = "https://github.com/waruingugi/fastapi-filter-sqlalchemy-lite/archive/refs/tags/v0.2.3.tar.gz",
+    keywords = ['FastAPI', 'SQLAlchemy', 'Filter'],
+    install_requires=[
+        'pydantic',
+        'fastapi',
+    ],
+)
