31652dc48b48cd5b6b183d54d264677d09720e1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:13:57 +0000

    Add package sra-pylib/1.2.0/sra-pylib-1.2.0.tar.gz

---
+++ packages/sra-pylib/sra-pylib-1.2.0.tar.gz/sra-pylib-1.2.0/sra/__init__.py
@@ -0,0 +1,9 @@
+from sra import animal
+from sra import animu
+from sra import canvas
+from sra import facts
+from sra import image
+from datetime import datetime
+
+__version__ = "1.0.0"
+__copyright__ = f"© Copyright 2023 {('- ' + str(datetime.today().year) + ' ') if int(datetime.today().year) != 2023 else ''}Sayad Uddin Tahsin."
