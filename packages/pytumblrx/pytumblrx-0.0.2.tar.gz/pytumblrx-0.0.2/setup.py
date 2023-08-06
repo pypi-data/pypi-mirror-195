e5942fe1aa9747818cb1d98ce22a6ca277ca50f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:29 +0000

    Add package pytumblrx/0.0.2/pytumblrx-0.0.2.tar.gz

---
+++ packages/pytumblrx/pytumblrx-0.0.2.tar.gz/pytumblrx-0.0.2/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['pytumblrx']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['Authlib>=1.2.0,<2.0.0', 'httpx>=0.23.3,<0.24.0', 'strenum>=0.4.9,<0.5.0']
+
+extras_require = \
+{'full': ['Pillow>=9.4.0,<10.0.0']}
+
+setup_kwargs = {
+    'name': 'pytumblrx',
+    'version': '0.0.2',
+    'description': 'A Python Tumblr API v2 Client based on httpx with async support',
+    'long_description': 'None',
+    'author': 'dj-ratty',
+    'author_email': '115014503+dj-ratty@users.noreply.github.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'https://github.com/dj-ratty/pytumblrx',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'extras_require': extras_require,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
