b7ba80a920e8e98dc59ac89598b04c181f605139
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:00 +0000

    Add package wizata-dsapi/0.0.30/wizata_dsapi-0.0.30-py3-none-any.whl

---
+++ packages/wizata-dsapi/wizata_dsapi-0.0.30-py3-none-any.whl/wizata_dsapi/dsapi_json_encoder.py
@@ -0,0 +1,20 @@
+import json
+import pandas
+import time
+from .mlmodel import MLModel
+
+
+class DSAPIEncoder(json.JSONEncoder):
+
+    def default(self, obj):
+        if isinstance(obj, MLModel):
+            json_obj = {
+                "model_id": str(obj.model_id),
+                "experiment_id": str(obj.experiment_id)
+            }
+            return json_obj
+        if isinstance(obj, pandas.Timestamp):
+            return int(time.mktime(obj.timetuple()) * 1000)
+        else:
+            type_name = obj.__class__.__name__
+            raise TypeError("Unexpected type {0}".format(type_name))
