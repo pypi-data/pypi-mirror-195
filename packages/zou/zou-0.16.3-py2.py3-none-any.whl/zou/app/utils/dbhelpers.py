39d1716a0ee8f8c8a4fb9fc2719186b1a7f71de4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:32:25 +0000

    Add package zou/0.16.3/zou-0.16.3-py2.py3-none-any.whl

---
+++ packages/zou/zou-0.16.3-py2.py3-none-any.whl/zou/app/utils/dbhelpers.py
@@ -0,0 +1,49 @@
+from sqlalchemy import create_engine
+from sqlalchemy_utils import database_exists, create_database
+from sqlalchemy.engine.url import URL
+from sqlalchemy.orm import close_all_sessions
+
+
+def get_db_uri():
+    from zou.app.config import DATABASE
+
+    return URL.create(**DATABASE)
+
+
+def is_db_exists():
+    """
+    Check that database exist.
+    """
+    engine = create_engine(get_db_uri())
+    return database_exists(engine.url)
+
+
+def reset_all():
+    """
+    Check that database exist.
+    """
+    drop_all()
+    return create_all()
+
+
+def create_all():
+    """
+    Create all database tables.
+    """
+    from zou.app import db
+
+    engine = create_engine(get_db_uri())
+    if not database_exists(engine.url):
+        create_database(engine.url)
+    return db.create_all()
+
+
+def drop_all():
+    """
+    Drop all database tables.
+    """
+    from zou.app import db
+
+    db.session.flush()
+    close_all_sessions()
+    return db.drop_all()
