fcf7f446c6b063ec8b13f3645016c54b01173659
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:12:50 +0000

    Add package wagtailsvg/0.0.37/wagtailsvg-0.0.37.tar.gz

---
+++ packages/wagtailsvg/wagtailsvg-0.0.37.tar.gz/wagtailsvg-0.0.37/tests/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+
+try:
+    from wagtail.fields import StreamField
+    from wagtail.models import Page
+    from wagtail.admin.panels import FieldPanel as StreamFieldPanel
+except ImportError:
+    from wagtail.core.fields import StreamField
+    from wagtail.core.models import Page
+    from wagtail.admin.edit_handlers import StreamFieldPanel
+
+from wagtailsvg.edit_handlers import SvgChooserPanel
+from wagtailsvg.models import Svg
+from .blocks import TextBlock, SvgBlock
+
+
+# =================================
+# TestPage used for testing purpose
+# =================================
+
+
+class TestPage(Page):
+    logo = models.ForeignKey(
+        Svg, related_name='+',
+        null=True, blank=True, on_delete=models.SET_NULL
+    )
+    body = StreamField([
+        ('text', TextBlock()),
+        ('svg', SvgBlock()),
+    ], blank=True, use_json_field=True)
+
+    content_panels = Page.content_panels + [
+        SvgChooserPanel('logo'),
+        StreamFieldPanel('body'),
+    ]
