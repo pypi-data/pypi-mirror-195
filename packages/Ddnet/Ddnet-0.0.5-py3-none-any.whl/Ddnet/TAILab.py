3e8c2896f6d8a9ed22e1a3494325fe4ce490f92c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:23:25 +0000

    Add package Ddnet/0.0.5/Ddnet-0.0.5-py3-none-any.whl

---
+++ packages/Ddnet/Ddnet-0.0.5-py3-none-any.whl/Ddnet/TAILab.py
@@ -0,0 +1,6 @@
+# TAILab Main
+import pandas as pd
+import numpy as np
+
+def info():
+    print('TAILab = T.+AI+Lab. TAILab is planned to be a model training lite platform. TAILab address:https://share.streamlit.io/tqthooo2021/tai-lab/main/labmain.py')
