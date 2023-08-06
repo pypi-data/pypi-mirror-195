690d84e6c17357b6f6ade9b42ee9d9f92c86da1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:12:46 +0000

    Add package chatgpt-telegram/0.1.4/chatgpt-telegram-0.1.4.tar.gz

---
+++ packages/chatgpt-telegram/chatgpt-telegram-0.1.4.tar.gz/chatgpt-telegram-0.1.4/setup.py
@@ -0,0 +1,16 @@
+
+import os
+from distutils.core import setup
+
+setup(
+    name="chatgpt-telegram",
+    version="0.1.4",
+    description="ChatGPT Bot For telegram",
+    author='learnforpractice',
+    license="Apache 2.0",
+    url="https://github.com/learnforpractice/chatgpt-telegram",
+    packages=['chatgpt_telegram'],
+    package_dir={'chatgpt_telegram': 'src'},
+    package_data={},
+    setup_requires=['wheel']
+)
---
+++ packages/chatgpt-telegram/chatgpt-telegram-0.1.4.tar.gz/chatgpt-telegram-0.1.4/src/__main__.py
@@ -0,0 +1,4 @@
+from . import telegrambot
+
+if __name__ == '__main__':
+    telegrambot.run()
