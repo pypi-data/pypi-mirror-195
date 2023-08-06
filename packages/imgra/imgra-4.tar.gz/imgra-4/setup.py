336355004675cb03514949982c2eb6c06c38a309
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:11:54 +0000

    Add package imgra/4/imgra-4.tar.gz

---
+++ packages/imgra/imgra-4.tar.gz/imgra-4/setup.py
@@ -0,0 +1,18 @@
+
+import setuptools
+
+setuptools.setup(
+	name = "imgra",
+	version = "4",
+	description = "Image Processing With Graphs",
+	url = "https://github.com/mnhrdt/imgra",
+	classifiers = [
+		"Operating System :: OS Independent",
+		"License :: OSI Approved :: GNU Affero General Public License v3",
+		"Topic :: Scientific/Engineering :: Image Processing",
+		"Topic :: Scientific/Engineering :: Mathematics"
+		],
+	author = "Enric Meinhardt-Llopis",
+	author_email = "enric.meinhardt@fastmail.com",
+	py_modules = ["imgra"]
+)
