7693c21c48bf661d287cb2352135aba78dea7683
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:20:03 +0000

    Add package fmcimage/0.23/fmcimage-0.23.tar.gz

---
+++ packages/fmcimage/fmcimage-0.23.tar.gz/fmcimage-0.23/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+from pathlib import Path
+
+setuptools.setup(
+    name="fmcimage",
+    version=0.23,
+    # long_description=Path("README.md").read_text(),
+    long_description="A lightweight python package that contains necessary modules for perceiving images and using ocr.",
+    packages=setuptools.find_packages(exclude=["data","tests"]),
+)
