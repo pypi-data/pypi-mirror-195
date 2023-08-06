297f1c5f0c2fbbac2e84277d081f4588f40c99a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:06:53 +0000

    Add package genai/0.9.0/genai-0.9.0.tar.gz

---
+++ packages/genai/genai-0.9.0.tar.gz/genai-0.9.0/genai/__init__.py
@@ -0,0 +1,20 @@
+"""Genai: generative AI tooling for IPython and Jupyter notebooks
+
+## Magic methods
+
+    - `%%assist`: Assist you in writing code.
+
+## Usage
+
+  `%load_ext genai` to load the extension in other notebook
+  projects
+"""
+
+__version__ = "0.9.0"
+
+
+def load_ipython_extension(ipython):
+    from .suggestions import register
+    from .magics import assist
+
+    register()
