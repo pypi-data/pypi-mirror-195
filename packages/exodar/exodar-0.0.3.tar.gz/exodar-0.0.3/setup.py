5d96cb12b4323bfbe5aa839cb8de4f23c17ab99b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:16:56 +0000

    Add package exodar/0.0.3/exodar-0.0.3.tar.gz

---
+++ packages/exodar/exodar-0.0.3.tar.gz/exodar-0.0.3/setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages, setup
+
+with open("./app/README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name="exodar",
+    version="0.0.3",
+    description="A test package",
+    package_dir={"": "app"},
+    packages=find_packages(where="app"),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="vincent2303",
+    author_email="vincent.martin23@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.10",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[],
+    extras_require={},
+    python_requires=">=3.6",
+)
