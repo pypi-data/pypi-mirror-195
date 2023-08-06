8b79d0ffa6b4b9290c48aa4fe4e5601f00ea45c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:12:31 +0000

    Add package pysgems/1.2.0/pysgems-1.2.0-py3-none-any.whl

---
+++ packages/pysgems/pysgems-1.2.0-py3-none-any.whl/pysgems/utils/sgutils.py
@@ -0,0 +1,13 @@
+#  Copyright (c) 2022. Robin Thibaut, Ghent University
+
+
+def joinlist(j, mylist):
+    """
+    Function that joins an array of numbers with j as separator. For example, joinlist('^', [1,2]) returns 1^2
+
+    :param j: separator
+    :param mylist: list of numbers
+    """
+    gp = j.join(map(str, mylist))
+
+    return gp
