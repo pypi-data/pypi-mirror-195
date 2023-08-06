60fb221087aa87573688da5ded66947fe3f1107e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:26:49 +0000

    Add package pinuccio/0.0.2/pinuccio-0.0.2.tar.gz

---
+++ packages/pinuccio/pinuccio-0.0.2.tar.gz/pinuccio-0.0.2/pinuccio/client.py
@@ -0,0 +1,27 @@
+from . import utils
+from . import interface as sc
+import socket, json
+
+
+class Client():
+    def __init__(self, IP, PORT):
+        self.IP = IP
+        self.PORT = PORT
+        self.socket = None
+
+    def start(self):
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.connect((self.IP, self.PORT))
+
+    def send(self, data, key=None):
+        data = json.dumps(data)
+        sc.msg_send(self.socket, data)
+    
+    def recv(self, key=None):
+        try:
+            data = sc.msg_recv(self.socket)
+            data = json.loads(data)
+        except json.decoder.JSONDecodeError:
+            utils.debug(data,"json Decode Error")
+            return ""
+        return data
---
+++ packages/pinuccio/pinuccio-0.0.2.tar.gz/pinuccio-0.0.2/setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(
+  name = 'pinuccio',         # How you named your package folder (MyLib)
+  packages = ['pinuccio'],   # Chose the same as "name"
+  version = '0.0.2',      # Start with a small number and increase it with every change you make
+  install_requires=[]
+)
