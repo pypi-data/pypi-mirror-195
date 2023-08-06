8ebf4c586ae14708ef2ce9908c393a5897467b03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:11:29 +0000

    Add package guilded-webhook/0.2.1/guilded_webhook-0.2.1-py3-none-any.whl

---
+++ packages/guilded-webhook/guilded_webhook-0.2.1-py3-none-any.whl/guilded_webhook/__init__.py
@@ -0,0 +1,85 @@
+import aiohttp
+import requests
+from typing import List, Union
+from .embed import Embed
+from .exceptions import BadRequestError
+
+class Empty:
+    ...
+
+class Webhook:
+    __slots__ = ("_url", "avatar", "username")
+    def __init__(self, url: str, *, avatar: str=None, username: str=None) -> None:
+        self._url = url
+        self.avatar = avatar
+        self.username = username
+
+    def __repr__(self) -> str:
+        return self._url
+
+    def __eq__(self, o: object) -> bool:
+        if isinstance(o, (AsyncWebhook, Webhook)):
+            return self._url == o._url
+        return False
+
+    def send(self, *, content: str=None, embeds: Union[List[Embed], Embed]=None, avatar: str=Empty, username: str=Empty) -> None:
+        json = {'content': content}
+        if avatar == Empty:
+            avatar = self.avatar
+        if avatar != None:
+            json['avatar_url'] = avatar
+
+        if username == Empty:
+            username = self.username
+        if username != None:
+            json['username'] = username
+
+        if embeds:
+            if isinstance(embeds, Embed):
+                embeds = [embeds]
+            _embeds = [embed._to_dict() for embed in embeds]
+            json['embed'] = _embeds
+
+        r = requests.post(self._url, json=json)
+        resp = r.json()
+
+        if 'code' in resp:
+            raise BadRequestError(resp['message'])
+
+class AsyncWebhook:
+    __slots__ = ("_url", "avatar", "username")
+    def __init__(self, url: str, *, avatar: str=None, username: str=None) -> None:
+        self._url = url
+
+    def __repr__(self) -> str:
+        return self._url
+
+    def __eq__(self, o: object) -> bool:
+        if isinstance(o, (AsyncWebhook, Webhook)):
+            return self._url == o._url
+        return False
+
+    async def send(self, *, content: str=None, embeds: Union[List[Embed], Embed]=None, avatar: str=Empty, username: str=Empty) -> None:
+        json = {'content': content}
+        if avatar == Empty:
+            avatar = self.avatar
+        if avatar != None:
+            json['avatar_url'] = avatar
+
+        if username == Empty:
+            username = self.username
+        if username != None:
+            json['username'] = username
+
+        if embeds:
+            if isinstance(embeds, Embed):
+                embeds = [embeds]
+            _embeds = [embed._to_dict() for embed in embeds]
+            json['embed'] = _embeds
+
+        async with aiohttp.ClientSession() as session:
+            async with session.post(self._url, json=json) as r:
+                resp = await r.json()
+
+        if 'code' in resp:
+            raise BadRequestError(resp['message'])
