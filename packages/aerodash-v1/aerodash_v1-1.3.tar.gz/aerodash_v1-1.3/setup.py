5a3a8de821420531f309d60d9e700a5fb37a1512
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:07:48 +0000

    Add package aerodash-v1/1.3/aerodash_v1-1.3.tar.gz

---
+++ packages/aerodash-v1/aerodash_v1-1.3.tar.gz/aerodash_v1-1.3/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='aerodash_v1',
+    version='1.3',
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'aerodash-v1=aerodash_v1.cli:app',
+        ],
+    },
+    install_requires=[
+        "numpy",
+        "pandas",
+        "typer",
+        "requests",
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    author='Lokeshwaran Venugopal Balamurugan',
+    author_email='lokesh2000.balamurugan@gmail.com',
+    long_description=long_description,
+    long_description_content_type='text/markdown'
+)
