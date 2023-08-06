e9edf3b34f4926eab214ffeca78ea8de9481bd11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:10:47 +0000

    Add package giant-mixins/0.3.4/giant_mixins-0.3.4.tar.gz

---
+++ packages/giant-mixins/giant_mixins-0.3.4.tar.gz/giant_mixins-0.3.4/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['mixins']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['django-filer>=2,<3']
+
+extras_require = \
+{'cms': ['django-cms']}
+
+setup_kwargs = {
+    'name': 'giant-mixins',
+    'version': '0.3.4',
+    'description': 'A mixins app that provides some standard mixins for Giant projects',
+    'long_description': '# Giant Mixins\n\nA small, re-usable package which can be used in any project that requires mixins (which is 99% of them)\nThis will include the standard mixins such as TimestampMixin, PublishingMixin and VideoURLMixin\n\n## Installation\n\nTo install the standard app with no extras simply run\n\n    $ poetry add giant-mixins\n\nHowever if your project has django-cms installed then you can make use of the full range of mixins in this app. For this, install the package with the extra dependencies,\n\n    $ poetry add giant-mixins --extras "cms"\n\nYou should then add `"mixins"` to the `INSTALLED_APPS` in your settings file.\n',
+    'author': 'Will-Hoey',
+    'author_email': 'will.hoey@giantmade.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'https://github.com/giantmade/giant-mixins',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'extras_require': extras_require,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
