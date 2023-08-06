3382b2fa8094faa74e6df09a5a22f05a4295be90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:16:52 +0000

    Add package fancy-abak/1.2.1/fancy-abak-1.2.1.tar.gz

---
+++ packages/fancy-abak/fancy-abak-1.2.1.tar.gz/fancy-abak-1.2.1/README.md
@@ -0,0 +1,31 @@
+# Fancy Abak
+
+![PyPI - Downloads](https://img.shields.io/pypi/dm/fancy-abak)
+![PyPI](https://img.shields.io/pypi/v/fancy-abak?style=flat)
+CLI tool to use ABAK
+
+- [Fancy Abak](#fancy-abak)
+  - [Requirements](#requirements)
+  - [Installation](#installation)
+    - [MacOS](#macos)
+
+## Requirements
+
+- (Tested and built using) Python >3.10
+
+## Installation
+
+Package is available in PyPi:
+
+```shell
+pip install fancy-abak
+```
+
+### MacOS
+
+Package is available in brew:
+
+```shell
+brew tap gcarrarom/fancygui
+brew install gcarrarom/fancygui/fancy-abak
+```
---
+++ packages/fancy-abak/fancy-abak-1.2.1.tar.gz/fancy-abak-1.2.1/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='fancy-abak',
+    version='1.2.1',
+    author='Gui Martins',
+    url='https://fancywhale.ca/',
+    author_email='gmartins@fancywhale.ca',
+    packages=find_packages(),
+    include_package_data=True,
+    py_modules=['abak'],
+    python_requires='>=3.6',
+    install_requires=[
+        "certifi==2022.12.7",
+        "charset-normalizer==3.0.1",
+        "click==8.1.3",
+        "idna==3.4",
+        "importlib-metadata==6.0.0",
+        "pyfzf==0.3.1",
+        "jaraco.classes==3.2.3",
+        "keyring==23.13.1",
+        "more-itertools==9.0.0",
+        "pyaml==21.10.1",
+        "PyYAML==6.0",
+        "requests==2.28.2",
+        "tabulate==0.9.0",
+        "urllib3==1.26.14",
+        "zipp==3.12.0",
+    ],
+    entry_points='''
+        [console_scripts]
+        abak=abak:abak
+    ''',
+    long_description="CLI Tool to interface with Abak"
+)
