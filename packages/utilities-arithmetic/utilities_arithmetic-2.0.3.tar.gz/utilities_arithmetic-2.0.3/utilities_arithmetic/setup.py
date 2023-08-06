a64d21bc06ecec3527913e5478e61403c50dba12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:59:56 +0000

    Add package utilities-arithmetic/2.0.3/utilities_arithmetic-2.0.3.tar.gz

---
+++ packages/utilities-arithmetic/utilities_arithmetic-2.0.3.tar.gz/utilities_arithmetic-2.0.3/utilities_arithmetic/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+    name='utilities_arithmetic',
+    version='2.0.3',
+    install_requires=[
+        "setuptools",
+        "twine",
+        "wheel"
+    ],
+    description='A package for performing arithmetic operations',
+    author='Kathir',
+    author_email='KathirvelMurugan.SivaSubramanian@cognizant.com',
+    packages=['utilities_arithmetic'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+)
