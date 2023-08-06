ba7a566fd0beedd3d19eadf3f32b6e934db90aa2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:26:55 +0000

    Add package Supbotai/0.2.2/Supbotai-0.2.2.tar.gz

---
+++ packages/Supbotai/Supbotai-0.2.2.tar.gz/Supbotai-0.2.2/setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+
+
+
+setup(name='Supbotai',
+        version='0.2.2',
+        description='Supbot is a customer service bot can be integrated with any website',
+        author='Supbot L.T.D',
+        author_email="yousef.gamal.2951@gmail.com",
+        packages=['supbotai'],
+        classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],)
+
