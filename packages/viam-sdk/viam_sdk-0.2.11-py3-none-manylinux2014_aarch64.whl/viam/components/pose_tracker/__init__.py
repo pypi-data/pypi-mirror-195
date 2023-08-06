95e49fcbe09735403bfdd899dd51452de5865fb4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:16:43 +0000

    Add package viam-sdk/0.2.11/viam_sdk-0.2.11-py3-none-manylinux2014_aarch64.whl

---
+++ packages/viam-sdk/viam_sdk-0.2.11-py3-none-manylinux2014_aarch64.whl/viam/components/pose_tracker/__init__.py
@@ -0,0 +1,17 @@
+from viam.resource.registry import ComponentRegistration, Registry
+
+from .client import PoseTrackerClient
+from .pose_tracker import PoseTracker
+from .service import PoseTrackerService
+
+__all__ = [
+    "PoseTracker",
+]
+
+Registry.register_subtype(
+    ComponentRegistration(
+        PoseTracker,
+        PoseTrackerService,
+        lambda name, channel: PoseTrackerClient(name, channel),
+    )
+)
