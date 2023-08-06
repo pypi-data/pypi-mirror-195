6c65dcba956ed109255ab153c3fdee394cf0e173
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:59 +0000

    Add package torrentcm/0.0.1/torrentcm-0.0.1.tar.gz

---
+++ packages/torrentcm/torrentcm-0.0.1.tar.gz/torrentcm-0.0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='torrentcm',
+    version='0.0.1',    
+    description='Torrent Client Manager is a CLI utility for managing torrent client instances.',
+    url='https://github.com/ezggeorge/torrent-client-manager',
+    author='EZGGeorge',
+    author_email='shudson@anl.gov',
+    license='BSD 2-clause',
+    
+    install_requires=['argparse','rich','qbittorrent-api','platformdirs'                
+                      ],
+)
