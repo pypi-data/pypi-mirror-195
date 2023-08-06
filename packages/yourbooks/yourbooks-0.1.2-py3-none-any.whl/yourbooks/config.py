0c561712896567a424c2777ada82caf343344b4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:38:22 +0000

    Add package yourbooks/0.1.2/yourbooks-0.1.2-py3-none-any.whl

---
+++ packages/yourbooks/yourbooks-0.1.2-py3-none-any.whl/yourbooks/config.py
@@ -0,0 +1,8 @@
+SERVER_URL = f"https://yourbooks.bengriffith.dev"
+BASE_URL = 'https://www.googleapis.com/books/v1/volumes'
+SEARCH_URL = BASE_URL + "?{}"
+BOOK_URL = BASE_URL + '/{}'
+NUM_SEARCH_RESULTS = 8
+NOT_FOUND = 'Not found'
+DEFAULT_LANGUAGE = "en"
+SLEEP_BETWEEN_REQUESTS = 2
