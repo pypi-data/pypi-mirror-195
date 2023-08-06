85d3cca390f4afdf81595fae74bad0e579a8265d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:45:54 +0000

    Add package openlineage-sql/0.21.1/openlineage_sql-0.21.1.tar.gz

---
+++ packages/openlineage-sql/openlineage_sql-0.21.1.tar.gz/openlineage_sql-0.21.1/local_dependencies/openlineage_sql/tests/table_lineage/mod.rs
@@ -0,0 +1,13 @@
+// Copyright 2018-2023 contributors to the OpenLineage project
+// SPDX-License-Identifier: Apache-2.0
+
+pub mod tests_copy;
+pub mod tests_create;
+pub mod tests_cte;
+pub mod tests_delete;
+pub mod tests_error_handling;
+pub mod tests_insert;
+pub mod tests_merge;
+pub mod tests_select;
+pub mod tests_tpcds;
+pub mod tests_update;
