73ef32d014afff41a01efceecc4e6499551ed89c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:11:45 +0000

    Add package shellexecute/1.0.5/shellexecute-1.0.5.tar.gz

---
+++ packages/shellexecute/shellexecute-1.0.5.tar.gz/shellexecute-1.0.5/README.md
@@ -0,0 +1,35 @@
+# shellexecute
+
+[![Linting](../../actions/workflows/lint.yml/badge.svg)](../../actions/workflows/lint.yml)
+
+[![MacOS_Tests](../../actions/workflows/push_macos.yml/badge.svg)](../../actions/workflows/push_macos.yml)
+[![Ubuntu_Tests](../../actions/workflows/push_ubuntu.yml/badge.svg)](../../actions/workflows/push_ubuntu.yml)
+[![Win_Tests](../../actions/workflows/push_win.yml/badge.svg)](../../actions/workflows/push_win.yml)
+
+`pip install shellexecute`
+
+```python
+from shellexecute import execute
+rtn = execute(
+    f"echo HI",
+    send_confirmation=[("HI", "y")],
+)
+```
+
+Cross platform way to run shell commands using pexpect (wexpect on windows). VERY useful for installers where
+you want to automate accepting prompts.
+
+To develop software, run `. ./activate.sh`
+
+# Windows
+
+This environment requires you to use `git-bash`.
+
+# Linting
+
+Run `./lint.sh` to find linting errors using `pylint`, `flake8` and `mypy`.
+
+
+# Versions
+  * 1.0.5: Fix coloram issue in non windows builds.
+  * 1.0.4: Timeout is now set to None by default.
---
+++ packages/shellexecute/shellexecute-1.0.5.tar.gz/shellexecute-1.0.5/src/shellexecute/__init__.py
@@ -0,0 +1,71 @@
+"""
+Shared utility functions
+"""
+
+# pylint: disable=consider-using-with,global-statement,too-many-arguments,too-many-locals,fixme,too-many-statements,chained-comparison
+
+import subprocess
+import sys
+from typing import Optional
+
+if sys.platform == "win32":
+    from wexpect import spawn, EOF  # type: ignore # pylint: disable=import-error
+else:
+    from pexpect import spawn, EOF  # type: ignore # pylint: disable=import-error
+
+if sys.platform == "win32":
+    import colorama
+
+    colorama.init()  # Fixes color breakages in win32
+
+
+def execute(
+    command,
+    cwd=None,
+    send_confirmation: Optional[list[tuple[str, str]]] = None,
+    ignore_errors=False,
+    timeout=None,
+    encoding="utf-8",
+    outstream=None,
+) -> int:
+    """Execute a command"""
+
+    out = ""
+    out += "####################################\n"
+    out += f"Executing\n  {command}\n"
+    if cwd:
+        out += f"  CWD={cwd}\n"
+    out += "####################################\n"
+
+    sys.stdout.write(out)
+    sys.stdout.flush()
+
+    if send_confirmation is None:
+        completed_process = subprocess.run(
+            command,
+            cwd=cwd,
+            shell=True,
+            check=not ignore_errors,
+            timeout=timeout,
+            encoding=encoding,
+        )
+        return completed_process.returncode
+    # temporary buffer for stderr
+    child = spawn(command, cwd=cwd, encoding=encoding, timeout=timeout)
+    child.logfile = outstream or sys.stdout
+    eof_reached = False
+    for expect, answer in send_confirmation:
+        which = child.expect_exact([expect, EOF], timeout=timeout)
+        if which == 1:
+            eof_reached = True
+            break  # EOF
+        child.sendline(answer)
+    if not eof_reached:
+        child.expect(EOF)
+    if sys.platform != "win32":
+        child.close()
+    else:
+        child.wait()
+    if child.exitstatus != 0 and not ignore_errors:
+        raise RuntimeError("Command failed: " + command)
+    return child.exitstatus
