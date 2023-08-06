054e8469efb611dcdf6b751480decb986ebe3410
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:44:45 +0000

    Add package psifx/0.0.0/psifx-0.0.0.tar.gz

---
+++ packages/psifx/psifx-0.0.0.tar.gz/psifx-0.0.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name="psifx",
+    version="0.0.0",
+    description="Psychological and Social Interactions Feature Extraction",
+    long_description="N/A",
+    url="https://github.com/GuillaumeRochette/psifx",
+    author="Guillaume Rochette",
+    author_email="rochette.guillaume@gmail.com",
+    license="MIT",
+    packages=["psifx"],
+    zip_safe=False,
+)
