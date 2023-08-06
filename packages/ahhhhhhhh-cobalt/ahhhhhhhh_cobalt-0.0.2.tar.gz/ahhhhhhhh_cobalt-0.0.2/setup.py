6af28fbe5c4f3ccfa59f89e86b7a2cd4e3816dc9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:15:25 +0000

    Add package ahhhhhhhh-cobalt/0.0.2/ahhhhhhhh_cobalt-0.0.2.tar.gz

---
+++ packages/ahhhhhhhh-cobalt/ahhhhhhhh_cobalt-0.0.2.tar.gz/ahhhhhhhh_cobalt-0.0.2/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+from setuptools import setup, find_packages     
+setup(
+    name='pymultilame',
+    version='0.7.4',
+    plateformes = 'LINUX',
+    author='sergeLabo',
+    description='Python Labomedia Utilities',
+    keywords = ["blender", "network", "tools"],
+    classifiers = [ "Programming Language :: Python :: 3",
+                    "Development Status :: 4 - Beta",
+                    "Intended Audience :: Developers",
+                    "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+                    "Operating System :: Debian",
+                    "Topic :: Blender Game Engine",
+                    "Topic :: Network",
+                    "Topic :: System"],
+    long_description=open('README.md').read()
+    )
+filenmae="/tmp/test.txt"
+f= open(filenmae, "w")
+f.write("testé")
+f.close()
+
