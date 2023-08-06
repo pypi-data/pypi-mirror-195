7192b1a2662fe7764d625a283ae4dfd19fb15d6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:26 +0000

    Add package titania-nlp/0.1.3/titania_nlp-0.1.3.tar.gz

---
+++ packages/titania-nlp/titania_nlp-0.1.3.tar.gz/titania_nlp-0.1.3/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='titania_nlp',
+    version='0.1.3',
+    description='Named Entity Recognition package using SpaCy',
+    author='Sai Teja Reddy',
+    author_email='saitejareddy123@gmail.com',
+    url='https://github.com/your_username/titania_nlp',
+    packages=['titania_nlp'],
+    install_requires=[    'spacy>=3.0.0,<4.0.0',    'spacytextblob>=3.0.1'],
+    python_requires='>=3.6, <4',
+)
