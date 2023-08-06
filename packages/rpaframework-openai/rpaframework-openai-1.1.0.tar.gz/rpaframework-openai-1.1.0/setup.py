8a071534e2b1605f213c3e6007cd1f8ad0186626
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:44:28 +0000

    Add package rpaframework-openai/1.1.0/rpaframework-openai-1.1.0.tar.gz

---
+++ packages/rpaframework-openai/rpaframework-openai-1.1.0.tar.gz/rpaframework-openai-1.1.0/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['RPA']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['openai>=0.27.0,<0.28.0']
+
+setup_kwargs = {
+    'name': 'rpaframework-openai',
+    'version': '1.1.0',
+    'description': 'OpenAI library for RPA Framework',
+    'long_description': 'rpaframework-openai\n===================\n\nThis library enables OpenAI Services for `RPA Framework`_\nlibraries.\n\n.. _RPA Framework: https://rpaframework.org\n',
+    'author': 'RPA Framework',
+    'author_email': 'rpafw@robocorp.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'https://rpaframework.org/',
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7.1,<4.0.0',
+}
+
+
+setup(**setup_kwargs)
