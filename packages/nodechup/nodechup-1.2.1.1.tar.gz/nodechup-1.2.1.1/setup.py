e2cce50e751520df96a29dd48caaaabc670a549c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:30 +0000

    Add package nodechup/1.2.1.1/nodechup-1.2.1.1.tar.gz

---
+++ packages/nodechup/nodechup-1.2.1.1.tar.gz/nodechup-1.2.1.1/setup.py
@@ -0,0 +1,35 @@
+"""A setuptools based setup module."""
+
+from setuptools import find_packages, setup
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="nodechup",
+    version="1.2.1.1",
+    description="Program to manage Node.js installations",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    python_requires=">=3.6",
+    author="William Edwards",
+    author_email="opensource@cyberfusion.nl",
+    url="https://github.com/CyberfusionNL/NodeCHUP",
+    platforms=["linux"],
+    packages=find_packages(
+        include=[
+            "nodechup",
+            "nodechup.*",
+        ]
+    ),
+    data_files=[],
+    entry_points={"console_scripts": ["nodechup=nodechup.CLI:main"]},
+    install_requires=["docopt==0.6.2", "schema==0.7.2", "requests==2.27.1"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    keywords=["cyberfusion", "nodejs"],
+    license="MIT",
+)
