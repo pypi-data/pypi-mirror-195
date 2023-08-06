b4a9d2c57c9ade7c4d04ff8d5ae3cee9245e56d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:32:24 +0000

    Add package dpytest/0.6.3/dpytest-0.6.3.tar.gz

---
+++ packages/dpytest/dpytest-0.6.3.tar.gz/dpytest-0.6.3/tests/test_edit.py
@@ -0,0 +1,14 @@
+
+import pytest
+import discord.ext.test as test  # noqa: F401
+
+
+@pytest.mark.asyncio
+async def test_edit(bot):
+    guild = bot.guilds[0]
+    channel = guild.channels[0]
+
+    mes = await channel.send("Test Message")
+    edited_mes = await mes.edit(content="New Message")
+
+    assert edited_mes.content == "New Message"
