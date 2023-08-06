85d9aa25095c5f0f6b5b36fd4261a98043e0ed00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:15:28 +0000

    Add package htrc-vsm/0.4.15.1/htrc_vsm-0.4.15.1.tar.gz

---
+++ packages/htrc-vsm/htrc_vsm-0.4.15.1.tar.gz/htrc_vsm-0.4.15.1/src/vsm/unit_tests/tests_model.py
@@ -0,0 +1,32 @@
+import unittest2 as unittest
+import numpy as np
+
+from vsm.corpus.util.corpusbuilders import random_corpus
+from vsm.model.base import BaseModel
+
+
+class TestBaseModel(unittest.TestCase):
+
+    def setUp(self):
+        self.c = random_corpus(1000, 50, 6, 100)
+        self.m = BaseModel(self.c, 'context')
+
+
+    def test_BaseModel_IO(self):
+           
+        from tempfile import NamedTemporaryFile as NTF
+        import os
+
+        c = random_corpus(1000, 50, 6, 100)
+        with NTF(delete=False, suffix='.npz') as tmp:
+            m0 = BaseModel(c.corpus, 'context')
+            m0.save(tmp.name)
+            m1 = BaseModel.load(tmp.name)
+
+            self.assertEqual(m0.context_type, m1.context_type)
+            self.assertTrue((m0.matrix == m1.matrix).all())
+
+        os.remove(tmp.name)
+
+suite = unittest.TestLoader().loadTestsFromTestCase(TestBaseModel)
+unittest.TextTestRunner(verbosity=2).run(suite)
---
+++ packages/htrc-vsm/htrc_vsm-0.4.15.1.tar.gz/htrc_vsm-0.4.15.1/src/vsm/unit_tests/tests_spatial.py
@@ -0,0 +1,59 @@
+from __future__ import division
+from past.utils import old_div
+import unittest2 as unittest
+import numpy as np
+
+from vsm.spatial import *
+
+#TODO: add tests for recently added methods.
+def KL(p,q):
+    return sum(p*np.log2(old_div(p,q)))
+def partial_KL(p,q):
+    return p * np.log2(old_div((2*p), (p+q)))
+def JS(p,q):
+    return 0.5*(KL(p,((p+q)*0.5)) + KL(q,((p+q)*0.5)))
+def JSD(p,q):
+    return (0.5*(KL(p,((p+q)*0.5)) + KL(q,((p+q)*0.5))))**0.5
+
+
+class TestSpatial(unittest.TestCase):
+
+    def setUp(self):
+        # 2 random distributions
+        self.p=np.random.random_sample((5,))
+        self.q=np.random.random_sample((5,))
+
+        # normalize
+        self.p /= self.p.sum()
+        self.q /= self.q.sum()
+
+    def test_KL_div(self):
+        self.assertTrue(np.allclose(KL_div(self.p,self.q), KL(self.p,self.q)))
+        
+    def test_JS_div(self):
+        self.assertTrue(np.allclose(JS_div(self.p,self.q), JS(self.p,self.q)))
+    
+    def test_JS_dist(self):
+        self.assertTrue(np.allclose(JS_dist(self.p,self.q), JSD(self.p,self.q)))
+
+
+    def test_count_matrix(self):
+    
+        arr = [1, 2, 4, 2, 1]
+        slices = [slice(0,1), slice(1, 3), slice(3,3), slice(3, 5)]
+        m = 6
+        result = coo_matrix([[0, 0, 0, 0],
+                         [1, 0, 0, 1],
+                         [0, 1, 0, 1],
+                         [0, 0, 0, 0],
+                         [0, 1, 0, 0],
+                         [0, 0, 0, 0]])
+    
+        self.assertTrue((result.toarray() == 
+                        count_matrix(arr, slices, m).toarray()).all())
+
+    
+
+
+suite = unittest.TestLoader().loadTestsFromTestCase(TestSpatial)
+unittest.TextTestRunner(verbosity=2).run(suite)
