8bd45321ab31d35dc66f09df85563d115543c6a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:57 +0000

    Add package py-lav/1.9.1/py_lav-1.9.1.tar.gz

---
+++ packages/py-lav/py_lav-1.9.1.tar.gz/py_lav-1.9.1/pylav/constants/config/utils.py
@@ -0,0 +1,32 @@
+from __future__ import annotations
+
+import os
+import pathlib
+
+
+def remove_keys(*keys, data: dict) -> dict:
+    for key in keys:
+        data.pop(key, None)
+    return data
+
+
+def in_container() -> bool:
+    try:
+        proc_1 = r"/proc/1/sched"
+
+        out = pathlib.Path(proc_1).read_text() if os.path.exists(proc_1) else ""
+        checks = [
+            "docker" in out,
+            "/lxc/" in out,
+            out.split(" ")[0]
+            not in (
+                "systemd",
+                "init",
+            ),
+            os.path.exists("./dockerenv"),
+            os.path.exists("/.dockerinit"),
+            os.getenv("container") is not None,
+        ]
+        return any(checks)
+    except Exception:
+        return False
---
+++ packages/py-lav/py_lav-1.9.1.tar.gz/py_lav-1.9.1/pylav/events/manager.py
@@ -0,0 +1,71 @@
+from __future__ import annotations
+
+import inspect
+from typing import TYPE_CHECKING
+
+from pylav.events import base, node, player, plugins, queue, track
+from pylav.events.plugins import sponsorblock
+from pylav.events.track import track_start
+from pylav.events.utils import get_event_name, get_simple_event_name
+
+if TYPE_CHECKING:
+    from pylav.core.client import Client
+
+
+class DispatchManager:
+    """
+    The Dispatcher is responsible for dispatching events to the appropriate
+    handlers.
+
+    The method names are the event names.
+
+    You can listen to events by adding the following to your client:
+
+
+    Examples
+    --------
+    >>> from discord.ext import commands
+
+    >>> @commands.Cog.listener()
+    >>> async def on_pylav_queue_end_event(self, event: queue.QueueEndEvent):
+    >>>    print(f"Queue ended: {event.player}")
+
+    >>> @commands.Cog.listener()
+    >>> async def on_pylav_track_stuck_event(self, event: track.TrackStuckEvent):
+    >>>    print(f"Track got stuck: {event.track}")
+
+    """
+
+    __slots__ = ("_client", "dispatcher", "mapping")
+
+    def __init__(self, client: Client) -> None:
+        self._client = client
+        self.dispatcher = client.bot.dispatch
+
+        self.mapping: dict[type[base.PyLavEvent], str] = {}
+        self._update_mapper(player)
+        self._update_mapper(node)
+        self._update_mapper(queue)
+        self._update_mapper(track)
+        self._update_mapper(plugins)
+        self._update_mapper(track_start)
+        self._update_mapper(sponsorblock)
+
+    def _update_mapper(self, module: node | player | queue | track) -> None:  # type: ignore
+        self.mapping.update(
+            {
+                c: get_event_name(c)
+                for _, c in inspect.getmembers(module, inspect.isclass)
+                if issubclass(c, base.PyLavEvent)
+            }
+        )
+
+    async def dispatch(self, event: base.PyLavEvent) -> None:
+        event_name = self.mapping[type(event)]
+        self.dispatcher(event_name, event)
+
+    def get_event_names(self) -> set[str]:
+        return set(self.mapping.values())
+
+    def simple_event_names(self) -> set[str]:
+        return {get_simple_event_name(k) for k in self.mapping.keys()}
