585cfc4127e76e42f6cc60fd5df3cf22bab81e88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:31:06 +0000

    Add package netbox-storage/0.0.0.0.503/netbox_storage-0.0.0.0.503-py3-none-any.whl

---
+++ packages/netbox-storage/netbox_storage-0.0.0.0.503-py3-none-any.whl/netbox_storage/views/simple.py
@@ -0,0 +1,29 @@
+from netbox.views import generic
+
+from netbox_storage.forms import LVMSimpleForm, VolumeSimpleForm
+from netbox_storage.models import LinuxVolume, LogicalVolume, Drive
+
+
+class LVMAddSimpleView(generic.ObjectEditView):
+    """View for editing a Drive instance."""
+
+    queryset = Drive.objects.all()
+    form = LVMSimpleForm
+    default_return_url = "plugins:netbox_storage:drive_list"
+
+
+# class LVAddSimpleView(generic.ObjectEditView):
+    """View for editing a Drive instance."""
+
+#    queryset = LinuxVolume.objects.all()
+#    form = LVSimpleForm
+#    default_return_url = "plugins:netbox_storage:drive_list"
+
+
+class AddVolume(generic.ObjectEditView):
+    """View for editing a Drive instance."""
+    template_name = "netbox_storage/inc/volume_add.html"
+    queryset = Drive.objects.all()
+    form = VolumeSimpleForm
+    default_return_url = "plugins:netbox_storage:drive_list"
+    # default_return_url = "virtualization:virtualmachine"
