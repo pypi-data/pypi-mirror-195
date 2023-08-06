bb360362b20e5238899fcd7b3c8ede74b3f8d8af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:15:19 +0000

    Add package accountingkits/0.0.0a0/accountingkits-0.0.0a0-py3-none-any.whl

---
+++ packages/accountingkits/accountingkits-0.0.0a0-py3-none-any.whl/accountingkits/__init__.py
@@ -0,0 +1,11 @@
+"""
+package principle:
+    1. Input and output should end with input/return type
+       Like list/arr/df/bool_arr/dict/int/float/str...
+    2. constant vars should be UPPER_CASE, others be snake_case
+    3. IMPORTANT: ALL CHANGEABLE TYPE(df,array,list) should 
+        DEEP COPY before CHANGED IN MEMORY!
+"""
+__version__ = '0.0.0.alpha'
+
+github_url = 'https://github.com/qihangZH/accountingkits'
