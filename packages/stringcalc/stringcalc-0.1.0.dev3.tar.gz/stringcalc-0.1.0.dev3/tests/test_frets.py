dc58b8f16e644324e399aaa22652750bcfad5a17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:12:45 +0000

    Add package stringcalc/0.1.0.dev3/stringcalc-0.1.0.dev3.tar.gz

---
+++ packages/stringcalc/stringcalc-0.1.0.dev3.tar.gz/stringcalc-0.1.0.dev3/tests/test_frets.py
@@ -0,0 +1,42 @@
+import numpy as np
+import pytest
+
+from stringcalc import frets
+
+
+@pytest.mark.parametrize(
+    "n, expected",
+    [
+        (12, 0.5),
+        (24, 0.75),
+        ([12, 24], np.array([0.5, 0.75], dtype=float)),
+        (1, 0.05613),
+        (5, 0.25085),
+    ],
+)
+def test_d_et_rel(n, expected):
+    np.testing.assert_allclose(frets.distance_et(n, L=1), expected, rtol=1e-4)
+
+
+@pytest.mark.parametrize(
+    "ab, d, expected",
+    [
+        ((0, 2), 2, 18.332),
+        ((2, None), 16.3316, 18.332),
+        ((2, None), 21, 23.572),
+        ((0, 1), 1, 17.817),
+        ((1, 0), 1, 17.817),
+    ],
+)
+def test_l_from_d(ab, d, expected):
+    assert frets.length_from_distance(ab, d) == pytest.approx(expected, abs=5e-4)
+
+
+def test_l_from_d_invalid_ab():
+    with pytest.raises(ValueError, match="must be different"):
+        frets.length_from_distance((0, 0), 10)
+
+
+def test_l_from_d_invalid_d():
+    with pytest.raises(ValueError, match="must be positive"):
+        frets.length_from_distance((0, 1), -10)
