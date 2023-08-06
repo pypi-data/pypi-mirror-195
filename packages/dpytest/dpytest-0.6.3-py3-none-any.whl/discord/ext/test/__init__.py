1c862600b0a530d4460cfba84d5872cd5207ec1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:32:24 +0000

    Add package dpytest/0.6.3/dpytest-0.6.3-py3-none-any.whl

---
+++ packages/dpytest/dpytest-0.6.3-py3-none-any.whl/discord/ext/test/__init__.py
@@ -0,0 +1,11 @@
+
+__title__ = "dpytest"
+__author__ = "Rune Tynan"
+__license__ = "MIT"
+__copyright__ = "Copyright 2018-2019 CraftSpider"
+__version__ = "0.6.3"
+
+from . import backend
+from .runner import *
+from .utils import embed_eq, activity_eq, embed_proxy_eq, PeekableQueue
+from .verify import verify, Verify, VerifyMessage, VerifyActivity
