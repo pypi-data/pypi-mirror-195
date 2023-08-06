a2af9281e6c337851da3ca8fa90aa89a62bcffbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:13:13 +0000

    Add package Bak-Bak/0.1/Bak-Bak-0.1.tar.gz

---
+++ packages/Bak-Bak/Bak-Bak-0.1.tar.gz/Bak-Bak-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='Bak-Bak',
+    version='0.1',
+    description='A Python library for speech synthesis using SAPI.SpVoice',
+    py_modules=['bakbak'],
+    install_requires=[
+        'pypiwin32'
+    ],
+)
