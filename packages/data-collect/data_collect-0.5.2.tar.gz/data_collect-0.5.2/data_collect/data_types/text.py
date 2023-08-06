cbb5903c62da35f9a0a568361df46fcce44ac5ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:24:49 +0000

    Add package data-collect/0.5.2/data_collect-0.5.2.tar.gz

---
+++ packages/data-collect/data_collect-0.5.2.tar.gz/data_collect-0.5.2/data_collect/data_types/text.py
@@ -0,0 +1,14 @@
+# !/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+#
+# Copyright (c) 2022 Baidu.com, Inc. All Rights Reserved
+from .blob import Blob
+
+
+class Text(Blob):
+    def __init__(self,
+                 data: bytes,
+                 correlation_id: str = '',
+                 metadata=None
+                 ):
+        super().__init__(data, correlation_id, metadata)
