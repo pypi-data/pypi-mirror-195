ccff01279b48ee6a27dea33c74a17d79767ebba3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:40:22 +0000

    Add package undate/0.1.0/undate-0.1.0.tar.gz

---
+++ packages/undate/undate-0.1.0.tar.gz/undate-0.1.0/src/undate/undate.py
@@ -0,0 +1,163 @@
+import datetime
+from calendar import monthrange
+
+# Pre 3.10 requires Union for multiple types, e.g. Union[int, None] instead of int | None
+from typing import Optional, Dict, Union
+
+from dateutil.relativedelta import relativedelta
+
+from undate.dateformat.base import BaseDateFormat
+
+
+# duration of a single day
+ONE_DAY = datetime.timedelta(days=1)
+
+
+class Undate:
+    """Simple object for representing uncertain, fuzzy or partially unknown dates"""
+
+    DEFAULT_FORMAT = "ISO8601"
+
+    earliest: Union[datetime.date, None] = None
+    latest: Union[datetime.date, None] = None
+    #: A string to label a specific undate, e.g. "German Unity Date 2022" for Oct. 3, 2022.
+    #: Labels are not taken into account when comparing undate objects.
+    label: Union[str, None] = None
+    formatter: Union[BaseDateFormat, None] = None
+
+    def __init__(
+        self,
+        year: Optional[int] = None,
+        month: Optional[int] = None,
+        day: Optional[int] = None,
+        formatter: Optional[BaseDateFormat] = None,
+        label: Optional[str] = None,
+    ):
+        # TODO: support initializing for unknown values in each of these
+        # e.g., maybe values could be string or int; if string with
+        # unknown digits, calculate min/max for unknowns
+
+        # for unknowns, assume smallest possible value for earliest and
+        # largest valid for latest
+        self.earliest = datetime.date(year or datetime.MINYEAR, month or 1, day or 1)
+        # if day is unknown but we have year and month, calculate max day
+        if day is None and year and month:
+            _, maxday = monthrange(year, month)
+        elif day is None and year is None and month:
+            # TODO: what to do if we don't have year and month?
+            # This will produce bad data if the year is a leap year and the month is February
+            # 2022 chosen below as it is not a not leap year
+            # Better than just setting 31, but still not great
+            _, maxday = monthrange(2022, month)
+        else:
+            maxday: int = 31
+        self.latest = datetime.date(
+            year or datetime.MAXYEAR, month or 12, day or maxday
+        )
+        # keep track of which values are known
+        self.known_values: Dict[str, bool] = {
+            "year": year is not None,
+            "month": month is not None,
+            "day": day is not None,
+        }
+
+        if not formatter:
+            # TODO subclass definitions not available unless they are imported where Undate() is called
+            formatter = BaseDateFormat.available_formatters()[self.DEFAULT_FORMAT]()
+        self.formatter = formatter
+
+        self.label = label
+
+    def __str__(self) -> str:
+        return self.formatter.to_string(self)
+
+    def __repr__(self) -> str:
+        if self.label:
+            return "<Undate '%s' (%s)>" % (self.label, self)
+        return "<Undate %s>" % self
+
+    def __eq__(self, other: "Undate") -> bool:
+        # question: should label be taken into account when checking equality?
+        # for now, assuming label differences don't matter for comparing dates
+        return (
+            self.earliest == other.earliest
+            and self.latest == other.latest
+            and self.known_values == other.known_values
+        )
+
+    @property
+    def known_year(self) -> bool:
+        return self.known_values["year"]
+
+    def duration(self) -> datetime.timedelta:
+        # what is the duration of this date?
+        # subtract earliest from latest, and add a day to count the starting day
+        return self.latest - self.earliest + ONE_DAY
+
+
+class UndateInterval:
+    """A date range between two uncertain dates.
+
+    :param earliest: Earliest undate
+    :type earliest: `undate.Undate`
+    :param latest: Latest undate
+    :type latest:  `undate.Undate`
+    :param label: A string to label a specific undate interval, similar to labels of `undate.Undate`.
+    :type label: `str`
+    """
+
+    # date range between two uncertain dates
+
+    def __init__(
+        self,
+        earliest: Union[Undate, None] = None,
+        latest: Union[Undate, None] = None,
+        label: Union[str, None] = None,
+    ):
+        # for now, assume takes two undate objects
+        self.earliest = earliest
+        self.latest = latest
+        self.label = label
+
+    def __str__(self) -> str:
+        # using EDTF syntax for open ranges
+        return "%s/%s" % (self.earliest or "..", self.latest or "")
+
+    def __repr__(self) -> str:
+        if self.label:
+            return "<UndateInterval '%s' (%s)>" % (self.label, self)
+        return "<UndateInterval %s>" % self
+
+    def __eq__(self, other) -> bool:
+        # consider interval equal if both dates are equal
+        return self.earliest == other.earliest and self.latest == other.latest
+
+    def duration(self) -> datetime.timedelta:
+        """Calculate the duration between two undates.
+
+        :returns: A duration
+        :rtype: timedelta
+        """
+        # what is the duration of this date range?
+
+        # if both years are known, subtract end of range from beginning of start
+        if self.latest.known_year and self.earliest.known_year:
+            return self.latest.latest - self.earliest.earliest + ONE_DAY
+
+        # if neither year is known...
+        elif not self.latest.known_year and not self.earliest.known_year:
+            # under what circumstances can we assume that if both years
+            # are unknown the dates are in the same year or sequential?
+            duration = self.latest.earliest - self.earliest.earliest + ONE_DAY
+            # if we get a negative, we've wrapped from end of one year
+            # to the beginning of the next
+            if duration.days < 0:
+                end = self.latest.earliest + relativedelta(years=1)
+                duration = end - self.earliest.earliest
+
+            return duration
+
+        else:
+            # is there any meaningful way to calculate duration
+            # if one year is known and the other is not?
+            raise NotImplementedError
---
+++ packages/undate/undate-0.1.0.tar.gz/undate-0.1.0/tests/test_undate.py
@@ -0,0 +1,139 @@
+from datetime import timedelta
+
+import pytest
+
+from undate.undate import Undate, UndateInterval
+
+
+class TestUndate:
+    def test_str(self):
+        assert str(Undate(2022, 11, 7)) == "2022-11-07"
+        assert str(Undate(2022, 11)) == "2022-11"
+        assert str(Undate(2022)) == "2022"
+        assert str(Undate(month=11, day=7)) == "--11-07"
+
+    def test_repr(self):
+        assert repr(Undate(2022, 11, 7)) == "<Undate 2022-11-07>"
+        assert (
+            repr(Undate(2022, 11, 7, label="A Special Day"))
+            == "<Undate 'A Special Day' (2022-11-07)>"
+        )
+
+    def test_invalid_date(self):
+        # invalid month should raise an error
+        with pytest.raises(ValueError):
+            Undate(1990, 22)
+
+    def test_eq(self):
+        assert Undate(2022) == Undate(2022)
+        assert Undate(2022, 10) == Undate(2022, 10)
+        assert Undate(2022, 10, 1) == Undate(2022, 10, 1)
+        assert Undate(month=2, day=7) == Undate(month=2, day=7)
+
+    def test_not_eq(self):
+        assert Undate(2022) != Undate(2023)
+        assert Undate(2022, 10) != Undate(2022, 11)
+        assert Undate(2022, 10, 1) != Undate(2022, 10, 2)
+        assert Undate(month=2, day=7) != Undate(month=2, day=8)
+        assert Undate(2022) != Undate(2022, 10)
+        assert Undate(2022, 10) != Undate(2022, 10, 1)
+
+    def test_duration(self):
+        day_duration = Undate(2022, 11, 7).duration()
+        assert isinstance(day_duration, timedelta)
+        assert day_duration.days == 1
+
+        january_duration = Undate(2022, 1).duration()
+        assert january_duration.days == 31
+        feb_duration = Undate(2022, 2).duration()
+        assert feb_duration.days == 28
+        # next leap year will be 2024
+        leapyear_feb_duration = Undate(2024, 2).duration()
+        assert leapyear_feb_duration.days == 29
+
+        year_duration = Undate(2022).duration()
+        assert year_duration.days == 365
+        leapyear_duration = Undate(2024).duration()
+        assert leapyear_duration.days == 366
+
+    def test_known_year(self):
+        assert Undate(2022).known_year is True
+        assert Undate(month=2, day=5).known_year is False
+
+
+class TestUndateInterval:
+    def test_str(self):
+        # 2022 - 2023
+        assert str(UndateInterval(Undate(2022), Undate(2023))) == "2022/2023"
+        # 2022 - 2023-05
+        assert str(UndateInterval(Undate(2022), Undate(2023, 5))) == "2022/2023-05"
+        # 2022-11-01 to 2022-11-07
+        assert (
+            str(UndateInterval(Undate(2022, 11, 1), Undate(2023, 11, 7)))
+            == "2022-11-01/2023-11-07"
+        )
+
+    def test_repr(self):
+        assert (
+            repr(UndateInterval(Undate(2022), Undate(2023)))
+            == "<UndateInterval 2022/2023>"
+        )
+        assert (
+            repr(UndateInterval(Undate(2022), Undate(2023), label="Fancy Epoch"))
+            == "<UndateInterval 'Fancy Epoch' (2022/2023)>"
+        )
+
+    def test_str_open_range(self):
+        # 900 -
+        assert str(UndateInterval(Undate(900))) == "0900/"
+        # - 1900
+        assert str(UndateInterval(latest=Undate(1900))) == "../1900"
+        # - 1900-12
+        assert str(UndateInterval(latest=Undate(1900, 12))) == "../1900-12"
+
+    def test_eq(self):
+        assert UndateInterval(Undate(2022), Undate(2023)) == UndateInterval(
+            Undate(2022), Undate(2023)
+        )
+        assert UndateInterval(Undate(2022), Undate(2023, 5)) == UndateInterval(
+            Undate(2022), Undate(2023, 5)
+        )
+        assert UndateInterval(Undate(2022, 5)) == UndateInterval(Undate(2022, 5))
+
+    def test_not_eq(self):
+        assert UndateInterval(Undate(2022), Undate(2023)) != UndateInterval(
+            Undate(2022), Undate(2024)
+        )
+        assert UndateInterval(Undate(2022), Undate(2023, 5)) != UndateInterval(
+            Undate(2022), Undate(2023, 6)
+        )
+        assert UndateInterval(Undate(2022), Undate(2023, 5)) != UndateInterval(
+            Undate(2022), Undate(2023)
+        )
+        assert UndateInterval(Undate(2022, 5)) != UndateInterval(Undate(2022, 6))
+
+    def test_duration(self):
+        week_duration = UndateInterval(
+            Undate(2022, 11, 1), Undate(2022, 11, 7)
+        ).duration()
+        assert isinstance(week_duration, timedelta)
+        assert week_duration.days == 7
+
+        twomonths = UndateInterval(Undate(2022, 11), Undate(2022, 12)).duration()
+        # november - december = 30 days + 31 days
+        assert twomonths.days == 30 + 31
+
+        twoyears = UndateInterval(Undate(2021), Undate(2022)).duration()
+        assert twoyears.days == 365 * 2
+
+        # special case: month/day with no year (assumes same year)
+        week_noyear_duration = UndateInterval(
+            Undate(None, 11, 1), Undate(None, 11, 7)
+        ).duration()
+        assert week_noyear_duration.days == 7
+        # special case 2: month/day with no year, wrapping from december to january
+        # (assumes sequential years)
+        month_noyear_duration = UndateInterval(
+            Undate(None, 12, 1), Undate(None, 1, 1)
+        ).duration()
+        assert month_noyear_duration.days == 31
