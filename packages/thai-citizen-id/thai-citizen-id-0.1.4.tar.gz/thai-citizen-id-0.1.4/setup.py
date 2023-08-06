f451205438be56817a52ec4fda45385fc86614e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:26:07 +0000

    Add package thai-citizen-id/0.1.4/thai-citizen-id-0.1.4.tar.gz

---
+++ packages/thai-citizen-id/thai-citizen-id-0.1.4.tar.gz/thai-citizen-id-0.1.4/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+# read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setuptools.setup(
+    name="thai-citizen-id",
+    version="0.1.4",
+    author="Nopparut Saelim",
+    author_email="ak3.nopparut@gmail.com",
+    description="Thailand citizen ID tools",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/nopparuts/thai-citizen-id",
+    package_dir={"": "."},
+    packages=['thai_citizen_id'],
+    python_requires='>=3.9',
+    classifiers=[
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+    ]
+)
