fc0ab10e7db30afff885d8ee2aa222d7340aebaa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:11:24 +0000

    Add package testpypipkgtest/0.1.1/testpypipkgtestjunk-0.1.1.tar.gz

---
+++ packages/testpypipkgtest/testpypipkgtestjunk-0.1.1.tar.gz/testpypipkgtest-0.1.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='testpypipkgtest',
+    version='0.1.1',    
+    description='A example Python package',
+    license='BSD 2-clause',
+    author='Caleb',
+    author_email='calebbrown@google.com',
+    url='https://github.com/ossf/package-analysis',
+    packages=['testpypipkgtest'],
+)
+
