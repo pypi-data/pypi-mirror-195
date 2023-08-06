dbbf68ba76df748a428dd171fda783bff7c31778
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:58 +0000

    Add package nriapp/1.0.15/nriapp-1.0.15.tar.gz

---
+++ packages/nriapp/nriapp-1.0.15.tar.gz/nriapp-1.0.15/setup.py
@@ -0,0 +1,34 @@
+
+from setuptools import setup, find_packages
+from glob import glob
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+
+setup(
+   name='nriapp',
+   version='1.0.15',
+   description='This is an internal tool for crawling MS 365 Defender web with NRI',
+   author='Llallum Victoria',
+   author_email='llallumvictoria@gmail.com',
+#   packages=find_packages('chrome'),
+   package_dir = {'':'src'},
+    
+   packages=['config', 'core', 'helper'],  #same as name
+#   packages=find_packages(exclude=['ez_setup', 'tests', 'tests.*']),
+   data_files=[('.', ['./requirements.txt', './src/changelog.txt']),
+               ('./session', glob('./src/session/*.*')),
+               ('./logs', glob('./src/logs/*.*')),
+               ('./output', glob('./src/output/*.*'))
+
+               ],
+   #setup_requires=['session', 'logs', 'output'],
+   include_package_data=True,  
+   install_requires=requirements, #external packages as dependencies,
+   entry_points = '''
+        [console_scripts]
+        nriapp=nriapp:main
+    '''
+
+)
