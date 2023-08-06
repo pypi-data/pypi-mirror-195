550b756a7b23922f1bf902d370f2cbcadf19fcc8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:59:55 +0000

    Add package je-auto-control/0.0.118/je_auto_control-0.0.118-py3-none-any.whl

---
+++ packages/je-auto-control/je_auto_control-0.0.118-py3-none-any.whl/je_auto_control/utils/exception/exceptions.py
@@ -0,0 +1,82 @@
+# general
+class AutoControlException(Exception):
+    pass
+
+
+# Keyboard
+
+
+class AutoControlKeyboardException(AutoControlException):
+    pass
+
+
+class AutoControlCantFindKeyException(AutoControlException):
+    pass
+
+
+# Mouse
+
+
+class AutoControlMouseException(AutoControlException):
+    pass
+
+
+# Screen
+
+
+class AutoControlScreenException(AutoControlException):
+    pass
+
+
+# Image detect
+
+
+class ImageNotFoundException(AutoControlException):
+    pass
+
+
+# Record
+
+
+class AutoControlRecordException(AutoControlException):
+    pass
+
+
+# Execute action
+
+
+class AutoControlJsonActionException(AutoControlException):
+    pass
+
+
+class AutoControlActionNullException(AutoControlException):
+    pass
+
+
+class AutoControlActionException(AutoControlException):
+    pass
+
+
+# timeout
+class AutoControlTimeoutException(AutoControlException):
+    pass
+
+
+# html exception
+
+class AutoControlHTMLException(AutoControlException):
+    pass
+
+
+# Json Exception
+
+class AutoControlGenerateJsonReportException(AutoControlException):
+    pass
+
+
+class AutoControlAddCommandException(AutoControlException):
+    pass
+
+
+class AutoControlArgparseException(AutoControlException):
+    pass
