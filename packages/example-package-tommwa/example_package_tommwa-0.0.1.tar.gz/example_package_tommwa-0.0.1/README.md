da60e4315b520726ebedc1ab8d7214a25d27fcdc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:07 +0000

    Add package example-package-tommwa/0.0.1/example_package_tommwa-0.0.1.tar.gz

---
+++ packages/example-package-tommwa/example_package_tommwa-0.0.1.tar.gz/example_package_tommwa-0.0.1/README.md
@@ -0,0 +1,3 @@
+# Test PyPi project
+
+Yep.
---
+++ packages/example-package-tommwa/example_package_tommwa-0.0.1.tar.gz/example_package_tommwa-0.0.1/src/example_package_tommwa/example.py
@@ -0,0 +1,13 @@
+import sc2reader
+
+def add_one(number):
+    return number + 1
+
+
+print("Hello world")
+
+
+if __name__ == "__main__":
+    print("name is main!")
+    
+
