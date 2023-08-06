acb151cb6e9904dc2fc397db2b9f080870eb5a96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:19:49 +0000

    Add package foledol-django/0.0.58/foledol_django-0.0.58-py3-none-any.whl

---
+++ packages/foledol-django/foledol_django-0.0.58-py3-none-any.whl/foledol/django/views/conditions.py
@@ -0,0 +1,27 @@
+from django.contrib.admin.views.decorators import staff_member_required
+from django.contrib.auth.decorators import login_required
+from foledol.django.tools.table import TableColumn, TableView
+
+from ..models import Condition
+
+
+class ConditionTables(TableView):
+    def __init__(self, rows):
+        super().__init__(rows, [
+            TableColumn('label', "Libellé"),
+            TableColumn('criteria', "Critère"),
+            TableColumn('value', "Valeur")
+        ], path='django:conditions', search=True)
+        self.update = 'django:condition_update'
+        self.create = 'django:condition_create'
+
+    def select(self, search, sort):
+        if len(search) > 0:
+            return Condition.objects.filter(label=search)
+        return Condition.objects.all()
+
+
+@login_required
+@staff_member_required
+def condition_list(request):
+    return ConditionTables(None).render(request)
