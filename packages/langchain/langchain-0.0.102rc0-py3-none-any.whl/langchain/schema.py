e6c489e93cb761eeb9afc93f2714f501aa8cad5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:21:07 +0000

    Add package langchain/0.0.102rc0/langchain-0.0.102rc0-py3-none-any.whl

---
+++ packages/langchain/langchain-0.0.102rc0-py3-none-any.whl/langchain/schema.py
@@ -0,0 +1,87 @@
+"""Common schema objects."""
+from typing import Any, Dict, List, NamedTuple, Optional
+
+from pydantic import BaseModel, Field, root_validator
+
+
+class AgentAction(NamedTuple):
+    """Agent's action to take."""
+
+    tool: str
+    tool_input: str
+    log: str
+
+
+class AgentFinish(NamedTuple):
+    """Agent's return value."""
+
+    return_values: dict
+    log: str
+
+
+class Generation(BaseModel):
+    """Output of a single generation."""
+
+    text: str
+    """Generated text output."""
+
+    generation_info: Optional[Dict[str, Any]] = None
+    """Raw generation info response from the provider"""
+    """May include things like reason for finishing (e.g. in OpenAI)"""
+    # TODO: add log probs
+
+
+class BaseMessage(BaseModel):
+    """Message object."""
+
+    content: str
+    additional_kwargs: dict = Field(default_factory=dict)
+
+
+class HumanMessage(BaseMessage):
+    """Type of message that is spoken by the human."""
+
+
+class AIMessage(BaseMessage):
+    """Type of message that is spoken by the AI."""
+
+
+class SystemMessage(BaseMessage):
+    """Type of message that is a system message."""
+
+
+class ChatMessage(BaseMessage):
+    """Type of message with arbitrary speaker."""
+
+    role: str
+
+
+class ChatGeneration(Generation):
+    """Output of a single generation."""
+
+    text = ""
+    message: BaseMessage
+
+    @root_validator
+    def set_text(cls, values: Dict[str, Any]) -> Dict[str, Any]:
+        values["text"] = values["message"].content
+        return values
+
+
+class ChatResult(BaseModel):
+    """Class that contains all relevant information for a Chat Result."""
+
+    generations: List[ChatGeneration]
+    """List of the things generated."""
+    llm_output: Optional[dict] = None
+    """For arbitrary LLM provider specific output."""
+
+
+class LLMResult(BaseModel):
+    """Class that contains all relevant information for an LLM Result."""
+
+    generations: List[List[Generation]]
+    """List of the things generated. This is List[List[]] because
+    each input could have multiple generations."""
+    llm_output: Optional[dict] = None
+    """For arbitrary LLM provider specific output."""
