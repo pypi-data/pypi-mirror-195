cfa967f18367db93fdb11c025ea5ca90c2832cf5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:44:47 +0000

    Add package demo-python-example-myworld/1.0/demo-python-example_myworld-1.0.tar.gz

---
+++ packages/demo-python-example-myworld/demo-python-example_myworld-1.0.tar.gz/demo-python-example_myworld-1.0/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+ 
+from setuptools import setup
+ 
+setup(
+    name='demo-python-example_myworld',
+    version='1.0',
+    description='Project example for building Python project with JFrog products',
+    author='SrinivasDurgam',
+    author_email='srinivas.durgam@accenture.com',
+    packages=['C:/myworld'],
+    install_requires=['PyYAML>3.11', 'nltk'],
+)
