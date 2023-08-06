28d65cda137563181682d31de523cbfa7b421fe6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:45 +0000

    Add package django-oasis4/1.0.7a4/django-oasis4-1.0.7a4.tar.gz

---
+++ packages/django-oasis4/django-oasis4-1.0.7a4.tar.gz/django-oasis4-1.0.7a4/oasis/coffee_offers/lib/managers/__init__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+#  Developed by CQ Inversiones SAS. Copyright ©. 2019 - 2023. All rights reserved.
+#  Desarrollado por CQ Inversiones SAS. Copyright ©. 2019 - 2023. Todos los derechos reservado
+
+# ****************************************************************
+# IDE:          PyCharm
+# Developed by: macercha
+# Date:         1/03/23 12:04
+# Project:      CFHL Transactional Backend
+# Module Name:  __init__.py
+# Description:
+# ****************************************************************
+from .offer import Offer
+
+__all__ = [
+    "Offer"
+]
