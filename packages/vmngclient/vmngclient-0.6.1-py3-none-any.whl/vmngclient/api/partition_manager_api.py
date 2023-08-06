cd8fcd105803590b30891101e962e9718d54059c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:23 +0000

    Add package vmngclient/0.6.1/vmngclient-0.6.1-py3-none-any.whl

---
+++ packages/vmngclient/vmngclient-0.6.1-py3-none-any.whl/vmngclient/api/partition_manager_api.py
@@ -0,0 +1,120 @@
+import logging
+from typing import Dict, List
+
+from vmngclient.api.versions_utils import DeviceCategory, DeviceVersions, RepositoryAPI
+from vmngclient.dataclasses import Device
+from vmngclient.session import vManageSession
+
+logger = logging.getLogger(__name__)
+
+
+class PartitionManagerAPI:
+    """
+    API methods for partitions actions. All methods
+    are exececutable on all device categories.
+
+    Usage example:
+        # Create session
+        session = create_vManageSession(...)
+
+        # Prepare devices list
+        cedges = [dev for dev in DevicesAPI(session).devices
+                    if dev.hostname in ["vm5", "vm6"]]
+
+        # Set default partition
+        partition_manager = PartitionManagerAPI(provider_session_as_tenant_session,DeviceCategory.VEDGES)
+        set_partition_id = partition_manager.set_default_partition(cedges, version="9.17.06.03a.0.56")
+
+        # Check action status
+        wait_for_completed(session, set_partition_id, 3000)
+
+    """
+
+    def __init__(self, session: vManageSession, device_category: DeviceCategory) -> None:
+
+        self.session = session
+        self.repository = RepositoryAPI(self.session)
+        self.device_versions = DeviceVersions(self.repository, device_category)
+
+    def _set_default_partition(self, payload_devices: List[dict]) -> str:
+        url = "/dataservice/device/action/defaultpartition"
+        payload = {
+            "action": "defaultpartition",
+            "devices": payload_devices,
+            "deviceType": "vmanage",
+        }
+        set_default = dict(self.session.post(url, json=payload).json())
+        return set_default["id"]
+
+    def set_current_partition_as_default(self, devices: List[Device]) -> str:
+        """
+        Method to set current software version as default version
+
+        Args:
+            devices (List[Device]): For those devices default partition
+            going to be set
+
+        Returns:
+            str: action id
+        """
+        devs = self.device_versions.get_devices_current_version(devices)
+        return self._set_default_partition(devs)
+
+    def set_default_partition(self, devices: List[Device], version: str) -> str:
+        """
+        Method to set choosen software version as current version
+
+        Args:
+            devices (List[Device]): For those devices default partition
+            going to be set
+            version (str): version to be set as default version
+
+        Returns:
+            str: action id
+        """
+        devs = self.device_versions.get_device_list_in_installed(version, devices)
+        return self._set_default_partition(devs)
+
+    def remove_partition(self, devices: List[Device], version: str, force: bool = False) -> str:
+        """
+        Method to remove choosen software version from Vmanage repository
+
+        Args:
+            devices (List[Device]): remove partition for those devices
+            version (str): software version to be removed from repository
+            force (bool): bypass version checks
+
+        Returns:
+            str: action id
+        """
+
+        url = "/dataservice/device/action/removepartition"
+        payload = {
+            "action": "removepartition",
+            "devices": self.device_versions.get_device_list_in_available(version, devices),
+            "deviceType": "vmanage",
+        }
+        if force is False:
+            invalid_devices = self._check_remove_partition_possibility(payload["devices"])
+            if invalid_devices:
+                raise ValueError(
+                    f"Current or default version of devices with ids {invalid_devices} \
+                        are equal to remove version. Action denied!"
+                )
+        remove_action: Dict[str, str] = self.repository.session.post(url, json=payload).json()
+        return remove_action["id"]
+
+    def _check_remove_partition_possibility(self, devices) -> List["str"]:
+
+        devices_versions_repository = self.repository.get_devices_versions_repository(
+            self.device_versions.device_category
+        )
+        invalid_devices = []
+        for device in devices:
+
+            if device["version"] in (
+                devices_versions_repository[device["deviceId"]].current_version,
+                devices_versions_repository[device["deviceId"]].default_version,
+            ):
+                invalid_devices.append((device["deviceId"]))
+        return invalid_devices
---
+++ packages/vmngclient/vmngclient-0.6.1-py3-none-any.whl/vmngclient/api/software_action_api.py
@@ -0,0 +1,239 @@
+import logging
+from enum import Enum
+from pathlib import PurePath
+from typing import Any, Dict, List, Optional, cast
+
+from attr import define  # type: ignore
+from clint.textui.progress import Bar as ProgressBar  # type: ignore
+from requests_toolbelt.multipart.encoder import MultipartEncoder, MultipartEncoderMonitor  # type: ignore
+
+from vmngclient.api.versions_utils import DeviceCategory, DeviceVersions, RepositoryAPI
+from vmngclient.dataclasses import Device
+from vmngclient.exceptions import VersionDeclarationError  # type: ignore
+from vmngclient.session import vManageSession
+
+logger = logging.getLogger(__name__)
+
+
+class Family(Enum):
+    VEDGE = "vedge"
+    VMANAGE = "vmanage"
+
+
+class VersionType(Enum):
+    VMANAGE = "vmanage"
+
+
+class DeviceType(Enum):
+    CONTROLLER = "controller"
+    VEDGE = "vedge"
+    VMANAGE = "vmanage"
+
+
+class DeviceClass(Enum):
+    VEDGE = "vedge"
+    VMANAGE = "vmanage"
+    VSMART = "vsmart"
+    VBOND = "vbond"
+    CEDGE = "cedge"
+
+
+@define
+class InstallSpecification:
+
+    family: Family
+    version_type: VersionType
+    device_type: DeviceType
+    device_class: DeviceClass
+
+
+class InstallSpecHelper(Enum):
+
+    VMANAGE = InstallSpecification(Family.VMANAGE, VersionType.VMANAGE, DeviceType.VMANAGE, DeviceClass.VMANAGE)
+    VSMART = InstallSpecification(Family.VEDGE, VersionType.VMANAGE, DeviceType.CONTROLLER, DeviceClass.VSMART)
+    VBOND = InstallSpecification(Family.VEDGE, VersionType.VMANAGE, DeviceType.CONTROLLER, DeviceClass.VBOND)
+    VEDGE = InstallSpecification(Family.VEDGE, VersionType.VMANAGE, DeviceType.VEDGE, DeviceClass.VEDGE)
+    CEDGE = InstallSpecification(Family.VEDGE, VersionType.VMANAGE, DeviceType.VEDGE, DeviceClass.CEDGE)
+
+
+class SoftwareActionAPI:
+    """
+    API methods for software actions. All methods
+    are exececutable on all device categories.
+
+    Usage example:
+    # Create session
+    session = create_vManageSession(...)
+
+    # Prepare devices list
+    devices = [dev for dev in DevicesAPI(session).devices if dev.hostname in ["vm5", "vm6"]]
+    software_image = PurePath("c8000v-universalk9.17.06.03a.0.56.SPA.bin")
+
+    # Upgrade
+    software_action = SoftwareActionAPI(session,DeviceCategory.VEDGES.value)
+    software_action_id = software_action.upgrade_software(devices,
+        InstallSpecHelper.CEDGE.value, reboot = False, sync = True, software_image=software_image)
+
+    # Check action status
+    wait_for_completed(session, software_action_id, 3000)
+
+    """
+
+    def __init__(self, session: vManageSession, device_category: DeviceCategory) -> None:
+
+        self.session = session
+        self.repository = RepositoryAPI(self.session)
+        self.device_versions = DeviceVersions(self.repository, device_category)
+
+    def activate_software(
+        self, devices: List[Device], version_to_activate: Optional[str] = "", software_image: Optional[str] = ""
+    ) -> str:
+        """
+        Method to set choosen version as current version
+
+        Args:
+            devices (List[Device]): For those devices software will be activated
+            version_to_activate (Optional[str]): version to be set as current version
+            software_image (Optional[str]): path to software image
+
+            Notice: Have to pass one of those arguments (version_to_activate,
+            software_image)
+
+        Returns:
+            str: action id
+        """
+        if software_image and not version_to_activate:
+            version = cast(str, self.repository.get_image_version(software_image))
+        elif version_to_activate and not software_image:
+            version = cast(str, version_to_activate)
+        else:
+            raise VersionDeclarationError("You can not provide software_image and image version at the same time")
+        url = "/dataservice/device/action/changepartition"
+        payload = {
+            "action": "changepartition",
+            "devices": self.device_versions.get_device_list_in_available(version, devices),
+            "deviceType": "vmanage",
+        }
+        activate = dict(self.session.post(url, json=payload).json())
+        return activate["id"]
+
+    def upgrade_software(
+        self,
+        devices: List[Device],
+        install_spec: InstallSpecification,
+        reboot: bool,
+        sync: bool = True,
+        software_image: Optional[str] = "",
+        image_version: Optional[str] = "",
+    ) -> str:
+        """
+        Method to install new software
+
+        Args:
+            devices (List[Device]): For those devices software will be activated
+            install_spec (InstallSpecification): specification of devices
+            on which the action is to be performed
+            reboot (bool): reboot device after action end
+            sync (bool, optional): Synchronize settings. Defaults to True.
+            software_image (Optional[str]): path to software image
+            image_version (Optional[str]): version of software image
+
+            Notice: Have to pass one of those arguments (version_to_activate,
+            software_image)
+
+        Raises:
+            ValueError: Raise error if downgrade in certain cases
+
+        Returns:
+            str: action id
+        """
+        if software_image and not image_version:
+            version = cast(str, self.repository.get_image_version(software_image))
+        elif image_version and not software_image:
+            version = cast(str, image_version)
+        else:
+            raise VersionDeclarationError("You can not provide software_image and image version at the same time")
+        url = "/dataservice/device/action/install"
+        payload: Dict[str, Any] = {
+            "action": "install",
+            "input": {
+                "vEdgeVPN": 0,
+                "vSmartVPN": 0,
+                "family": install_spec.family.value,
+                "version": version,
+                "versionType": install_spec.version_type.value,
+                "reboot": reboot,
+                "sync": sync,
+            },
+            "devices": self.device_versions.get_device_list(devices),
+            "deviceType": install_spec.device_type.value,
+        }
+        if install_spec.family.value in (DeviceClass.VMANAGE.value, DeviceClass.CEDGE.value):
+            incorrect_devices = self._downgrade_check(
+                payload["devices"],
+                payload["input"]["version"],
+                install_spec.family.value,
+            )
+            if incorrect_devices:
+                raise ValueError(
+                    f"Current version of devices with id's {incorrect_devices} is \
+                    higher than upgrade version. Action denied!"
+                )
+        upgrade = dict(self.session.post(url, json=payload).json())
+        return upgrade["id"]
+
+    def _create_callback(self, encoder: MultipartEncoder):
+
+        bar = ProgressBar(expected_size=encoder._calculate_length(), filled_char="=")
+
+        def callback(monitor: MultipartEncoderMonitor):
+            bar.show(monitor.bytes_read)
+
+        return callback
+
+    def upload_image(self, image_path: str) -> int:
+        """
+        Upload software image 'tar.gz' to Vmanage
+        software repository
+
+        Args:
+            image_path (str): path to software image
+
+        Returns:
+            str: Response status code
+        """
+        url = "/dataservice/device/action/software/package"
+        encoder = MultipartEncoder(
+            fields={"file": (PurePath(image_path).name, open(image_path, "rb"), "application/x-gzip")}
+        )
+        callback = self._create_callback(encoder)
+        monitor = MultipartEncoderMonitor(encoder, callback)
+        upload = self.session.post(url, data=monitor, headers={"content-type": monitor.content_type})
+        return upload.status_code
+
+    def _downgrade_check(self, devices, version_to_upgrade: str, family) -> List[str]:
+        """
+        Check if upgrade operation is not actually a downgrade opeartion.
+        If so, in some cases action is being blocked.
+
+        Args:
+            version_to_upgrade (str): version to upgrade
+            devices_category (DeviceCategory): devices category
+
+        Returns:
+            Union[None, List[str]]: [None, list of devices with no permission to downgrade]
+        """
+        incorrect_devices = []
+        devices_versions_repo = self.repository.get_devices_versions_repository(self.device_versions.device_category)
+        for dev in devices:
+            dev_current_version = str(devices_versions_repo[dev["deviceId"]].current_version)
+            splited_version_to_upgrade = version_to_upgrade.split(".")
+            for priority, label in enumerate(dev_current_version.split("-")[0].split(".")):
+                if str(label) > str(splited_version_to_upgrade[priority]):
+                    if family == "vmanage" and label == 2:
+                        continue
+                    incorrect_devices.append(dev["deviceId"])
+                    break
+                elif str(label) < str(splited_version_to_upgrade[priority]):
+                    break
+        return incorrect_devices
