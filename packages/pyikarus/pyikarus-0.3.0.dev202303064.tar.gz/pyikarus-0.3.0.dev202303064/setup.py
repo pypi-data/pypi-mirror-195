02b4c61f9d32dc597fb59b620f29c05b4504cbe5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:31:12 +0000

    Add package pyikarus/0.3.0.dev202303064/pyikarus-0.3.0.dev202303064.tar.gz

---
+++ packages/pyikarus/pyikarus-0.3.0.dev202303064.tar.gz/pyikarus-0.3.0.dev202303064/setup.py
@@ -0,0 +1,41 @@
+# SPDX-FileCopyrightText: 2022 The Ikarus Developers mueller@ibb.uni-stuttgart.de
+# SPDX-License-Identifier: LGPL-3.0-or-later
+# this file is modified from dumux https://git.iws.uni-stuttgart.de/dumux-repositories/dumux/-/blob/master/setup.py
+
+try:
+    from dune.packagemetadata import metaData
+except ImportError:
+    from packagemetadata import metaData
+from setuptools import find_namespace_packages
+from skbuild import setup
+
+# When building a new package, update the version numbers below and run:
+# > python setup.py sdist
+# > python -m twine upload dist/*
+
+pyikarusVersion = "0.3.0.dev202303064"
+duneVersion = "2.9.0"
+
+metadata = metaData(duneVersion)[1]
+metadata["version"] = pyikarusVersion
+
+metadata["packages"] = find_namespace_packages(where="python", include=["pyikarus.*"])
+
+# auto-generate pyproject.toml with duneVersion when building sdist
+from skbuild.command.sdist import sdist
+
+
+class mysdist(sdist):
+    def run(self):
+        # requires = ["setuptools", "wheel", "scikit-build", "cmake", "ninja", "requests"]
+        # requires += metadata["install_requires"]
+        # with open("pyproject.toml", "w") as f:
+        #     f.write("[build-system]\n")
+        #     f.write("requires = ['" + "', '".join(requires) + "']\n")
+        #     f.write("build-backend = 'setuptools.build_meta'\n")
+        sdist.run(self)
+
+
+metadata["cmdclass"] = {"sdist": mysdist}
+
+setup(**metadata)
