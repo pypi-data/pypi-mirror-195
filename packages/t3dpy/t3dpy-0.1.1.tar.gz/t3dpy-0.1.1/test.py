87bd91d2c83474196e2b4a095a0cc90f73ed52d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:00:17 +0000

    Add package t3dpy/0.1.1/t3dpy-0.1.1.tar.gz

---
+++ packages/t3dpy/t3dpy-0.1.1.tar.gz/t3dpy-0.1.1/test.py
@@ -0,0 +1,11 @@
+from t3dpy import read_t3d, T3dObject
+from pprint import pprint
+
+with open('./src/tests/data/terraininfo.t3d', 'r') as fp:
+    objects = read_t3d(fp.read())
+    map_object = next(filter(lambda x: x.type_ == "Map", objects))
+    actor_objects = filter(lambda child: child.type_ == "Actor", map_object.children)
+    for actor_object in actor_objects:
+        print(actor_object.properties)
+        for layer in actor_object["Layers"]:
+            print(layer)
