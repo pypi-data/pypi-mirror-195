6b4e5b97fe952ae095234db494fff3e53175eb3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:17:02 +0000

    Add package Fragmenstein/0.9.13/Fragmenstein-0.9.13.tar.gz

---
+++ packages/Fragmenstein/Fragmenstein-0.9.13.tar.gz/Fragmenstein-0.9.13/fragmenstein/faux_victors/__init__.py
@@ -0,0 +1,22 @@
+"""
+Victor alternatives not meant for production use, but for testing and debugging.
+
+* ``Mictor``: MCS hack for negative benchmarking: Victor with MCSMerger, i.e. no positional info used.
+* ``Wictor``: Victor without pyrosetta, i.e. no energy minimization.
+* ``AccountableBRICS``: Not a Victor, but generates a BRICS decomposition of the hits and returns a dataframe
+    with the built molecules usable in ``Laboratory.place``.
+
+To run in ``Laboratory`` class set the ``.Victor`` attribute to the desired ``Victor`` alternative.
+
+... code-block:: python
+    from fragmenstein import Laboratory
+    from fragmenstein.faux_victors import Mictor  # MCS hack for negative benchmarking
+    lab = Laboratory(pdbblock=pdbblock, covalent_resi=None)
+    lab.Victor = Mictor
+    combinations: pd.DataFrame = lab.combine(hits, n_cores=28)
+    combinations.to_pickle(f'👾👾👾.p'))
+"""
+
+from .mcs_victor import Mictor, MCSMerger
+from .no_pyrosetta import Wictor
+from .brics import AccountableBRICS
