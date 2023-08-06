2c3e8c5b892c76887b909fced2de1b1a74b3dcb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:07:06 +0000

    Add package loadwave/0.0.3/loadwave-0.0.3.tar.gz

---
+++ packages/loadwave/loadwave-0.0.3.tar.gz/loadwave-0.0.3/loadwave/__init__.py
@@ -0,0 +1,30 @@
+import sys
+import threading
+import time
+
+class Loader:
+    def __init__(self, message):
+        self.message = message
+        self.stop_flag = None
+        self.loader_thread = None
+        self.i = 0
+
+    def start(self):
+        self.stop_flag = False
+        self.loader_thread = threading.Thread(target=self.loader)
+        self.loader_thread.start()
+
+    def stop(self):
+        self.stop_flag = True
+        self.loader_thread.join()
+
+    def loader(self):
+        while not self.stop_flag:
+            sys.stdout.write('\r' + self.message + ' [' + self.animate() + ']')
+            sys.stdout.flush()
+            time.sleep(0.1)
+
+    def animate(self):
+        animation = '|/-\\'
+        self.i = (self.i + 1) % 4
+        return animation[self.i]
