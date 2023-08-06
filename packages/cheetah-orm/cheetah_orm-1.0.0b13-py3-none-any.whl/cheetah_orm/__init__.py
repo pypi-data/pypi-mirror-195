408380eb118b1f63507eb4e49bc8720ec529b9ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:40 +0000

    Add package cheetah-orm/1.0.0b13/cheetah_orm-1.0.0b13-py3-none-any.whl

---
+++ packages/cheetah-orm/cheetah_orm-1.0.0b13-py3-none-any.whl/cheetah_orm/__init__.py
@@ -0,0 +1,12 @@
+"""Cheetah ORM
+
+Cheetah ORM is a lightweight and high-performance object relational mapper writtn in pure Python. 
+It supports multiple database backends including sqlite3, MySQL/MariaDB, and PostgreSQL.
+"""
+
+# Constants
+# =========
+__author__ = "Cybermals"
+__copyright__ = "Copyright (c) 2022 by Cybermals"
+__license__ = "MIT"
+__version__ = "1.0.0b13"
