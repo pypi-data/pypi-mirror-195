f0614fc7446b12178a33bf8029462f85f9a41a5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:11:26 +0000

    Add package dcmrtstruct2nii/3/dcmrtstruct2nii-3.tar.gz

---
+++ packages/dcmrtstruct2nii/dcmrtstruct2nii-3.tar.gz/dcmrtstruct2nii-3/dcmrtstruct2nii/adapters/output/niioutputadapter.py
@@ -0,0 +1,11 @@
+from dcmrtstruct2nii.adapters.output.abstractoutputadapter import AbstractOutputAdapter
+
+import SimpleITK as sitk
+
+
+class NiiOutputAdapter(AbstractOutputAdapter):
+    def write(self, image, output_path, gzip):
+        if gzip:
+            sitk.WriteImage(image, output_path + '.nii.gz')
+        else:
+            sitk.WriteImage(image, output_path + '.nii')
