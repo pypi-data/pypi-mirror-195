ce8efaf04d6babd35216443d6d22fc6fd5d2bb2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:30:47 +0000

    Add package deeper-rpg/0.1.9/deeper_rpg-0.1.9-pp38-pypy38_pp73-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/deeper-rpg/deeper_rpg-0.1.9-pp38-pypy38_pp73-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/deeper/__init__.py
@@ -0,0 +1,7 @@
+from .deeper import *
+from .resources import *
+from .data import Data
+from .blueprint import Blueprint
+from .component import Component
+from .node import Node
+from .components.block import Block
---
+++ packages/deeper-rpg/deeper_rpg-0.1.9-pp38-pypy38_pp73-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/deeper/blueprints/component/component_blueprint.py
@@ -0,0 +1,29 @@
+from loguru import logger
+
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column
+
+from ...blueprint import Blueprint
+
+class ComponentBlueprint(Blueprint):
+    id: Mapped[int] = mapped_column(ForeignKey('Blueprint.id'), primary_key=True)
+    __mapper_args__ = {
+        'polymorphic_identity': 'ComponentBlueprint',
+        'inherit_condition': (id == Blueprint.id),
+    }
+
+    def __init__(self, catalog, name, config, parent):
+        super().__init__(catalog, name, config, parent)
+
+    def configure(self, config):
+        self.xconfig = config = self.borrow(config, self.parent)
+
+        for key, value in config.items():
+            setattr(self, key, value)
+
+    def update(self):
+        self.xconfig = config = self.extend(self.config) if self.base else self.config
+        self.xconfig = config = self.borrow(config, self.parent)
+        #logger.debug(config)
+        for key, value in config.items():
+            setattr(self, key, value)
