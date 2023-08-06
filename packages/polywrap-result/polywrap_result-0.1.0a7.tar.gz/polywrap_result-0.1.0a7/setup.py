27255966f9a5e1c48f921bb2324a4ff430451fd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:07:46 +0000

    Add package polywrap-result/0.1.0a7/polywrap_result-0.1.0a7.tar.gz

---
+++ packages/polywrap-result/polywrap_result-0.1.0a7.tar.gz/polywrap_result-0.1.0a7/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['polywrap_result']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'polywrap-result',
+    'version': '0.1.0a7',
+    'description': 'Result object',
+    'long_description': '',
+    'author': 'Danilo Bargen',
+    'author_email': 'mail@bargen.dev',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
