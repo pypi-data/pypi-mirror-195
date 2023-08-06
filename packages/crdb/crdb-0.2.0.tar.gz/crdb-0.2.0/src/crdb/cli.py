fdde13a41f6815540129ba0212be37cabcd29eee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:16:11 +0000

    Add package crdb/0.2.0/crdb-0.2.0.tar.gz

---
+++ packages/crdb/crdb-0.2.0.tar.gz/crdb-0.2.0/src/crdb/cli.py
@@ -0,0 +1,39 @@
+import argparse
+from crdb._lib import _url, _server_request, query
+import inspect
+import re
+
+
+def main(args=None):
+    """
+    Command line interface for CRDB.
+
+    Returns unprocessed DB output to stdout.
+    """
+    parser = argparse.ArgumentParser(description=main.__doc__)
+
+    descriptions = {
+        k: re.sub(r"\s+", " ", v.strip())
+        for (k, v) in re.findall(
+            r"^ {4}([a-z_]+): *[a-z, ]+\n((?: {8}.+\n)+)", query.__doc__, re.MULTILINE
+        )
+    }
+
+    for name, par in inspect.signature(_url).parameters.items():
+        name2 = name.replace("_", "-")
+        h = descriptions.get(name, "").replace("%", "%%")
+        tp = par.annotation
+        if isinstance(tp, str):
+            tp = eval(tp)
+        if par.default is inspect.Parameter.empty:
+            parser.add_argument(name2, type=tp, help=h)
+        else:
+            parser.add_argument(f"--{name2}", type=tp, default=par.default, help=h)
+
+    parser.add_argument("--timeout", type=int, default=120)
+
+    args = parser.parse_args(args=args)
+    kwargs = {k.replace("-", "_"): v for (k, v) in vars(args).items() if k != "timeout"}
+    url = _url(**kwargs)
+    data = _server_request(url, timeout=args.timeout)
+    print("\n".join(data))
