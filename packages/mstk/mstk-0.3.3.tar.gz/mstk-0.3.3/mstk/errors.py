3b33372be223c844ba146273786f2e5ce3721d14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:57 +0000

    Add package mstk/0.3.3/mstk-0.3.3.tar.gz

---
+++ packages/mstk/mstk-0.3.3.tar.gz/mstk-0.3.3/mstk/errors.py
@@ -0,0 +1,30 @@
+class DffError(Exception):
+    pass
+
+
+class LammpsError(Exception):
+    pass
+
+
+class PackmolError(Exception):
+    pass
+
+
+class GmxError(Exception):
+    pass
+
+
+class GaussError(Exception):
+    pass
+
+
+class OpenBabelError(Exception):
+    pass
+
+
+class RDKitError(Exception):
+    pass
+
+
+class SchedulerError(Exception):
+    pass
