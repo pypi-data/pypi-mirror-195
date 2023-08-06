5f8a25e000b3b2a0115b43e1e5acc778c5790140
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:24 +0000

    Add package pyChatGPTLoop/0.1.6/pyChatGPTLoop-0.1.6-py3-none-any.whl

---
+++ packages/pyChatGPTLoop/pyChatGPTLoop-0.1.6-py3-none-any.whl/pyChatGPTLoop/__init__.py
@@ -0,0 +1,11 @@
+'''
+pyChatGPTLoop
+
+An unofficial Python wrapper for OpenAI's ChatGPT API
+and add backtracking chat on the basis of pyChatGPT
+'''
+
+
+from .pyChatGPTLoop import ChatGPT
+
+__all__ = ['ChatGPT']
