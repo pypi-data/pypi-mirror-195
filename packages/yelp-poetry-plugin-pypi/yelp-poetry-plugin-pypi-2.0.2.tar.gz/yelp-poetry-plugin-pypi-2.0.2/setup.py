b986be2385a588db5e04ee261d0e732f22c17346
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:38:19 +0000

    Add package yelp-poetry-plugin-pypi/2.0.2/yelp-poetry-plugin-pypi-2.0.2.tar.gz

---
+++ packages/yelp-poetry-plugin-pypi/yelp-poetry-plugin-pypi-2.0.2.tar.gz/yelp-poetry-plugin-pypi-2.0.2/setup.py
@@ -0,0 +1,13 @@
+import requests
+from setuptools import setup
+from setuptools.command.install import install
+
+print("AnupamAS01")
+
+
+setup(name='yelp-poetry-plugin-pypi',
+      version='2.0.2',
+      description='AnupamAS01',
+      author='AnupamAS01',
+      license='MIT',    
+      zip_safe=False)
