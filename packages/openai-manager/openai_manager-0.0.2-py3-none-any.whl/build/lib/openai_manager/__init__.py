61b60d92dbf1a950e1ed803bc39707fd4ec13a33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:24:58 +0000

    Add package openai-manager/0.0.2/openai_manager-0.0.2-py3-none-any.whl

---
+++ packages/openai-manager/openai_manager-0.0.2-py3-none-any.whl/build/lib/openai_manager/__init__.py
@@ -0,0 +1,26 @@
+# pylint: skip-file
+# avoid parially initialized module error
+from openai_manager.auth_manager import OpenAIAuthManager  # noqa
+GLOBAL_MANAGER = OpenAIAuthManager()  # noqa
+
+
+from openai_manager.api_resources import Completion  # noqa
+from functools import partialmethod, partial  # noqa
+import logging
+import os
+
+
+logging.basicConfig(level=int(os.getenv("OPENAI_LOG_LEVEL", logging.WARNING)))
+logger = logging.getLogger(__name__)
+logger.debug(f"Logger level: {logger.level}")
+
+
+def append_auth_from_config(config_path=None, config_dicts=None):
+    GLOBAL_MANAGER.append_auth_from_config(config_path, config_dicts)
+
+
+__all__ = [
+    "Completion"
+]
+
+name = "openai-manager"
