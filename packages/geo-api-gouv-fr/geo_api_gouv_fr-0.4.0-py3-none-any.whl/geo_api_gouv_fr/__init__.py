84b57e7c4623ba4e7377b67ffb75546bd84c801c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:24:24 +0000

    Add package geo-api-gouv-fr/0.4.0/geo_api_gouv_fr-0.4.0-py3-none-any.whl

---
+++ packages/geo-api-gouv-fr/geo_api_gouv_fr-0.4.0-py3-none-any.whl/geo_api_gouv_fr/__init__.py
@@ -0,0 +1,3 @@
+from .department import Api as DepartmentApi, DepartmentsResponse  # noqa
+from .adress import Api as AdressApi, SearchResponse, ReverseResponse  # noqa
+from .region import Api as RegionApi, RegionsResponse  # noqa
