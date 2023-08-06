1331bcfbac065caa1c7c72e1b35cb732bc350e15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:42:40 +0000

    Add package pbmd/1.0.0/pbmd-1.0.0-py3-none-any.whl

---
+++ packages/pbmd/pbmd-1.0.0-py3-none-any.whl/pbmd/__init__.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- encoding: utf-8 -*-
+"""
+@File    :   __init__.py
+@Time    :   2022-10-24 00:20
+@Author  :   坐公交也用券
+@Version :   1.1.5
+@Contact :   liumou.site@qq.com
+@Homepage : https://liumou.site
+@Desc    :   这是一个Linux管理脚本的基础库，通过对Linux基本功能进行封装，实现快速开发的效果
+"""
+
+from .intCheck import IntCheck
+from .listHandle import ListToStr, ListRemoveNone
+from .strCheck import CheckSqlInjection
+
+__all__ = ["IntCheck", "CheckSqlInjection", "ListToStr", "ListRemoveNone"]
---
+++ packages/pbmd/pbmd-1.0.0-py3-none-any.whl/pbmd/strHandle.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- encoding: utf-8 -*-
+"""
+@File    :   strCheck.py
+@Time    :   2023-03-02 23:52
+@Author  :   坐公交也用券
+@Version :   1.0
+@Contact :   liumou.site@qq.com
+@Homepage : https://liumou.site
+@Desc    :   字符串处理
+"""
+
+
+def RemoveSpecialSql(txt):
+	"""
+	移除Sql敏感字符
+	:param txt: 需要处理的语句
+	:return:
+	"""
