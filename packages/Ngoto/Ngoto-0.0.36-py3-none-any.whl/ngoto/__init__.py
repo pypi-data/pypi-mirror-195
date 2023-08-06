9eb722e92c86baeb347ef57824abe8c2818a1b59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:13 +0000

    Add package Ngoto/0.0.36/Ngoto-0.0.36-py3-none-any.whl

---
+++ packages/Ngoto/Ngoto-0.0.36-py3-none-any.whl/ngoto/__init__.py
@@ -0,0 +1,25 @@
+from ngoto.core.ngoto import Ngoto
+from ngoto.core.util.interface import output, get_input, logo
+from ngoto.core.util.interface import show_options, show_commands, clear_screen
+from ngoto.core.util.notify import notify
+from ngoto.core.util.logging import Logging
+from ngoto.core.decorators import plugin, command, task
+from ngoto.core.util.rich.table import Table
+from ngoto.core.util.rich.style import Style
+
+__all__ = [
+    'Ngoto',
+    'Table',
+    'Style',
+    'output',
+    'get_input',
+    'logo',
+    'show_options',
+    'show_commands',
+    'clear_screen',
+    'notify',
+    'Logging',
+    'command',
+    'task',
+    'plugin'
+]
