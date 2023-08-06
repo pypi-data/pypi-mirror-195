f583e389ce848d0c587db682c335d5f8fa8747fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:12:53 +0000

    Add package supadef/0.0.15/supadef-0.0.15.tar.gz

---
+++ packages/supadef/supadef-0.0.15.tar.gz/supadef-0.0.15/supadef/__init__.py
@@ -0,0 +1,36 @@
+
+def compose():
+    """Main wrapper to begin composing flows"""
+    pass
+
+
+def text_input(arg: str, hidden=False):
+    """
+    Text Input (function argument)
+    :param arg: the argument this input will be passed into
+    :param hidden: if True, will hide the input password-style
+    :return:
+    """
+    pass
+
+
+def button(title: str, action='run', color='theme:action'):
+    """
+    UI Button (run function, go to another function)
+    :param title: title of the button
+    :param action: run = run the function, goto:[func] opens another function
+    :param color: color of the button
+    :return:
+    """
+    pass
+
+
+def card(title: str, color='theme:action'):
+    """
+    UI Card (display prominent Text)
+    :param title: text to display
+    :param color: color of the button
+    :return:
+    """
+    pass
+
