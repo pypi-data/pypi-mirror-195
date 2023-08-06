e02faabb9463462a80133546ae65f56d969a30fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:08:13 +0000

    Add package uitk/0.5.9/uitk-0.5.9-py3-none-any.whl

---
+++ packages/uitk/uitk-0.5.9-py3-none-any.whl/uitk/slots/editors.py
@@ -0,0 +1,13 @@
+# !/usr/bin/python
+# coding=utf-8
+from uitk.slots import Slots
+
+
+
+class Editors(Slots):
+	'''
+	'''
+	def __init__(self, *args, **kwargs):
+		super().__init__(*args, **kwargs)
+		'''
+		'''
