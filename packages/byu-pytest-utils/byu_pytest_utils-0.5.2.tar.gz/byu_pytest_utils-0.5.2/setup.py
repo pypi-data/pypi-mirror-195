8f27722a4a25f699554940ada75b22e1d396c930
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:54 +0000

    Add package byu-pytest-utils/0.5.2/byu_pytest_utils-0.5.2.tar.gz

---
+++ packages/byu-pytest-utils/byu_pytest_utils-0.5.2.tar.gz/byu_pytest_utils-0.5.2/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['byu_pytest_utils']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['pytest>=7.0.1,<8.0.0']
+
+entry_points = \
+{'pytest11': ['byu_pytest_utils = byu_pytest_utils.pytest_plugin']}
+
+setup_kwargs = {
+    'name': 'byu-pytest-utils',
+    'version': '0.5.2',
+    'description': 'A few utilities for pytest to help with integration into gradescope',
+    'long_description': None,
+    'author': 'Gordon Bean',
+    'author_email': 'gbean@cs.byu.edu',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
