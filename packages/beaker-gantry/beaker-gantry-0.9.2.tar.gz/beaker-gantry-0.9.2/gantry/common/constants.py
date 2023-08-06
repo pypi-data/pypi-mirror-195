af055f652d378cec82dcce3579c095562d1bdbb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:44:03 +0000

    Add package beaker-gantry/0.9.2/beaker-gantry-0.9.2.tar.gz

---
+++ packages/beaker-gantry/beaker-gantry-0.9.2.tar.gz/beaker-gantry-0.9.2/gantry/common/constants.py
@@ -0,0 +1,17 @@
+DEFAULT_IMAGE = "ai2/conda"
+
+ENTRYPOINT = "entrypoint.sh"
+
+GITHUB_TOKEN_SECRET = "GITHUB_TOKEN"
+
+CONDA_ENV_FILE = "environment.yml"
+
+PIP_REQUIREMENTS_FILE = "requirements.txt"
+
+RUNTIME_DIR = "/gantry-runtime"
+
+RESULTS_DIR = "/results"
+
+METRICS_FILE = f"{RESULTS_DIR}/metrics.json"
+
+NFS_MOUNT = "/net/nfs.cirrascale"
