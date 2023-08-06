4f877bb8ec4680b945030765f66961591dacbb03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:25:20 +0000

    Add package owl-model-extractor/0.1.8/owl_model_extractor-0.1.8.tar.gz

---
+++ packages/owl-model-extractor/owl_model_extractor-0.1.8.tar.gz/owl_model_extractor-0.1.8/owl_model_extractor/owl_model_extractor_constants_values.py
@@ -0,0 +1,6 @@
+
+PREFERRED_PREFIX = ["http://purl.org/vocab/vann/preferredNamespacePrefix"]
+LICENSE = ["http://purl.org/dc/terms/license"]
+TITLE = ["http://purl.org/dc/terms/title","http://purl.org/dc/elements/1.1/title"]
+
+SPARQL_STR = "SELECT ?o WHERE {?s <##PH1##> ?o}"
