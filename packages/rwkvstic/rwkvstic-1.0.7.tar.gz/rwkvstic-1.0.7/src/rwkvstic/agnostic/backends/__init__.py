6381093b02240162af43254d98b782f77be08cda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:06:47 +0000

    Add package rwkvstic/1.0.7/rwkvstic-1.0.7.tar.gz

---
+++ packages/rwkvstic/rwkvstic-1.0.7.tar.gz/rwkvstic-1.0.7/src/rwkvstic/agnostic/backends/__init__.py
@@ -0,0 +1,37 @@
+from rwkvstic.agnostic.backends.torch import RWKVCudaOps, RWKVPTTSExportOps, RWKVSplitCudaOps, RWKVCudaQuantOps, RWKVStreamBigOps, RWKVCudaDeepspeedOps
+from rwkvstic.agnostic.backends.jax import RWKVJaxOps, RWKVNumpyOps
+from rwkvstic.agnostic.backends.tensorflow import RWKVTFExport, RWKVTFOps
+from rwkvstic.agnostic.backends.base import module
+from rwkvstic.agnostic.backends.onnx import RWKVOnnxOps
+from rwkvstic.agnostic.backends.coreml import RWKVCoreMLOps
+from typing import Dict
+
+Backends: Dict[str, module] = {
+    "tensorflow(cpu/gpu)": RWKVTFOps,
+    "pytorch(cpu/gpu)": RWKVCudaOps,
+    "numpy(cpu)": RWKVNumpyOps,
+    "jax(cpu/gpu/tpu)": RWKVJaxOps,
+    "pytorch-deepspeed(gpu)": RWKVCudaDeepspeedOps,
+    "pytorch-quant(gpu-8bit)": RWKVCudaQuantOps,
+    "pytorch-stream(gpu-config-vram)": RWKVStreamBigOps,
+    "pytorch-split(2xgpu)": RWKVSplitCudaOps,
+    "export-torchscript": RWKVPTTSExportOps,
+    "export-tensorflow": RWKVTFExport,
+    "onnx_export": RWKVOnnxOps,
+    # "coreml": RWKVCoreMLOps
+}
+
+
+TORCH = "pytorch(cpu/gpu)"
+JAX = "jax(cpu/gpu/tpu)"
+TF = "tensorflow(cpu/gpu)"
+TORCH_DEEPSPEED = "pytorch-deepspeed(gpu)"
+TORCH_QUANT = "pytorch-quant(gpu-8bit)"
+TORCH_STREAM = "pytorch-stream(gpu-config-vram)"
+TORCH_SPLIT = "pytorch-split(2xgpu)"
+TORCH_EXPORT = "export-torchscript"
+TF_EXPORT = "export-tensorflow"
+ONNX_EXPORT = "onnx_export"
+NUMPY = "numpy(cpu)"
+
+# COREML = "coreml"
