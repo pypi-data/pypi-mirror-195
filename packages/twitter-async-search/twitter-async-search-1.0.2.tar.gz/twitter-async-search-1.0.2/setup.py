d2c07002a19e8379f6667263d9eb08c55e1842d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:32:37 +0000

    Add package twitter-async-search/1.0.2/twitter-async-search-1.0.2.tar.gz

---
+++ packages/twitter-async-search/twitter-async-search-1.0.2.tar.gz/twitter-async-search-1.0.2/setup.py
@@ -0,0 +1,22 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name="twitter-async-search",
+    version="1.0.2",
+    description="Fast twitter search API",
+    author="Trevor Hobenshield",
+    author_email="trevorhobenshield@gmail.com",
+    packages=find_packages(),
+    url="https://github.com/trevorhobenshield/twitter-async-search",
+    install_requires=[
+        "aiohttp",
+        "requests",
+    ],
+    entry_points="""
+        [console_scripts]
+        twitter-async-search=src.main:main
+    """,
+    keywords="twitter search api async",
+    include_package_data=True,
+)
