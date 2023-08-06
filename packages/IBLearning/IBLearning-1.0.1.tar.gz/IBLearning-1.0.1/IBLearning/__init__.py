7d67118777a28c928cf16bccf66dca2dfa50ccb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:45 +0000

    Add package IBLearning/1.0.1/IBLearning-1.0.1.tar.gz

---
+++ packages/IBLearning/IBLearning-1.0.1.tar.gz/IBLearning-1.0.1/IBLearning/__init__.py
@@ -0,0 +1,7 @@
+"""
+    IBLearning
+"""
+
+__version__ = "1.0.1"
+__author__ = 'Alix Hamidou'
+__credits__ = 'Alix Hamidou'
---
+++ packages/IBLearning/IBLearning-1.0.1.tar.gz/IBLearning-1.0.1/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+
+with open("README.md","r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='IBLearning',
+    version='1.0.1',
+    description='IBLearning is a Python module that contains a set of tools to help you to make your own machine learning algorithms.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/IB-Solution/IBLearning',
+    author='Alix Hamidou',
+    author_email='alix.hamidou@gmail.com',
+    license='MIT',
+    packages=['IBLearning'],
+    install_requires=[
+        'nltk>=1.1.0',
+    ],
+
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: Unix',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'Topic :: Scientific/Engineering :: Information Analysis',
+        'Topic :: Text Processing :: Linguistic',
+    ],
+)
