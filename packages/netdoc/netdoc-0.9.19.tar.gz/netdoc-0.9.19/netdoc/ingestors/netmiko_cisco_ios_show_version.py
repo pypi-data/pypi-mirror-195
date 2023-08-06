800f51ff9d2b225920c28de099bd3b5cc1490a7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:11 +0000

    Add package netdoc/0.9.19/netdoc-0.9.19.tar.gz

---
+++ packages/netdoc/netdoc-0.9.19.tar.gz/netdoc-0.9.19/netdoc/ingestors/netmiko_cisco_ios_show_version.py
@@ -0,0 +1,28 @@
+"""Ingestor for netmiko_cisco_ios_show_version."""
+__author__ = "Andrea Dainese"
+__contact__ = "andrea@adainese.it"
+__copyright__ = "Copyright 2022, Andrea Dainese"
+__license__ = "GPLv3"
+__date__ = "2022-09-07"
+__version__ = "0.9.19"
+
+from netdoc.schemas import device
+from netdoc import utils
+
+
+def ingest(log):
+    """Processing parsed output."""
+    device_o = log.discoverable.device
+
+    # Show version contains only one item
+    item = next(iter(log.parsed_output))
+
+    # See https://github.com/networktocode/ntc-templates/tree/master/tests/cisco_ios/show_version # pylint: disable=line-too-long
+    data = {
+        "serial": utils.normalize_serial(next(iter(item.get("serial")), None)),
+    }
+    device.update(device_o, **data)
+
+    # Update the log
+    log.ingested = True
+    log.save()
