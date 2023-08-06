a34f5a0a4e66fcb82a5bda3db5da0e8ff367e637
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:44 +0000

    Add package vk-fetch/0.5.1/vk_fetch-0.5.1-py3-none-any.whl

---
+++ packages/vk-fetch/vk_fetch-0.5.1-py3-none-any.whl/vk_fetch/models/attachments.py
@@ -0,0 +1,61 @@
+import dataclasses as dc
+import datetime as dt
+import pathlib
+import typing as t
+
+from vk_fetch import constants, utils, core
+from vk_fetch.models import media_types
+
+
+@dc.dataclass(frozen=True, slots=True)
+class Attachment:
+    type: constants.MediaType
+    photo: media_types.Photo | None = None
+    video: dict | None = None
+    audio: media_types.Audio | None = None
+    doc: dict | None = None
+    link: dict | None = None
+    market: dict | None = None
+    wall: dict | None = None
+    poll: dict | None = None
+    gift: dict | None = None
+    sticker: dict | None = None
+
+    @classmethod
+    def of(cls, d: dict[str, t.Any]) -> t.Self:
+        return cls(
+            type=constants.MediaType(d.get("type")),
+            photo=media_types.Photo.of(d.get("photo")),
+            audio=media_types.Audio.of(d.get("audio")),
+            **utils.keys_excluded_dict(d, ["photo", "audio", "type"]),
+        )
+
+    def content(self) -> media_types.Media:
+        match self.type:
+            case constants.MediaType.Photo:
+                return self.photo
+            case constants.MediaType.Audio:
+                return self.audio
+            case _:
+                raise NotImplementedError
+
+
+@dc.dataclass(frozen=True, slots=True)
+class AttachmentItem:
+    message_id: int
+    date: dt.datetime
+    from_id: int
+    cmid: int
+    attachment: Attachment
+    forward_level: int | None = None
+
+    @classmethod
+    def of(cls, d: dict[str, t.Any]) -> t.Self:
+        return cls(
+            attachment=Attachment.of(d.get("attachment")),
+            date=dt.datetime.fromtimestamp(int(d.get("date"))),
+            **utils.keys_excluded_dict(d, ["attachment", "date"]),
+        )
+
+    def download_item(self, destination: pathlib.Path) -> core.DownloadItem:
+        return self.attachment.content().download_item(destination)
