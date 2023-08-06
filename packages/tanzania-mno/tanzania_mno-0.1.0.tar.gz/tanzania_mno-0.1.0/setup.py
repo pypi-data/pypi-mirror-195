52d36997f7a03339aa37dd0c49e2dbf6397f1a9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:26 +0000

    Add package tanzania-mno/0.1.0/tanzania_mno-0.1.0.tar.gz

---
+++ packages/tanzania-mno/tanzania_mno-0.1.0.tar.gz/tanzania_mno-0.1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    packages=['tanzania_mno'],
+    name="tanzania_mno",
+    version="0.1.0",
+    license='MIT',
+    author='HERMES GIDO',
+    author_email='hermessgido@gmail.com',
+    description="A package for checking the Mobile Network Operator (MNO) for a given phone number in Tanzania, can also be used to validate Tanzania phone number and generate random valid phone numbers",
+    install_requires=["phonenumbers"],
+)
