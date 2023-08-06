6726cc3b5cb66b74d39c04180d44567a0fccae2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:47 +0000

    Add package nlpipes/0.1.9/nlpipes-0.1.9-py3-none-any.whl

---
+++ packages/nlpipes/nlpipes-0.1.9-py3-none-any.whl/nlpipes/data/data_processors.py
@@ -0,0 +1,190 @@
+from typing import Callable
+from typing import List
+
+from dataclasses import dataclass
+
+import copy
+
+import numpy as np
+import tensorflow as tf
+
+from transformers import (
+    PretrainedConfig,
+    PreTrainedTokenizer,
+    AutoTokenizer,
+)
+
+from nlpipes.data.data_types import InputExample
+from nlpipes.data.data_types import InputFeatures
+
+
+""" The data processor classes.
+
+The data processor encapsulates all the transformations
+needed to encode input examples into input features 
+expected by the model. """
+
+
+@dataclass
+class DataProcessorForLanguageModeling:
+    
+    """ 
+    Data processor for Masked Language Modeling:
+    80% MASK, 10% random, 10% original.
+    Inputs are dynamically padded to the maximum 
+    length of a batch if they are not all of the 
+    same length.
+    
+    Args
+    ----------
+
+    tokenizer (transformers.PreTrainedTokenizer):
+        The tokenizer used for encoding the data.
+
+    config (transformers.PreTrainedCOnfig):
+        The configuration of the model.
+
+    """
+    name: str = 'LanguageModelingProcessor'
+    tokenizer: PreTrainedTokenizer = None
+    config: PretrainedConfig = None
+    mlm_proba: float = None
+    
+    def __call__(self, 
+                 examples: List[InputExample],
+                ) -> InputFeatures:
+        
+        texts = [example.text for example in examples]
+        
+        inputs = self.tokenizer(
+            texts,
+            add_special_tokens=True,
+            padding=True,
+            truncation=True,
+            max_length=512,
+            is_split_into_words=False,
+            return_tensors='tf',
+        )
+        
+        input_ids = tf.identity(inputs.input_ids)
+        
+        mask = self.create_mask(
+            inputs=inputs.input_ids,
+            mask_frac=self.mlm_proba,
+            excluded=self.tokenizer.all_special_ids,
+        )
+        
+        inputs['input_ids'] = self.apply_mask(
+            inputs=inputs.input_ids,
+            mask=mask,
+            mask_token_id=self.tokenizer.mask_token_id,
+        )
+        
+        labels = self.apply_mask(
+            inputs=input_ids,
+            mask=~mask,
+            mask_token_id=-100,
+        )
+        
+        inputs['label'] = tf.one_hot(
+            [label for label in labels],
+            depth=len(self.tokenizer)
+        )
+        
+        input_features = InputFeatures(
+            input_ids=inputs['input_ids'],
+            attention_mask=inputs['attention_mask'],
+            token_type_ids=inputs['token_type_ids'],
+            label=inputs['label'],
+        )
+        
+        return input_features
+    
+    def create_mask(
+        self,
+        inputs: tf.Tensor,
+        mask_frac: float,
+        excluded: tf.Tensor,  
+    ) -> tf.Tensor:
+        """ Create the mask used for the masked 
+        language modeling """
+        
+        random = tf.random.uniform(inputs.shape)
+        
+        mask = tf.math.logical_and(
+            random < mask_frac, 
+            ~np.isin(inputs, excluded),
+        )
+        
+        return mask
+    
+    def apply_mask(
+        self,
+        inputs: tf.Tensor,
+        mask: tf.Tensor,
+        mask_token_id: int,
+    ) -> tf.Tensor:
+        """ Apply the mask to the given inputs """
+        
+        masked_inputs = tf.where(
+            condition=mask,
+            x=mask_token_id,
+            y=inputs,
+        )
+        
+        return masked_inputs
+
+
+@dataclass
+class DataProcessorForSequenceClassification:
+    
+    """
+    Data Processor for Sequence Classification.
+    Inputs are dynamically padded to the maximum 
+    length of a batch if they are not all of the 
+    same length.
+    
+    Args
+    ----------
+
+    tokenizer (transformers.PreTrainedTokenizer):
+        The tokenizer used for encoding the data.
+
+    config (transformers.PreTrainedCOnfig):
+        The configuration of the model.
+
+    """
+    name: str = 'SequenceClassificationProcessor'
+    tokenizer: PreTrainedTokenizer = None
+    config: PretrainedConfig = None
+    
+    def __call__(self, 
+                 examples: List[InputExample],
+                ) -> InputFeatures:
+        
+        texts = [example.text for example in examples]
+        
+        inputs = self.tokenizer(
+            texts,
+            add_special_tokens=True,
+            padding=True,
+            truncation = True,
+            max_length=512,
+            is_split_into_words = False,
+            return_tensors='tf',
+        )
+        
+        inputs['label'] = tf.one_hot(
+            [example.label for example in examples],
+            depth=self.config.num_labels
+        )
+        
+        input_features = InputFeatures(
+            input_ids=inputs['input_ids'],
+            attention_mask=inputs['attention_mask'],
+            token_type_ids=inputs['token_type_ids'],
+            label=inputs['label'],
+        )
+        
+        return input_features
+    
---
+++ packages/nlpipes/nlpipes-0.1.9-py3-none-any.whl/nlpipes/metrics/confusion.py
@@ -0,0 +1,128 @@
+import numpy as np
+import tensorflow as tf
+
+from typeguard import typechecked
+
+
+class ConfusionMatrix(tf.metrics.Metric):
+    """ Computes confusion matrix for mono-label classification. """
+    
+    @typechecked
+    def __init__(self, 
+                 num_classes: int,
+                 name: str = "Confusion_matrix",
+                 dtype = tf.dtypes.int32,
+                ):
+        super().__init__(name=name, dtype=dtype)
+        self.num_classes = num_classes
+        self.confusion_matrix = self.add_weight(
+            'total',
+            shape=[num_classes, num_classes],
+            initializer='zeros',
+            dtype=self.dtype,
+        )
+
+    def update_state(self, y_true, y_pred):
+        batch = tf.math.confusion_matrix(
+            y_true, y_pred,
+            num_classes=self.num_classes,
+            dtype=self.dtype,
+        )
+        self.confusion_matrix.assign_add(batch)
+
+    def result(self):
+        return self.confusion_matrix
+
+
+class MultiLabelConfusionMatrix(tf.metrics.Metric):
+    """ Computes confusion matrix for multi-label classification.
+    Every class has a dedicated matrix of shape `(2, 2)` 
+    that contains:
+    - true negatives in cell `(0,0)`
+    - false positives in cell `(0,1)`
+    - false negatives in cell `(1,0)`
+    - true positives in cell `(1,1)`
+    Args:
+        num_classes: `int`, the number of labels the 
+                     prediction task can have.
+        name: (Optional) string name of the metric 
+              instance.
+        dtype: (Optional) data type of the metric 
+               result.
+    """
+
+    @typechecked
+    def __init__(self,
+                 num_classes: int,
+                 name: str = "Multilabel_confusion_matrix",
+                 dtype = tf.dtypes.int32,
+        **kwargs,
+    ):
+        super().__init__(name=name, dtype=dtype)
+        self.num_classes = num_classes
+        self.true_positives = self.add_weight(
+            "true_positives",
+            shape=[self.num_classes],
+            initializer="zeros",
+            dtype=self.dtype,
+        )
+        self.false_positives = self.add_weight(
+            "false_positives",
+            shape=[self.num_classes],
+            initializer="zeros",
+            dtype=self.dtype,
+        )
+        self.false_negatives = self.add_weight(
+            "false_negatives",
+            shape=[self.num_classes],
+            initializer="zeros",
+            dtype=self.dtype,
+        )
+        self.true_negatives = self.add_weight(
+            "true_negatives",
+            shape=[self.num_classes],
+            initializer="zeros",
+            dtype=self.dtype,
+        )
+
+    def update_state(self, y_true, y_pred):
+        
+        y_true = tf.cast(y_true, tf.int32)
+        y_pred = tf.cast(y_pred, tf.int32)
+        
+        true_positive = tf.math.count_nonzero(y_true * y_pred, 0)
+        pred_sum = tf.math.count_nonzero(y_pred, 0)
+        true_sum = tf.math.count_nonzero(y_true, 0)
+        false_positive = pred_sum - true_positive
+        false_negative = true_sum - true_positive
+        y_true_negative = tf.math.not_equal(y_true, 1)
+        y_pred_negative = tf.math.not_equal(y_pred, 1)
+        true_negative = tf.math.count_nonzero(
+            tf.math.logical_and(y_true_negative, y_pred_negative), axis=0
+        )
+
+        self.true_positives.assign_add(tf.cast(true_positive, self.dtype))
+        self.false_positives.assign_add(tf.cast(false_positive, self.dtype))
+        self.false_negatives.assign_add(tf.cast(false_negative, self.dtype))
+        self.true_negatives.assign_add(tf.cast(true_negative, self.dtype))
+
+    def result(self):
+        flat_confusion_matrix = tf.convert_to_tensor(
+            [
+                self.true_negatives,
+                self.false_positives,
+                self.false_negatives,
+                self.true_positives,
+            ]
+        )
+        
+        confusion_matrix = tf.reshape(
+            tf.transpose(flat_confusion_matrix), [-1, 2, 2]
+        )
+
+        return confusion_matrix
+    
+    def reset_state(self):
+        reset_value = np.zeros(self.num_classes, dtype=np.int32)
+        tf.keras.backend.batch_set_value([(v, reset_value) for v in self.variables])
+    
