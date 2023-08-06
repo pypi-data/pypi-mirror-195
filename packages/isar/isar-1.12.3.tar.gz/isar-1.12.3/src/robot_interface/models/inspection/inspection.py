a12b3801e16c48e9c9c175c59f2e65b31be262fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:35:09 +0000

    Add package isar/1.12.3/isar-1.12.3.tar.gz

---
+++ packages/isar/isar-1.12.3.tar.gz/isar-1.12.3/src/robot_interface/models/inspection/inspection.py
@@ -0,0 +1,64 @@
+from abc import ABC
+from dataclasses import dataclass, field
+from datetime import datetime
+from typing import List, Optional
+from uuid import UUID, uuid4
+
+from alitra import Pose
+
+
+@dataclass
+class InspectionMetadata(ABC):
+    start_time: datetime
+    pose: Pose
+    file_type: str
+    analysis: Optional[List] = field(default_factory=list, init=False)
+    tag_id: Optional[str] = field(default=None, init=False)
+    additional: Optional[dict] = field(default_factory=dict, init=False)
+
+
+@dataclass
+class ImageMetadata(InspectionMetadata):
+    pass
+
+
+@dataclass
+class ThermalImageMetadata(InspectionMetadata):
+    pass
+
+
+@dataclass
+class VideoMetadata(InspectionMetadata):
+    duration: Optional[float] = field(default=None)
+
+
+@dataclass
+class ThermalVideoMetadata(InspectionMetadata):
+    duration: Optional[float] = field(default=None)
+
+
+@dataclass
+class Inspection:
+    id: UUID = field(default_factory=uuid4, init=False)
+    metadata: InspectionMetadata
+    data: Optional[bytes] = field(default=None, init=False)
+
+
+@dataclass
+class Image(Inspection):
+    metadata: ImageMetadata
+
+
+@dataclass
+class ThermalImage(Inspection):
+    metadata: ThermalImageMetadata
+
+
+@dataclass
+class Video(Inspection):
+    metadata: VideoMetadata
+
+
+@dataclass
+class ThermalVideo(Inspection):
+    metadata: ThermalVideoMetadata
