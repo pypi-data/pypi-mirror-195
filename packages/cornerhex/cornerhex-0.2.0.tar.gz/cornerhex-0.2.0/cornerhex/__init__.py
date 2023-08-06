5361b34a985e48261e01052bae5a9600211d316a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:13:50 +0000

    Add package cornerhex/0.2.0/cornerhex-0.2.0.tar.gz

---
+++ packages/cornerhex/cornerhex-0.2.0.tar.gz/cornerhex-0.2.0/cornerhex/__init__.py
@@ -0,0 +1,10 @@
+from cornerhex.plot import cornerplot
+from cornerhex.plot import sigma_to_quantile
+
+__name__ = "cornerhex"
+__version__ = "0.2.0"
+
+__all__ = [
+    "cornerplot",
+    "sigma_to_quantile"
+]
