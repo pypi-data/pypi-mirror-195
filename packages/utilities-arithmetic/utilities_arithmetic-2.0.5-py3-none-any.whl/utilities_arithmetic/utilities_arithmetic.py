66efafc39eed43340614d0e5863b1e463b5ba81f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:32:23 +0000

    Add package utilities-arithmetic/2.0.5/utilities_arithmetic-2.0.5-py3-none-any.whl

---
+++ packages/utilities-arithmetic/utilities_arithmetic-2.0.5-py3-none-any.whl/utilities_arithmetic/utilities_arithmetic.py
@@ -0,0 +1,8 @@
+class ArithmeticOperation:
+    def add(self, x, y):
+        return x + y
+
+   # This function takes in two parameters and returns their subtraction result
+    def sub(self, x, y):
+        return x - y
+   
