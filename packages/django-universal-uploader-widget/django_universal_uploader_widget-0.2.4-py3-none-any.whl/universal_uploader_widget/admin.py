475110ba85b1e96699fec2fdd5039cd6a7cb5835
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:09:55 +0000

    Add package django-universal-uploader-widget/0.2.4/django_universal_uploader_widget-0.2.4-py3-none-any.whl

---
+++ packages/django-universal-uploader-widget/django_universal_uploader_widget-0.2.4-py3-none-any.whl/universal_uploader_widget/admin.py
@@ -0,0 +1,22 @@
+from django import forms
+from django.contrib import admin
+from django.conf import settings
+
+class UniversalUploaderInline(admin.StackedInline):
+    template = 'admin/edit_inline/universal_uploader.html'
+    extra = 0
+
+    @property
+    def media(self):
+        extra = '' if settings.DEBUG else '.min'
+        return forms.Media(
+            js = [
+                'admin/js/universal-uploader-inline%s.js' % extra
+            ],
+            css = {
+                'screen': [
+                    'admin/css/universal-uploader-inline%s.css' % extra,
+                ]
+            }
+        )
+
