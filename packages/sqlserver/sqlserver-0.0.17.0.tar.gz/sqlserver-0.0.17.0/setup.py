3ac2ae44b1bea4a2b5c737467638ac28197d63cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:52 +0000

    Add package sqlserver/0.0.17.0/sqlserver-0.0.17.0.tar.gz

---
+++ packages/sqlserver/sqlserver-0.0.17.0.tar.gz/sqlserver-0.0.17.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name="sqlserver",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    version="0.0.17.0",
+    author="Ashlin Darius Govindasamy",
+    author_email="adg@adgstudios.co.za",
+    url="https://www.adgstudios.co.za",
+    description="a module that makes queries easier to SQL Server than PYODBC",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    license='MIT', 
+    install_requires=["pyodbc","pandas","lxml"]
+)
