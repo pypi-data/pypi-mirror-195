62e380eaa79cbeab05bbcda00812e75c6909ad7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:25:51 +0000

    Add package data-collect/0.5.4/data_collect-0.5.4-py3-none-any.whl

---
+++ packages/data-collect/data_collect-0.5.4-py3-none-any.whl/data_collect/setup.py
@@ -0,0 +1,10 @@
+# !/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+#
+# Copyright (c) 2022 Baidu.com, Inc. All Rights Reserved
+"""
+capture client package setup
+"""
+import setuptools
+
+setuptools.setup()
