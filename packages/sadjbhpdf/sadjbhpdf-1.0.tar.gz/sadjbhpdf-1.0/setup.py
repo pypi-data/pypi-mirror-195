6d6900f16430972294d37ff715fb2d0e61354572
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:53 +0000

    Add package sadjbhpdf/1.0/sadjbhpdf-1.0.tar.gz

---
+++ packages/sadjbhpdf/sadjbhpdf-1.0.tar.gz/sadjbhpdf-1.0/setup.py
@@ -0,0 +1,8 @@
+from pathlib import Path
+import setuptools
+setuptools.setup(
+    name="sadjbhpdf",
+    version=1.0,
+    long_description=Path("README.md").read_text(),
+    packages=setuptools.find_packages(exclude=["data", "tests"])
+)
