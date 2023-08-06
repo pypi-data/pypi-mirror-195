cb4e187273bce99d869b60c9b4413414dcdffa13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:16:07 +0000

    Add package supervision/0.3.0/supervision-0.3.0-py3-none-any.whl

---
+++ packages/supervision/supervision-0.3.0-py3-none-any.whl/supervision/detection/utils.py
@@ -0,0 +1,57 @@
+from typing import Tuple
+
+import cv2
+import numpy as np
+
+
+def generate_2d_mask(polygon: np.ndarray, resolution_wh: Tuple[int, int]) -> np.ndarray:
+    """Generate a 2D mask from a polygon.
+
+    Properties:
+        polygon (np.ndarray): The polygon for which the mask should be generated, given as a list of vertices.
+        resolution_wh (Tuple[int, int]): The width and height of the desired resolution.
+
+    Returns:
+        np.ndarray: The generated 2D mask, where the polygon is marked with `1`'s and the rest is filled with `0`'s.
+    """
+    width, height = resolution_wh
+    mask = np.zeros((height, width), dtype=np.uint8)
+    cv2.fillPoly(mask, [polygon], color=1)
+    return mask
+
+
+def non_max_suppression(boxes: np.ndarray, scores: np.ndarray, threshold: float):
+    assert boxes.shape[0] == scores.shape[0]
+    ys1 = boxes[:, 0]
+    xs1 = boxes[:, 1]
+    ys2 = boxes[:, 2]
+    xs2 = boxes[:, 3]
+
+    areas = (ys2 - ys1) * (xs2 - xs1)
+    scores_indexes = scores.argsort().tolist()
+    boxes_keep_index = []
+    while len(scores_indexes):
+        index = scores_indexes.pop()
+        boxes_keep_index.append(index)
+        if not len(scores_indexes):
+            break
+        iou = compute_iou(
+            boxes[index], boxes[scores_indexes], areas[index], areas[scores_indexes]
+        )
+        filtered_indexes = set((iou > threshold).nonzero()[0])
+        scores_indexes = [
+            v for (i, v) in enumerate(scores_indexes) if i not in filtered_indexes
+        ]
+    return np.array(boxes_keep_index)
+
+
+def compute_iou(box, boxes, box_area, boxes_area):
+    assert boxes.shape[0] == boxes_area.shape[0]
+    ys1 = np.maximum(box[0], boxes[:, 0])
+    xs1 = np.maximum(box[1], boxes[:, 1])
+    ys2 = np.minimum(box[2], boxes[:, 2])
+    xs2 = np.minimum(box[3], boxes[:, 3])
+    intersections = np.maximum(ys2 - ys1, 0) * np.maximum(xs2 - xs1, 0)
+    unions = box_area + boxes_area - intersections
+    iou = intersections / unions
+    return iou
