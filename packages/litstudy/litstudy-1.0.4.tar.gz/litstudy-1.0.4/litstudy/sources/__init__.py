f5f6cbeef6590cfadd311f2b3a4d6b708da2726b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:08:05 +0000

    Add package litstudy/1.0.4/litstudy-1.0.4.tar.gz

---
+++ packages/litstudy/litstudy-1.0.4.tar.gz/litstudy-1.0.4/litstudy/sources/__init__.py
@@ -0,0 +1,28 @@
+from .scopus import search_scopus, refine_scopus, fetch_scopus
+from .bibtex import load_bibtex
+from .semanticscholar import fetch_semanticscholar, search_semanticscholar, \
+                             refine_semanticscholar
+from .crossref import fetch_crossref, refine_crossref, search_crossref
+from .ieee import load_ieee_csv
+from .springer import load_springer_csv
+from .dblp import search_dblp
+from .ris import load_ris_file
+from .arxiv import search_arxiv
+
+__all__ = [
+    'refine_crossref',
+    'fetch_crossref',
+    'search_crossref',
+    'refine_semanticscholar',
+    'search_semanticscholar',
+    'fetch_semanticscholar',
+    'refine_scopus',
+    'search_scopus',
+    'fetch_scopus',
+    'load_bibtex',
+    'load_ieee_csv',
+    'load_springer_csv',
+    'load_ris_file',
+    'search_dblp',
+    'search_arxiv'
+]
