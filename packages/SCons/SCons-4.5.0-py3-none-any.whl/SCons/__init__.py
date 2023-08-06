b177357abc7aa4bdd0d0f6fc66f43148265d43d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:58:10 +0000

    Add package SCons/4.5.0/SCons-4.5.0-py3-none-any.whl

---
+++ packages/SCons/SCons-4.5.0-py3-none-any.whl/SCons/__init__.py
@@ -0,0 +1,9 @@
+__version__="4.5.0"
+__copyright__="Copyright (c) 2001 - 2023 The SCons Foundation"
+__developer__="bdbaddog"
+__date__="Sun, 05 Mar 2023 16:59:21 -0400"
+__buildsys__="M1DOG2021"
+__revision__="2be4e5dc83319222cc725d13f6e98e033c042197"
+__build__="2be4e5dc83319222cc725d13f6e98e033c042197"
+# make sure compatibility is always in place
+import SCons.compat # noqa
