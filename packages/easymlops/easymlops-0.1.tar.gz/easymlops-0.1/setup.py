4d4e6df50b5dd29c2a82c68e09794d22ee928d09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:37 +0000

    Add package easymlops/0.1/easymlops-0.1.tar.gz

---
+++ packages/easymlops/easymlops-0.1.tar.gz/easymlops-0.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import find_packages, setup
+
+with open('requirements.txt') as f:
+    requirements = f.read()
+
+# with open('README.rst',encoding="utf8") as f:
+#     long_description = f.read()
+
+setup(
+    name='easymlops',
+    version='0.1',
+    python_requires='>=3.6',
+    # long_description=long_description,
+    author='zhulei227',
+    url='https://github.com/zhulei227/EasyMLOps',
+    description='MLOps Toolkit In Pipeline',
+    packages=find_packages(),
+    license='Apache-2.0',
+    install_requires=requirements)
