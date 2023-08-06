0bf1a8b94998a8117038e08263c829c65316f312
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:41 +0000

    Add package abhifibonacciseries/0.0.2/abhifibonacciseries-0.0.2.tar.gz

---
+++ packages/abhifibonacciseries/abhifibonacciseries-0.0.2.tar.gz/abhifibonacciseries-0.0.2/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+classifiers=[
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Education',
+    'Operating System :: Microsoft :: Windows',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3'
+]
+
+setup(
+      name='abhifibonacciseries',
+      version='0.0.2',
+      description='it checks and prints fibonacci numbers',
+      long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
+      url='',
+      author='Abhijith Krishna G',
+      author_email='abhijithkrishnag234@gmail.com',
+      license='MIT',
+      classifiers=classifiers,
+      keywords=['fibonacci','test','printfibonacci'],
+      packages=find_packages(),
+      install_requires=['']
+)
+
