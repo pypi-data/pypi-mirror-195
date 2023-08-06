cad5cc582207a8b85276b1b887b2b187248e105b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:44:25 +0000

    Add package rayter-generator/1.0.7/rayter_generator-1.0.7.tar.gz

---
+++ packages/rayter-generator/rayter_generator-1.0.7.tar.gz/rayter_generator-1.0.7/rayter_generator/render.py
@@ -0,0 +1,42 @@
+import os
+import json
+
+from jinja2 import Environment, FileSystemLoader
+
+from .settings import ROOT_DIR
+
+
+def render_game_page(env, game):
+    filename = os.path.join(env.output_path, game['slug'], 'index.html')
+    env.render_template("game.html", {
+        "name": game["game_name"],
+        "ratings": game["ratings"],
+        "game_name": game["game_name"],
+        "slug": game["slug"],
+        "players": [], # FIXME Fix players list
+        "STATIC_URL": "../static/",
+    }, output_path=filename)
+
+
+def render_game_json(env, game):
+    filename = os.path.join(env.output_path, game['slug'], 'game.json')
+    with open(filename, 'w') as f:
+        json.dump(game, f)
+
+def render_player_page(env, player):
+    filename = os.path.join(env.output_path, 'player', player['slug'], 'index.html')
+    env.render_template("player.html", {
+        "player": player,
+        "STATIC_URL": "../../static/",
+    }, output_path=filename)
+
+def render_index_page(env, games):
+    games.sort(key=lambda g: g["count"], reverse=True)
+    filename = os.path.join(env.output_path, 'index.html')
+    env.render_template("index.html", {
+        "games": games,
+        "players": [],
+        "global_chart": [],
+        "log": [],
+        "STATIC_URL": "static/",
+    }, output_path=filename)
