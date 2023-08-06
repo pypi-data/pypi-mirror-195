9f02b8a5df8d96f3cb26af0fad3868a508baff6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:20:20 +0000

    Add package addana/0.0.13/addana-0.0.13.tar.gz

---
+++ packages/addana/addana-0.0.13.tar.gz/addana-0.0.13/tests/test_addana.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+"""Tests for `addana` package."""
+
+
+import unittest
+import sys
+
+sys.path.append('..')
+import  addana
+
+
+class TestAddana(unittest.TestCase):
+    """Tests for `addana` package."""
+
+    def setUp(self):
+        """Set up test fixtures, if any."""
+
+    def tearDown(self):
+        """Tear down test fixtures, if any."""
+
+    def test_000_something(self):
+        """Test something."""
