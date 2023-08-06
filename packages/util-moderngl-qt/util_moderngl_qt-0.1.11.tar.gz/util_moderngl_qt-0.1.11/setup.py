90a909bf3ff4a3d2917c6900293bc84a8b8a203b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:02 +0000

    Add package util-moderngl-qt/0.1.11/util_moderngl_qt-0.1.11.tar.gz

---
+++ packages/util-moderngl-qt/util_moderngl_qt-0.1.11.tar.gz/util_moderngl_qt-0.1.11/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+def _requires_from_file(filename):
+    return open(filename).read().splitlines()
+
+setup(
+    name="util_moderngl_qt",
+    version="0.1.11",
+    install_requires=_requires_from_file('requirements.txt')
+)
