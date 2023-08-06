604f68437e0930da3305647c6828c292116a646f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:11:07 +0000

    Add package Izmon/0.1.3/Izmon-0.1.3-py3-none-any.whl

---
+++ packages/Izmon/Izmon-0.1.3-py3-none-any.whl/Izmon/Class/Start_End/End.py
@@ -0,0 +1,17 @@
+class End:
+	from ..Basic.Common_Function import Common_Function as co_f
+	from ..Basic.Settings import Settings as st
+	
+	def save(self): #データをセーブする
+		from ..Save_Code.Encode import Encode
+		print("不具合のためデータのセーブ機能を一時的に削除しています。Ver1.0.0リリース時に再度追加予定です。")
+		# while True:
+		# 	ty = self.co_f.typing("データをセーブしますか？ 1:する 0:しない", 1, 2)
+		# 	if ty == 1:
+		# 		code = Encode.encode()
+		# 		print("プレイヤー名と共に保管してください。")
+		# 		print("コード：", code)
+		# 		break
+		# 	else:
+		# 		break
+		return
