1fbc4a055de227764a9bc4248eea07f4469bad72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:26:48 +0000

    Add package helper-for-maxi/0.1.4/helper_for_maxi-0.1.4.tar.gz

---
+++ packages/helper-for-maxi/helper_for_maxi-0.1.4.tar.gz/helper_for_maxi-0.1.4/helper/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+"""
+Maxi's Functions
+~~~~~~~~~~~~~~~~
+
+Contains all Functions
+
+"""
+
+from .better_color_input        import better_color_input       as BetterColorInput
+from .better_input              import better_input             as BetterInput
+from .better_print              import better_print             as BetterPrint
+from .color_input               import color_input              as ColorInput
+from .output                    import output                   as Output
+from .terminal                  import Terminal                 as Terminal
