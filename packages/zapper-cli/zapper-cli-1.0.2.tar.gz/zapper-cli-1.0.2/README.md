acabca98e8717e8814442c1eca4077f06c060ba9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:38:06 +0000

    Add package zapper-cli/1.0.2/zapper-cli-1.0.2.tar.gz

---
+++ packages/zapper-cli/zapper-cli-1.0.2.tar.gz/zapper-cli-1.0.2/README.md
@@ -0,0 +1,36 @@
+<div align="center">
+
+<img src="https://user-images.githubusercontent.com/98690785/222817990-3fc05ad4-989c-437e-877d-cffdac794fef.png">
+
+
+
+[𝗙𝗘𝗔𝗧𝗨𝗥𝗘𝗦](#features) •  [𝗜𝗡𝗦𝗧𝗔𝗟𝗟](#install)
+  
+# 🇿​🇦​🇵​🇵​🇪​🇷​
+   
+*Is a python library for text styling*
+
+
+</div>
+<div align="left">
+
+## Features
+- • ⌛ Choose the waiting time you want to add to your text
+- • ✏️ Animated text with effects
+- • ⚡ Effects with gradients (in development)
+  
+## Install
+
+  <a href="https://linux.org/"> On Linux </a>
+  
+  ```bash
+  pip install zapper
+  ```
+  
+  
+  <a href="https://www.microsoft.com/ch-ch/windows"> On Windows </a>
+  
+  ```bash
+  pip3 install zapper
+  ```
+</div>
---
+++ packages/zapper-cli/zapper-cli-1.0.2.tar.gz/zapper-cli-1.0.2/src/zapper-cli/__init__.py
@@ -0,0 +1,58 @@
+#           ◜𝗭𝗮𝗽𝗽𝗲𝗿𝗽𝘆◞ 
+
+import time
+
+class Color:
+    list = {
+        'black': '\033[30m',
+        'red': '\033[31m',
+        'green': '\033[32m',
+        'yellow': '\033[33m',
+        'blue': '\033[34m',
+        'magenta': '\033[35m',
+        'cyan': '\033[36m',
+        'white': '\033[37m',
+        'bright_black': '\033[90m',
+        'bright_red': '\033[91m',
+        'bright_green': '\033[92m',
+        'bright_yellow': '\033[93m',
+        'bright_blue': '\033[94m',
+        'bright_magenta': '\033[95m',
+        'bright_cyan': '\033[96m',
+        'bright_white': '\033[97m',
+        'bg_black': '\033[40m',
+        'bg_red': '\033[41m',
+        'bg_green': '\033[42m',
+        'bg_yellow': '\033[43m',
+        'bg_blue': '\033[44m',
+        'bg_magenta': '\033[45m',
+        'bg_cyan': '\033[46m',
+        'bg_white': '\033[47m',
+        'bg_bright_black': '\033[100m',
+        'bg_bright_red': '\033[101m',
+        'bg_bright_green': '\033[102m',
+        'bg_bright_yellow': '\033[103m',
+        'bg_bright_blue': '\033[104m',
+        'bg_bright_magenta': '\033[105m',
+        'bg_bright_cyan': '\033[106m',
+        'bg_bright_white': '\033[107m',
+        'reset': '\033[0m',
+        'bold': '\033[1m',
+        'underline': '\033[4m',
+        'reversed': '\033[7m'
+    }
+
+def write(arg='- Put your text on function', color=None, sequence=0.1):
+        for letter in arg:
+            if color in Color.list:
+                code = Color.list[color]
+                print(f'{code}{letter}{Color.list["reset"]}', end="", flush=True)
+                time.sleep(sequence)
+            else:
+                try:
+                    print(f'{code}{letter}{Color.list["reset"]}', end="", flush=True)
+                except:
+                    write('- Invalid color', 'blue', 0.01)
+
+if __name__ == "__main__":
+    write('This package is for import on other file', 31)
