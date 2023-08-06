f668022a1d4b536e8c968c64f6383b5b6b306973
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:52:41 +0000

    Add package napari-btrack-reader/1.0.0/napari-btrack-reader-1.0.0.tar.gz

---
+++ packages/napari-btrack-reader/napari-btrack-reader-1.0.0.tar.gz/napari-btrack-reader-1.0.0/setup.py
@@ -0,0 +1,13 @@
+import sys
+from setuptools import setup
+
+if 'sdist' not in sys.argv:
+    sys.exit(
+        "\n*** Please install the `btrack` package "
+        "instead of `napari-btrack-reader`) ***\n"
+    )
+
+setup(
+    name="napari-btrack-reader",
+    version="1.0.0",
+)
