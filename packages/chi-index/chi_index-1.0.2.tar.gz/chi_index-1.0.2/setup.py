f706693fed31b371eef1393f88e198929e66e239
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:26:13 +0000

    Add package chi-index/1.0.2/chi_index-1.0.2.tar.gz

---
+++ packages/chi-index/chi_index-1.0.2.tar.gz/chi_index-1.0.2/setup.py
@@ -0,0 +1,30 @@
+from distutils.core import setup
+setup(
+  name = 'chi_index',
+  packages = ['chi_index'],
+  version = '1.0.2',
+  license='MIT',
+  description = 'External Clustering Validation Chi index',
+  author = 'José María Luna-Romera',
+  author_email = 'josemarialunaromera@gmail.com',
+  url = 'https://github.com/josemarialuna/ChiIndex',
+  download_url = 'https://github.com/josemarialuna/ChiIndex/tarball/0.1',
+  keywords = ['chi index','cvi','clustering','machine learning'],
+  install_requires=[
+          'sklearn',
+          'scipy',
+          'numpy',
+          'pandas',
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Science/Research',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+    'Programming Language :: Python :: 3.10',
+  ],
+)
