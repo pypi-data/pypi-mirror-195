d976d99e6723161f9a207696f1f349227545d1e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:15:18 +0000

    Add package modern-treasury/1.8.0/modern_treasury-1.8.0-py3-none-any.whl

---
+++ packages/modern-treasury/modern_treasury-1.8.0-py3-none-any.whl/modern_treasury/types/paper_item.py
@@ -0,0 +1,67 @@
+# File generated from our OpenAPI spec by Stainless.
+
+from typing import Optional
+from typing_extensions import Literal
+
+from ..types import shared
+from .._models import BaseModel
+
+__all__ = ["PaperItem"]
+
+
+class PaperItem(BaseModel):
+    account_number: Optional[str]
+    """The account number on the paper item."""
+
+    account_number_safe: Optional[str]
+    """The last 4 digits of the account_number."""
+
+    amount: int
+    """The amount of the paper item."""
+
+    check_number: Optional[str]
+    """The check number on the paper item."""
+
+    created_at: str
+
+    currency: Optional[shared.Currency]
+    """The currency of the paper item."""
+
+    deposit_date: str
+    """The date the paper item was deposited into your organization's bank account."""
+
+    id: str
+
+    live_mode: bool
+    """
+    This field will be true if this object exists in the live environment or false
+    if it exists in the test environment.
+    """
+
+    lockbox_number: str
+    """The identifier for the lockbox assigned by the bank."""
+
+    memo_field: Optional[str]
+    """The memo field on the paper item."""
+
+    object: str
+
+    remitter_name: Optional[str]
+    """The name of the remitter on the paper item."""
+
+    routing_number: Optional[str]
+    """The routing number on the paper item."""
+
+    status: Literal["completed", "pending", "returned"]
+    """The current status of the paper item.
+
+    One of `pending`, `completed`, or `returned`.
+    """
+
+    transaction_id: Optional[str]
+    """The ID of the reconciled Transaction or `null`."""
+
+    transaction_line_item_id: Optional[str]
+    """The ID of the reconciled Transaction Line Item or `null`."""
+
+    updated_at: str
