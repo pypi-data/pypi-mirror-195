8d295e5e8f0ab97a6a2d2e18dbdd7ce11812f8b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:12:00 +0000

    Add package pyees/1.0.5/pyees-1.0.5.tar.gz

---
+++ packages/pyees/pyees-1.0.5.tar.gz/pyees-1.0.5/pyees/__init__.py
@@ -0,0 +1,12 @@
+# import the necessary modules
+
+from variable import variable
+from fit import dummy_fit, pol_fit, lin_fit, exp_fit, pow_fit, logistic_fit, logistic_100_fit
+from readData import readData
+from prop import prop
+from solve import solve
+from unitSystem import getDicts
+
+
+knownUnits, knownCharacters, knownUnitsDict, knownPrefixes, baseUnit = getDicts()
+    
