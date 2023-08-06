848a8bcce3a91cc971cfaf23bcfc26c92745b524
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:57:53 +0000

    Add package truststore/0.6.0/truststore-0.6.0-py3-none-any.whl

---
+++ packages/truststore/truststore-0.6.0-py3-none-any.whl/truststore/__init__.py
@@ -0,0 +1,16 @@
+"""Verify certificates using OS trust stores. This is useful when your system contains
+custom certificate authorities such as when using a corporate proxy or using test certificates.
+Supports macOS, Windows, and Linux (with OpenSSL).
+"""
+
+import sys as _sys
+
+if _sys.version_info < (3, 10):
+    raise ImportError("truststore requires Python 3.10 or later")
+
+from ._api import SSLContext, extract_from_ssl, inject_into_ssl  # noqa: E402
+
+del _api, _sys  # type: ignore[name-defined] # noqa: F821
+
+__all__ = ["SSLContext", "inject_into_ssl", "extract_from_ssl"]
+__version__ = "0.6.0"
