aaade84c842e8b4395a68a935d9b7b730c1b9a24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:12:20 +0000

    Add package gpt-assistant/1.1/gpt_assistant-1.1.tar.gz

---
+++ packages/gpt-assistant/gpt_assistant-1.1.tar.gz/gpt_assistant-1.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='gpt_assistant',
+      version='1.1',
+      description='Speech-based GPT using Python',
+      author='Vinay',
+      author_email='vinay@connect.vin',
+      url='https://github.com/2vin/gpt-assistant',
+      packages=['gpt_assistant'])
