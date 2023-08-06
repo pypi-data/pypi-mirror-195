ab806422ff805eca000ef73fc066622c164dcf0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:11:45 +0000

    Add package commonfate-provider/0.6.0/commonfate_provider-0.6.0.tar.gz

---
+++ packages/commonfate-provider/commonfate_provider-0.6.0.tar.gz/commonfate_provider-0.6.0/commonfate_provider/tests/resources_test.py
@@ -0,0 +1,36 @@
+import typing
+
+import pytest
+from syrupy.extensions.json import JSONSnapshotExtension
+
+from commonfate_provider import resources
+
+
+@pytest.fixture
+def snapshot_json(snapshot):
+    """use JSON, rather than AmberSnapshotExtension as our schema is serialized as JSON"""
+    return snapshot.use_extension(JSONSnapshotExtension)
+
+
+class FirstResource(resources.Resource):
+    """
+    An example resource
+    """
+
+    value: str
+    related_field: typing.Optional[str] = resources.Related("FirstResource")
+
+
+class SecondResource(resources.Resource):
+    value: str
+    related_field: typing.Optional[str] = resources.Related(FirstResource)
+
+
+@resources.loader
+def load_resources():
+    pass
+
+
+def test_resource_schema_works(snapshot_json):
+    got = resources.export_schema()
+    assert got.dict() == snapshot_json
