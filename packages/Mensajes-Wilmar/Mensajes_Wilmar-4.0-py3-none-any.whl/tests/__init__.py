72681ecf8a005df61fefe2752680d7cfcbfb703e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:13:02 +0000

    Add package Mensajes-Wilmar/4.0/Mensajes_Wilmar-4.0-py3-none-any.whl

---
+++ packages/Mensajes-Wilmar/Mensajes_Wilmar-4.0-py3-none-any.whl/tests/__init__.py
@@ -0,0 +1,5 @@
+print("Cargando el paquete tests...\n")
+
+# este se ejecuta de manera automatica cuando importamos un paquete
+# además, de que sirve para identificar que es un paquete, o un subpaquete
+# no es necesario crearlo, pero puede servir como guía
