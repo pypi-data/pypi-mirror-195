1c3b071b738a49a1d782ffc3c9b7a8a38b1d8b55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:34:58 +0000

    Add package bluetree-shopify-book/0.0.1/bluetree_shopify_book-0.0.1.tar.gz

---
+++ packages/bluetree-shopify-book/bluetree_shopify_book-0.0.1.tar.gz/bluetree_shopify_book-0.0.1/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+    name="bluetree_shopify_book",
+    version="0.0.1",
+    description="A Python package for managing books in Shopify",
+    long_description=open("README.md").read().strip(),
+    author="Blue Tree Management (Canada), Ltd.",
+    author_email="bluetree@digitalnomad.jp",
+    url="https://bluetreemanagement.com",
+    py_modules=["bluetree_shopify_book"],
+    install_requires=[],
+    license="MIT License",
+    zip_safe=False,
+    keywords="shopify isbn book python",
+)
