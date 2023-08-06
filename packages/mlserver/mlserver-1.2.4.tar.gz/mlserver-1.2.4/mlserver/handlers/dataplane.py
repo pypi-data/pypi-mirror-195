babc7bc297298240d2352c644edf42612e0dddbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:31:14 +0000

    Add package mlserver/1.2.4/mlserver-1.2.4.tar.gz

---
+++ packages/mlserver/mlserver-1.2.4.tar.gz/mlserver-1.2.4/mlserver/handlers/dataplane.py
@@ -0,0 +1,103 @@
+from ..settings import Settings
+from ..registry import MultiModelRegistry
+from ..types import (
+    MetadataModelResponse,
+    MetadataServerResponse,
+    InferenceRequest,
+    InferenceResponse,
+)
+from ..middleware import InferenceMiddlewares
+from ..cloudevents import CloudEventsMiddleware
+from ..utils import generate_uuid
+from prometheus_client import (
+    Counter,
+    Summary,
+)
+from typing import Optional
+
+
+_ModelInferRequestSuccess = Counter(
+    "model_infer_request_success",
+    "Model infer request success count",
+    ["model", "version"],
+)
+_ModelInferRequestFailure = Counter(
+    "model_infer_request_failure",
+    "Model infer request failure count",
+    ["model", "version"],
+)
+_ModelInferRequestDuration = Summary(
+    "model_infer_request_duration", "Model infer request duration", ["model", "version"]
+)
+
+
+class DataPlane:
+    """
+    Internal implementation of handlers, used by both the gRPC and REST
+    servers.
+    """
+
+    def __init__(self, settings: Settings, model_registry: MultiModelRegistry):
+        self._settings = settings
+        self._model_registry = model_registry
+
+        self._inference_middleware = InferenceMiddlewares(
+            CloudEventsMiddleware(settings)
+        )
+
+    async def live(self) -> bool:
+        return True
+
+    async def ready(self) -> bool:
+        models = await self._model_registry.get_models()
+        return all([model.ready for model in models])
+
+    async def model_ready(self, name: str, version: Optional[str] = None) -> bool:
+        model = await self._model_registry.get_model(name, version)
+        return model.ready
+
+    async def metadata(self) -> MetadataServerResponse:
+        return MetadataServerResponse(
+            name=self._settings.server_name,
+            version=self._settings.server_version,
+            extensions=self._settings.extensions,
+        )
+
+    async def model_metadata(
+        self, name: str, version: Optional[str] = None
+    ) -> MetadataModelResponse:
+        model = await self._model_registry.get_model(name, version)
+        # TODO: Make await optional for sync methods
+        return await model.metadata()
+
+    async def infer(
+        self,
+        payload: InferenceRequest,
+        name: str,
+        version: Optional[str] = None,
+    ) -> InferenceResponse:
+
+        with _ModelInferRequestDuration.labels(
+            model=name, version=version
+        ).time(), _ModelInferRequestFailure.labels(
+            model=name, version=version
+        ).count_exceptions():
+
+            if payload.id is None:
+                payload.id = generate_uuid()
+
+            model = await self._model_registry.get_model(name, version)
+
+            self._inference_middleware.request_middleware(payload, model.settings)
+
+            # TODO: Make await optional for sync methods
+            prediction = await model.predict(payload)
+
+            # Ensure ID matches
+            prediction.id = payload.id
+
+            self._inference_middleware.response_middleware(prediction, model.settings)
+
+            _ModelInferRequestSuccess.labels(model=name, version=version).inc()
+
+            return prediction
---
+++ packages/mlserver/mlserver-1.2.4.tar.gz/mlserver-1.2.4/mlserver/model.py
@@ -0,0 +1,173 @@
+from typing import Any, Dict, Optional, List
+
+from .codecs import (
+    encode_response_output,
+    encode_inference_response,
+    decode_request_input,
+    decode_inference_request,
+    has_decoded,
+    get_decoded,
+    InputCodecLike,
+    RequestCodecLike,
+)
+from .codecs.errors import CodecNotFound
+from .settings import ModelSettings
+from .types import (
+    InferenceRequest,
+    InferenceResponse,
+    RequestInput,
+    RequestOutput,
+    ResponseOutput,
+    MetadataModelResponse,
+    MetadataTensor,
+)
+from .types import (
+    Parameters,
+)
+
+
+def _generate_metadata_index(
+    metadata_tensors: Optional[List[MetadataTensor]],
+) -> Dict[str, MetadataTensor]:
+    metadata_index: Dict[str, MetadataTensor] = {}
+
+    if not metadata_tensors:
+        return metadata_index
+
+    for metadata_tensor in metadata_tensors:
+        metadata_index[metadata_tensor.name] = metadata_tensor
+
+    return metadata_index
+
+
+class MLModel:
+    """
+    Abstract class which serves as the main interface to interact with ML
+    models.
+    """
+
+    def __init__(self, settings: ModelSettings):
+        self._settings = settings
+        self._inputs_index: Dict[str, MetadataTensor] = {}
+
+        self._inputs_index = _generate_metadata_index(self._settings.inputs)
+        self._outputs_index = _generate_metadata_index(self._settings.outputs)
+
+        self.ready = False
+
+    @property
+    def name(self) -> str:
+        return self._settings.name
+
+    @property
+    def version(self) -> Optional[str]:
+        return self._settings.version
+
+    @property
+    def settings(self) -> ModelSettings:
+        return self._settings
+
+    @property
+    def inputs(self) -> Optional[List[MetadataTensor]]:
+        return self._settings.inputs
+
+    @inputs.setter
+    def inputs(self, value: List[MetadataTensor]):
+        self._settings.inputs = value
+        self._inputs_index = _generate_metadata_index(self._settings.inputs)
+
+    @property
+    def outputs(self) -> Optional[List[MetadataTensor]]:
+        return self._settings.outputs
+
+    @outputs.setter
+    def outputs(self, value: List[MetadataTensor]):
+        self._settings.outputs = value
+        self._outputs_index = _generate_metadata_index(self._settings.outputs)
+
+    def decode(
+        self,
+        request_input: RequestInput,
+        default_codec: Optional[InputCodecLike] = None,
+    ) -> Any:
+        decode_request_input(request_input, self._inputs_index)
+
+        if has_decoded(request_input):
+            return get_decoded(request_input)
+
+        if default_codec:
+            return default_codec.decode_input(request_input)
+
+        return request_input.data
+
+    def decode_request(
+        self,
+        inference_request: InferenceRequest,
+        default_codec: Optional[RequestCodecLike] = None,
+    ) -> Any:
+        decode_inference_request(inference_request, self._settings, self._inputs_index)
+
+        if has_decoded(inference_request):
+            return get_decoded(inference_request)
+
+        if default_codec:
+            return default_codec.decode_request(inference_request)
+
+        return inference_request
+
+    def encode_response(
+        self,
+        payload: Any,
+        default_codec: Optional[RequestCodecLike] = None,
+    ) -> InferenceResponse:
+        inference_response = encode_inference_response(payload, self._settings)
+
+        if inference_response:
+            return inference_response
+
+        if default_codec:
+            return default_codec.encode_response(self.name, payload, self.version)
+
+        payload_type = str(type(payload))
+        raise CodecNotFound(payload_type=payload_type, is_input=False, is_request=True)
+
+    def encode(
+        self,
+        payload: Any,
+        request_output: RequestOutput,
+        default_codec: Optional[InputCodecLike] = None,
+    ) -> ResponseOutput:
+        response_output = encode_response_output(
+            payload, request_output, self._outputs_index
+        )
+
+        if response_output:
+            return response_output
+
+        if default_codec:
+            return default_codec.encode_output(request_output.name, payload)
+
+        raise CodecNotFound(name=request_output.name, is_input=False, is_request=False)
+
+    async def metadata(self) -> MetadataModelResponse:
+        model_metadata = MetadataModelResponse(
+            name=self.name,
+            platform=self._settings.platform,
+            versions=self._settings.versions,
+            inputs=self._settings.inputs,
+            outputs=self._settings.outputs,
+        )
+
+        if self._settings.parameters:
+            model_metadata.parameters = Parameters(
+                content_type=self._settings.parameters.content_type
+            )
+
+        return model_metadata
+
+    async def load(self) -> bool:
+        self.ready = True
+        return self.ready
+
+    async def predict(self, payload: InferenceRequest) -> InferenceResponse:
+        raise NotImplementedError("predict() method not implemented")
