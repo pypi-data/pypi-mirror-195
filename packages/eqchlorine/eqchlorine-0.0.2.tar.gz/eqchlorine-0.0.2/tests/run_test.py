96e3eb435979aa1c80eeb311ef730ea439cb2c4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:28:31 +0000

    Add package eqchlorine/0.0.2/eqchlorine-0.0.2.tar.gz

---
+++ packages/eqchlorine/eqchlorine-0.0.2.tar.gz/eqchlorine-0.0.2/tests/run_test.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar  2 17:02:15 2023
+
+@author: llinares
+"""
+
+import numpy
+import scipy
+import matplotlib
+import colorama
