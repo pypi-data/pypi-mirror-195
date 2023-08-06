b584c109a2927f64a2bb07ac88a468e68bd91161
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:35:00 +0000

    Add package pcb-parser/0.1.0rc1/pcb-parser-0.1.0rc1.tar.gz

---
+++ packages/pcb-parser/pcb-parser-0.1.0rc1.tar.gz/pcb-parser-0.1.0rc1/setup.py
@@ -0,0 +1,22 @@
+import setuptools 
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+    
+setuptools.setup(
+    name = 'pcb-parser',
+    version = '0.1.0.rc1',
+    author = 'Changyun Choi',
+    author_email = "cyun9601@gmail.com",
+    description = "PCB Parser",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    url = "https://github.com/cyun9601/PCB_Parser",
+    packages = setuptools.find_packages(where='src/pcb_parser'),
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires = '>=3.7.0',
+)
