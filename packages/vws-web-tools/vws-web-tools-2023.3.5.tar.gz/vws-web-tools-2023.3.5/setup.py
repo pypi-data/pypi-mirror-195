ca7c4cb1b6713e360b2da4632c216633d307ec7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:34 +0000

    Add package vws-web-tools/2023.3.5/vws-web-tools-2023.3.5.tar.gz

---
+++ packages/vws-web-tools/vws-web-tools-2023.3.5.tar.gz/vws-web-tools-2023.3.5/setup.py
@@ -0,0 +1,40 @@
+"""
+Setup script.
+"""
+
+from __future__ import annotations
+
+from pathlib import Path
+
+from setuptools import setup
+
+
+def _get_dependencies(requirements_file: Path) -> list[str]:
+    """
+    Return requirements from a requirements file.
+
+    This expects a requirements file with no ``--find-links`` lines.
+    """
+    lines = requirements_file.read_text().strip().split("\n")
+    return [line for line in lines if not line.startswith("#")]
+
+
+_DIRECT_REQUIRES = _get_dependencies(
+    requirements_file=Path("requirements/requirements.txt"),
+)
+
+INSTALL_REQUIRES = _DIRECT_REQUIRES
+DEV_REQUIRES = _get_dependencies(
+    requirements_file=Path("requirements/dev-requirements.txt"),
+)
+
+SETUP_REQUIRES = _get_dependencies(
+    requirements_file=Path("requirements/setup-requirements.txt"),
+)
+
+setup(
+    use_scm_version=True,
+    setup_requires=SETUP_REQUIRES,
+    install_requires=INSTALL_REQUIRES,
+    extras_require={"dev": DEV_REQUIRES},
+)
