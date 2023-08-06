4cf6245fa49a1794c939eff244c28a8c1973d0cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:47:34 +0000

    Add package pyshade/0.0.8/pyshade-0.0.8.tar.gz

---
+++ packages/pyshade/pyshade-0.0.8.tar.gz/pyshade-0.0.8/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.8'
+
+setup(
+    name="pyshade",
+    description="a python module that will allow you to make a colorful text with dynamics colors and display your text slowly",
+    version=VERSION,
+    packages=find_packages(),
+    install_requires= [],
+    keywords=['python', 'sockets'],
+    classifiers=[]
+)
