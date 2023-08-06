4a56884cb555bb7737145006e929f1e9825b6e61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:31:53 +0000

    Add package canonicalwebteam.discourse/5.2.4/canonicalwebteam.discourse-5.2.4-py3-none-any.whl

---
+++ packages/canonicalwebteam.discourse/canonicalwebteam.discourse-5.2.4-py3-none-any.whl/tests/test_parser.py
@@ -0,0 +1,72 @@
+import unittest
+from unittest.mock import MagicMock
+from canonicalwebteam.discourse.parsers.base_parser import BaseParser
+
+
+class TestParser(unittest.TestCase):
+    def test_parser_username_link(self):
+        discourse_api_mock = MagicMock()
+        discourse_api_mock.base_url = "https://base.url"
+
+        parser = BaseParser(
+            api=discourse_api_mock,
+            index_topic_id=1,
+            url_prefix="/",
+        )
+
+        parsed_topic = parser.parse_topic(
+            {
+                "id": 1,
+                "category_id": 1,
+                "title": "Sample",
+                "slug": "sample—text",
+                "post_stream": {
+                    "posts": [
+                        {
+                            "id": 11,
+                            "cooked": (
+                                "<a href='/u/evilnick'>@evilnick</a>"
+                                "<a>No Link</a>"
+                                "<a></a>"
+                            ),
+                            "updated_at": "2018-10-02T12:45:44.259Z",
+                        }
+                    ],
+                },
+            }
+        )
+
+        self.assertIn(
+            '<a href="https://base.url/u/evilnick">@evilnick</a>',
+            parsed_topic["body_html"],
+        )
+
+    def test_emdash_in_slug(self):
+        discourse_api_mock = MagicMock()
+        discourse_api_mock.base_url = "https://base.url"
+
+        parser = BaseParser(
+            api=discourse_api_mock,
+            index_topic_id=1,
+            url_prefix="/",
+        )
+
+        parsed_topic = parser.parse_topic(
+            {
+                "id": 1,
+                "category_id": 1,
+                "title": "Sample",
+                "slug": "sample—text",
+                "post_stream": {
+                    "posts": [
+                        {
+                            "id": 11,
+                            "cooked": ("empty"),
+                            "updated_at": "2018-10-02T12:45:44.259Z",
+                        }
+                    ],
+                },
+            }
+        )
+
+        self.assertEqual("/t/sample--text/1", parsed_topic["topic_path"])
