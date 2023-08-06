657710b576bc73b9a8deb6f1a2f74e1a71ca4a65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:03 +0000

    Add package GANetic/0.0.11/GANetic-0.0.11.tar.gz

---
+++ packages/GANetic/GANetic-0.0.11.tar.gz/GANetic-0.0.11/ganetic/layers/blocks.py
@@ -0,0 +1,36 @@
+import torch.nn as nn
+
+
+class ResidualBlock(nn.Module):
+    def __init__(self, channels):
+        super(ResidualBlock, self).__init__()
+        self.conv1 = nn.Conv2d(channels, channels, kernel_size=3, padding=1)
+        self.bn1 = nn.BatchNorm2d(channels)
+        self.prelu = nn.PReLU()
+        self.conv2 = nn.Conv2d(channels, channels, kernel_size=3, padding=1)
+        self.bn2 = nn.BatchNorm2d(channels)
+
+    def forward(self, x):
+        _x = x
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.prelu(x)
+        x = self.conv2(x)
+        x = self.bn2(x)
+        return x + _x
+
+
+class UpsampleBlock(nn.Module):
+    def __init__(self, in_channels, up_scale):
+        super(UpsampleBlock, self).__init__()
+        self.conv = nn.Conv2d(
+            in_channels, in_channels * up_scale**2, kernel_size=3, padding=1
+        )
+        self.pixel_shuffle = nn.PixelShuffle(up_scale)
+        self.prelu = nn.PReLU()
+
+    def forward(self, x):
+        x = self.conv(x)
+        x = self.pixel_shuffle(x)
+        x = self.prelu(x)
+        return x
