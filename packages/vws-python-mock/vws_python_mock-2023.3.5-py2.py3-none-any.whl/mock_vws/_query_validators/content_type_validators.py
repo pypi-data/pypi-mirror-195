91f9ede138aca91020e285db655ea5480cb8014b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:58:18 +0000

    Add package vws-python-mock/2023.3.5/vws_python_mock-2023.3.5-py2.py3-none-any.whl

---
+++ packages/vws-python-mock/vws_python_mock-2023.3.5-py2.py3-none-any.whl/mock_vws/_query_validators/content_type_validators.py
@@ -0,0 +1,46 @@
+"""
+Validators for the ``Content-Type`` header.
+"""
+
+
+import mock_vws._cgi as cgi
+from mock_vws._query_validators.exceptions import (
+    ImageNotGiven,
+    NoBoundaryFound,
+    NoContentType,
+    UnsupportedMediaType,
+)
+
+
+def validate_content_type_header(
+    request_headers: dict[str, str],
+    request_body: bytes,
+) -> None:
+    """
+    Validate the ``Content-Type`` header.
+
+    Args:
+        request_headers: The headers sent with the request.
+        request_body: The body of the request.
+
+    Raises:
+        UnsupportedMediaType: The ``Content-Type`` header main part is not
+            'multipart/form-data'.
+        NoBoundaryFound: The ``Content-Type`` header does not contain a
+            boundary.
+        ImageNotGiven: The boundary is not in the request body.
+        NoContentType: The content type header is either empty or not given.
+    """
+    content_type_header = request_headers.get("Content-Type", "")
+    main_value, pdict = cgi.parse_header(content_type_header)
+    if content_type_header == "":
+        raise NoContentType
+
+    if main_value not in ("multipart/form-data", "*/*"):
+        raise UnsupportedMediaType
+
+    if "boundary" not in pdict:
+        raise NoBoundaryFound
+
+    if pdict["boundary"].encode() not in request_body:
+        raise ImageNotGiven
---
+++ packages/vws-python-mock/vws_python_mock-2023.3.5-py2.py3-none-any.whl/mock_vws/_services_validators/content_type_validators.py
@@ -0,0 +1,30 @@
+"""
+Content-Type header validators to use in the mock.
+"""
+
+
+from requests_mock import POST, PUT
+
+from mock_vws._services_validators.exceptions import AuthenticationFailure
+
+
+def validate_content_type_header_given(
+    request_headers: dict[str, str],
+    request_method: str,
+) -> None:
+    """
+    Validate that there is a non-empty content type header given if required.
+
+    Args:
+        request_headers: The headers sent with the request.
+        request_method: The HTTP method of the request.
+
+    Raises:
+        AuthenticationFailure: No ``Content-Type`` header is given and the
+            request requires one.
+    """
+    request_needs_content_type = bool(request_method in (POST, PUT))
+    if request_headers.get("Content-Type") or not request_needs_content_type:
+        return
+
+    raise AuthenticationFailure
