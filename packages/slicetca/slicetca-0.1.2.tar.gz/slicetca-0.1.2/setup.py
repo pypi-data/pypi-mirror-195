743efc83ac4a3c35066ca03451e076cae1494868
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:32:28 +0000

    Add package slicetca/0.1.2/slicetca-0.1.2.tar.gz

---
+++ packages/slicetca/slicetca-0.1.2.tar.gz/slicetca-0.1.2/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+setup(
+    name='slicetca',
+    packages=['slicetca'],
+    version='0.1.2',
+
+    description='Package to perform Slice Tensor Component Analysis',
+    long_description='README.md',
+    long_description_content_type='text/markdown',
+
+    url='https://github.com/arthur-pe/slicetca',
+    author='Arthur Pellegrino',
+    license='MIT',
+    install_requires=['torch',
+                      'numpy',
+                      'matplotlib',
+                      'tqdm'
+                      ],
+    python_requires='>=3.8',
+    classifiers=[
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Mathematics',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.8',
+    ],
+)
