687c0d21c357752dce6912ec6b9f7951bf3d695f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:53 +0000

    Add package jax-metrics/0.2.1/jax_metrics-0.2.1-py3-none-any.whl

---
+++ packages/jax-metrics/jax_metrics-0.2.1-py3-none-any.whl/jax_metrics/__init__.py
@@ -0,0 +1,20 @@
+__version__ = "0.2.1"
+
+
+from jax_metrics.losses import Loss
+from jax_metrics.metrics import AuxMetrics, Losses, Metric, Metrics, SumMetric
+from jax_metrics.types import Named
+
+from . import losses, metrics, regularizers
+
+__all__ = [
+    "Loss",
+    "Losses",
+    "Metrics",
+    "Metric",
+    "SumMetric",
+    "Named",
+    "losses",
+    "metrics",
+    "regularizers",
+]
