e834734029b2c71b64db77de6d2824cc7b357f37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:08:35 +0000

    Add package sarus-data-spec-public/2.6.2.dev1/sarus_data_spec_public-2.6.2.dev1.tar.gz

---
+++ packages/sarus-data-spec-public/sarus_data_spec_public-2.6.2.dev1.tar.gz/sarus_data_spec_public-2.6.2.dev1/sarus_data_spec/config/__init__.py
@@ -0,0 +1,17 @@
+import os
+
+import yaml
+
+routing_file = os.path.join(os.path.dirname(__file__), "routing.yaml")
+with open(routing_file) as f:
+    ROUTING = yaml.load(f.read(), Loader=yaml.Loader)
+
+properties_file = os.path.join(
+    os.path.dirname(__file__), "privacy_properties.yaml"
+)
+with open(properties_file) as f:
+    privacy_properties = yaml.load(f.read(), Loader=yaml.Loader)
+
+WHITELISTED_TRANSFORMS = privacy_properties["WHITELISTED_TRANSFORMS"]
+
+__all__ = ["ROUTING", "WHITELISTED_TRANSFORMS"]
