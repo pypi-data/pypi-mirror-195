2cb79f496f71f16ce064b84caa4c9465b40cd00d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:26:51 +0000

    Add package GLOBALMART-FUNCTION-BDIST/0.1/GLOBALMART_FUNCTION_BDIST-0.1.tar.gz

---
+++ packages/GLOBALMART-FUNCTION-BDIST/GLOBALMART_FUNCTION_BDIST-0.1.tar.gz/GLOBALMART_FUNCTION_BDIST-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+setup(name='GLOBALMART_FUNCTION_BDIST',
+version='0.1',
+description='Testing installation of Package',
+author='Nishpa Vanapalli',
+author_email='nishpa.vanapalli@fractal.ai',
+license='MIT',
+install_requires=['pandas'],
+packages=['globalmart_api'])
