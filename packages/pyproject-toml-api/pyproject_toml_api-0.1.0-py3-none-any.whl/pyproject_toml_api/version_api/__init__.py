2a5b0e3fd31ac867b222e1a08fc3d1fb393179d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:35:34 +0000

    Add package pyproject-toml-api/0.1.0/pyproject_toml_api-0.1.0-py3-none-any.whl

---
+++ packages/pyproject-toml-api/pyproject_toml_api-0.1.0-py3-none-any.whl/pyproject_toml_api/version_api/__init__.py
@@ -0,0 +1,18 @@
+"""
+__init__.py
+
+
+
+Author: Zack Hankin
+
+Started: 4/03/2023
+"""
+from __future__ import annotations
+
+
+def main() -> int:
+    return 0
+
+
+if __name__ == "__main__":
+    raise SystemExit(main())
