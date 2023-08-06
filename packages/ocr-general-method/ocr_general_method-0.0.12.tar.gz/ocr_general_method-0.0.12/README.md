d3fa68b1536b06d85123ec6af504c220544a091f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:35:14 +0000

    Add package ocr-general-method/0.0.12/ocr_general_method-0.0.12.tar.gz

---
+++ packages/ocr-general-method/ocr_general_method-0.0.12.tar.gz/ocr_general_method-0.0.12/README.md
@@ -0,0 +1,9 @@
+# ocr封装接口通用方法
+
+* /src/example
+  * 测试
+
+* /src/common
+  * file_utils：文件工具类
+  * exception：错误码工具类
+  * http_utils：远程请求工具类
---
+++ packages/ocr-general-method/ocr_general_method-0.0.12.tar.gz/ocr_general_method-0.0.12/src/ocr_general_method/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# @Time: 2023/3/2 10:58
+# @Author: zyq
+# @File: __init__.py.py
+# @Software: PyCharm
+
+__all__ = ["example", "common"]
