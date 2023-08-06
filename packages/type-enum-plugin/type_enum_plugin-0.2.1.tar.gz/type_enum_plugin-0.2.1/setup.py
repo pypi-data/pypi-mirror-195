ef7f76831d089225cb8cdca42a419f537f651eb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:14:47 +0000

    Add package type-enum-plugin/0.2.1/type_enum_plugin-0.2.1.tar.gz

---
+++ packages/type-enum-plugin/type_enum_plugin-0.2.1.tar.gz/type_enum_plugin-0.2.1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['type_enum_plugin']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['mypy']
+
+setup_kwargs = {
+    'name': 'type-enum-plugin',
+    'version': '0.2.1',
+    'description': 'Mypy plugin for type-enum.',
+    'long_description': '# type-enum-plugin\n\nMypy plugin for TypeEnum.\n',
+    'author': 'Thomas MK',
+    'author_email': 'tmke8@posteo.net',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'https://github.com/tmke8/type_enum',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
