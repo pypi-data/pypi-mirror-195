5eac87c402ce3c39cafea7214d6542eab90f9b43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:53 +0000

    Add package aiotaipit/2.1.2/aiotaipit-2.1.2-py3-none-any.whl

---
+++ packages/aiotaipit/aiotaipit-2.1.2-py3-none-any.whl/aiotaipit/helpers.py
@@ -0,0 +1,13 @@
+"""Helpers and utils."""
+from __future__ import annotations
+
+from .const import REGIONS, METER_MODELS, UNKNOWN
+
+
+def get_region_name(region_id: int) -> str:
+    region_name = REGIONS.get(region_id, f'{UNKNOWN} <{region_id}>')
+    return region_name
+
+
+def get_model_name(model_id: int) -> str:
+    return METER_MODELS.get(model_id, f'{UNKNOWN} <{model_id}>')
