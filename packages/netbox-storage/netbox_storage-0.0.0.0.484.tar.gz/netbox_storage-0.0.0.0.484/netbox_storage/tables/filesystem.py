9b77a49c159163e87445b02fe99b182c95ec99ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:34:27 +0000

    Add package netbox-storage/0.0.0.0.484/netbox_storage-0.0.0.0.484.tar.gz

---
+++ packages/netbox-storage/netbox_storage-0.0.0.0.484.tar.gz/netbox_storage-0.0.0.0.484/netbox_storage/tables/filesystem.py
@@ -0,0 +1,30 @@
+import django_tables2 as tables
+
+from netbox.tables import (
+    NetBoxTable,
+    ToggleColumn,
+    ActionsColumn,
+)
+
+from netbox_storage.models import Filesystem
+
+
+class FilesystemTable(NetBoxTable):
+    """Table for displaying Filesystem objects."""
+
+    pk = ToggleColumn()
+    filesystem = tables.Column(
+        linkify=True,
+    )
+
+    class Meta(NetBoxTable.Meta):
+        model = Filesystem
+        fields = (
+            "pk",
+            "filesystem",
+            "description",
+        )
+        default_columns = (
+            "filesystem",
+            "description"
+        )
