ee6f796a07614da649f9ecbc1c4fd8bf16f7a6e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:05 +0000

    Add package data-science-libs/0.0.2/data-science-libs-0.0.2.tar.gz

---
+++ packages/data-science-libs/data-science-libs-0.0.2.tar.gz/data-science-libs-0.0.2/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+setup(
+    name='data-science-libs',
+    version='0.0.2',
+    description='data science related tools & helpers',
+    url='https://github.com/cjkepinsky/libs',
+    author='Chris J. Kepinsky',
+    author_email='chris.kepinski@gmail.com',
+    license='MIT',
+    packages=['libs'],
+    install_requires=[
+        'babyplots',
+        'matplotlib',
+        'numpy',
+        'pandas',
+        'scipy',
+        'seaborn',
+        'sklearn',
+        'statistics',
+        'umap',
+        'xgboost',
+    ],
+    zip_safe=False
+)
