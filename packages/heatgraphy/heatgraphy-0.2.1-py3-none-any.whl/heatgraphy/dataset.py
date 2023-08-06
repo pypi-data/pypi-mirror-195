483689f3602331332a1f5ff45963b8eb6335aafa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:13:17 +0000

    Add package heatgraphy/0.2.1/heatgraphy-0.2.1-py3-none-any.whl

---
+++ packages/heatgraphy/heatgraphy-0.2.1-py3-none-any.whl/heatgraphy/dataset.py
@@ -0,0 +1,88 @@
+from urllib.request import urlretrieve
+
+import pandas as pd
+from platformdirs import user_cache_path
+
+NAME = "Heatgraphy"
+BASE_URL = "https://raw.githubusercontent.com/heatgraphy/heatgraphy-data/main"
+
+
+def load_data(name, cache=True):
+    """To load heatgraphy dataset
+
+    - 'imdb': The IMDB Top 100 Movies
+    - 'pbmc3k': single-cell RNA-seq dataset from 10X Genomics
+    - 'oncoprint': Subsample of Breast Invasive Carcinoma (TCGA, PanCancer Atlas)
+
+    Parameters
+    ----------
+    name : str
+        The name of the dataset
+    cache : bool
+        Download data and cache locally
+
+    Returns
+    -------
+
+    """
+    if name == "imdb":
+        return _load_imdb(cache)
+    elif name == "pbmc3k":
+        return _load_pbmc3k(cache)
+    elif name == "oncoprint":
+        return _load_oncoprint(cache)
+    else:
+        raise NameError("Dataset not found")
+
+
+def _cache_remote(files, cache=True):
+    if isinstance(files, str):
+        files = [files]
+    data_dir = user_cache_path(appname=NAME)
+    data_dir.mkdir(exist_ok=True, parents=True)
+
+    download_files = [f.replace("/", "_") for f in files]
+
+    for fname, dfname in zip(files, download_files):
+        url = f"{BASE_URL}/{fname}"
+        # Will not download if cache and file exist
+        dest = data_dir / dfname
+        # Not download when cache and exist
+        if not (cache and dest.exists()):
+            urlretrieve(url, dest)
+
+    if len(files) == 1:
+        return data_dir / download_files[0]
+    else:
+        return [data_dir / f for f in download_files]
+
+
+def _load_imdb(cache=True):
+    imdb = _cache_remote("imdb.csv", cache=cache)
+    return pd.read_csv(imdb)
+
+
+def _load_pbmc3k(cache=True):
+    exp, pct_cells, count = _cache_remote(['pbmc3k/exp.csv',
+                                           'pbmc3k/pct_cells.csv',
+                                           'pbmc3k/count.csv'],
+                                          cache=cache)
+    return {
+        'exp': pd.read_csv(exp, index_col=0),
+        'pct_cells': pd.read_csv(pct_cells, index_col=0),
+        'count': pd.read_csv(count, index_col=0)
+    }
+
+
+def _load_oncoprint(cache=True):
+    cna, mrna, methyl, clinical = _cache_remote(
+        ['oncoprint/cna.csv', 'oncoprint/mrna_exp.csv',
+         'oncoprint/methyl_exp.csv', 'oncoprint/clinical.csv'],
+        cache=cache
+    )
+    return {
+        'cna': pd.read_csv(cna, index_col=0),
+        'mrna_exp': pd.read_csv(mrna, index_col=0),
+        'methyl_exp': pd.read_csv(methyl, index_col=0),
+        'clinical': pd.read_csv(clinical, index_col=0)
+    }
---
+++ packages/heatgraphy/heatgraphy-0.2.1-py3-none-any.whl/heatgraphy/plotter/bar.py
@@ -0,0 +1,248 @@
+from typing import Callable, Mapping
+
+import numpy as np
+import pandas as pd
+from legendkit import CatLegend
+from matplotlib import pyplot as plt
+from matplotlib.axes import Axes
+
+from .base import StatsBase
+from ..utils import ECHARTS16
+
+
+def simple_bar(data,
+               ax: Axes = None,
+               orient="v",
+               width=.8,
+               show_value=True,
+               fmt=None,
+               label_pad=2,
+               text_props=None,
+               **kwargs
+               ):
+    if ax is None:
+        ax = plt.gca()
+    if text_props is None:
+        text_props = {}
+    bar = ax.bar if orient == "v" else ax.barh
+    bars = bar(np.arange(0, len(data)) + 0.5, data, width, **kwargs)
+    if show_value:
+        ax.bar_label(bars, data, fmt=fmt,
+                     padding=label_pad,
+                     **text_props)
+    return ax
+
+
+def _fmt_func(v):
+    if v != 0:
+        return v
+    else:
+        return ""
+
+
+class Numbers(StatsBase):
+    """Show numbers in bar plot
+
+    Parameters
+    ----------
+    data : np.ndarray
+        1D data
+    width : float
+        The width of bar
+    color : color
+        The color of bar
+    show_value : bool
+        Whether to show value on the bar
+    fmt : str
+        Format the value show on the bar
+    label : str
+        The label of the plot
+    label_pad : float
+        The spacing between label and the plot
+    props : dict
+        See :class:`matplotlib.text.Text`
+    
+    """
+
+    def __init__(self, data, width=.7, color="C0", orient=None,
+                 show_value=True, fmt=None, label=None, label_pad=2.,
+                 props=None, **kwargs):
+        self.data = self.data_validator(data, target="1d")
+        self.width = width
+        self.color = color
+        self.show_value = show_value
+        self.axis_label = label
+        self.fmt = fmt
+        self.label_pad = label_pad
+        self.props = props if props is not None else {}
+        self.options = kwargs
+        self.bars = None
+        self.orient = orient
+
+    def render_ax(self, ax: Axes, data):
+        orient = self.get_orient()
+        bar = ax.bar if orient == "v" else ax.barh
+        if orient == "h":
+            data = data[::-1]
+        self.bars = bar(np.arange(0, len(data)) + 0.5, data,
+                        self.width, color=self.color, **self.options)
+        if orient == "v":
+            ax.set_xlim(0, len(data))
+        else:
+            ax.set_ylim(0, len(data))
+        if self.side == "left":
+            ax.invert_xaxis()
+
+        if self.show_value:
+            ax.bar_label(self.bars, data, fmt=self.fmt,
+                         padding=self.label_pad,
+                         **self.props)
+
+
+# TODO: Not fully implemented
+#       Align the axis lim
+
+
+class StackBar(StatsBase):
+    """Stacked Bar
+
+
+     Parameters
+     ----------
+     data : np.ndarray, pd.DataFrame
+        2D data, index of dataframe is used as the name of items.
+     items : list of str
+        The name of items.
+     colors : list of colors, mapping of (item, color)
+        The colors of the bar for each item.
+
+
+
+    Examples
+    --------
+
+    .. plot::
+        :context: close-figs
+
+        >>> from heatgraphy.plotter import StackBar
+        >>> stack_data = pd.DataFrame(data=np.random.randint(0, 10, (5, 3)),
+        ...                           index=list("abcde"))
+        >>> _, ax = plt.subplots()
+        >>> StackBar(stack_data).render(ax)
+
+
+    You may find the text is too big for a bar to display on, to not display
+    certain value.
+
+    .. plot::
+        :context: close-figs
+
+        >>> cut_off = lambda v: v if v > 2 else ""
+        >>> _, ax = plt.subplots()
+        >>> StackBar(stack_data, show_value=cut_off).render(ax)
+
+     """
+
+    def __init__(self, data,
+                 items=None, colors=None,
+                 orient=None,
+                 show_value=False,
+                 value_loc="center",
+                 width=.5,
+                 value_size=6,
+                 fmt=None,
+                 props=None,
+                 label=None,
+                 legend_kws=None,
+                 **kwargs,
+                 ):
+        # TODO: Support bare bone numpy array as input
+        item_names = None
+        if isinstance(data, pd.DataFrame):
+            item_names = data.index
+            data = data.to_numpy()
+        data = self.data_validator(data, target="2d")
+
+        if items is not None:
+            item_names = items
+
+        if colors is None:
+            bar_colors = ECHARTS16
+        else:
+            if isinstance(colors, Mapping):
+                if item_names is None:
+                    raise ValueError("Please provide the name of each item "
+                                     "before assigning color.")
+                bar_colors = [colors[name] for name in item_names]
+            else:
+                bar_colors = colors
+
+        fmt_func = None
+        if isinstance(show_value, Callable):
+            fmt_func = show_value
+            show_value = True
+        else:
+            if show_value:
+                fmt_func = _fmt_func
+
+        self.data = data
+        self.labels = item_names
+        self.bar_colors = bar_colors
+        self.show_value = show_value
+        self.fmt_func = fmt_func
+        self.width = width
+        self.value_size = value_size
+        self.fmt = "%g" if fmt is None else fmt
+        self.kwargs = kwargs
+        self.orient = orient
+        self.axis_label = label
+
+        props = {} if props is None else props
+        value_props = dict(label_type=value_loc)
+        value_props.update(props)
+        self.props = value_props
+        self._legend_kws = dict(title=self.axis_label, size=1)
+        if legend_kws is not None:
+            self._legend_kws.update(legend_kws)
+
+    ''''''
+
+    def get_legends(self):
+        if self.labels is not None:
+            return CatLegend(colors=self.bar_colors, labels=self.labels,
+                             **self._legend_kws)
+
+    def render_ax(self, ax, data):
+        orient = self.get_orient()
+        bar = ax.bar if orient == "v" else ax.barh
+
+        lim = data.shape[1]
+        if orient == "h":
+            ax.set_ylim(0, lim)
+        else:
+            ax.set_xlim(0, lim)
+        if self.side == "left":
+            ax.invert_xaxis()
+
+        locs = np.arange(0, lim) + 0.5
+        bottom = np.zeros(lim)
+
+        # reverse to make the order more visually intuitive
+        if self.labels is not None:
+            labels = self.labels[::-1]
+        else:
+            labels = [None for _ in range(len(data))]
+        colors = self.bar_colors[:len(data)][::-1]
+        for ix, row in enumerate(data[::-1]):
+            bars = bar(locs, row, self.width, bottom,
+                       fc=colors[ix],
+                       label=labels[ix], **self.kwargs)
+            bottom += row
+
+            display_value = row
+            if self.fmt_func is not None:
+                display_value = [self.fmt_func(i) for i in row]
+
+            if self.show_value:
+                ax.bar_label(bars, display_value, fmt=self.fmt,
+                             **self.props)
