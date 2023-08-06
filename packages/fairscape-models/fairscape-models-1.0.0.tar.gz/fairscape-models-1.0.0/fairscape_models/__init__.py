7872a4a2296732de4945caeac4e78b81d84908ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:32:54 +0000

    Add package fairscape-models/1.0.0/fairscape-models-1.0.0.tar.gz

---
+++ packages/fairscape-models/fairscape-models-1.0.0.tar.gz/fairscape-models-1.0.0/fairscape_models/__init__.py
@@ -0,0 +1,9 @@
+from models.dataset import Dataset
+from models.computation import Computation
+from models.software import Software
+
+__all__ = [
+    'dataset',
+    'computation',
+    'software'
+]
