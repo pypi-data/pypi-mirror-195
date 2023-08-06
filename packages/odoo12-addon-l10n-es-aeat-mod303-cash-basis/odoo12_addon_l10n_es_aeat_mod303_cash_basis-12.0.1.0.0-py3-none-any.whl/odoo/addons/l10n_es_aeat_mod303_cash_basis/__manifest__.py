0fec583fc16985a1665065a2f5912d7c5e90356f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:01 +0000

    Add package odoo12-addon-l10n-es-aeat-mod303-cash-basis/12.0.1.0.0/odoo12_addon_l10n_es_aeat_mod303_cash_basis-12.0.1.0.0-py3-none-any.whl

---
+++ packages/odoo12-addon-l10n-es-aeat-mod303-cash-basis/odoo12_addon_l10n_es_aeat_mod303_cash_basis-12.0.1.0.0-py3-none-any.whl/odoo/addons/l10n_es_aeat_mod303_cash_basis/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2018 Tecnativa - Pedro M. Baeza
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0
+{
+    "name": "AEAT modelo 303 - Extensión para criterio de caja",
+    "version": "12.0.1.0.0",
+    'category': "Accounting & Finance",
+    'author': "Tecnativa,"
+              "Odoo Community Association (OCA)",
+    'website': "https://github.com/OCA/l10n-spain",
+    "license": "AGPL-3",
+    "depends": [
+        "l10n_es_aeat_mod303",
+    ],
+    "data": [
+        "data/aeat_export_mod303_2018_data.xml",
+        "data/tax_code_map_mod303_data.xml",
+        "views/mod303_view.xml",
+    ],
+    'installable': True,
+}
