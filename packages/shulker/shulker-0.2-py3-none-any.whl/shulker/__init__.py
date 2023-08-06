ea43bd8b86a4da78876faa44b027eee3306a51de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:44:19 +0000

    Add package shulker/0.2/shulker-0.2-py3-none-any.whl

---
+++ packages/shulker/shulker-0.2-py3-none-any.whl/shulker/__init__.py
@@ -0,0 +1,17 @@
+# Classes
+from .server.connect import RconClient as connect
+from .server.create import DockerInstance as create
+from .components import *
+from .functions import *
+
+# Functions
+
+from .functions.set_text import set_text, _set_text
+from .functions.set_zone import set_zone, _set_zone
+from .functions.set_block import set_block, _set_block
+from .functions.set_image import set_image, _set_image, print_palette
+
+# Objects
+from .server.singleton import singleton
+
+post = singleton.post
