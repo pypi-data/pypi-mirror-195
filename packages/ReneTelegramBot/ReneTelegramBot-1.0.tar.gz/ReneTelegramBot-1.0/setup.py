7ba977152848f1b6d71fce96c5cd8bd3d892be80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:03 +0000

    Add package ReneTelegramBot/1.0/ReneTelegramBot-1.0.tar.gz

---
+++ packages/ReneTelegramBot/ReneTelegramBot-1.0.tar.gz/ReneTelegramBot-1.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='ReneTelegramBot',
+    version='1.0',
+    packages=['ReneTelegramBot', 'ReneTelegramBot.Clases', 'ReneTelegramBot.Utiles'],
+    description="genial",
+    author="René Lázaro Collado Arteaga",
+    author_email="renearteaga261998@gmail.com",
+    license="GPLv3",
+    include_package_data=True,
+    classifiers = ["Programming Language :: Python :: 3",
+	"License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+	"Development Status :: 4 - Beta", "Intended Audience :: Developers",
+	"Operating System :: OS Independent"],
+    url="https://github.com/critBus/ReneTelegramBot",
+)
