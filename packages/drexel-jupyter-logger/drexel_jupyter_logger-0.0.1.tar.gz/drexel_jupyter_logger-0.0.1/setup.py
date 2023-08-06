d48a3f817de902df52bb273cbfe519b305b77b0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:28 +0000

    Add package drexel-jupyter-logger/0.0.1/drexel_jupyter_logger-0.0.1.tar.gz

---
+++ packages/drexel-jupyter-logger/drexel_jupyter_logger-0.0.1.tar.gz/drexel_jupyter_logger-0.0.1/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name = "drexel_jupyter_logger",
+    version= "0.0.1",
+    author = "Joshua C. Agar",
+    description= "drexel jupyter logger package",
+    packages=["drexel_jupyter_logger"],
+    install_requires=["cryptography"]
+)
