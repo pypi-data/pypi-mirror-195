0036cbf6a81c3d055fd4809ffc7696c0e114d867
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:11:10 +0000

    Add package canisius-cyb600-mcmahan-lab1/1.0.4/canisius_cyb600_mcmahan_lab1-1.0.4-py3-none-any.whl

---
+++ packages/canisius-cyb600-mcmahan-lab1/canisius_cyb600_mcmahan_lab1-1.0.4-py3-none-any.whl/canisius_cyb600_example/main.py
@@ -0,0 +1,39 @@
+
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import time
+
+hostName = "localhost"
+serverPort = 8080
+
+from datetime import datetime
+
+# datetime object containing current date and time
+now = datetime.now()
+
+print("now =", now)
+
+# dd/mm/YY H:M:S
+dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+print("date and time =", dt_string)
+class MyServer(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(bytes("<html><head><title>https://pythonbasics.org</title></head>", "utf-8"))
+        self.wfile.write(bytes("<p>Request: %s</p>" % self.path, "utf-8"))
+        self.wfile.write(bytes("The current time is: ", "utf-8"))
+        self.wfile.write(bytes(now.strftime("%d/%m/%Y %H:%M:%S"), "utf-8"))
+        self.wfile.write(bytes("</body></html>", "utf-8"))
+
+if __name__ == "__main__":
+    webServer = HTTPServer((hostName, serverPort), MyServer)
+    print("Server started http://%s:%s" % (hostName, serverPort))
+
+    try:
+        webServer.serve_forever()
+    except KeyboardInterrupt:
+        pass
+
+    webServer.server_close()
+    print("Server stopped.")
