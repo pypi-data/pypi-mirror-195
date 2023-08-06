0c9dbf69ded00378ad5ad25a6f74b2414540e9cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:09:11 +0000

    Add package yyxx-game-pkg-dev/2023.3.7.1/yyxx_game_pkg_dev-2023.3.7.1-py3-none-any.whl

---
+++ packages/yyxx-game-pkg-dev/yyxx_game_pkg_dev-2023.3.7.1-py3-none-any.whl/yyxx_game_pkg_dev/temp/hello.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# @Author   : KaiShin
+# @Time     : 2023/3/2
+
+def hello():
+    print("this is a helper function")
+
+
+def say_hi():
+    print("this is a say hi function")
+
+
+def foo():
+    print("this is a foo function")
