c60b622c81be14934f6ae1df594a01f86ce83d73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:09:32 +0000

    Add package beat.core/1.13.5/beat.core-1.13.5.zip

---
+++ packages/beat.core/beat.core-1.13.5.zip/beat.core-1.13.5/beat/core/prototypes/algorithm.py
@@ -0,0 +1,5 @@
+class Algorithm:
+    def process(self, inputs, data_loaders, outputs):
+        # TODO: Implement this algorithm
+
+        return True
---
+++ packages/beat.core/beat.core-1.13.5.zip/beat.core-1.13.5/beat/core/prototypes/plotter.py
@@ -0,0 +1,6 @@
+class Plotter:
+    def process(self, input):
+        # TODO: Implement this plotter
+
+        # return image as a string
+        pass
