e04c827538302b59a8cddb48fe408a17ff03040e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:07:53 +0000

    Add package moana-golmschenk/0.1.0/moana_golmschenk-0.1.0.tar.gz

---
+++ packages/moana-golmschenk/moana_golmschenk-0.1.0.tar.gz/moana_golmschenk-0.1.0/setup.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['moana',
+ 'moana.corner',
+ 'moana.david_bennett_fit',
+ 'moana.dbc',
+ 'moana.external_format_io',
+ 'moana.viewer']
+
+package_data = \
+{'': ['*'], 'moana': ['stylelib/*']}
+
+install_requires = \
+['bokeh>=2.3.1,<3.0.0',
+ 'file-read-backwards>=2.0.0,<3.0.0',
+ 'scipy>=1.6.2,<2.0.0',
+ 'tabulate>=0.8.9,<0.9.0']
+
+setup_kwargs = {
+    'name': 'moana-golmschenk',
+    'version': '0.1.0',
+    'description': '',
+    'long_description': 'None',
+    'author': 'Greg Olmschenk',
+    'author_email': 'golmschenk@gmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.9,<3.10',
+}
+
+
+setup(**setup_kwargs)
