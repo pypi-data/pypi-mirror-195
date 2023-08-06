3237162bb0d8f14fdb47692004f3d3ed323c515d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:47:01 +0000

    Add package energyplus-idd-idf-utilities/0.87/energyplus_idd_idf_utilities-0.87.tar.gz

---
+++ packages/energyplus-idd-idf-utilities/energyplus_idd_idf_utilities-0.87.tar.gz/energyplus_idd_idf_utilities-0.87/setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+import codecs
+import os
+import setuptools
+
+from energyplus_iddidf import __version__
+
+
+this_dir = os.path.abspath(os.path.dirname(__file__))
+with codecs.open(os.path.join(this_dir, 'README.md'), encoding='utf-8') as i_file:
+    long_description = i_file.read()
+
+setuptools.setup(
+    name='energyplus_idd_idf_utilities',
+    version=__version__,
+    packages=['energyplus_iddidf'],
+    description='EnergyPlus idd/idf manipulation in Python.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/myoldmopar/py-idd-idf',
+    author='Edwin Lee',
+    author_email='leeed2001@gmail.com',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Topic :: Utilities',
+        'License :: Public Domain',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ],
+    license='UnlicensedForNow',
+    install_requires=[],
+    entry_points={
+        'console_scripts': ['energyplus_idd_idf=energyplus_iddidf.cli:main_cli']
+    }
+)
