7225b6818fefe33571243ddacba0c48efabe02db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:55 +0000

    Add package okome-lib/1.1.2/okome-lib-1.1.2.tar.gz

---
+++ packages/okome-lib/okome-lib-1.1.2.tar.gz/okome-lib-1.1.2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="okome-lib",
+    version="1.1.2",
+    install_requires=["requests"],  
+    description="m.kuku.lu wrapper",
+    long_description=open("README.txt").read(),
+    url="https://github.com/AntiTikTok/Okome-Lib",
+)
