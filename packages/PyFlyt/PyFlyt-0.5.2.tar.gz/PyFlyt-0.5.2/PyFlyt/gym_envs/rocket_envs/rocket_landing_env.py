2d4623626592c094b5ae9d542346d2b8f8c12e43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:17:12 +0000

    Add package PyFlyt/0.5.2/PyFlyt-0.5.2.tar.gz

---
+++ packages/PyFlyt/PyFlyt-0.5.2.tar.gz/PyFlyt-0.5.2/PyFlyt/gym_envs/rocket_envs/rocket_landing_env.py
@@ -0,0 +1,177 @@
+"""Rocket Landing Environment."""
+from __future__ import annotations
+
+import os
+
+import numpy as np
+from gymnasium.spaces import Box
+
+from .rocket_base_env import RocketBaseEnv
+
+
+class RocketLandingEnv(RocketBaseEnv):
+    """Rocket Landing Environment.
+
+    Actions are finlet_x, finlet_z, roll, booster ignition, throttle, booster gimbal x, booster gimbal z
+
+    The goal is to land the rocket on the landing pad
+
+    Reward:
+        -100 for collisions or out of bounds,
+        -0.1 otherwise
+    """
+
+    def __init__(
+        self,
+        ceiling: float = 300.0,
+        max_displacement: float = 300.0,
+        max_duration_seconds: float = 10.0,
+        angle_representation: str = "quaternion",
+        agent_hz: int = 40,
+        render_mode: None | str = None,
+    ):
+        """__init__.
+
+        Args:
+            ceiling (float): the absolute ceiling of the flying area
+            max_displacement (float): the maximum horizontal distance the rocket can go
+            max_duration_seconds (float): maximum simulatiaon time of the environment
+            angle_representation (str): can be "euler" or "quaternion"
+            agent_hz (int): looprate of the agent to environment interaction
+            render_mode (None | str): can be "human" or None
+        """
+        super().__init__(
+            ceiling=ceiling,
+            max_displacement=max_displacement,
+            max_duration_seconds=max_duration_seconds,
+            angle_representation=angle_representation,
+            agent_hz=agent_hz,
+            render_mode=render_mode,
+        )
+
+        """GYMNASIUM STUFF"""
+        low = self.combined_space.low
+        low = np.concatenate((low, [0.0]))
+        high = self.combined_space.high
+        high = np.concatenate((high, [1.0]))
+        self.observation_space = Box(low=low, high=high, dtype=np.float64)
+
+        # the landing pad
+        file_dir = os.path.dirname(os.path.realpath(__file__))
+        self.targ_obj_dir = os.path.join(file_dir, "../../models/landing_pad.urdf")
+
+    def reset(self, seed=None, options=None):
+        """Resets the environment.
+
+        Args:
+            seed: int
+            options: None
+        """
+        options = dict(randomize_drop=True)
+
+        super().begin_reset(seed, options)
+
+        # impart some sidewards velocity
+        start_lin_vel = self.np_random.uniform(-5.0, 5.0, size=(3,))
+        start_ang_vel = self.np_random.uniform(-0.5, 0.5, size=(3,))
+        self.env.resetBaseVelocity(self.env.drones[0].Id, start_lin_vel, start_ang_vel)
+
+        # reset the tracked parameters
+        self.previous_ang_vel = np.zeros((3,))
+        self.previous_lin_vel = np.zeros((3,))
+        self.landing_pad_contact = 0.0
+
+        # randomly generate the target landing location
+        theta = self.np_random.uniform(0.0, 2.0 * np.pi)
+        distance = self.np_random.uniform(0.0, 0.1 * self.max_displacement)
+        target = np.array([np.cos(theta), np.sin(theta), 0.1]) * distance
+        self.landing_pad_id = self.env.loadURDF(
+            self.targ_obj_dir,
+            basePosition=target,
+            useFixedBase=True,
+        )
+
+        super().end_reset(seed, options)
+
+        return self.state, self.info
+
+    def compute_state(self):
+        """Computes the state of the current timestep.
+
+        This returns the observation.
+        - ang_vel (vector of 3 values)
+        - ang_pos (vector of 3/4 values)
+        - lin_vel (vector of 3 values)
+        - lin_pos (vector of 3 values)
+        - previous_action (vector of 4 values)
+        - auxiliary information (vector of 4 values)
+        """
+        ang_vel, ang_pos, lin_vel, lin_pos, quarternion = super().compute_attitude()
+        aux_state = super().compute_auxiliary()
+
+        # combine everything
+        if self.angle_representation == 0:
+            self.state = np.array(
+                [
+                    *ang_vel,
+                    *ang_pos,
+                    *lin_vel,
+                    *lin_pos,
+                    *self.action,
+                    *aux_state,
+                    self.landing_pad_contact,
+                ]
+            )
+        elif self.angle_representation == 1:
+            self.state = np.array(
+                [
+                    *ang_vel,
+                    *quarternion,
+                    *lin_vel,
+                    *lin_pos,
+                    *self.action,
+                    *aux_state,
+                    self.landing_pad_contact,
+                ]
+            )
+
+    def compute_term_trunc_reward(self):
+        """Computes the termination, truncation, and reward of the current timestep."""
+        super().compute_base_term_trunc_reward(
+            collision_ignore_mask=[self.env.drones[0].Id, self.landing_pad_id]
+        )
+
+        # check if we touched the landing pad
+        if not self.env.collision_array[self.env.drones[0].Id, self.landing_pad_id]:
+            # track the velocity for the next time
+            self.previous_ang_vel = self.state[0:3]
+            self.previous_lin_vel = self.state[6:9]
+            self.landing_pad_contact = 0.0
+            return
+
+        # update that we touched the landing pad
+        self.landing_pad_contact = 1.0
+
+        # if collision has more than 0.35 rad/s angular velocity, we dead
+        # truthfully, if collision has more than 0.55 m/s linear acceleration, we dead
+        # number taken from here:
+        # https://cosmosmagazine.com/space/launch-land-repeat-reusable-rockets-explained/
+        # but doing so is kinda impossible for RL, so I've lessened the requirement to 1.0
+        if (
+            np.linalg.norm(self.previous_ang_vel) > 0.35
+            or np.linalg.norm(self.previous_lin_vel) > 1.0
+        ):
+            self.reward = -100.0
+            self.info["fatal_collision"] = True
+            self.termination |= True
+            return
+
+        # if our both velocities are less than 0.02 m/s, we LANDED!
+        if (
+            np.linalg.norm(self.previous_ang_vel) < 0.02
+            or np.linalg.norm(self.previous_lin_vel) < 0.02
+        ):
+            self.reward = 100.0
+            self.info["env_complete"] = True
+            self.termination |= True
+            return
---
+++ packages/PyFlyt/PyFlyt-0.5.2.tar.gz/PyFlyt-0.5.2/readme.md
@@ -0,0 +1,146 @@
+# PyFlyt - UAV Flight Simulator Gymnasium Environments for Reinforcement Learning Research
+
+This is a library for testing reinforcement learning algorithms on UAVs.
+This repo is still under development.
+We are also actively looking for users and developers, if this sounds like you, don't hesitate to get in touch!
+
+PyFlyt currently supports two separate UAV platforms:
+- QuadX UAV
+  - Inspired by [the original pybullet drones by University of Toronto's Dynamic Systems Lab](https://github.com/utiasDSL/gym-pybullet-drones)
+  - Quadrotor UAV in the X configuration
+  - Actual full cascaded PID flight controller implementations for each drone.
+  - Actual motor RPM simulation using first order differential equation.
+  - Modular control structure
+  - For developers - 8 implemented flight modes that use tuned cascaded PID flight controllers, available in `PyFlyt/core/drones/quadx.py`.
+
+- Fixedwing UAV
+  - Flight model designed for a small fixed wing UAV (< 10 Kg)
+  - Assumes a conventional tube and wing design
+  - Single puller propeller with thrust line passing through CG
+  - Aerofoil characteristics derived from the paper: [*Real-time modeling of agile fixed-wing UAV aerodynamics*](https://ieeexplore.ieee.org/document/7152411)
+
+## Table of Contents
+- [Table of Contents](#table-of-contents)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Environments](#environments)
+  - [`PyFlyt/QuadX-Hover-v0`](#pyflytquadx-hover-v0)
+  - [`PyFlyt/QuadX-Waypoints-v0`](#pyflytquadx-waypoints-v0)
+  - [`PyFlyt/Fixedwing-Waypoints-v0`](#pyflytfixedwing-waypoints-v0)
+- [Non-Gymnasium examples](#non-gymnasium-examples)
+  - [Simulation Only](#simulation-only)
+    - [`sim_single.py`](#sim_singlepy)
+    - [`sim_swarm.py`](#sim_swarmpy)
+    - [`sim_cube.py`](#sim_cubepy)
+  - [Hardware Only](#hardware-only)
+    - [`fly_single.py`](#fly_singlepy)
+    - [`fly_swarm.py`](#fly_swarmpy)
+  - [Simulation or Hardware](#simulation-or-hardware)
+    - [`sim_n_fly_single.py`](#sim_n_fly_singlepy)
+    - [`sim_n_fly_multiple.py`](#sim_n_fly_multiplepy)
+    - [`sim_n_fly_cube_from_scratch.py`](#sim_n_fly_cube_from_scratchpy)
+
+
+## Installation
+
+```
+pip3 install pyflyt
+```
+
+## Usage
+
+Usage is similar to any other Gymnasium and (soon) PettingZoo environment:
+
+```py
+import gymnasium
+import PyFlyt.gym_envs
+
+env = gymnasium.make("PyFlyt/QuadX-Hover-v0")
+
+# omit the below line to remove rendering and let
+# the simulation go as fast as possible
+env.render()
+obs = env.reset()
+
+done = False
+while not done:
+    observation, reward, termination, truncation, info = env.step(env.action_space.sample())
+```
+
+## Environments
+
+### `PyFlyt/QuadX-Hover-v0`
+
+A simple environment where an agent can learn to hover.
+The environment ends when either the quadcopter collides with the ground or exits the permitted flight dome.
+
+```py
+env = gymnasium.make(
+  "PyFlyt/QuadX-Hover-v0",
+  flight_dome_size: float = 3.0,
+  max_duration_seconds: float = 10.0,
+  angle_representation: str = "quaternion",
+  agent_hz: int = 40,
+  render_mode: None | str = None,
+)
+```
+
+> `angle_representation` can be either `"quaternion"` or `"euler"`.
+>
+> `render_mode` can be either `"human"` or `None`.
+
+### `PyFlyt/QuadX-Waypoints-v0`
+
+A simple environment where the goal is to fly the quadrotor to a collection of random waypoints in space within the permitted flight dome.
+The environment ends when either the quadrotor collides with the ground or exits the permitted flight dome.
+
+```py
+env = gymnasium.make(
+  "PyFlyt/QuadX-Waypoints-v0",
+  sparse_reward: bool = False,
+  num_targets: int = 4,
+  use_yaw_targets: bool = False,
+  goal_reach_distance: float = 0.2,
+  goal_reach_angle: float = 0.1,
+  flight_dome_size: float = 5.0,
+  max_duration_seconds: float = 10.0,
+  angle_representation: str = "quaternion",
+  agent_hz: int = 30,
+  render_mode: None | str = None,
+)
+```
+
+> `angle_representation` can be either `"quaternion"` or `"euler"`.
+>
+> `render_mode` can be either `"human"` or `None`.
+
+<p align="center">
+    <img src="https://github.com/jjshoots/PyFlyt/blob/master/readme_assets/quadx_waypoint.gif?raw=true" width="500px"/>
+</p>
+
+### `PyFlyt/Fixedwing-Waypoints-v0`
+
+A simple environment where the goal is to fly a fixedwing aircraft towards set of random waypionts in space within the permitted flight dome.
+The environment ends when either the aircraft collides with the ground or exits the permitted flight dome.
+
+```py
+env = gymnasium.make(
+  "PyFlyt/Fixedwing-Waypoints-v0",
+  sparse_reward: bool = False,
+  num_targets: int = 4,
+  goal_reach_distance: float = 2.0,
+  flight_dome_size: float = 100.0,
+  max_duration_seconds: float = 120.0,
+  angle_representation: str = "quaternion",
+  agent_hz: int = 30,
+  render_mode: None | str = None,
+)
+```
+
+> `angle_representation` can be either `"quaternion"` or `"euler"`.
+>
+> `render_mode` can be either `"human"` or `None`.
+
+<p align="center">
+    <img src="https://github.com/jjshoots/PyFlyt/blob/master/readme_assets/fixedwing_waypoint.gif?raw=true" width="500px"/>
+</p>
