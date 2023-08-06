1e55cbc9f51e78c7242a676f4f3a5ef3d85c4ca8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:54 +0000

    Add package graph-envs/0.0.18/graph_envs-0.0.18-py3-none-any.whl

---
+++ packages/graph-envs/graph_envs-0.0.18-py3-none-any.whl/graph_envs/__init__.py
@@ -0,0 +1,40 @@
+
+import gymnasium as gym
+from gymnasium.envs.registration import register
+
+name = "graph_envs"
+
+## For local testing and running:
+
+# register(
+#     id='ShortestPath-v0',
+#     entry_point='src.graph_envs.shortest_path:ShortestPathEnv',
+# )
+
+# register(
+#     id='SteinerTree-v0',
+#     entry_point='src.graph_envs.steiner_tree:SteinerTreeEnv',
+# )
+
+# register(
+#     id='MaxIndependentSet-v0',
+#     entry_point='src.graph_envs.max_independent_set:MaxIndependentSet',
+# )
+
+
+## For packaging:
+
+register(
+    id='ShortestPath-v0',
+    entry_point='graph_envs.shortest_path:ShortestPathEnv',
+)
+
+register(
+    id='SteinerTree-v0',
+    entry_point='graph_envs.steiner_tree:SteinerTreeEnv',
+)
+
+register(
+    id='MaxIndependentSet-v0',
+    entry_point='graph_envs.max_independent_set:MaxIndependentSet',
+)
