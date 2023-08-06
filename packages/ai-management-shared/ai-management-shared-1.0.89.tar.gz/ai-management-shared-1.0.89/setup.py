3df1eee05472c29afa1f54930cab9b10424a2a1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:22 +0000

    Add package ai-management-shared/1.0.89/ai-management-shared-1.0.89.tar.gz

---
+++ packages/ai-management-shared/ai-management-shared-1.0.89.tar.gz/ai-management-shared-1.0.89/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+requires = [
+    "requests>=2.23.0",
+    "PyYAML>5.3",
+    "mergedict>=1.0.0"
+]
+
+setuptools.setup(
+    name="ai-management-shared",
+    version="1.0.89",
+    author="johnson",
+    author_email="",
+    description="A set of lightweight utilities",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=requires,
+    url="",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
