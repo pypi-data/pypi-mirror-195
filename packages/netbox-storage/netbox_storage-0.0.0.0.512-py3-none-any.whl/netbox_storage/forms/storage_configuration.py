7b27d921fe5dad978b78efe47784ec530553e83d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:15:32 +0000

    Add package netbox-storage/0.0.0.0.512/netbox_storage-0.0.0.0.512-py3-none-any.whl

---
+++ packages/netbox-storage/netbox_storage-0.0.0.0.512-py3-none-any.whl/netbox_storage/forms/storage_configuration.py
@@ -0,0 +1,78 @@
+from netbox.forms import (
+    NetBoxModelBulkEditForm,
+    NetBoxModelFilterSetForm,
+    NetBoxModelImportForm,
+    NetBoxModelForm,
+)
+from netbox_storage.models import StorageConfiguration
+from utilities.forms import (
+    CSVModelChoiceField,
+    DynamicModelChoiceField,
+)
+from virtualization.models import VirtualMachine
+
+
+class StorageConfigurationForm(NetBoxModelForm):
+    virtual_machine = DynamicModelChoiceField(
+        queryset=VirtualMachine.objects.all(),
+        help_text="Mapping between drive and virtual machine  e.g. vm-testinstall-01",
+    )
+
+    fieldsets = (
+        (
+            "Drive Cluster Config",
+            (
+                "virtual_machine",
+            ),
+        ),
+    )
+
+    class Meta:
+        model = StorageConfiguration
+
+        fields = (
+            "virtual_machine",
+        )
+
+
+class StorageConfigurationFilterForm(NetBoxModelFilterSetForm):
+    model = StorageConfiguration
+
+    virtual_machine = DynamicModelChoiceField(
+        queryset=VirtualMachine.objects.all(),
+        required=False
+    )
+
+
+class StorageConfigurationImportForm(NetBoxModelImportForm):
+    virtual_machine = CSVModelChoiceField(
+        queryset=VirtualMachine.objects.all(),
+        required=False,
+        to_field_name='name',
+        help_text='Required'
+    )
+
+    class Meta:
+        model = StorageConfiguration
+
+        fields = (
+            "virtual_machine",
+        )
+
+
+class StorageConfigurationBulkEditForm(NetBoxModelBulkEditForm):
+    model = StorageConfiguration
+
+    virtual_machine = CSVModelChoiceField(
+        queryset=VirtualMachine.objects.all(),
+        required=False,
+        to_field_name='name',
+        help_text='Required'
+    )
+
+    fieldsets = (
+        (
+            None,
+            ["virtual_machine"],
+        ),
+    )
