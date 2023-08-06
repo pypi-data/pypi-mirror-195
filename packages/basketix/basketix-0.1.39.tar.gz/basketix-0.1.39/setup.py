8fbdc7ce41ea14f1b987c8ac1fdda8b97dae658e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:44:00 +0000

    Add package basketix/0.1.39/basketix-0.1.39.tar.gz

---
+++ packages/basketix/basketix-0.1.39.tar.gz/basketix-0.1.39/setup.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['basketix',
+ 'basketix.entities',
+ 'basketix.handlers',
+ 'basketix.helpers',
+ 'basketix.models',
+ 'basketix.models.attributes',
+ 'basketix.tables']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['boto3>=1.26.69,<2.0.0',
+ 'isoweek>=1.3.3,<2.0.0',
+ 'python-dateutil>=2.8.2,<3.0.0',
+ 'pytz>=2022.7.1,<2023.0.0',
+ 'typing-extensions>=4.4.0,<5.0.0']
+
+setup_kwargs = {
+    'name': 'basketix',
+    'version': '0.1.39',
+    'description': '',
+    'long_description': '',
+    'author': 'tlartigau',
+    'author_email': 'thomas.lartigau@atinternet.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
