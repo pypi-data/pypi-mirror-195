1f4baaea0a7288cea23a2f295d0310704c0006ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:07:55 +0000

    Add package phd-utils/0.1.9/phd-utils-0.1.9.tar.gz

---
+++ packages/phd-utils/phd-utils-0.1.9.tar.gz/phd-utils-0.1.9/setup.py
@@ -0,0 +1,30 @@
+import subprocess
+import sys
+from setuptools import setup, find_packages
+
+# Define the required system package
+required_packages = ['openexr']
+
+# Use subprocess to check if the required package is installed
+try:
+    subprocess.check_call(['dpkg', '-s'] + required_packages,
+                          stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+except subprocess.CalledProcessError:
+    print('Error: the required system package ({}) is not installed. '
+          'Please install it manually.'.format(required_packages),
+          file=sys.stderr)
+    sys.exit(1)
+
+setup(
+    name='phd-utils',
+    version='0.1.9',
+    description='A collection of utilities for PhD students',
+    author='Henrique Weber',
+    packages=find_packages(),
+    install_requires=[
+        'skylibs',
+        'openexr',
+        'opencv-python',
+        'numpy',
+    ],
+)
