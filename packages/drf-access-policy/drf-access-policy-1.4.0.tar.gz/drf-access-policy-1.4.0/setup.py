a724778fe7c110321ab2f66bb0d480830008c4d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:15 +0000

    Add package drf-access-policy/1.4.0/drf-access-policy-1.4.0.tar.gz

---
+++ packages/drf-access-policy/drf-access-policy-1.4.0.tar.gz/drf-access-policy-1.4.0/setup.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+from codecs import open
+
+from setuptools import setup
+
+
+def readme():
+    with open("README.md", "r") as infile:
+        return infile.read()
+
+
+classifiers = [
+    # Pick your license as you wish (should match "license" above)
+    "License :: OSI Approved :: MIT License",
+    "Framework :: Django",
+    "Framework :: Django :: 2.0",
+    "Framework :: Django :: 3.0",
+    "Framework :: Django :: 3.1",
+    "Framework :: Django :: 3.2",
+    "Programming Language :: Python :: 3.5",
+    "Programming Language :: Python :: 3.6",
+    "Programming Language :: Python :: 3.7",
+    "Programming Language :: Python :: 3.8",
+    "Programming Language :: Python :: 3.9",
+]
+setup(
+    name="drf-access-policy",
+    version="1.4.0",
+    description="Declarative access policies/permissions modeled after AWS' IAM policies.",
+    author="Robert Singer",
+    author_email="robertgsinger@gmail.com",
+    packages=["rest_access_policy"],
+    package_data={"rest_access_policy": ["py.typed"]},
+    url="https://github.com/rsinger86/drf-access-policy",
+    license="MIT",
+    keywords="django restframework drf access policy authorization declaritive",
+    long_description=readme(),
+    classifiers=classifiers,
+    long_description_content_type="text/markdown",
+    install_requires=["pyparsing", "djangorestframework"],
+)
