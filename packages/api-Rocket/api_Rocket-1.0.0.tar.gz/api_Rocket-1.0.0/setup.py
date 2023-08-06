e52d9931c20e0008d1c0575493c730ac92509855
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:46 +0000

    Add package api-Rocket/1.0.0/api_Rocket-1.0.0.tar.gz

---
+++ packages/api-Rocket/api_Rocket-1.0.0.tar.gz/api_Rocket-1.0.0/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+	name='api_Rocket',
+	version='1.0.0',
+	author='Redpiar',
+	author_email='Regeonwix@gmail.com',
+	description='api ton rocket telegram)',
+	packages=['api_Rocket'],
+	classifiers=[
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+	],
+	python_requires='>=3.6',
+)
