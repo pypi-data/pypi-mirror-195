dcb346d22a442bde7270de864712810b8b23a192
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:11:48 +0000

    Add package ixi-algs/0.2/ixi_algs-0.2.tar.gz

---
+++ packages/ixi-algs/ixi_algs-0.2.tar.gz/ixi_algs-0.2/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(name='ixi_algs',
+      version='0.2',
+      description='ixi_algs',
+      packages=find_packages(),
+      author_email='zxcbbtihs37docxh@yandex.ru',
+      zip_safe=False)
