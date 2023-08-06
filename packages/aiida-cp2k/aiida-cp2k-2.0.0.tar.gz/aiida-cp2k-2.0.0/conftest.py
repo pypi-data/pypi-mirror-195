1d554b5086fbd5957c5c35cdecd3eed7d81be034
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:31 +0000

    Add package aiida-cp2k/2.0.0/aiida-cp2k-2.0.0.tar.gz

---
+++ packages/aiida-cp2k/aiida-cp2k-2.0.0.tar.gz/aiida-cp2k-2.0.0/conftest.py
@@ -0,0 +1,30 @@
+"""
+For pytest initialise a test database and profile
+"""
+import pytest
+
+pytest_plugins = ["aiida.manage.tests.pytest_fixtures"]  # pylint: disable=invalid-name
+
+
+@pytest.fixture(scope="function")
+def cp2k_code(aiida_local_code_factory):  # pylint: disable=unused-argument
+    return aiida_local_code_factory("cp2k", "cp2k")
+
+
+@pytest.fixture(scope="function", autouse=True)
+def clear_database(aiida_profile_clean):
+    """Automatically clear database in between tests."""
+
+
+# from https://stackoverflow.com/a/25188424
+# required for examples for optional features to show appropriate error messages
+def pytest_configure(config):  # pylint: disable=unused-argument
+    import sys
+
+    sys._called_from_test = True  # pylint: disable=protected-access
+
+
+def pytest_unconfigure(config):  # pylint: disable=unused-argument
+    import sys
+
+    del sys._called_from_test  # pylint: disable=protected-access
