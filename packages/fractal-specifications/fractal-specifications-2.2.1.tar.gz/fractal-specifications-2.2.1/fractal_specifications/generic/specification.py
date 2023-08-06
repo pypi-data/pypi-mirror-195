86e3e9f9ea7c0a2cf1b4f9d1536622d7c94da055
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:14:03 +0000

    Add package fractal-specifications/2.2.1/fractal-specifications-2.2.1.tar.gz

---
+++ packages/fractal-specifications/fractal-specifications-2.2.1.tar.gz/fractal-specifications-2.2.1/fractal_specifications/generic/specification.py
@@ -0,0 +1,110 @@
+from __future__ import annotations
+
+from abc import ABC, abstractmethod
+from enum import Enum
+from typing import Any, Collection, Iterator, Optional
+
+
+class Operators(str, Enum):
+    EQUALS = "equals"
+    IN = "in"
+    CONTAINS = "contains"
+    LESS_THAN = "lt"
+    LESS_THAN_EQUAL = "lte"
+    GREATER_THAN = "gt"
+    GREATER_THAN_EQUAL = "gte"
+
+
+def get_op_specs():
+    from fractal_specifications.generic import operators
+
+    return {
+        Operators.EQUALS: operators.EqualsSpecification,
+        Operators.IN: operators.InSpecification,
+        Operators.CONTAINS: operators.ContainsSpecification,
+        Operators.LESS_THAN: operators.LessThanSpecification,
+        Operators.LESS_THAN_EQUAL: operators.LessThanEqualSpecification,
+        Operators.GREATER_THAN: operators.GreaterThanSpecification,
+        Operators.GREATER_THAN_EQUAL: operators.GreaterThanEqualSpecification,
+    }
+
+
+def _parse_specification_item(field_op: str, value: Any) -> Optional[Specification]:
+    if "__" not in field_op:
+        return get_op_specs()[Operators.EQUALS](field_op, value)
+
+    field, operator = field_op.split("__")
+    for op, spec in get_op_specs().items():
+        if op == operator:
+            return spec(field, value)
+    return None
+
+
+def parse_specification(**kwargs) -> Iterator[Specification]:
+    for field_op, value in kwargs.items():
+        if spec := _parse_specification_item(field_op, value):
+            yield spec
+
+
+class Specification(ABC):
+    @abstractmethod
+    def is_satisfied_by(self, obj: Any) -> bool:
+        raise NotImplementedError
+
+    @abstractmethod
+    def to_collection(self) -> Collection:
+        raise NotImplementedError
+
+    def And(self, specification: "Specification") -> "Specification":
+        from fractal_specifications.generic.collections import AndSpecification
+
+        if isinstance(specification, AndSpecification):
+            return specification.And(self)
+        return AndSpecification([self, specification])
+
+    def Or(self, specification: "Specification") -> "Specification":
+        from fractal_specifications.generic.collections import OrSpecification
+
+        if isinstance(specification, OrSpecification):
+            return specification.Or(self)
+        return OrSpecification([self, specification])
+
+    def __and__(self, other):
+        return self.And(other)
+
+    def __or__(self, other):
+        return self.Or(other)
+
+    def __str__(self):
+        raise NotImplementedError
+
+    @staticmethod
+    def Not(specification: "Specification") -> "Specification":
+        from fractal_specifications.generic.operators import NotSpecification
+
+        return NotSpecification(specification)
+
+    @staticmethod
+    def parse(**kwargs):
+        specs = list(parse_specification(**kwargs))
+        if len(specs) > 1:
+            from fractal_specifications.generic.collections import AndSpecification
+
+            return AndSpecification(specs)
+        elif len(specs) == 1:
+            return specs[0]
+        return None
+
+
+class EmptySpecification(Specification):
+    def is_satisfied_by(self, obj: Any) -> bool:
+        return True
+
+    def to_collection(self) -> Collection:
+        return []
+
+    def __str__(self):
+        return self.__class__.__name__
+
+    def __eq__(self, other):
+        return isinstance(other, EmptySpecification)
---
+++ packages/fractal-specifications/fractal-specifications-2.2.1.tar.gz/fractal-specifications-2.2.1/tests/contrib/pandas/test_specifications.py
@@ -0,0 +1,63 @@
+from typing import Any, Collection
+
+import pandas as pd  # type: ignore
+import pytest  # type: ignore
+
+df = pd.DataFrame(
+    {
+        "id": [1, 2, 3, 4],
+        "name": [2, 3, 4, 5],
+        "field": [3, 4, 5, 6],
+    }
+)
+
+specifications = [
+    (None, None),
+    (pytest.lazy_fixture("equals_specification"), df[df.id == 1].to_dict()),  # type: ignore
+    (
+        pytest.lazy_fixture("or_specification"),  # type: ignore
+        (df[df.id == 1] | df[df.name == "test"]).to_dict(),
+    ),
+    (
+        pytest.lazy_fixture("and_specification"),  # type: ignore
+        (df[df.id == 1] & df[df.name == "test"]).to_dict(),
+    ),
+    (pytest.lazy_fixture("in_specification"), df[df.field.isin([1, 2, 3])].to_dict()),  # type: ignore
+    (pytest.lazy_fixture("less_than_specification"), df[df.id < 1].to_dict()),  # type: ignore
+    (pytest.lazy_fixture("less_than_equal_specification"), df[df.id <= 1].to_dict()),  # type: ignore
+    (pytest.lazy_fixture("greater_than_specification"), df[df.id > 1].to_dict()),  # type: ignore
+    (pytest.lazy_fixture("greater_than_equal_specification"), df[df.id >= 1].to_dict()),  # type: ignore
+    (pytest.lazy_fixture("empty_specification"), None),  # type: ignore
+]
+
+
+@pytest.mark.parametrize("specification, expected", specifications)
+def test_build(specification, expected):
+    from fractal_specifications.contrib.pandas.specifications import (
+        PandasSpecificationBuilder,
+    )
+
+    f = PandasSpecificationBuilder.build(specification)
+
+    assert f == expected or f(df).to_dict() == expected
+
+
+def test_specification_not_mapped():
+    from fractal_specifications.contrib.pandas.specifications import (
+        PandasSpecificationBuilder,
+        SpecificationNotMappedToPandas,
+    )
+    from fractal_specifications.generic.specification import Specification
+
+    class ErrorSpecification(Specification):
+        def is_satisfied_by(self, obj: Any) -> bool:
+            return False
+
+        def to_collection(self) -> Collection:
+            return []
+
+        def __str__(self):
+            return self.__class__.__name__
+
+    with pytest.raises(SpecificationNotMappedToPandas):
+        PandasSpecificationBuilder.build(ErrorSpecification())
