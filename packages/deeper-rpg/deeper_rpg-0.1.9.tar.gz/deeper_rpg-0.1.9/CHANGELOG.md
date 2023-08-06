d9af9b273ce3203ce031fbeddd683cf248cead97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:30:52 +0000

    Add package deeper-rpg/0.1.9/deeper_rpg-0.1.9.tar.gz

---
+++ packages/deeper-rpg/deeper_rpg-0.1.9.tar.gz/deeper_rpg-0.1.9/CHANGELOG.md
@@ -0,0 +1,11 @@
+# DeeperRPG release 0.1.9 (2023-02-28)
+
+## Bug fixes
+
+Copy imgui.ini settings from resources to cwd
+
+# DeeperRPG release 0.1.7 (2023-02-28)
+
+## Features
+
+Switched to Hatch from Poetry
---
+++ packages/deeper-rpg/deeper_rpg-0.1.9.tar.gz/deeper_rpg-0.1.9/deeper/settings/entity_settings.py
@@ -0,0 +1,28 @@
+from deeper.setting import SettingGroup, BoolSetting, StringSetting, Vec3Setting, Vec2Setting
+from .component import BlockSettings, SpriteVuSettings, SpriteAnimationSettings
+
+class ComponentsGroup(SettingGroup):
+    pass
+
+
+ComponentsGroup.setting_map = {
+    'Block': BlockSettings,
+    'SpriteVu': SpriteVuSettings,
+    'SpriteAnimation': SpriteAnimationSettings
+}
+
+class EntitySettings(SettingGroup):
+    pass
+
+
+EntitySettings.setting_map = {
+    '_abstract': BoolSetting,
+    'extends': StringSetting,
+    'category': StringSetting,
+    'description': StringSetting,
+    'components': ComponentsGroup,
+
+    'size': Vec3Setting,
+    'image': StringSetting,
+    'offset': Vec2Setting
+}
