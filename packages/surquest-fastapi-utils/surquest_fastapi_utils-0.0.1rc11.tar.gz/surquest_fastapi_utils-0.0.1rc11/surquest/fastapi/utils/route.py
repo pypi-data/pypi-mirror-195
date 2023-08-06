b3f4f2b07ef9ae3c2e7e68274dabe414e4a4c085
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:24:23 +0000

    Add package surquest-fastapi-utils/0.0.1rc11/surquest_fastapi_utils-0.0.1rc11.tar.gz

---
+++ packages/surquest-fastapi-utils/surquest_fastapi_utils-0.0.1rc11.tar.gz/surquest_fastapi_utils-0.0.1rc11/surquest/fastapi/utils/route.py
@@ -0,0 +1,19 @@
+from fastapi.openapi.docs import get_swagger_ui_html
+from fastapi.responses import FileResponse
+import os
+
+
+class Route(object):
+
+    @staticmethod
+    def get_favicon(
+            path: str = f"{os.getenv('HOME','/opt/project')}/app/static/favicon.ico"
+    ):
+        return FileResponse(path)
+
+    @staticmethod
+    def get_documentation(title: str = 'API Documentation'):
+        return get_swagger_ui_html(
+            openapi_url=F"{os.getenv('PATH_PREFIX','')}/openapi.json",
+            title=title
+        )
