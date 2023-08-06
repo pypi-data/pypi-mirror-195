188c3889d70c94a938a789bfc3e0c8b604749ffd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:23:01 +0000

    Add package fast-crossing/0.0.3/fast_crossing-0.0.3.tar.gz

---
+++ packages/fast-crossing/fast_crossing-0.0.3.tar.gz/fast_crossing-0.0.3/src/main.cpp
@@ -0,0 +1,40 @@
+// https://github.com/microsoft/vscode-cpptools/issues/9692
+#if __INTELLISENSE__
+#undef __ARM_NEON
+#undef __ARM_NEON__
+#endif
+#define _USE_MATH_DEFINES
+#include <cmath>
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#endif
+
+#include <Eigen/Core>
+
+#include <pybind11/eigen.h>
+#include <pybind11/iostream.h>
+#include <pybind11/pybind11.h>
+
+#include "fast_crossing.hpp"
+#include "pybind11_fast_crossing.hpp"
+#include "pybind11_flatbush.hpp"
+#include "pybind11_polyline_ruler.hpp"
+
+#define STRINGIFY(x) #x
+#define MACRO_STRINGIFY(x) STRINGIFY(x)
+
+namespace py = pybind11;
+using namespace pybind11::literals;
+
+PYBIND11_MODULE(fast_crossing, m)
+{
+    cubao::bind_fast_crossing(m);
+    cubao::bind_flatbush(m);
+    cubao::bind_polyline_ruler(m);
+
+#ifdef VERSION_INFO
+    m.attr("__version__") = MACRO_STRINGIFY(VERSION_INFO);
+#else
+    m.attr("__version__") = "dev";
+#endif
+}
