c236fff8cd4593d360c714a31568e37fa414f79e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:57 +0000

    Add package wizata-dsapi/0.0.27/wizata-dsapi-0.0.27.tar.gz

---
+++ packages/wizata-dsapi/wizata-dsapi-0.0.27.tar.gz/wizata-dsapi-0.0.27/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='wizata-dsapi',
+    version='0.0.27',
+    description='Wizata Data Science Toolkit',
+    author='Wizata S.A.',
+    author_email='info@wizata.com',
+    packages=['wizata_dsapi'],
+    install_requires=[
+        'flask',
+        'pandas',
+        'numpy',
+        'scikit-learn==1.2.1'
+    ]
+)
