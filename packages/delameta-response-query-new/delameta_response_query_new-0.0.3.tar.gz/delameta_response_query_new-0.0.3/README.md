fa43adac997dc6e2fc71579abb67bd77b989e9cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:26:08 +0000

    Add package delameta-response-query-new/0.0.3/delameta_response_query_new-0.0.3.tar.gz

---
+++ packages/delameta-response-query-new/delameta_response_query_new-0.0.3.tar.gz/delameta_response_query_new-0.0.3/README.md
@@ -0,0 +1,12 @@
+# Psycopg2 Query and response request
+## Introduction
+Package khusus query yang dugunakan internal
+
+## 0.0.1
+Test upload 
+
+## 0.0.2
+Bug untuk import module
+
+## 0.0.3
+Penamaan module
---
+++ packages/delameta-response-query-new/delameta_response_query_new-0.0.3.tar.gz/delameta_response_query_new-0.0.3/package_psycopg/__init__.py
@@ -0,0 +1,5 @@
+from src.create_pool_psycopg2 import create_connection_postgresql
+from src.query_psycopg2 import (action_additional_response, action_build_message_detail, action_check_existing_data, action_datatables_view, action_delete_data, action_generate_footers, action_generate_headers,
+                                action_insert_data, action_return_of_response, action_update_data, action_validate)
+
+
