0d7fc4ae39505544270f96473eda7b3b390245db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:11:28 +0000

    Add package cpanlp/1.2.65/cpanlp-1.2.65.tar.gz

---
+++ packages/cpanlp/cpanlp-1.2.65.tar.gz/cpanlp-1.2.65/cpanlp/corporate_law/entity/conglomerate/__init__.py
@@ -0,0 +1,3 @@
+from cpanlp.corporate_law.entity.conglomerate.associatecompany import *
+from cpanlp.corporate_law.entity.conglomerate.jointventure import *
+from cpanlp.corporate_law.entity.conglomerate.subsidiary import *
