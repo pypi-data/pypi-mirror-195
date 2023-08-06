bba4295c1ad985a706b474ea5d7f11d125d8adcd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:13:33 +0000

    Add package zibanu-django/1.0.0b4/zibanu-django-1.0.0b4.tar.gz

---
+++ packages/zibanu-django/zibanu-django-1.0.0b4.tar.gz/zibanu-django-1.0.0b4/zibanu/django/logging/models.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+#  Developed by CQ Inversiones SAS. Copyright ©. 2019 - 2022. All rights reserved.
+#  Desarrollado por CQ Inversiones SAS. Copyright ©. 2019 - 2022. Todos los derechos reservado
+
+# ****************************************************************
+# IDE:          PyCharm
+# Developed by: macercha
+# Date:         10/12/22 10:27 AM
+# Project:      CFHL Transactional Backend
+# Module Name:  models
+# Description:
+# ****************************************************************
+from django.db import models
+
