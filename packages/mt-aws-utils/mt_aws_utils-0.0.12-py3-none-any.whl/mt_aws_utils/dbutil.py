aef7ebf5ece906f9860fed2b7d51f7f915e6f7d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:17:36 +0000

    Add package mt-aws-utils/0.0.12/mt_aws_utils-0.0.12-py3-none-any.whl

---
+++ packages/mt-aws-utils/mt_aws_utils-0.0.12-py3-none-any.whl/mt_aws_utils/dbutil.py
@@ -0,0 +1,81 @@
+import pymysql
+
+
+class Db():
+    '''database access layer'''
+
+    def __init__(self, dbsvr, dbuser, dbpswd, dbname):
+
+        self.dbname = dbname
+        self.dbsrvr = dbsvr
+        self.dbuser = dbuser
+        self.dbpswd = dbpswd
+
+        self.cnn = self.connect()
+
+    def __enter__(self):
+        self.cnn = self.connect()
+        return self
+
+    def __exit__(self, exc_type, exc_value, exc_traceback):
+        self.cnn.close()
+
+    def connect(self):
+        '''connects to mysql'''
+        return pymysql.connect(host=self.dbsrvr,
+                               user=self.dbuser,
+                               password=self.dbpswd,
+                               database=self.dbname)
+
+    def close(self):
+        '''closes the db connection'''
+        self.cnn.close()
+
+    def singlequery(self, qry, arglist=()):
+        '''Performs a single query returns the number of rows affected'''
+
+        csr = self.cnn.cursor()
+        res = csr.execute(qry, arglist)
+        self.cnn.commit()
+
+        return res
+
+    def namedselect(self, qry, arglist=()):
+        '''Performs a single query returns the rows selected'''
+
+        csr = self.cnn.cursor()
+        csr.execute(qry, arglist)
+
+        self.cnn.commit()
+
+        for row in csr.fetchall():
+            yield {d[0]: r for d, r in zip(csr.description, row)}
+
+    def executemany(self, qry, arglist):
+        '''Performs a single qry returns the number of rows affected'''
+
+        csr = self.cnn.cursor()
+        res = csr.executemany(qry, arglist)
+        self.cnn.commit()
+
+        return res
+
+    def singleselect(self, qry, arglist=()):
+        '''Performs a single query returns the rows selected'''
+
+        csr = self.cnn.cursor()
+        csr.execute(qry, arglist)
+
+        self.cnn.commit()
+
+        return csr.fetchall()
+
+    def singlerow(self, qry, arglist=()):
+        '''Performs a single query returns the first/only row selected'''
+
+        csr = self.cnn.cursor()
+        csr.execute(qry, arglist)
+
+        self.cnn.commit()
+
+        return csr.fetchall()[0]
