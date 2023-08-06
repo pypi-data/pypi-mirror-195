1721a14e6cf8e1a13e48ec778d7f8a1449fe861c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:23:12 +0000

    Add package invenio-app-rdm/10.1.1/invenio-app-rdm-10.1.1.tar.gz

---
+++ packages/invenio-app-rdm/invenio-app-rdm-10.1.1.tar.gz/invenio-app-rdm-10.1.1/invenio_app_rdm/theme/assets/semantic-ui/js/invenio_app_rdm/search/index.js
@@ -0,0 +1,43 @@
+// This file is part of InvenioRDM
+// Copyright (C) 2020 CERN.
+// Copyright (C) 2020 Northwestern University.
+// Copyright (C) 2021 Graz University of Technology.
+//
+// Invenio App RDM is free software; you can redistribute it and/or modify it
+// under the terms of the MIT License; see LICENSE file for more details.
+
+import { createSearchAppInit } from "@js/invenio_search_ui";
+import {
+  RDMCountComponent,
+  RDMEmptyResults,
+  RDMErrorComponent,
+  RDMRecordResultsGridItem,
+  RDMRecordResultsListItem,
+  RDMRecordSearchBarContainer,
+  RDMRecordMultipleSearchBarElement,
+  RDMToggleComponent,
+} from "./components";
+import { parametrize } from "react-overridable";
+import {
+  ContribSearchAppFacets,
+  ContribBucketAggregationElement,
+  ContribBucketAggregationValuesElement,
+} from "@js/invenio_search_ui/components";
+
+const ContribSearchAppFacetsWithConfig = parametrize(ContribSearchAppFacets, {
+  toogle: true,
+});
+
+createSearchAppInit({
+  "BucketAggregation.element": ContribBucketAggregationElement,
+  "BucketAggregationValues.element": ContribBucketAggregationValuesElement,
+  "ResultsGrid.item": RDMRecordResultsGridItem,
+  "EmptyResults.element": RDMEmptyResults,
+  "ResultsList.item": RDMRecordResultsListItem,
+  "SearchApp.facets": ContribSearchAppFacetsWithConfig,
+  "SearchApp.searchbarContainer": RDMRecordSearchBarContainer,
+  "SearchBar.element": RDMRecordMultipleSearchBarElement,
+  "Count.element": RDMCountComponent,
+  "Error.element": RDMErrorComponent,
+  "SearchFilters.Toggle.element": RDMToggleComponent,
+});
