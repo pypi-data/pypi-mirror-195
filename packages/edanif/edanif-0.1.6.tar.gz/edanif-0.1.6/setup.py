80212da59858322effce330fb17b6a0f94813f3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:12:18 +0000

    Add package edanif/0.1.6/edanif-0.1.6.tar.gz

---
+++ packages/edanif/edanif-0.1.6.tar.gz/edanif-0.1.6/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="edanif",
+    version="0.1.6",
+    author="parkminwoo",
+    author_email="parkminwoo1991@gmail.com",
+    description="EDA-NIf creates a dataframe containing meta information of NIfTi files and provides several useful features.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/DSDanielPark/EDA-NIf",
+    packages=setuptools.find_packages(),
+    python_requires='>=3.6',
+    install_requires=[
+        "nibabel",
+        "pandas",
+        "numpy",
+        "matplotlib",
+        "tqdm",
+        "SimpleITK",
+        "monai"
+    ])
