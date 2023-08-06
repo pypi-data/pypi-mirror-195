664838ade9aeaf04d35b8b93c19d65262badac25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:44:00 +0000

    Add package memorylru/1.1.2/memorylru-1.1.2.tar.gz

---
+++ packages/memorylru/memorylru-1.1.2.tar.gz/memorylru-1.1.2/setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from platform import system
+from setuptools import Extension, find_packages, setup
+
+README = open("README.md").read()
+
+
+lru_module = Extension("memorylru.lru", sources=["memorylru/lru.c"])
+
+
+extensions = []
+if system() != "Windows":
+    extensions.append(lru_module)
+
+
+setup(
+    name="memorylru",
+    version="1.1.2",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    url="https://github.com/vangheem/memorylru",
+    license="BSD",
+    setup_requires=[],
+    zip_safe=True,
+    include_package_data=True,
+    ext_modules=extensions,
+    packages=find_packages(),
+)
