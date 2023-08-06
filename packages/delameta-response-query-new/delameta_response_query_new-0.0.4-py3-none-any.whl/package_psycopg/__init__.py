8b184db2e9acf6e3547a6ebf88342a10e49f71e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:10:59 +0000

    Add package delameta-response-query-new/0.0.4/delameta_response_query_new-0.0.4-py3-none-any.whl

---
+++ packages/delameta-response-query-new/delameta_response_query_new-0.0.4-py3-none-any.whl/package_psycopg/__init__.py
@@ -0,0 +1,5 @@
+from package_psycopg.create_pool_psycopg2 import create_connection_postgresql
+from package_psycopg.query_psycopg2 import (action_additional_response, action_build_message_detail, action_check_existing_data, action_datatables_view, action_delete_data, action_generate_footers, action_generate_headers,
+                                action_insert_data, action_return_of_response, action_update_data, action_validate)
+
+
