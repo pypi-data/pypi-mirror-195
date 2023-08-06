42270b22437dcfdcc5c71b3ab47228d60a3fcc1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:15:34 +0000

    Add package netbox-storage/0.0.0.0.513/netbox_storage-0.0.0.0.513-py3-none-any.whl

---
+++ packages/netbox-storage/netbox_storage-0.0.0.0.513-py3-none-any.whl/netbox_storage/filters/volume_group.py
@@ -0,0 +1,25 @@
+from django.db.models import Q
+
+from netbox.filtersets import NetBoxModelFilterSet
+
+from netbox_storage.models import VolumeGroup
+
+
+class VolumeGroupFilter(NetBoxModelFilterSet):
+    """Filter capabilities for VolumeGroup instances."""
+
+    class Meta:
+        model = VolumeGroup
+        fields = [
+            "vg_name",
+            "description"
+        ]
+
+    def search(self, queryset, name, value):
+        """Perform the filtered search."""
+        if not value.strip():
+            return queryset
+        qs_filter = (
+            Q(vg_name__icontains=value)
+        )
+        return queryset.filter(qs_filter)
---
+++ packages/netbox-storage/netbox_storage-0.0.0.0.513-py3-none-any.whl/netbox_storage/forms/logical_volume.py
@@ -0,0 +1,147 @@
+from django.core.validators import MinValueValidator
+from django.forms import (
+    CharField, FloatField,
+)
+from django.urls import reverse_lazy
+
+from netbox.forms import (
+    NetBoxModelBulkEditForm,
+    NetBoxModelFilterSetForm,
+    NetBoxModelImportForm,
+    NetBoxModelForm,
+)
+from netbox_storage.models import Filesystem, LogicalVolume, VolumeGroup, Drive, StorageConfiguration
+from utilities.forms import (
+    DynamicModelChoiceField,
+    APISelect,
+)
+
+
+class LogicalVolumeForm(NetBoxModelForm):
+    """Form for creating a new LogicalVolume object."""
+    drive = DynamicModelChoiceField(
+        queryset=Drive.objects.all(),
+        help_text="The Storage Cluster of the drive",
+    )
+    vg = DynamicModelChoiceField(
+        queryset=VolumeGroup.objects.all(),
+        label="VG Name",
+        widget=APISelect(
+            attrs={"data-url": reverse_lazy("plugins-api:netbox_storage-api:volumegroup-list")}
+        ),
+        help_text="The Volume Group for the LogicalVolume e.g. vg_docker",
+    )
+    lv_name = CharField(
+        label="LV Name",
+        help_text="Logical Volume Name e.g. lv_docker",
+    )
+    size = FloatField(
+        label="Size (GB)",
+        help_text="The size of the logical volume e.g. 25",
+        validators=[MinValueValidator(1)],
+    )
+    path = CharField(
+        label="Path",
+        help_text="The mounted path of the volume e.g. /var/lib/docker",
+    )
+    fs = DynamicModelChoiceField(
+        queryset=Filesystem.objects.all(),
+        label="Filesystem Name",
+        widget=APISelect(
+            attrs={"data-url": reverse_lazy("plugins-api:netbox_storage-api:filesystem-list")}
+        ),
+        help_text="The Filesystem of the Volume e.g. ext4",
+    )
+
+    class Meta:
+        model = LogicalVolume
+
+        fields = (
+            "drive",
+            "vg",
+            "lv_name",
+            "size",
+            "fs",
+            "path",
+            "description",
+        )
+
+
+class LogicalVolumeFilterForm(NetBoxModelFilterSetForm):
+    """Form for filtering LogicalVolume instances."""
+
+    model = LogicalVolume
+
+    vg = DynamicModelChoiceField(
+        queryset=VolumeGroup.objects.all(),
+        label="VG Name",
+        required=False
+    )
+    lv_name = CharField(
+        required=False,
+        label="LV Name",
+    )
+    path = CharField(
+        required=False,
+        label="Path",
+    )
+    fs = DynamicModelChoiceField(
+        queryset=Filesystem.objects.all(),
+        required=False,
+        widget=APISelect(
+            attrs={"data-url": reverse_lazy("plugins-api:netbox_storage-api:filesystem-list")}
+        ),
+        label="Filesystem Name",
+    )
+
+
+class LogicalVolumeImportForm(NetBoxModelImportForm):
+    fs = DynamicModelChoiceField(
+        queryset=Filesystem.objects.all(),
+        required=False
+    )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    class Meta:
+        model = LogicalVolume
+
+        fields = (
+            "vg",
+            "lv_name",
+            "fs",
+            "path",
+            "description",
+        )
+
+
+class LogicalVolumeBulkEditForm(NetBoxModelBulkEditForm):
+    model = LogicalVolume
+
+    vg = DynamicModelChoiceField(
+        queryset=VolumeGroup.objects.all(),
+        required=False
+    )
+    lv_name = CharField(
+        required=False,
+        label="LV Name",
+    )
+    path = CharField(
+        required=False,
+        label="Path",
+    )
+    fs = DynamicModelChoiceField(
+        queryset=Filesystem.objects.all(),
+        required=False,
+        label="Filesystem Name",
+    )
+    description = CharField(max_length=255, required=False)
+
+    fieldsets = (
+        (
+            None,
+            ("vg", "lv_name", "path", "fs", "description"),
+        ),
+    )
+    nullable_fields = ["description"]
