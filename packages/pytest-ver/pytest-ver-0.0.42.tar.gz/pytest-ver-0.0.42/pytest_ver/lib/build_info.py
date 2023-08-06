99bdc7a1251903fd3c5580d40bbf724a5bbe8640
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:34 +0000

    Add package pytest-ver/0.0.42/pytest-ver-0.0.42.tar.gz

---
+++ packages/pytest-ver/pytest-ver-0.0.42.tar.gz/pytest-ver-0.0.42/pytest_ver/lib/build_info.py
@@ -0,0 +1,16 @@
+class BuildInfo:  # pylint: disable=too-few-public-methods
+    python_version = '3.10.9'
+    os_name = 'nt'
+    version = '0.0.42'
+    git_sha = 'dabdd90669f94722fe321d4c9951b69c2e04190f'
+    git_branch = 'master'
+    git_uncommitted = [
+        'M  LICENSE.txt',
+        'M  pytest_ver/lib/version.json',
+        'M  requirements.txt',
+        'M  setup.py',
+        'M  todo.md',
+        'A  tools/common.py',
+    ]
+    git_unpushed = [
+    ]
