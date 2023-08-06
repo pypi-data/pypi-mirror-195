a50f5a3e8b5ee082e9b34df9492cd24cffa543be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:12:49 +0000

    Add package python-invest/0.2.0/python_invest-0.2.0-py3-none-any.whl

---
+++ packages/python-invest/python_invest-0.2.0-py3-none-any.whl/python_invest/invest/utils.py
@@ -0,0 +1,36 @@
+__all__ = ['convert_dates_to_str', 'dict_to_url_params']
+import re
+from datetime import date
+from urllib import parse
+
+
+def convert_dates_to_str(
+    from_date: str | date, to_date: str | date
+) -> tuple[str, str]:
+    """
+    Convert date objects to string and validate string date format.
+    Return tuple of string dates in format d/m/Y.
+    """
+    pattern = r'([0-9]{2}\/[0-9]{2}\/[0-9]{4})'
+    try:
+        from_date = from_date.strftime('%m/%d/%Y')
+    except AttributeError:
+        if not re.fullmatch(pattern, from_date):
+            raise ValueError(
+                f'The date format is invalid, please use d/m/Y format in from_date argument.'
+            )
+    try:
+        to_date = to_date.strftime('%m/%d/%Y')
+    except AttributeError:
+        if not re.fullmatch(pattern, to_date):
+            raise ValueError(
+                f'The date format is invalid, please use d/m/Y format in to_date argument.'
+            )
+    return from_date, to_date
+
+
+def dict_to_url_params(data: dict) -> str:
+    """
+    Convert dictionary to url parameters.
+    """
+    return parse.urlencode(data)
