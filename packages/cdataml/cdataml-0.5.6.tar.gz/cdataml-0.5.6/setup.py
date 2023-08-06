492b64c1fcaea8a11e697642acecdc9dc612f65e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:40 +0000

    Add package cdataml/0.5.6/cdataml-0.5.6.tar.gz

---
+++ packages/cdataml/cdataml-0.5.6.tar.gz/cdataml-0.5.6/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="cdataml",
+    version='0.5.6',
+    packages = ['cdataml'],
+    url="https://github.com/joshuandwilliams/cdataml",
+    license='LICENSE.txt',
+    author="Joshua Williams",
+    author_email="<jowillia@nbi.ac.uk>",
+    description='Cell Death Area Data Collection for Machine Learning',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    scripts=['scripts/cdataml-run'],
+    include_package_data=True,
+    package_data={"cdataml": ['lesion_score_key.tif']},
+    install_requires=['opencv-python', 'pandas', 'natsort', 'importlib_resources']
+)
