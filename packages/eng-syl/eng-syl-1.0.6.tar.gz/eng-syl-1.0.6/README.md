a61dbe1a0e7bc9d8d3cc806c7aff74f0b97b9581
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:57 +0000

    Add package eng-syl/1.0.6/eng-syl-1.0.6.tar.gz

---
+++ packages/eng-syl/eng-syl-1.0.6.tar.gz/eng-syl-1.0.6/README.md
@@ -0,0 +1,15 @@
+﻿# English Syllabifier (eng_syl)
+This program implements a sequence labelling Bidirectional LSTM to identify syllable boundaries in English words. The model was trained on data from the  [WebCelex](http://celex.mpi.nl/) English wordform corpus.
+
+Use the `syllabify()` function from the `Syllabel` class to syllabify your words:
+
+>     >>> from eng_syl import Syllabel
+>     >>> sylabler = Syllabel()
+>     >>> syllabler.syllabify("chomsky")
+>     'chom-sky'
+
+`syllabify()` parameters
+
+ - **text**: *string*- English text to be syllabified. Input should only contain alphabetic characters.
+
+`syllabify()` returns the given word with hyphens inserted at syllable boundaries.
---
+++ packages/eng-syl/eng-syl-1.0.6.tar.gz/eng-syl-1.0.6/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(name='eng-syl',
+      version='1.0.6',
+      description='English word syllabifier',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      url='https://github.com/ellipse-liu/eng-syl',
+      author='ellipse-liu',
+      author_email='timothys.new.email@gmail.comm',
+      license='MIT',
+      packages=['eng_syl'],
+      package_data={'eng_syl': ['e2i.pkl', 'syllabler_best_weights.h5']},
+      install_requires=[
+          'tensorflow',
+          'numpy',
+      ],
+      keywords=['Syllable', 'NLP', 'psycholinguistics'],
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Intended Audience :: Developers',
+          'Topic :: Software Development :: Build Tools',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 3.8',
+      ],
+      zip_safe=False)
