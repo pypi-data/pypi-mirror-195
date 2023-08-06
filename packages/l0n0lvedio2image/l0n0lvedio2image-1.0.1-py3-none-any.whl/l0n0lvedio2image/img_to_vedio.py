4511a01da35cc8edde92ff078cb43d3e56788a4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:06 +0000

    Add package l0n0lvedio2image/1.0.1/l0n0lvedio2image-1.0.1-py3-none-any.whl

---
+++ packages/l0n0lvedio2image/l0n0lvedio2image-1.0.1-py3-none-any.whl/l0n0lvedio2image/img_to_vedio.py
@@ -0,0 +1,27 @@
+import cv2
+import os
+
+def png2vedio(img_path, vedio_path, fps = 24):
+    img_list = sorted(os.listdir(img_path)) # 获取图片列表
+    img = cv2.imread(os.path.join(img_path, img_list[0]))
+    video_writer = cv2.VideoWriter(vedio_path, 
+                                   cv2.VideoWriter_fourcc(*'avc1'), 
+                                   fps, (img.shape[1], img.shape[0])) # 创建视频写入对象
+    for img_name in img_list: # 遍历图片列表
+        img = cv2.imread(os.path.join(img_path, img_name)) # 读取图片文件
+        video_writer.write(img) # 写入视频
+
+    video_writer.release() # 释放资源
+
+def run():
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("img_path", help="图片目录")
+    parser.add_argument("vedio_path", help="生成除来的视频目录")
+    parser.add_argument("-f", "--fps", type=int, default=24, help="保存成视频的帧率")
+    args = parser.parse_args()
+    # 调用函数
+    png2vedio(args.img_path,args.vedio_path,args.fps)
+
+
+    
