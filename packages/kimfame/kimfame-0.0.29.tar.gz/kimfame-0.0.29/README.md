ef228a88c885b682720a9212f5e272a43c32fa39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:35:27 +0000

    Add package kimfame/0.0.29/kimfame-0.0.29.tar.gz

---
+++ packages/kimfame/kimfame-0.0.29.tar.gz/kimfame-0.0.29/README.md
@@ -0,0 +1,12 @@
+# Kimfame
+
+[![PyPI version](https://img.shields.io/pypi/v/kimfame)](https://pypi.org/project/kimfame/)
+[![Python Versions](https://img.shields.io/pypi/pyversions/kimfame)](https://pypi.org/project/kimfame/)
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+[![Code style: black](https://img.shields.io/badge/code%20style-black-000000.svg)](https://github.com/psf/black)
+
+PyPI Test Project
+
+"Hello world"
+
+"How are you?"
---
+++ packages/kimfame/kimfame-0.0.29.tar.gz/kimfame-0.0.29/setup.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['kimfame']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['faker>=17.0.0,<18.0.0']
+
+entry_points = \
+{'console_scripts': ['kimfame = kimfame.kimfame:main',
+                     'triplea = kimfame.aaa:aaa',
+                     'tripled = kimfame.ddd:ddd']}
+
+setup_kwargs = {
+    'name': 'kimfame',
+    'version': '0.0.29',
+    'description': 'Nike!',
+    'long_description': '# Kimfame\n\n[![PyPI version](https://img.shields.io/pypi/v/kimfame)](https://pypi.org/project/kimfame/)\n[![Python Versions](https://img.shields.io/pypi/pyversions/kimfame)](https://pypi.org/project/kimfame/)\n[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)\n[![Code style: black](https://img.shields.io/badge/code%20style-black-000000.svg)](https://github.com/psf/black)\n\nPyPI Test Project\n\n"Hello world"\n\n"How are you?"\n',
+    'author': 'kimfame',
+    'author_email': 'renownkim@gmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'https://github.com/kimfame/kimfame',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.11,<4.0',
+}
+
+
+setup(**setup_kwargs)
