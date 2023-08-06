48202343667de3129e338b9139d0537afa5149fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:26:42 +0000

    Add package ezctl/0.1.24/ezctl-0.1.24.tar.gz

---
+++ packages/ezctl/ezctl-0.1.24.tar.gz/ezctl-0.1.24/ezctl/commands/check.py
@@ -0,0 +1,10 @@
+from commons import common as common
+
+
+class Check:
+    def __init__(self, command_id):
+        self.command_id = command_id
+
+    def apply(self):
+        check_cmd = f'aws ssm list-commands --command-id {self.command_id} --region ap-southeast-1'
+        return common.shell(cmd=check_cmd, keyword='Status', s3=True).strip().split(':')[1].strip()
---
+++ packages/ezctl/ezctl-0.1.24.tar.gz/ezctl-0.1.24/ezctl/commons/common.py
@@ -0,0 +1,105 @@
+import subprocess
+import random
+import string
+
+PIPE = 'pipe'
+
+
+def get_random_string(length):
+    letters = string.ascii_lowercase
+    result_str = ''.join(random.choice(letters) for _ in range(length))
+    return result_str
+
+
+def shell(cmd, keyword='', ignore_error=False, output=True, s3=False):
+    if s3 is True:
+        cmd += ' --output yaml'
+    if output is False:
+        p = subprocess.Popen(cmd, stdout=subprocess.DEVNULL,
+                             stderr=subprocess.STDOUT, cwd=None, shell=True)
+    else:
+        p = subprocess.Popen(cmd, stdout=subprocess.PIPE,
+                             stderr=subprocess.PIPE, cwd=None, shell=True)
+    res = ''
+    if keyword != '':
+        for line in p.stdout.readlines():
+            if keyword in line.decode():
+                res = line.decode()
+                break
+    else:
+        for line in p.stdout.readlines():
+            res = line.decode()
+    p.stdout.close()
+    error = ''
+    for line in p.stderr.readlines():
+        print(line.decode())
+        error += line.decode()
+    if len(error) > 0 and not ignore_error:
+        raise Exception(f'shell return error {error[:-1]}')
+    p.stderr.close()
+    p.wait()
+    return res
+
+
+def get_file_size(file_path, provider):
+    if provider == 's3':
+        get_size_cmd = f'aws s3 ls --summarize --human-readable {file_path}'
+        info = shell(cmd=get_size_cmd, keyword='Total Size', s3=True)
+        size = 0
+        if 'MiB' in info:
+            size = int(float(info.split(':')[1].strip().split(' ')[0])) * 1024 
+        if 'GiB' in info:
+            size = int(float(info.split(':')[1].strip().split(' ')[0])) * 1024 * 1024
+        if 'TiB' in info:
+            size = int(float(info.split(':')[1].strip().split(' ')[0])) * 1024 * 1024 * 1024
+        return size
+    elif provider == 'gs':
+        get_size_cmd = f'gsutil du {file_path}'
+        size = shell(cmd=get_size_cmd, s3=False)
+        return int(size.split('gs://', 1)[0].strip()) / 1024
+
+def get_instance_type(pbf, sp, ssp, provider):
+    instance_type = 'r5.large'
+    coefficient = 10
+
+    size = get_file_size(pbf, provider) + get_file_size(sp, provider)
+    if ssp != '':
+        size += get_file_size(ssp, provider)
+
+    size = coefficient * size / 1024 / 1024  # in gb
+    if size < 1:
+        print(f'get_instance_type: {instance_type}')
+        return instance_type
+    if size <= 32:
+        instance_type = 'c4.4xlarge'
+    elif 32 < size <= 64:
+        instance_type = 'c4.8xlarge'
+    elif 64 < size <= 128:
+        instance_type = 'm5.8xlarge'
+    elif size > 128:
+        instance_type = 'r5.8xlarge'
+
+    print(f'get_instance_type: {instance_type}')
+    return instance_type
+
+
+def get_memory_size(instance_type):
+    memory_size = 16
+    if instance_type == 'c4.4xlarge':
+        memory_size = 30
+    elif instance_type == 'c4.8xlarge':
+        memory_size = 60
+    elif instance_type == 'm5.8xlarge':
+        memory_size = 128
+    elif instance_type == 'r5.8xlarge':
+        memory_size = 256
+    return memory_size
+
+
+def get_volume_size(pbf, sp, ssp, provider):
+    size = get_file_size(pbf, provider) + get_file_size(sp, provider)
+    if ssp != '':
+        size += get_file_size(ssp, provider)
+    if 25 * (size / 1024 / 1024) < 8:
+        return 8
+    return 25 * (size / 1024 / 1024)
