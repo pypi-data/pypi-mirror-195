6a1be50a0a53dd7210c4a5644effda36dbeee3d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:58:03 +0000

    Add package resoto-plugin-aws/3.2.5/resoto_plugin_aws-3.2.5-py3-none-any.whl

---
+++ packages/resoto-plugin-aws/resoto_plugin_aws-3.2.5-py3-none-any.whl/test/__init__.py
@@ -0,0 +1,43 @@
+from queue import Queue
+
+from pytest import fixture
+
+from resoto_plugin_aws.collector import AwsAccountCollector
+from resoto_plugin_aws.configuration import AwsConfig
+from resoto_plugin_aws.aws_client import AwsClient
+from resoto_plugin_aws.resource.base import AwsAccount, AwsRegion, GraphBuilder, ExecutorQueue
+from resotolib.baseresources import Cloud
+from resotolib.core.actions import CoreFeedback
+from resotolib.graph import Graph
+from test.resources import BotoFileBasedSession, DummyExecutor
+
+
+@fixture
+def aws_config() -> AwsConfig:
+    config = AwsConfig()
+    config.sessions().session_class_factory = BotoFileBasedSession
+    return config
+
+
+@fixture
+def aws_client(aws_config: AwsConfig) -> AwsClient:
+    return AwsClient(aws_config, "test", region="us-east-1")
+
+
+@fixture
+def builder(aws_client: AwsClient, no_feedback: CoreFeedback) -> GraphBuilder:
+    queue = ExecutorQueue(DummyExecutor(), "dummy")
+    return GraphBuilder(
+        Graph(), Cloud(id="aws"), AwsAccount(id="test"), AwsRegion(id="us-east-1"), aws_client, queue, no_feedback
+    )
+
+
+@fixture
+def no_feedback() -> CoreFeedback:
+    return CoreFeedback("123", "step1", "collect", Queue())
+
+
+@fixture
+def account_collector(aws_config: AwsConfig, no_feedback: CoreFeedback) -> AwsAccountCollector:
+    account = AwsAccount(id="test")
+    return AwsAccountCollector(aws_config, Cloud(id="aws"), account, ["us-east-1"], no_feedback)
---
+++ packages/resoto-plugin-aws/resoto_plugin_aws-3.2.5-py3-none-any.whl/test/configuration_test.py
@@ -0,0 +1,32 @@
+from resotolib.proc import num_default_threads
+from resotolib.config import Config
+from resoto_plugin_aws import AWSCollectorPlugin
+from resoto_plugin_aws.configuration import AwsConfig
+
+
+def test_default_config() -> None:
+    config = Config("dummy", "dummy")
+    AWSCollectorPlugin.add_config(config)
+    Config.init_default_config()
+    assert Config.aws.access_key_id is None
+    assert Config.aws.secret_access_key is None
+    assert Config.aws.role is None
+    assert Config.aws.role_override is False
+    assert Config.aws.account is None
+    assert Config.aws.region is None
+    assert Config.aws.scrape_org is False
+    assert Config.aws.fork_process is True
+    assert Config.aws.scrape_exclude_account == []
+    assert Config.aws.assume_current is False
+    assert Config.aws.do_not_scrape_current is False
+    assert Config.aws.account_pool_size == num_default_threads()
+    assert Config.aws.region_pool_size == 4
+    assert len(Config.aws.collect) == 0
+    assert len(Config.aws.no_collect) == 0
+
+
+def test_session() -> None:
+    config = AwsConfig("test", "test", "test")
+    # direct session
+    assert config.sessions()._session("1234", aws_role=None) == config.sessions()._session("1234", aws_role=None)
+    # no test for sts session, since this requires sts setup
