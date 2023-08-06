938ba43f70a036f3d2ed5062e6549986bf269370
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:12:18 +0000

    Add package poem-plugins/0.17.0/poem_plugins-0.17.0-py3-none-any.whl

---
+++ packages/poem-plugins/poem_plugins-0.17.0-py3-none-any.whl/poem_plugins/general/version/drivers/__init__.py
@@ -0,0 +1,15 @@
+import abc
+
+from poem_plugins.general.version import Version
+
+
+class IVervsionDriver(abc.ABC):
+    @abc.abstractmethod
+    def get_version(self) -> Version:
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def render_version_file(
+        self, version: Version,
+    ) -> str:
+        raise NotImplementedError
