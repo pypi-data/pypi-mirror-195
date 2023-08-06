10c7ad95700039f4f35b285b678e7b249c1dc7d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:27:49 +0000

    Add package scrapy-poet/0.10.1/scrapy-poet-0.10.1.tar.gz

---
+++ packages/scrapy-poet/scrapy-poet-0.10.1.tar.gz/scrapy-poet-0.10.1/scrapy_poet/commands.py
@@ -0,0 +1,111 @@
+import datetime
+from pathlib import Path
+from typing import Type
+
+import andi
+import scrapy
+import time_machine
+from scrapy import Request
+from scrapy.commands import ScrapyCommand
+from scrapy.crawler import Crawler
+from scrapy.exceptions import UsageError
+from scrapy.http import Response
+from scrapy.utils.misc import load_object
+from twisted.internet.defer import inlineCallbacks
+from web_poet import ItemPage
+from web_poet.testing import Fixture
+from web_poet.utils import ensure_awaitable
+
+from scrapy_poet import DummyResponse
+from scrapy_poet.downloadermiddlewares import DEFAULT_PROVIDERS, InjectionMiddleware
+from scrapy_poet.injection import Injector
+
+saved_dependencies = []
+saved_items = []
+frozen_time = None
+
+
+class SavingInjector(Injector):
+    @inlineCallbacks
+    def build_instances_from_providers(
+        self, request: Request, response: Response, plan: andi.Plan
+    ):
+        instances = yield super().build_instances_from_providers(
+            request, response, plan
+        )
+        saved_dependencies.extend(instances.values())
+        return instances
+
+
+class SavingPipeline:
+    def process_item(self, item, spider):
+        saved_items.append(item)
+        return item
+
+
+class SavingInjectionMiddleware(InjectionMiddleware):
+    def __init__(self, crawler: Crawler) -> None:
+        super().__init__(crawler)
+        self.injector = SavingInjector(
+            crawler,
+            default_providers=DEFAULT_PROVIDERS,
+            registry=self.registry,
+        )
+
+
+def spider_for(injectable: Type[ItemPage]) -> Type[scrapy.Spider]:
+    class InjectableSpider(scrapy.Spider):
+        name = "injectable"
+        url = None
+
+        def start_requests(self):
+            yield scrapy.Request(self.url, self.cb)
+
+        async def cb(self, response: DummyResponse, page: injectable):  # type: ignore[valid-type]
+            global frozen_time
+            frozen_time = datetime.datetime.now(datetime.timezone.utc).replace(
+                microsecond=0
+            )
+            with time_machine.travel(frozen_time):
+                yield await ensure_awaitable(page.to_item())  # type: ignore[attr-defined]
+
+    return InjectableSpider
+
+
+class SaveFixtureCommand(ScrapyCommand):
+    def syntax(self):
+        return "<page object class> <URL>"
+
+    def short_desc(self):
+        return "Generate a web-poet test for the provided page object and URL"
+
+    def run(self, args, opts):
+        if len(args) != 2:
+            raise UsageError()
+        type_name = args[0]
+        url = args[1]
+
+        cls = load_object(type_name)
+        if not issubclass(cls, ItemPage):
+            raise UsageError(f"Error: {type_name} is not a descendant of ItemPage")
+
+        self.settings["ITEM_PIPELINES"][SavingPipeline] = 100
+        self.settings["DOWNLOADER_MIDDLEWARES"][
+            "scrapy_poet.downloadermiddlewares.InjectionMiddleware"
+        ] = None
+        self.settings["DOWNLOADER_MIDDLEWARES"][InjectionMiddleware] = None
+        self.settings["DOWNLOADER_MIDDLEWARES"][SavingInjectionMiddleware] = 543
+        self.settings["_SCRAPY_POET_SAVEFIXTURE"] = True
+
+        spider_cls = spider_for(cls)
+        self.crawler_process.crawl(spider_cls, url=url)
+        self.crawler_process.start()
+
+        deps = saved_dependencies
+        item = saved_items[0]
+        meta = {
+            "frozen_time": frozen_time.isoformat(timespec="seconds"),
+        }
+        basedir = Path(self.settings.get("SCRAPY_POET_TESTS_DIR", "fixtures"))
+        fixture = Fixture.save(basedir / type_name, inputs=deps, item=item, meta=meta)
+        print(f"\nThe test fixture has been written to {fixture.path}.")
---
+++ packages/scrapy-poet/scrapy-poet-0.10.1.tar.gz/scrapy-poet-0.10.1/tests/test_commands.py
@@ -0,0 +1,55 @@
+import datetime
+import json
+import subprocess
+import sys
+import tempfile
+from pathlib import Path
+
+from web_poet.testing import Fixture
+
+
+def call_scrapy_command(cwd: str, *args: str) -> None:
+    with tempfile.TemporaryFile() as out:
+        args = (sys.executable, "-m", "scrapy.cmdline") + args
+        subprocess.call(args, stdout=out, stderr=out, cwd=cwd)
+
+
+def test_savefixture(tmp_path) -> None:
+    project_name = "foo"
+    cwd = Path(tmp_path)
+    call_scrapy_command(str(cwd), "startproject", project_name)
+    cwd /= project_name
+    type_name = "foo.po.BTSBookPage"
+    (cwd / project_name / "po.py").write_text(
+        """
+import attrs
+from web_poet import HttpClient, ResponseUrl
+from web_poet.pages import WebPage
+
+
+@attrs.define
+class BTSBookPage(WebPage):
+
+    response_url: ResponseUrl
+    client: HttpClient
+
+    async def to_item(self):
+        await self.client.request("http://toscrape.com")
+        return {
+            'url': self.url,
+            'name': self.css("title::text").get(),
+        }
+"""
+    )
+    url = "http://books.toscrape.com/catalogue/the-wedding-pact-the-omalleys-2_767/index.html"
+    call_scrapy_command(str(cwd), "savefixture", type_name, url)
+    fixtures_dir = cwd / "fixtures"
+    fixture_dir = fixtures_dir / type_name / "test-1"
+    fixture = Fixture(fixture_dir)
+    assert fixture.is_valid()
+    assert (fixture.input_path / "HttpResponse-body.html").exists()
+    assert fixture.meta_path.exists()
+    assert (fixture.input_path / "HttpClient-0-HttpResponse.body.html").exists()
+    frozen_time_str = json.loads(fixture.meta_path.read_bytes())["frozen_time"]
+    frozen_time = datetime.datetime.fromisoformat(frozen_time_str)
+    assert frozen_time.microsecond == 0
