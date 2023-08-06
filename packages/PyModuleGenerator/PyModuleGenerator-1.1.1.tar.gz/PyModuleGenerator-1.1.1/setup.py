5e4704e38f6c7353f74e2045fadf537cde593596
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:26:14 +0000

    Add package PyModuleGenerator/1.1.1/PyModuleGenerator-1.1.1.tar.gz

---
+++ packages/PyModuleGenerator/PyModuleGenerator-1.1.1.tar.gz/PyModuleGenerator-1.1.1/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+with open("README.md","r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='PyModuleGenerator',
+    version='1.1.1',
+    description='A simple python module generator',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/ProfesseurIssou/PyModuleGenerator',
+    author='Alix Hamidou',
+    author_email='alix.hamidou@gmail.com',
+    license='MIT',
+    packages=find_packages(),
+    install_requires=[
+        'setuptools==63.2.0',
+        'twine==3.8.0',
+    ],
+
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: Unix',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Build Tools',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
