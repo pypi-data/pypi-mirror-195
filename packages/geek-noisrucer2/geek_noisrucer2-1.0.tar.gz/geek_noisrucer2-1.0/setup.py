abb08d621da01550a172a00d0016fd6d2c7c7974
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:42 +0000

    Add package geek-noisrucer2/1.0/geek_noisrucer2-1.0.tar.gz

---
+++ packages/geek-noisrucer2/geek_noisrucer2-1.0.tar.gz/geek_noisrucer2-1.0/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+setuptools.setup(
+    name="geek_noisrucer2",
+    version="1.0",
+    license='MIT',
+    author="Changjin Lee",
+    author_email="changjin9792@gmail.com",
+    description="Test",
+    long_description=open('README.md').read(),
+    url="https://github.com/noisrucer/geek-noisrucer",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        # 패키지에 대한 태그
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
+)
