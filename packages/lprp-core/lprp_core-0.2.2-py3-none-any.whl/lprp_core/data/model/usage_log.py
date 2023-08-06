04f3a409ce53e56b1e6e81505082a6e86681cbdb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:14:47 +0000

    Add package lprp-core/0.2.2/lprp_core-0.2.2-py3-none-any.whl

---
+++ packages/lprp-core/lprp_core-0.2.2-py3-none-any.whl/lprp_core/data/model/usage_log.py
@@ -0,0 +1,36 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from lprp_core.data.model.model_base import ModelBase
+from lprp_core.data.model.client import Client
+
+if TYPE_CHECKING:
+    from lprp_core.data.model.user import User
+    from lprp_core.data.model.vehicle import Vehicle
+
+
+class UsageLog(ModelBase):
+    __tablename__ = "UsageLogs"
+
+    uuid: Mapped[str] = mapped_column(default=None, primary_key=True)
+    timestamp: Mapped[float] = mapped_column(default=None)
+    user_username: Mapped[str] = mapped_column(
+        ForeignKey("Users.username"), init=False, default=None, repr=False
+    )
+    user: Mapped[User] = relationship(init=False, default=None, back_populates="_usage_logs")
+    vehicle_id: Mapped[int] = mapped_column(
+        ForeignKey("Vehicles.id"), init=False, default=None, repr=False
+    )
+    vehicle: Mapped[Vehicle] = relationship(init=False, default=None, back_populates="_usage_logs")
+
+    client_id: Mapped[str] = mapped_column(
+        ForeignKey("Clients.uuid"), init=False, default=None, repr=False
+    )
+
+    client: Mapped[Client] = relationship(init=False, default=None, back_populates="_usage_logs")
+
+    image_path: Mapped[str] = mapped_column(default=None)
