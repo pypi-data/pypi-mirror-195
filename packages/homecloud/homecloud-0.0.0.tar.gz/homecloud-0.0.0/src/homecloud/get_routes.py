834e055fbb4a33b9ffc8b7bb77b4d6fd02137c67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:55 +0000

    Add package homecloud/0.0.0/homecloud-0.0.0.tar.gz

---
+++ packages/homecloud/homecloud-0.0.0.tar.gz/homecloud-0.0.0/src/homecloud/get_routes.py
@@ -0,0 +1,15 @@
+from fastapi import APIRouter
+
+from homecloud import homecloud_logging, request_models
+
+router = APIRouter()
+logger = homecloud_logging.get_logger("$app_name_server")
+
+
+@router.get("/homecloud")
+def homecloud(request: request_models.Request) -> dict:
+    # You can add to the payload here if you want
+    # but don't remove anything or the server will be
+    # undiscoverable by homecloud clients.
+    logger.info(f"{request.host} says hello")
+    return {"app_name": "$app_name", "host": request.host}
