8edfee1c29a625cc821fa6b5e63818a715e6533b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:13:22 +0000

    Add package feature-engineer-hurlok/0.0.3/feature_engineer_hurlok-0.0.3.tar.gz

---
+++ packages/feature-engineer-hurlok/feature_engineer_hurlok-0.0.3.tar.gz/feature_engineer_hurlok-0.0.3/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    readme = fh.read()
+
+setup(name='feature_engineer_hurlok',
+    version='0.0.3',
+    url='https://github.com/illiamw/PackagePiP',
+    license='MIT License',
+    author='William Ferreira',
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    author_email='williambox37@gmail.com',
+    keywords='Pacote',
+    description=u'Exemplo de pacote PyPI',
+    packages=['feature_engineer_hurlok'],
+    install_requires=['numpy', 'pandas'])
