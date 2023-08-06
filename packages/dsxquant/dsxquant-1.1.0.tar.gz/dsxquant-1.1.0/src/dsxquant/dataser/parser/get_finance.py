cd04cb9b757bb2c59f5d6af9752e7224045cfaa9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:29:40 +0000

    Add package dsxquant/1.1.0/dsxquant-1.1.0.tar.gz

---
+++ packages/dsxquant/dsxquant-1.1.0.tar.gz/dsxquant-1.1.0/src/dsxquant/dataser/parser/get_finance.py
@@ -0,0 +1,36 @@
+from dsxquant.dataser.parser.base import BaseParser
+from dsxquant.config import config
+class GetFinanceParser(BaseParser):
+
+    def setApiName(self):
+        self.api_name = "finance"
+    
+    def setParams(self, symbol,market:int,report_type:config.REPORT_TYPE=config.REPORT_TYPE.DEFAULT,report_date=""):
+        """构建请求参数
+        Args:
+            symbols (list): 证券代码数组
+        Returns:
+            _type_: _description_
+        """
+        datas = self.transdata({
+            "symbol":symbol,
+            "market":market,
+            "report_type":report_type,
+            "report_date":report_date,
+        })
+        self.send_datas = datas
+        
+    
+    def parseResponse(self, datas):
+        """解析返回的数据
+
+        Args:
+            datas (str): 服务端返回
+        """
+
+        # logger.debug("执行  "+__name__+"  数据解析方法")
+
+        return datas
+
+
+        
---
+++ packages/dsxquant/dsxquant-1.1.0.tar.gz/dsxquant-1.1.0/src/dsxquant/dataser/parser/get_sharebonus.py
@@ -0,0 +1,36 @@
+from dsxquant.dataser.parser.base import BaseParser
+class GetShareBonusParser(BaseParser):
+
+    def setApiName(self):
+        self.api_name = "sharebonus"
+    
+    def setParams(self, symbol:str,market:int,start:str=None,end:str=None):
+        """构建请求参数
+        Args:
+            symbol (str): 证券代码
+            market (int): 市场代码
+            start (str): 开始日期 Y-m-d
+            end (str): 结束日期 Y-m-d
+        """
+        datas = self.transdata({
+            "symbol":symbol,
+            "market":market,
+            "start":start,
+            "end":end
+        })
+        self.send_datas = datas
+        
+    
+    def parseResponse(self, datas):
+        """解析返回的数据
+
+        Args:
+            datas (str): 服务端返回
+        """
+
+        # logger.debug("parseResponse  "+__name__+" ")
+
+        return datas
+
+
+        
