721f626206acd4a95a6e8bcf8bb413efcd443256
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:33:48 +0000

    Add package brzcode/1.0.8/brzcode-1.0.8.tar.gz

---
+++ packages/brzcode/brzcode-1.0.8.tar.gz/brzcode-1.0.8/README.md
@@ -0,0 +1,4 @@
+
+
+**Atualmente na versão 1.0.7 o BrzCode** pensa, e acredita que, __pessoinhas podem mudar o mundo atrvés da programação__ de uma maneira simples, completa e veloz.
+
---
+++ packages/brzcode/brzcode-1.0.8.tar.gz/brzcode-1.0.8/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    readme = fh.read()
+
+setup(name='brzcode',
+    version='1.0.8',
+    license='MIT License',
+    author='Gustavo Martinez',
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    author_email='gamezinhoo@outlook.com',
+    keywords='brzcode',
+    description=u'Uma programação simples, completa, e veloz.',
+    packages=['brzcode'],
+    install_requires=['colorama'],)
