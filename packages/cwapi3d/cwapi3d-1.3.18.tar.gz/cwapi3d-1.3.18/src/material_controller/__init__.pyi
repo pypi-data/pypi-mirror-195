4e3d709385277789fcf139400f0c683fbed2ab86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:09:15 +0000

    Add package cwapi3d/1.3.18/cwapi3d-1.3.18.tar.gz

---
+++ packages/cwapi3d/cwapi3d-1.3.18.tar.gz/cwapi3d-1.3.18/src/material_controller/__init__.pyi
@@ -0,0 +1,385 @@
+from typing import List
+
+
+def create_material(name: str) -> int: 
+    """
+
+    Args:
+        name (str): material name
+
+    Returns:
+        int: material ID
+    """
+def get_name(number: int) -> str: 
+    """Get material name
+    
+    [:information_source: Available for script filled attributes](#){.mark-text}
+
+    Args:
+        number (int): element ID
+
+    Returns:
+        str: material name
+    """
+def set_name(number: int, value: str) -> None: 
+    """
+
+    Args:
+        number (int): element ID
+        value (str): a material name
+    """
+def get_group(number: int) -> str: 
+    """get material group
+    
+    [:information_source: Available for script filled attributes](#){.mark-text}
+
+    Args:
+        number (int): element ID
+
+    Returns:
+        str: material group
+    """
+def set_group(number: int, value: str) -> None: 
+    """set material group
+
+    Args:
+        number (int): element ID
+        value (str): material group (name)
+    """
+def get_code(number: int) -> str: 
+    """get material code
+    
+    [:information_source: Available for script filled attributes](#){.mark-text}
+
+    Args:
+        number (int): element ID
+
+    Returns:
+        str: material code
+    """
+def set_code(number: int, value: str) -> None: 
+    """set material code
+
+    Args:
+        number (int): element ID
+        value (str): material code
+    """
+def get_modulus_elasticity_1(number: int) -> float: 
+    """
+    
+    [:information_source: Available for script filled attributes](#){.mark-text}
+
+    Args:
+        number (int): element ID
+
+    Returns:
+        float: value
+    """
+def set_modulus_elasticity_1(number: int, value: float) -> None: 
+    """
+
+    Args:
+        number (int): element ID
+        value (float): value
+    """
+def get_modulus_elasticity_2(number: int) -> float: 
+    """
+    
+    [:information_source: Available for script filled attributes](#){.mark-text}
+
+    Args:
+        number (int): element ID
+
+    Returns:
+        float: value
+    """
+def set_modulus_elasticity_2(number: int, value: float) -> None: 
+    """
+
+    Args:
+        number (int): element ID
+        value (float): value
+    """
+def get_modulus_elasticity_3(number: int) -> float: 
+    """
+    
+    [:information_source: Available for script filled attributes](#){.mark-text}
+
+    Args:
+        number (int): element ID
+
+    Returns:
+        float: value
+    """
+def set_modulus_elasticity_3(number: int, value: float) -> None: 
+    """
+
+    Args:
+        number (int): element ID
+        value (float): value
+    """
+def get_shear_modulus_1(number: int) -> float: 
+    """
+    
+    [:information_source: Available for script filled attributes](#){.mark-text}
+
+    Args:
+        number (int): element ID
+
+    Returns:
+        float: value
+    """
+def set_shear_modulus_1(number: int, value: float) -> None: 
+    """
+
+    Args:
+        number (int): element ID
+        value (float): value
+    """
+def get_shear_modulus_2(number: int) -> float: 
+    """
+    
+    [:information_source: Available for script filled attributes](#){.mark-text}
+
+    Args:
+        number (int): element ID
+
+    Returns:
+        float: value
+    """
+def set_shear_modulus_2(number: int, value: float) -> None: 
+    """
+
+    Args:
+        number (int): element ID
+        value (float): value
+    """
+def get_price(number: int) -> float: 
+    """
+    
+    [:information_source: Available for script filled attributes](#){.mark-text}
+
+    Args:
+        number (int): element ID
+
+    Returns:
+        float: value
+    """
+def set_price(number: int, value: float) -> None: 
+    """
+
+    Args:
+        number (int): element ID
+        value (float): value
+    """
+def get_price_type(number: int) -> str: 
+    """
+    [:information_source: Available for script filled attributes](#){.mark-text}
+
+    Args:
+        number (int): element ID
+
+    Returns:
+        str: _description_
+    """
+def set_price_type(number: int, value: str) -> None: 
+    """
+
+    Args:
+        number (int): element ID
+        value (str): _description_
+    """
+def get_thermal_conductivity(number: int) -> float: 
+    """
+    
+    [:information_source: Available for script filled attributes](#){.mark-text}
+
+    Args:
+        number (int): element ID
+
+    Returns:
+        float: value
+    """
+def set_thermal_conductivity(number: int, value: float) -> None: 
+    """
+
+    Args:
+        number (int): element ID
+        value (float): value
+    """
+def get_heat_capacity(number: int) -> float: 
+    """
+    
+    [:information_source: Available for script filled attributes](#){.mark-text}
+
+    Args:
+        number (int): element ID
+
+    Returns:
+        float: value
+    """
+def set_heat_capacity(number: int, value: float) -> None: 
+    """
+
+    Args:
+        number (int): element ID
+        value (float): value
+    """
+def get_u_min(number: int) -> float: 
+    """
+    
+    [:information_source: Available for script filled attributes](#){.mark-text}
+
+    Args:
+        number (int): element ID
+
+    Returns:
+        float: value
+    """
+def set_u_min(number: int, value: float) -> None: 
+    """
+
+    Args:
+        number (int): element ID
+        value (float): value
+    """
+def get_u_max(number: int) -> float: 
+    """
+    
+    [:information_source: Available for script filled attributes](#){.mark-text}
+
+    Args:
+        number (int): element ID
+
+    Returns:
+        float: value
+    """
+def set_u_max(number: int, value: float) -> None: 
+    """
+
+    Args:
+        number (int): element ID
+        value (float): value
+    """
+def get_fire_resistance_class(number: int) -> str: 
+    """
+    [:information_source: Available for script filled attributes](#){.mark-text}
+
+    Args:
+        number (int): element ID
+
+    Returns:
+        str: _description_
+    """
+def set_fire_resistance_class(number: int, value: str) -> None: 
+    """
+
+    Args:
+        number (int): element ID
+        value (str): _description_
+    """
+def get_smoke_class(number: int) -> str: 
+    """
+    
+    [:information_source: Available for script filled attributes](#){.mark-text}
+
+    Args:
+        number (int): element ID
+
+    Returns:
+        str: _description_
+    """
+def set_smoke_class(number: int, value: str) -> None: 
+    """
+
+    Args:
+        number (int): element ID
+        value (str): _description_
+    """
+def get_drop_forming_class(number: int) -> str: 
+    """
+    
+    [:information_source: Available for script filled attributes](#){.mark-text}
+
+    Args:
+        number (int): element ID
+
+    Returns:
+        str: _description_
+    """
+def set_drop_forming_class(number: int, value: str) -> None: 
+    """
+
+    Args:
+        number (int): element ID
+        value (str): _description_
+    """
+def get_burn_off_rate(number: int) -> float: 
+    """
+    
+    [:information_source: Available for script filled attributes](#){.mark-text}
+
+    Args:
+        number (int): element ID
+
+    Returns:
+        float: value
+    """
+def set_burn_off_rate(number: int, value: float) -> None: 
+    """
+
+    Args:
+        number (int): element ID
+        value (float): value
+    """
+def get_weight(number: int) -> float: 
+    """get material weight
+    
+    [:information_source: Available for script filled attributes](#){.mark-text}
+
+    Args:
+        number (int): element ID
+
+    Returns:
+        float: value
+    """
+def set_weight(number: int, value: float) -> None: 
+    """set material weight
+
+    Args:
+        number (int): element ID
+        value (float): value
+    """
+def get_weight_type(number: int) -> str: 
+    """gets the material weight type
+    
+    [:information_source: Available for script filled attributes](#){.mark-text}
+
+    Args:
+        number (int): element ID
+
+    Returns:
+        str: weight type
+    """
+def set_weight_type(number: int, value: str) -> None: 
+    """sets the material weight type
+
+    Args:
+        number (int): element ID
+        value (str): weight type
+    """
+def get_material_id(name: str) -> None: 
+    """
+    [:information_source: Available for script filled attributes](#){.mark-text}
+
+    Args:
+        name (str): material name
+    """
+def get_all_materials() -> List[int]: 
+    """
+    
+    [:information_source: Available for script filled attributes](#){.mark-text}
+
+    Returns:
+        List[int]: all materials
+    """
