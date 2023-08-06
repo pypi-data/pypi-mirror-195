ec10214600cb758b1e466ab102f0485569a6661f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:37:15 +0000

    Add package level4/0.0.1b0/level4-0.0.1b0.tar.gz

---
+++ packages/level4/level4-0.0.1b0.tar.gz/level4-0.0.1b0/level4/__init__.py
@@ -0,0 +1,7 @@
+from .model import EnvironmentProvider
+from .model import ManifestLoader
+from .model import ManifestStack
+
+__VERSION__ = '0.0.1b'
+
+__all__ = ['EnvironmentProvider', 'ManifestStack', 'ManifestLoader']
---
+++ packages/level4/level4-0.0.1b0.tar.gz/level4-0.0.1b0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='level4',
+    version='0.0.1b',
+    packages=['level4'],
+    url='',
+    license='',
+    author='Spencer Young',
+    author_email='spencer.young@yesolutions.io',
+    description='level4',
+)
