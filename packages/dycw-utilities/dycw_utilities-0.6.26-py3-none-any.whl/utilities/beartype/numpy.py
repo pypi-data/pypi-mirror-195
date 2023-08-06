1f1871b02fb05a2e25f132fd872f124fe14e77bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:05 +0000

    Add package dycw-utilities/0.6.26/dycw_utilities-0.6.26-py3-none-any.whl

---
+++ packages/dycw-utilities/dycw_utilities-0.6.26-py3-none-any.whl/utilities/beartype/numpy.py
@@ -0,0 +1,18 @@
+from beartype.vale import IsAttr, IsEqual
+
+from utilities.numpy import datetime64D, datetime64ns, datetime64Y
+
+# dtype
+DTypeB = IsAttr["dtype", IsEqual[bool]]
+DTypeDD = IsAttr["dtype", IsEqual[datetime64D]]
+DTypeDY = IsAttr["dtype", IsEqual[datetime64Y]]
+DTypeDns = IsAttr["dtype", IsEqual[datetime64ns]]
+DTypeF = IsAttr["dtype", IsEqual[float]]
+DTypeI = IsAttr["dtype", IsEqual[int]]
+DTypeO = IsAttr["dtype", IsEqual[object]]
+
+# ndim
+NDim0 = IsAttr["ndim", IsEqual[0]]
+NDim1 = IsAttr["ndim", IsEqual[1]]
+NDim2 = IsAttr["ndim", IsEqual[2]]
+NDim3 = IsAttr["ndim", IsEqual[3]]
