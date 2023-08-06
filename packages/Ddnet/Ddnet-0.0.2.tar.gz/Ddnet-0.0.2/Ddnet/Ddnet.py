0aac48ce5510db3761292871c362e86f63949bf2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:27:35 +0000

    Add package Ddnet/0.0.2/Ddnet-0.0.2.tar.gz

---
+++ packages/Ddnet/Ddnet-0.0.2.tar.gz/Ddnet-0.0.2/Ddnet/Ddnet.py
@@ -0,0 +1,6 @@
+# TAILab Main
+import pandas as pd
+import numpy as np
+
+def info():
+    print('wzh is pig and sb')
---
+++ packages/Ddnet/Ddnet-0.0.2.tar.gz/Ddnet-0.0.2/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md",'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name = "Ddnet",
+    version = "0.0.2",
+    author = "Zichuana",
+    author_email = "2092653757@qq.com",
+    description = "This is a test.",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    url="https://github.com/",
+    packages=setuptools.find_packages(),
+    install_requires=['pandas','matplotlib','numpy','scipy','pandas_profiling','folium','seaborn'],
+    # add any additional packages that needs to be installed along with SSAP package.
+
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
