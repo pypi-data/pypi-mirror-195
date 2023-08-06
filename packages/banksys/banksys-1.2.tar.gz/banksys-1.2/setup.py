f0d3bad848135b692a44e7802527754f4dd6f1df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:30:50 +0000

    Add package banksys/1.2/banksys-1.2.tar.gz

---
+++ packages/banksys/banksys-1.2.tar.gz/banksys-1.2/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+name = "banksys",
+author = "Akshat Sabharwal",
+version = "1.2",
+packages = ["banksys"],
+author_email = "akshatsabharwal35@gmail.com",
+description = "A system for Login and Sign Up with all the necessary functions such as sign_up() and login() with the support of MySql database",
+install_requires = [
+    'mysql.connector',
+    'maskpass',
+    'pyttsx3',
+    'numpy',
+    'speech_recognition'
+]
+)
