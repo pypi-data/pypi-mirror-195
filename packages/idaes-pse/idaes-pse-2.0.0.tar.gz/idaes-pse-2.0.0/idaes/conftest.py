8b4e6b411267f2a9a53d9bc3beb0d71122ccd8b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:33:50 +0000

    Add package idaes-pse/2.0.0/idaes-pse-2.0.0.tar.gz

---
+++ packages/idaes-pse/idaes-pse-2.0.0.tar.gz/idaes-pse-2.0.0/idaes/conftest.py
@@ -0,0 +1,132 @@
+#################################################################################
+# The Institute for the Design of Advanced Energy Systems Integrated Platform
+# Framework (IDAES IP) was produced under the DOE Institute for the
+# Design of Advanced Energy Systems (IDAES).
+#
+# Copyright (c) 2018-2023 by the software owners: The Regents of the
+# University of California, through Lawrence Berkeley National Laboratory,
+# National Technology & Engineering Solutions of Sandia, LLC, Carnegie Mellon
+# University, West Virginia University Research Corporation, et al.
+# All rights reserved.  Please see the files COPYRIGHT.md and LICENSE.md
+# for full copyright and license information.
+#################################################################################
+import pytest
+import sys
+
+####
+# Uncomment this to collect list of all test files
+# into 'test_files.txt'
+#
+# def pytest_collection_modifyitems(config, items):
+#     output = open("test_files.txt", "w")
+#     fspaths = set()
+#     for item in items:
+#         fspaths.add(item.fspath)
+#     for p in fspaths:
+#         output.write(str(p))
+#         output.write("\n")
+#     output.close()
+####
+
+####
+
+
+REQUIRED_MARKERS = {"unit", "component", "integration", "performance"}
+ALL_PLATFORMS = {"darwin", "linux", "win32"}
+
+
+@pytest.hookimpl
+def pytest_runtest_setup(item):
+    _validate_required_markers(
+        item, required_markers=REQUIRED_MARKERS, expected_count=1
+    )
+    _skip_for_unsupported_platforms(
+        item,
+        all_platforms=ALL_PLATFORMS,
+        negate_tag=(lambda tag: f"no{tag}"),
+    )
+
+
+def _skip_for_unsupported_platforms(item, all_platforms=None, negate_tag=None):
+    """
+    Only run the tests for your particular platform(s), if it is marked with those platform(s)
+    e.g. to mark tests for linux:
+
+    import pytest
+    @pytest.mark.linux
+    def test_something():
+       print("this only runs on linux")
+
+    In addition, you can use "no<platform>" to exclude
+
+    @pytest.mark.nowin32
+    def test_something():
+       print("this will not run on windows")
+
+    The names of the platforms should match what is returned by `sys.platform`, in particular:
+       Linux = 'linux'
+       Windows = 'win32'
+       macOS = 'darwin'
+    """
+
+    all_platforms = set(all_platforms or [])
+    if negate_tag is None:
+
+        def negate_tag(tag):
+            return f"no{tag}"
+
+    all_negated_platforms = {negate_tag(tag) for tag in all_platforms}
+    item_markers = {marker.name for marker in item.iter_markers()}
+    supported_platforms = all_platforms & item_markers
+    excluded_platforms = all_negated_platforms & item_markers
+    plat = sys.platform
+    if (excluded_platforms and negate_tag(plat) in excluded_platforms) or (
+        supported_platforms and plat not in supported_platforms
+    ):
+        pytest.skip("cannot run on platform {}".format(plat))
+
+
+def _validate_required_markers(item, required_markers=None, expected_count=1):
+    required_markers = set(required_markers or [])
+    item_markers = {marker.name for marker in item.iter_markers()}
+    required_markers_on_item = item_markers & required_markers
+    required_count = len(required_markers_on_item)
+    reason_to_fail = None
+    if required_count < expected_count:
+        reason_to_fail = "Too few required markers"
+    if required_count > expected_count:
+        reason_to_fail = "Too many required markers"
+    if reason_to_fail:
+        msg = (
+            f'{reason_to_fail} for test function "{item.name}". '
+            f"Expected: {expected_count} of {required_markers}; "
+            f"found: {required_markers_on_item or required_count}"
+        )
+        pytest.fail(msg)
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        "--performance",
+        action="store_true",
+        dest="performance",
+        default=False,
+        help="enable performance decorated tests",
+    )
+
+
+def pytest_configure(config):
+    if not config.option.performance:
+        if len(config.option.markexpr) > 0:
+            setattr(
+                config.option,
+                "markexpr",
+                f"{config.option.markexpr} and not performance",
+            )
+        else:
+            setattr(config.option, "markexpr", "not performance")
+    else:
+        setattr(config.option, "markexpr", "performance")
+
+
+####
---
+++ packages/idaes-pse/idaes-pse-2.0.0.tar.gz/idaes-pse-2.0.0/idaes/models_extra/power_generation/unit_models/helm/tests/test_turbine_stage.py
@@ -0,0 +1,84 @@
+#################################################################################
+# The Institute for the Design of Advanced Energy Systems Integrated Platform
+# Framework (IDAES IP) was produced under the DOE Institute for the
+# Design of Advanced Energy Systems (IDAES).
+#
+# Copyright (c) 2018-2023 by the software owners: The Regents of the
+# University of California, through Lawrence Berkeley National Laboratory,
+# National Technology & Engineering Solutions of Sandia, LLC, Carnegie Mellon
+# University, West Virginia University Research Corporation, et al.
+# All rights reserved.  Please see the files COPYRIGHT.md and LICENSE.md
+# for full copyright and license information.
+#################################################################################
+"""
+Tests for turbine stage model.
+
+Author: John Eslick
+"""
+import pytest
+
+from pyomo.environ import ConcreteModel
+
+from idaes.core import FlowsheetBlock
+from idaes.models_extra.power_generation.unit_models.helm import HelmTurbineStage
+from idaes.models.properties import iapws95
+from idaes.core.util.model_statistics import (
+    degrees_of_freedom,
+    activated_equalities_generator,
+)
+from idaes.core.solvers import get_solver
+from idaes.models.properties.general_helmholtz import helmholtz_available
+
+# Set up solver
+solver = get_solver()
+
+
+@pytest.fixture()
+def build_turbine():
+    m = ConcreteModel()
+    m.fs = FlowsheetBlock(dynamic=False)
+    m.fs.properties = iapws95.Iapws95ParameterBlock()
+    m.fs.turb = HelmTurbineStage(property_package=m.fs.properties)
+    return m
+
+
+@pytest.fixture()
+def build_turbine_dyn():
+    m = ConcreteModel()
+    m.fs = FlowsheetBlock(dynamic=True)
+    m.fs.properties = iapws95.Iapws95ParameterBlock()
+    m.fs.turb = HelmTurbineStage(dynamic=False, property_package=m.fs.properties)
+    return m
+
+
+@pytest.mark.skipif(not helmholtz_available(), reason="General Helmholtz not available")
+@pytest.mark.unit
+def test_basic_build(build_turbine):
+    """Make a turbine model and make sure it doesn't throw exception"""
+    m = build_turbine
+
+
+@pytest.mark.skipif(not helmholtz_available(), reason="General Helmholtz not available")
+@pytest.mark.component
+def test_initialize(build_turbine):
+    """Initialize a turbine model"""
+    m = build_turbine
+    # set inlet
+    m.fs.turb.inlet.enth_mol[0].value = 70000
+    m.fs.turb.inlet.flow_mol[0].value = 15000
+    m.fs.turb.inlet.pressure[0].value = 8e6
+    m.fs.turb.efficiency_isentropic.fix(0.8)
+    m.fs.turb.ratioP.fix(0.7)
+    m.fs.turb.initialize(outlvl=4)
+
+    eq_cons = activated_equalities_generator(m)
+    for c in eq_cons:
+        assert abs(c.body() - c.lower) < 1e-4
+    assert degrees_of_freedom(m) == 3  # inlet was't fixed and still shouldn't be
+
+
+@pytest.mark.skipif(not helmholtz_available(), reason="General Helmholtz not available")
+@pytest.mark.unit
+def test_report(build_turbine):
+    m = build_turbine
+    m.fs.turb.report()
