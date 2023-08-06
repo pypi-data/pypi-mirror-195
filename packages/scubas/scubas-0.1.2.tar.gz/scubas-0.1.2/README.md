1a5a1bdaa4919ed368b392e07c9ad1521d0bda47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:10:10 +0000

    Add package scubas/0.1.2/scubas-0.1.2.tar.gz

---
+++ packages/scubas/scubas-0.1.2.tar.gz/scubas-0.1.2/README.md
@@ -0,0 +1,13 @@
+# SCUBAS
+SUCBA: **S**ubmarine **C**ables **U**pset **b**y **A**uroral **S**treams.
+
+
+## Installation
+
+## Getting Started
+
+## Folder Structure
+
+## Contributing
+
+## Citing this Project
---
+++ packages/scubas/scubas-0.1.2.tar.gz/scubas-0.1.2/setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup
+
+setup(
+    name="scubas",
+    version="0.1.2",
+    packages=["scubas"],
+    package_dir={"scubas": "scubas"},
+    package_data={"scubas": []},
+    author="Shibaji Chakraborty",
+    author_email="shibaji7@vt.edu",
+    maintainer="Shibaji Chakraborty",
+    maintainer_email="shibaji7@vt.edu",
+    license="MIT License",
+    description="SCUBAS: Submarine Cables Upset by Auroral Streams.",
+    long_description="Model compute electrical surges in submarine cables induced by geomagnetic activities",
+    install_requires=[
+        "pandas",
+        "matplotlib>=3.2",
+        "pyproj",
+        "loguru",
+        "scipy",
+        "SciencePlots",
+    ],
+    keywords=[
+        "python",
+        "submarine cable",
+        "electrical surge",
+        "geomagnetic induction",
+        "geomagnetic disturbance",
+    ],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Education",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Operating System :: OS Independent",
+    ],
+    url="https://github.com/shibaji7/SCUBAS",
+)
