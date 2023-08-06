c67d5f5e1e865cbe9e2cfd48c1651ab178c7662c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:44:04 +0000

    Add package dyslexic-readability/0.0.9/dyslexic-readability-0.0.9.tar.gz

---
+++ packages/dyslexic-readability/dyslexic-readability-0.0.9.tar.gz/dyslexic-readability-0.0.9/setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+
+requirements_lines = [line.strip() for line in open("requirements.txt").readlines()]
+install_requires = list(filter(None, requirements_lines))
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="dyslexic-readability",
+    version="0.0.9",
+    author="Akin Gunduz",
+    author_email="akngndz93@gmail.com",
+    description="A readability scoring library tailored to the specific needs of Turkish dyslexic readers.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/AkinGunduz/dyslexic_readability_index",
+    project_urls={
+        "Bug Tracker": "https://github.com/AkinGunduz/dyslexic_readability_index/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    install_requires = install_requires,
+    python_requires=">=3.6",
+)
