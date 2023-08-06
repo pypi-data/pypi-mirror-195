aa5b434905fb9351a5ca4dca2df9e89e5560f640
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:03 +0000

    Add package fabra/0.5.2/fabra-0.5.2.tar.gz

---
+++ packages/fabra/fabra-0.5.2.tar.gz/fabra-0.5.2/src/fabra/models/operations/get_namespaces.py
@@ -0,0 +1,31 @@
+from __future__ import annotations
+import dataclasses
+import requests as requests_http
+from dataclasses_json import Undefined, dataclass_json
+from fabra import utils
+from typing import Optional
+
+
+@dataclasses.dataclass
+class GetNamespacesQueryParams:
+    connection_id: int = dataclasses.field(metadata={'query_param': { 'field_name': 'connectionID', 'style': 'form', 'explode': True }})
+    
+
+@dataclasses.dataclass
+class GetNamespacesRequest:
+    query_params: GetNamespacesQueryParams = dataclasses.field()
+    
+
+@dataclass_json(undefined=Undefined.EXCLUDE)
+@dataclasses.dataclass
+class GetNamespaces200ApplicationJSON:
+    namespaces: Optional[list[str]] = dataclasses.field(default=None, metadata={'dataclasses_json': { 'letter_case': utils.get_field_name('namespaces'), 'exclude': lambda f: f is None }})
+    
+
+@dataclasses.dataclass
+class GetNamespacesResponse:
+    content_type: str = dataclasses.field()
+    status_code: int = dataclasses.field()
+    get_namespaces_200_application_json_object: Optional[GetNamespaces200ApplicationJSON] = dataclasses.field(default=None)
+    raw_response: Optional[requests_http.Response] = dataclasses.field(default=None)
+    
