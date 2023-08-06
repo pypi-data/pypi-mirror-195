ec7e14d3b49b73e510ce1289adc19448bda26bb4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:12:57 +0000

    Add package bslz4-to-sparse/0.0.1/bslz4_to_sparse-0.0.1.tar.gz

---
+++ packages/bslz4-to-sparse/bslz4_to_sparse-0.0.1.tar.gz/bslz4_to_sparse-0.0.1/src/__init__.py
@@ -0,0 +1,39 @@
+
+import numpy as np
+from .bslz4_to_sparse import bslz4_u32, bslz4_u16
+
+version = '0.0.1'
+
+def bslz4_to_sparse( ds, num, cut, mask = None, pixelbuffer = None):
+    """
+    Reads a bitshuffle compressed hdf5 dataset and converts this 
+    directly into a sparse format (indices, values) when decoding
+    the data.
+    
+    ds = hdf5 dataset containing [nframes, ni, nj] pixels
+    num = frame number to read
+    cut = threshold, pixels below this value are ignored
+    mask = detector mask. Active pixels > 0.
+    pixelbuffer = None or (values, indices) storage space
+    
+    returns (number_of_pixels, (values, indices))
+    """
+    # fixme : h5py malloc free version see https://github.com/h5py/h5py/pull/2232
+    filtinfo, buffer = ds.id.read_direct_chunk( (num, 0, 0) )
+    npbuffer = np.frombuffer( buffer, np.uint8 )
+    if mask is None:
+        mask = np.ones( (ds.shape[1], ds.shape[2]), np.uint8 )
+    if pixelbuffer is None:
+        indices = np.empty( (ds.shape[1], ds.shape[2]), np.uint32 ).ravel()
+        values  = np.empty( (ds.shape[1], ds.shape[2]), ds.dtype  ).ravel()
+    else:
+        indices, values = pixelbuffer
+    if ds.dtype == np.uint16:
+        npixels = bslz4_u16(npbuffer, mask, values, indices, cut)
+    elif ds.dtype == np.uint32:
+        npixels = bslz4_u32(npbuffer, mask, values, indices, cut)
+    else:
+        raise Exception("no decoder for your type")
+    if npixels < 0:
+        raise Exception("Error decoding: %d"%(npixels))
+    return npixels, (values, indices)
---
+++ packages/bslz4-to-sparse/bslz4_to_sparse-0.0.1.tar.gz/bslz4_to_sparse-0.0.1/test/test1.py
@@ -0,0 +1,56 @@
+
+import h5py, hdf5plugin
+import numpy as np
+import sys
+sys.path.insert(0,'../build/lib.linux-x86_64-cpython-38')
+import bslz4_to_sparse
+print("Running from", bslz4_to_sparse.__file__)
+
+CASES = [( "/data/id11/jon/hdftest/eiger4m_u32.h5", "/entry_0000/ESRF-ID11/eiger/data"),
+         ( "/data/id11/nanoscope/blc12454/id11/WAu5um/WAu5um_DT3/scan0001/eiger_0000.h5",
+           "/entry_0000/ESRF-ID11/eiger/data"),
+         ("/data/id11/jon/hdftest/kevlar.h5", "/entry/data/data" ) ]
+
+
+indices = np.zeros(2)
+
+def pysparse( ds, num, cut, mask = None ):
+    frame = ds[num]
+    if mask is not None:
+        frame *= mask
+        assert frame.dtype == ds.dtype
+    pixels = frame > cut
+    values = frame[pixels]
+    global indices
+    if indices.size != frame.size:
+        indices = np.arange( frame.size )
+    return values, indices[pixels.ravel()]
+
+        
+def testok():
+    for hname, dset in CASES:
+        with h5py.File(hname, 'r') as hin:
+            dataset = hin[dset]
+            print(dataset.shape, dataset.dtype, hname)
+            mbool = dataset[0] == pow(2,16)-1
+            if dataset.dtype == np.uint32:
+                mbool |=(dataset[0] == pow(2,32)-1) 
+            mask = mbool.astype(np.uint8)
+            for frame in np.arange(0,len(dataset),len(dataset)//10):
+                for cut in (0,10,100,1000):
+                    pv, pi = pysparse( dataset, frame, cut, mask )
+                    npx, (cv, ci) = bslz4_to_sparse.bslz4_to_sparse( dataset, 
+                                                                    frame, cut, mask )
+                    if len(pv) != npx:
+                        print(npx, cv[:10],ci[:10])
+                        print(pv.shape, pv[:10],pi[:10])
+                        raise
+                    assert (cv[:npx] == pv).all()
+                    assert (ci[:npx] == pi).all()
+    #                    print('...', frame, cut, len(pv))
+    print('No errors found')
+
+if __name__=='__main__':
+    testok()
+                
+        # py-spy record -n -r 200 -f speedscope python3 test1.py
