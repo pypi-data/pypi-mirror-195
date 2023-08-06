8dff208789874df3de7ac5fab128844f1f5986b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:58:15 +0000

    Add package lftk/1.0b0/lftk-1.0b0.tar.gz

---
+++ packages/lftk/lftk-1.0b0.tar.gz/lftk-1.0b0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+setup(
+    name = 'lftk',  
+    version='1.0-beta',
+    license='CC BY-NC 4.0',   
+    description = 'Comprehensive Multilingual Linguistic Features Extraction in Python',
+    author = 'Bruce W. Lee',
+    author_email = 'brucelws@seas.upenn.edu', 
+    packages=find_packages('lftk'),
+    package_dir={'': 'lftk'},
+    url='https://github.com/brucewlee/lftk',
+    keywords='linguistic feature',
+    install_requires=[
+          'pandas',
+          'ndjson',
+          'spacy'
+      ],
+
+)
