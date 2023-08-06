ff791a8b336098a8d7b3a4d8e4f604aaed7b9965
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:57:44 +0000

    Add package nanoqa/0.0.24/nanoqa-0.0.24.tar.gz

---
+++ packages/nanoqa/nanoqa-0.0.24.tar.gz/nanoqa-0.0.24/nanoqa/schemas/__init__.py
@@ -0,0 +1,9 @@
+from .pipeline import PipelineResponse, PipelineAnswer
+from .reader import (
+    ReaderArguments,
+    AdapterTrainingArguments,
+    ChunkPrediction,
+    DocumentPrediction,
+    FinalPrediction,
+)
+from .retriever import Document, SplitUnit
