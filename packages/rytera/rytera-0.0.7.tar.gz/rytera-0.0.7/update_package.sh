9c7ef9c4f0a38e815c1a0f974bfb69ac7c51e3b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:11:45 +0000

    Add package rytera/0.0.7/rytera-0.0.7.tar.gz

---
+++ packages/rytera/rytera-0.0.7.tar.gz/rytera-0.0.7/update_package.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# build
+python3 -m build
+
+# publish new version
+USERNAME="vincent2303"
+PASSWORD="g9sA3h7tY8#xFfYs"
+
+echo "Uploading to PyPI..."
+echo "$USERNAME" | python3 -m twine upload --skip-existing --username "$USERNAME" --password "$PASSWORD" dist/*
