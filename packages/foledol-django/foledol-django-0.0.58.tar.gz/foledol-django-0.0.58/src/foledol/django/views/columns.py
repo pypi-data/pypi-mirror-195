7b192f2f0499b9db702425dc69c2a5deb965a41f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:19:49 +0000

    Add package foledol-django/0.0.58/foledol-django-0.0.58.tar.gz

---
+++ packages/foledol-django/foledol-django-0.0.58.tar.gz/foledol-django-0.0.58/src/foledol/django/views/columns.py
@@ -0,0 +1,25 @@
+from django.contrib.admin.views.decorators import staff_member_required
+from django.contrib.auth.decorators import login_required
+from foledol.django.tools.table import TableColumn, TableView
+
+from ..models import Column
+
+
+class ColumnTable(TableView):
+    def __init__(self, rows):
+        super().__init__(rows, [
+            TableColumn('label', "Libellé")
+        ], path='django:columns', search=True)
+        self.update = 'django:column_update'
+        self.create = 'django:column_create'
+
+    def select(self, search, sort):
+        if len(search) > 0:
+            return Column.objects.filter(label=search)
+        return Column.objects.all()
+
+
+@login_required
+@staff_member_required
+def column_list(request):
+    return ColumnTable(None).render(request)
