e2fc019323e17b9ea55dfe05245976460887cf90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:22 +0000

    Add package dreams/0.1.12.1/dreams-0.1.12.1-py3-none-any.whl

---
+++ packages/dreams/dreams-0.1.12.1-py3-none-any.whl/dreams/__init__.py
@@ -0,0 +1,27 @@
+'''
+it project is a idea for a easy way for getting video's porn data from the best site's content porn
+from internet.
+For it, please, help it.
+# '''
+# # import toml
+# import dreams.spankbang as bg
+# import dreams.ukevids as uk
+# import dreams.pornone as pn
+# import dreams.noodlemagazine as nd
+# import dreams.playvids as pv
+# #import dreams.xvideos as xv
+# #import dreams.pornhub as ph
+
+
+# spankbang = bg.search_porn
+
+# config = toml.load('settings/config.toml')
+# print(config['INFO'])
+from dreams.sites import pornone,tnaflix, ukdevilz,noodlemagazine
+from dreams.sites import spankbang
+
+all = [ukdevilz,spankbang,pornone,tnaflix,noodlemagazine]
+
+
+__version__ = '0.1.10'
+__author__ = 'ReinanBr <slimchatuba@gmail.com>'
