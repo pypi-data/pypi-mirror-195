efbf4c9abd2eee6aee223d9494cd58f87ff3b554
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:17:02 +0000

    Add package alienHTML/0.0.2/alienHTML-0.0.2.tar.gz

---
+++ packages/alienHTML/alienHTML-0.0.2.tar.gz/alienHTML-0.0.2/tests/test_webpage.py
@@ -0,0 +1,34 @@
+##########################
+# Test WebPage functions #
+##########################
+
+import unittest
+from alienHTML import WebPage
+
+class WebPageError(Exception):
+    def __init__(self,message="Could not load webpage"):
+        self.message=message
+        super().__init__(self.message)
+
+class TestWebPage(unittest.TestCase):
+    def setUp(self):
+        self.mypage = WebPage("./test.html","w",bgcolour="powderblue")
+    
+    def test_init(self):
+        self.assertEqual(self.mypage.file,"./test.html")
+        self.assertEqual(self.mypage.action, "w")
+        self.assertEqual(self.mypage.tabhead, "HTML with Python!")
+        self.assertEqual(self.mypage.bgcolour, "background-color:powderblue")
+    
+    def test_showfile(self):
+        __openfile = open("./test.html","rt")
+        self.__lines = __openfile.readlines()
+        self.assertEqual(self.__lines, self.mypage.List())
+        cleanup()
+
+def cleanup():
+    import os
+    os.remove("./test.html")
+    
+if __name__ == '__main__':
+    unittest.main()
