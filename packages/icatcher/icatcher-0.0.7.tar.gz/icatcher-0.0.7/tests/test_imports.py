697ed6b34574ebcf1a9a785193a8a55a7fe6813f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:32:47 +0000

    Add package icatcher/0.0.7/icatcher-0.0.7.tar.gz

---
+++ packages/icatcher/icatcher-0.0.7.tar.gz/icatcher-0.0.7/tests/test_imports.py
@@ -0,0 +1,10 @@
+def test_imports():
+    """
+    tests that the environment has the dependencies installed
+    @return:
+    """
+    import torch
+    import numpy as np
+    import pooch
+    import PIL
+    
