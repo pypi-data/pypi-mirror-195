4a4127a13900f7ca8c3d95b6278ef821aac89e06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:57:37 +0000

    Add package sonicare-bletb/0.0.5/sonicare_bletb-0.0.5-py3-none-any.whl

---
+++ packages/sonicare-bletb/sonicare_bletb-0.0.5-py3-none-any.whl/sonicare_bletb/__init__.py
@@ -0,0 +1,16 @@
+from __future__ import annotations
+
+__version__ = "0.0.5"
+
+
+from bleak_retry_connector import get_device
+
+from .sonicare_bletb import BLEAK_EXCEPTIONS, SonicareBLETB, SonicareBLETBState
+
+__all__ = [
+    "BLEAK_EXCEPTIONS",
+    "CharacteristicMissingError",
+    "SonicareBLETB",
+    "SonicareBLETBState",
+    "get_device",
+]
