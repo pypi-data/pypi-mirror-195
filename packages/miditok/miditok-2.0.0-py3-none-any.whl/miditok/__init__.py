a25d18cbadb78d64d2614fbc0827c1547dd83f06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:11:23 +0000

    Add package miditok/2.0.0/miditok-2.0.0-py3-none-any.whl

---
+++ packages/miditok/miditok-2.0.0-py3-none-any.whl/miditok/__init__.py
@@ -0,0 +1,15 @@
+from .tokenizations import (
+    MIDILike,
+    REMI,
+    TSD,
+    Structured,
+    Octuple,
+    OctupleMono,
+    CPWord,
+    MuMIDI,
+)
+from .midi_tokenizer import MIDITokenizer
+from .classes import Event, TokSequence
+
+from .utils import utils
+from .data_augmentation import data_augmentation
