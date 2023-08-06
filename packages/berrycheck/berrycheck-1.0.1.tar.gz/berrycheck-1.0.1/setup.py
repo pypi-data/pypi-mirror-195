3b897edc906b12d0ec5205a808d6602a7348abda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:17:04 +0000

    Add package berrycheck/1.0.1/berrycheck-1.0.1.tar.gz

---
+++ packages/berrycheck/berrycheck-1.0.1.tar.gz/berrycheck-1.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+with open("README.md", "r") as arq:
+    readme = arq.read()
+
+setup(name='berrycheck',
+    version='1.0.1',
+    license='MIT License',
+    author='Eduardo de Sousa',
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    author_email='eduardosousa1718@gmail.com',
+    keywords='berry check',
+    description=u'Fornecer aos usuários uma maneira fácil e acessível de obter cotações atualizadas de várias moedas, incluindo as principais moedas do mundo, para fins de negociação, investimento ou outras finalidades financeiras. Além dos preços de ações das principais empresas brasileiras.',
+    packages=['berry_check'],
+    install_requires=['requests', 'bs4', 'typing', 'yfinance'],)
