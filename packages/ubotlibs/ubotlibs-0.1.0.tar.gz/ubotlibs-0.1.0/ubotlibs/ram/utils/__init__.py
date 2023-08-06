7b1bc4a044bd0cabf7d0dcae9032560a4cc515ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:12:49 +0000

    Add package ubotlibs/0.1.0/ubotlibs-0.1.0.tar.gz

---
+++ packages/ubotlibs/ubotlibs-0.1.0.tar.gz/ubotlibs-0.1.0/ubotlibs/ram/utils/__init__.py
@@ -0,0 +1,5 @@
+from ubotlibs.ram.utils.misc import *
+from ubotlibs.ram.utils.pastebin import *
+from ubotlibs.ram.utils.sections import *
+from ubotlibs.ram.utils.tools import *
+from ubotlibs.ram.utils._adzan import __fetch_adzan
