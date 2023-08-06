12b8bb091655a02cd0c0232466ef337a6306d578
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:10:20 +0000

    Add package hyc-utils/0.5.33/hyc-utils-0.5.33.tar.gz

---
+++ packages/hyc-utils/hyc-utils-0.5.33.tar.gz/hyc-utils-0.5.33/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+import pathlib
+
+this_directory = pathlib.Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name='hyc-utils',
+    version='0.5.33',
+    description='Commonly used tools across my own personal projects',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'matplotlib',
+        'statsmodels',
+        'torch',
+        'tomli',
+        'h5py',
+        'pandas',
+        'tables', # optional dependency for pandas that is needed here
+        'tqdm',
+        'mat73',
+        'platformdirs',
+    ],
+    extras_require={
+        'test': ['pytest', 'scipy'],
+    },
+    include_package_data=True,
+)
