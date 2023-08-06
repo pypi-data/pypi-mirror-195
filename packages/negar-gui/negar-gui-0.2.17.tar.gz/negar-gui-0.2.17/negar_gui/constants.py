53525e5273017339a53cff189c2c1ea02e006310
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:21 +0000

    Add package negar-gui/0.2.17/negar-gui-0.2.17.tar.gz

---
+++ packages/negar-gui/negar-gui-0.2.17.tar.gz/negar-gui-0.2.17/negar_gui/constants.py
@@ -0,0 +1,10 @@
+from pathlib import Path
+import platform
+
+__version__ = "0.2.17"
+
+if platform.system() == 'Windows':
+    LOGO = (":/images/icons/logo_small.png")
+else:
+    LOGO = (Path(__file__).parent.absolute()/"icons/logo.png").as_posix()
+
