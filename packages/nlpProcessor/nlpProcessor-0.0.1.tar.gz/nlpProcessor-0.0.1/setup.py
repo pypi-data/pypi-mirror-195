ea583dbc0e86d7c02b01f08a704ffd5b4e4f7c4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:24:58 +0000

    Add package nlpProcessor/0.0.1/nlpProcessor-0.0.1.tar.gz

---
+++ packages/nlpProcessor/nlpProcessor-0.0.1.tar.gz/nlpProcessor-0.0.1/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+VERSION = '0.0.1'
+DESCRIPTION = 'Natural language processing package'
+LONG_DESCRIPTION = 'This package deals with emojis, abbreviations, contraction words etc. by removing or replacing them.'
+
+# Setting up
+setup(
+    name="nlpProcessor",
+    version=VERSION,
+    author="Afnanurrahim Ansari",
+    author_email="afnanurrahim150102@gmail.com",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['pandas','emoji'],
+    keywords=['nlp','natural language processing','processor'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
