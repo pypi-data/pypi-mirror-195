3b3a7080b5d3b26740fb748afec4bc4c4abdcc0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:59:17 +0000

    Add package streamlit-datalist/0.0.5/streamlit_datalist-0.0.5-py3-none-any.whl

---
+++ packages/streamlit-datalist/streamlit_datalist-0.0.5-py3-none-any.whl/streamlit_datalist/frontend/build/precache-manifest.315dace4a9cc8b0b86a6e26a95ace93c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfafe4481068a2b8abaab7f7679e922e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "965376dbfc6508f1ad2a",
+    "url": "./static/js/2.96dc815c.chunk.js"
+  },
+  {
+    "revision": "3dc2da520b7611f998bec3f8b32ee24e",
+    "url": "./static/js/2.96dc815c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c225e386f6761b930b46",
+    "url": "./static/js/main.b5ef2d84.chunk.js"
+  },
+  {
+    "revision": "b06f8eed806227215735",
+    "url": "./static/js/runtime-main.5ae4fbea.js"
+  }
+]);
