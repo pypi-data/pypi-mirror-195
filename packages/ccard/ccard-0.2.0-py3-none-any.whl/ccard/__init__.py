76cbfafbf2af28e69ead18e7288bbf656e193ef9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:33:56 +0000

    Add package ccard/0.2.0/ccard-0.2.0-py3-none-any.whl

---
+++ packages/ccard/ccard-0.2.0-py3-none-any.whl/ccard/__init__.py
@@ -0,0 +1,46 @@
+from __future__ import unicode_literals
+from os.path import abspath, join, dirname
+import random
+
+
+__name__ = 'ccard'
+__title__ = 'ccard'
+__version__ = '0.2.0'
+__author__ = 'David Samir'
+__email__ = 'dsamir@davidsamir.com'
+__link__ = 'https://github.com/davidsamir/credit-card-generator/'
+__info__ = 'credit card generator'
+__email__ = 'dsamir@davidsamir.com'
+__license__ = 'MIT'
+
+
+full_path = lambda filename: abspath(join(dirname(__file__), filename))
+
+
+FILES = {
+    'visa': full_path('visa'),
+    'discover': full_path('discover'),
+    'mastercard': full_path('mastercard'),
+    'americanexpress': full_path('americanexpress'),
+}
+
+
+def get_number(filename):
+    selected = random.random() * 30000
+    with open(filename) as name_file:
+        for index,line in enumerate(name_file):
+            if index > selected:
+                return int(line)
+    return ''  # Return empty string if file is empty
+
+def visa():
+    return get_number(FILES['visa'])
+
+def discover():
+    return get_number(FILES['discover'])
+
+def mastercard():
+    return get_number(FILES['mastercard'])
+
+def americanexpress():
+    return get_number(FILES['americanexpress'])
