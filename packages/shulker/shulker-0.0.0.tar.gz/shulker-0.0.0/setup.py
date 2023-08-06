90927710ae40a3396d5d989e3eb69ce62f248c1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:43:32 +0000

    Add package shulker/0.0.0/shulker-0.0.0.tar.gz

---
+++ packages/shulker/shulker-0.0.0.tar.gz/shulker-0.0.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='shulker',
+    version='',
+    author='PortalHubYT',
+    author_email='portalhub.business@gmail.com',
+    description='A minecraft interface using RCON',
+    url='https://github.com/PortalHubYT/mc_api',
+    packages=['shulker'],
+    install_requires=['docker', 'mctools', 'pillow', 'mcstatus'], # add your package dependencies here
+)
