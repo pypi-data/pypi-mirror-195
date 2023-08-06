ee53219a7d73109235bacd9e7f1184775a62c110
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:58:30 +0000

    Add package autotui/0.4.6/autotui-0.4.6.tar.gz

---
+++ packages/autotui/autotui-0.4.6.tar.gz/autotui-0.4.6/autotui/__init__.py
@@ -0,0 +1,25 @@
+import sys
+import warnings
+
+
+# check python version
+if sys.version_info[0] == 3 and sys.version_info[1] < 8:
+    warnings.warn("autotui requires at least python3.8")
+
+from .namedtuple_prompt import (
+    namedtuple_prompt_funcs,
+    prompt_namedtuple,
+    AutoHandler,
+)
+from .serialize import (
+    serialize_namedtuple,
+    deserialize_namedtuple,
+)
+from .fileio import (
+    namedtuple_sequence_dump,
+    namedtuple_sequence_dumps,
+    namedtuple_sequence_load,
+    namedtuple_sequence_loads,
+)
+from .options import options
+from .pick import pick_namedtuple
