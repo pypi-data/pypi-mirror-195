6a8c789c731bc65f2c6cb04c704a564a92858967
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:13:55 +0000

    Add package flare-utilities/0.0.8/Flare_Utilities-0.0.8.tar.gz

---
+++ packages/flare-utilities/Flare_Utilities-0.0.8.tar.gz/Flare_Utilities-0.0.8/setup.py
@@ -0,0 +1,56 @@
+import io
+import os
+
+from setuptools import find_packages, setup
+
+
+def read(*paths, **kwargs):
+    """Read the contents of a text file safely.
+    >>> read("README.md")
+    ...
+    """
+
+    content = ""
+    print(os.path.join(os.path.dirname(__file__), *paths))
+    with io.open(
+        os.path.join(os.path.dirname(__file__), *paths),
+        encoding=kwargs.get("encoding", "utf8"),
+    ) as open_file:
+        content = open_file.read().strip()
+    return content
+
+
+def read_requirements(path):
+    return [
+        line.strip()
+        for line in read(path).split("\n")
+        if not line.startswith(('"', "#", "-", "git+"))
+    ]
+
+
+# def clean_project_directory():
+#     return os.system('rm -vrf ./build ./dist ./*.pyc ./*.tgz ./*.egg-info')
+
+
+setup(
+    name="Flare_Utilities",
+    version="0.0.8",
+    description="""
+        Flare Utilities having support libraries for Flare-BDD framework for reporting purposes.
+    """,
+    long_description=read("README.md"),
+    long_description_content_type="text/markdown",
+    author="Aravinth Alliraj",
+    packages=find_packages('Reporting'),
+    package_dir={'': 'Reporting'},
+    # install_requires=read_requirements("requirements.txt"),
+    install_requires=[
+        'pyarmor'
+    ],
+    classifiers=[
+          'Intended Audience :: Developers',
+          'Operating System :: MacOS :: MacOS X',
+          'Operating System :: Microsoft :: Windows',
+          'Programming Language :: Python'
+    ],
+)
