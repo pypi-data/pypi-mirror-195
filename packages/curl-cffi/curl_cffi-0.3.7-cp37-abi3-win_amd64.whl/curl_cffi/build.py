55582031ea47cf6ba260297d4ee987d7ef579751
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:06 +0000

    Add package curl-cffi/0.3.7/curl_cffi-0.3.7-cp37-abi3-win_amd64.whl

---
+++ packages/curl-cffi/curl_cffi-0.3.7-cp37-abi3-win_amd64.whl/curl_cffi/build.py
@@ -0,0 +1,38 @@
+import os
+import platform
+
+from cffi import FFI
+
+ffibuilder = FFI()
+# arch = "%s-%s" % (os.uname().sysname, os.uname().machine)
+uname = platform.uname()
+
+
+ffibuilder.set_source(
+    "curl_cffi._wrapper",
+    """
+        #include "shim.h"
+    """,
+    libraries=["curl-impersonate-chrome"] if uname.system != "Windows" else ["libcurl"],
+    library_dirs=[
+        "/usr/local/lib" if uname.system == "Linux" else
+        "/Users/runner/work/_temp/install/lib" if platform.uname().system == "Darwin" else
+        "./lib"
+    ],
+    source_extension=".c",
+    include_dirs=[
+        os.path.join(os.path.dirname(__file__), "include"),
+    ],
+    sources=[
+        os.path.join(os.path.dirname(__file__), "shim.c"),
+    ],
+    # extra_link_args=["-Wl,-rpath,$ORIGIN/../libcurl/" + arch],
+)
+
+with open(os.path.join(os.path.dirname(__file__), "cdef.c")) as f:
+    cdef_content = f.read()
+    ffibuilder.cdef(cdef_content)
+
+
+if __name__ == "__main__":
+    ffibuilder.compile(verbose=False)
