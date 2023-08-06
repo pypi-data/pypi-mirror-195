76fd2a5952447b58d9723447a43783fec1e2dfb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:23:20 +0000

    Add package sixdrepnet/0.1.3/sixdrepnet-0.1.3.tar.gz

---
+++ packages/sixdrepnet/sixdrepnet-0.1.3.tar.gz/sixdrepnet-0.1.3/sixdrepnet/__init__.py
@@ -0,0 +1,11 @@
+from sixdrepnet.regressor import SixDRepNet_Detector as SixDRepNet
+
+
+"""
+6DRepNet.
+
+Accurate and unconstrained head pose estimation.
+"""
+
+__version__ = "0.1.3"
+__author__ = 'Thorsten Hempel'
