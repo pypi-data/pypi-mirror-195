7d6f1c72e4c0e470ce2da4e605e811501965ecbc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:01 +0000

    Add package estimenergy/1.5.0/estimenergy-1.5.0.tar.gz

---
+++ packages/estimenergy/estimenergy-1.5.0.tar.gz/estimenergy-1.5.0/estimenergy/main.py
@@ -0,0 +1,55 @@
+
+import os
+import asyncio
+from fastapi import FastAPI
+from tortoise.contrib.fastapi import register_tortoise
+import yaml
+
+from estimenergy.routers import energy_data, collector
+from estimenergy.models import Collector
+from estimenergy.common import instrumentator, settings
+
+
+app = FastAPI(
+    title="EstimEnergy",
+)
+
+register_tortoise(
+    app,
+    db_url=f"sqlite://{settings.db_path}",
+    modules={"models": ["estimenergy.models"]},
+    generate_schemas=True,
+    add_exception_handlers=True,
+)
+
+instrumentator.instrument(app)
+instrumentator.expose(app, include_in_schema=True)
+
+app.include_router(energy_data.router)
+app.include_router(collector.router)
+
+@app.on_event("startup")
+async def start_energy_collectors():
+    
+    with open(settings.config_path) as f:
+        config_dict = yaml.safe_load(f)
+
+    collector_names = [collector_dict["name"] for collector_dict in config_dict["collectors"]]
+    collectors = await Collector.all()
+    
+    for collector in collectors:
+        if collector.name not in collector_names:
+            await collector.delete()
+    
+
+    for collector_dict in config_dict["collectors"]:
+        collector = await Collector.filter(name=collector_dict["name"]).first()
+        if collector is None:
+            collector = Collector(**collector_dict)
+            await collector.save()
+    
+
+    collectors = await Collector.all()
+
+    for collector in collectors:
+        asyncio.create_task(collector.connect())
