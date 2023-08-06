c9939eba33d69bc0a6f7241733c00ca89d380da2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:07 +0000

    Add package colorpalette-master/0.0.7/colorpalette-master-0.0.7.tar.gz

---
+++ packages/colorpalette-master/colorpalette-master-0.0.7.tar.gz/colorpalette-master-0.0.7/cpmaster/__init__.py
@@ -0,0 +1,13 @@
+"""
+CPM stands for Color Palette Algorithms
+https://github.com/gejirz/colorpalette-master - you can visit this script repository on github
+In order for you to use functions from here, I tried my best to briefly document my work.
+Feel free to contact me for any help! mail - superavb222@gmail.com
+"""
+from sys import path
+from pathlib import Path
+path.append(str(Path(__file__).parent.resolve()))
+
+from ___main___ import output_palette, output_image_with_palette, output_palette_img
+
+__version__ = "0.0.7"
