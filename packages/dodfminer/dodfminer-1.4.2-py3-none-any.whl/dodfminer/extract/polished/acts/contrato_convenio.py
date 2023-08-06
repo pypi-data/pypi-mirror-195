05b96dc1553650e3d71d5dd23f4d8f11e9b8d9a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:58:31 +0000

    Add package dodfminer/1.4.2/dodfminer-1.4.2-py3-none-any.whl

---
+++ packages/dodfminer/dodfminer-1.4.2-py3-none-any.whl/dodfminer/extract/polished/acts/contrato_convenio.py
@@ -0,0 +1,191 @@
+import warnings
+warnings.filterwarnings('ignore')
+
+import pandas as pd
+import joblib
+import nltk
+import json
+import re
+import os
+
+from sklearn.pipeline import Pipeline
+from dodfminer.extract.polished.backend.pipeline import feature_extractor, PipelineCRF
+
+class Contrato_Convenio():
+
+  @property
+  def acts_str(self):
+    if len(self.atos_encontrados) == 0: return []
+    return self.atos_encontrados['texto']
+
+  def __init__(self, file, backend = None, pipeline = None):
+    self.pipeline = pipeline
+    self.filename = file
+    self.file = None
+    self.atos_encontrados = []
+    self.predicted = []
+    self.data_frame = []
+    self.enablePostProcess = True
+    self.useDefault = True
+
+    # Inicializar fluxo
+    self.flow()
+
+  def flow(self):
+    self.load()
+    if len(self.atos_encontrados) == 0: 
+      self.data_frame = pd.DataFrame()
+      return 
+    self.ner_extraction()
+    if self.enablePostProcess: 
+      self.post_process()
+    else:
+      self.data_frame = pd.DataFrame(self.predicted)
+
+  def load(self):
+    # Load model
+    if self.pipeline is None:
+      f_path = os.path.dirname(__file__)
+      f_path += '/models/modelo_contrato_convenio.pkl'
+      contrato_convenio_model = joblib.load(f_path)
+      pipeline_CRF_default = Pipeline([('feat', feature_extractor()), ('crf', PipelineCRF(contrato_convenio_model))])
+      self.pipeline = pipeline_CRF_default
+    else:
+      self.useDefault = False
+      try:
+        self.pipeline['pre-processing'].transform(["test test"])
+      except KeyError:
+        self.enablePostProcess = False
+
+    # Segmentation
+    if self.filename[-5:] == '.json':
+      with open(self.filename, 'r') as f:
+        self.file = json.load(f)
+        self.atos_encontrados = self.segment(self.file)
+    else:
+      pass
+
+  def segment(self, file):
+    atos_contrato_convenio = {
+      'numero_dodf':[],
+      'titulo':[],
+      'texto':[]
+    }
+    df_atos_contrato_convenio = None
+
+    principal_contrato = r'(?:CONTRATO)'
+
+    regex_titulo_contrato = r'(?:(EXTRATO\sD[OE]\sCONTRATO)|(CONTRATO\sSIMPLIFICADO)|(CONTRATO\sPARA\sAQUISIÇÃO)|(^CONTRATO)'\
+                            '|(EXTRATO\sD[OE]\sTERMO\sD[OE]\sCONTRATO\sNº\s)|(EXTRATO\sAO\sCONTRATO)|(CONTRATO\sDE\sPRESTAÇÃO\sDE\sSERVIÇOS\sNº)'\
+                            '|(CONTRATO\sDE\sPATROCÍNIO)|(CONTRATO\sDE\sCONCESSÃO\sDE)|(^CONTRATO\sNº)|(^EXTRATOS\sDE\sCONTRATO[S]*))'
+
+    principal_convenio = r'(?:(CONVÊNIO)|(CONVENIO))'
+
+    regex_titulo_convenio = r'(?:(EXTRATO\sD[OE]\sCONVÊNIO)|(EXTRATO\sDE\sTERMO\sDE\sCONVÊNIO)|(CONVÊNIO\sSIMPLIFICADO)|(CONVÊNIO\sPARA\sAQUISIÇÃO)|(^CONVÊNIO)'\
+                            '|(EXTRATO\sD[OE]\sTERMO\sD[OE]\sCONVÊNIO\sNº\s)|(EXTRATO\sAO\sCONVÊNIO)|(CONVÊNIO\sDE\sPRESTAÇÃO\sDE\sSERVIÇOS\sNº)'\
+                            '|(CONVÊNIO\sDE\sPATROCÍNIO)|(CONVÊNIO\sDE\sCONCESSÃO\sDE)|(^CONVÊNIO\sNº)|(^EXTRATOS\sDE\sCONVÊNIO[S]*))'
+    
+    try:
+
+      section_3 = file['json']['INFO']['Seção III']
+
+      for orgao in section_3:
+        for documento in section_3[orgao]:
+          for ato in section_3[orgao][documento]:
+
+            titulo = section_3[orgao][documento][ato]['titulo']
+
+            if re.search(principal_contrato, titulo) is not None:
+              if re.search(r'(?:ADITIVO)', titulo) is None:
+                if re.search(regex_titulo_contrato, titulo) is not None:
+                  if 'termo aditivo ao contrato' not in section_3[orgao][documento][ato]['texto'].lower():
+                    atos_contrato_convenio['numero_dodf'].append(file['json']['nu_numero'])
+                    atos_contrato_convenio['titulo'].append(titulo)
+                    atos_contrato_convenio['texto'].append(re.sub(r'<[^>]*>', '', titulo + " " + section_3[orgao][documento][ato]['texto']))
+
+            if re.search(principal_convenio, titulo) is not None:
+              if re.search(r'(?:ADITIVO)', titulo) is None:
+                if re.search(regex_titulo_convenio, titulo) is not None:
+                  if 'termo aditivo ao con' not in section_3[orgao][documento][ato]['texto'].lower():
+                    atos_contrato_convenio['numero_dodf'].append(file['json']['nu_numero'])
+                    atos_contrato_convenio['titulo'].append(titulo)
+                    atos_contrato_convenio['texto'].append(re.sub(r'<[^>]*>', '', titulo + " " + section_3[orgao][documento][ato]['texto']))
+
+      df_atos_contrato_convenio = pd.DataFrame(atos_contrato_convenio)
+    except KeyError:
+      print(f"Chave 'Seção III' não encontrada no DODF {file['lstJornalDia']}!")
+    print(f"Foram encontrados {len(atos_contrato_convenio['texto'])} atos de contrato/convênio")
+    return df_atos_contrato_convenio
+
+  def ner_extraction(self):
+    pred = self.pipeline.predict(self.atos_encontrados['texto'])
+    self.predicted = pred
+
+  # Montar dataframe com as predições e seus IOB's
+  def post_process(self):
+    for IOB, text, numdodf, titulo in zip(self.predicted, self.atos_encontrados['texto'], self.atos_encontrados['numero_dodf'], self.atos_encontrados['titulo']):
+      ent_dict = {
+        'numero_dodf': '',
+        'titulo': '',
+        'text': '',
+      } 
+      ent_dict['numero_dodf'] = numdodf
+      ent_dict['titulo'] = titulo
+      ent_dict['text'] = ""
+
+      if self.useDefault:
+        text_split = nltk.word_tokenize(text)
+      else:
+        text_split = self.pipeline['pre-processing'].transform([text])[0]
+
+      ent_list = []
+
+      aux_text_token = []
+      aux_text_string = ""
+
+      i = 0
+      while i < len(IOB):
+          current_ent = {
+              "name": [],
+              "start": None,
+              "end": None
+          }
+
+          if "B-" in IOB[i]:
+              entity_name = IOB[i].replace("B-", "")
+              aux_text_string = " ".join(aux_text_token).strip()
+              aux_text_token.append(text_split[i])
+
+              current_ent["start"] = len(aux_text_string) + 1
+              current_ent["name"].append(text_split[i])
+
+              i += 1
+
+              while (i < len(IOB)) and ("I-" in IOB[i]):
+                  current_ent["name"].append(text_split[i])
+                  aux_text_token.append(text_split[i])
+
+                  i += 1
+
+              aux_text_string = " ".join(aux_text_token)
+              current_ent["end"] = len(aux_text_string)
+              current_ent["name"] = " ".join(current_ent["name"]).strip()
+              ent_list.append(current_ent)
+              if entity_name in ent_dict:
+          
+                new_list = [ent_dict[entity_name]]
+     
+                new_list.append(current_ent)
+                ent_dict[entity_name] = new_list
+              else:
+                ent_dict[entity_name] = current_ent
+          
+          elif IOB[i] == 'O':
+              aux_text_token.append(text_split[i])
+              aux_text_string = " ".join(aux_text_token).strip()
+
+          i += 1
+
+      ent_dict['text'] = aux_text_string
+      self.data_frame.append(ent_dict)
+    self.data_frame = pd.DataFrame(self.data_frame)
