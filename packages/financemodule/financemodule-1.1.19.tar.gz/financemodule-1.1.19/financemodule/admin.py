ce9b44b85d5433fe8ccf9f3df79eccee209e223c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:44:34 +0000

    Add package financemodule/1.1.19/financemodule-1.1.19.tar.gz

---
+++ packages/financemodule/financemodule-1.1.19.tar.gz/financemodule-1.1.19/financemodule/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Finance , FinanceAccounting , Exchangerate
+
+
+# Register your models here.
+admin.site.register(Finance)
+admin.site.register(FinanceAccounting)
+admin.site.register(Exchangerate)
