90c82f1d39c87700f916c1eb793d45e95beb2eb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:05 +0000

    Add package pyPhasesRecordloader/0.2.7/pyPhasesRecordloader-0.2.7.tar.gz

---
+++ packages/pyPhasesRecordloader/pyPhasesRecordloader-0.2.7.tar.gz/pyPhasesRecordloader-0.2.7/pyPhasesRecordloader/__init__.py
@@ -0,0 +1,4 @@
+from .RecordLoader import RecordLoader, AnnotationInvalid, AnnotationNotFound, ParseError, ChannelsNotPresent
+from .Event import Event as Event
+from .RecordSignal import RecordSignal as RecordSignal
+from .Signal import Signal as Signal
