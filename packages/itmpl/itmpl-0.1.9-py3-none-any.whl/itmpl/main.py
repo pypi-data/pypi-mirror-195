f804e1ad642ce1cd75162a5c8d2317d544c59edf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:17:39 +0000

    Add package itmpl/0.1.9/itmpl-0.1.9-py3-none-any.whl

---
+++ packages/itmpl/itmpl-0.1.9-py3-none-any.whl/itmpl/main.py
@@ -0,0 +1,165 @@
+import subprocess
+from pathlib import Path
+from typing import Optional
+
+import typer
+from rich import print
+from typer import Typer
+
+from itmpl import config, global_vars, templating, utils
+
+app = Typer()
+app.add_typer(config.app, name="config", help="Manage iTmpl configuration.")
+
+
+@app.command("list")
+def list_():
+    """List all available templates."""
+    print(
+        utils.construct_table_from_templates(
+            templating.get_template_options().values(),
+        ),
+    )
+
+
+@app.command()
+def new(
+    template: str,
+    name: str,
+    path: Path = typer.Option(
+        Path("."),
+        "--path",
+        "-p",
+        exists=True,
+        help="The path to create the project in.",
+    ),
+    force: bool = typer.Option(
+        False,
+        "--force",
+        "-f",
+        help="Overwrite any files that already exist without prompting.",
+    ),
+):
+    """Create a new project from a template.
+
+    Parameters
+    ----------
+    template : str
+        The name of the template to use.
+    name : str
+        The name of the project to create.
+    path : Path
+        The path to create the project in.
+    force : bool
+        If True, overwrite any files that already exist without prompting.
+    """
+    try:
+        template_options = templating.get_template_options()
+    except templating.DuplicateTemplateError as e:
+        print("[red]Duplicate templates found:[/red]")
+        print(utils.construct_table_from_templates(e.duplicate_templates.values()))
+        print("[red]Please remove the duplicates and try again.[/red]")
+        raise typer.Exit(1)
+
+    if template not in template_options:
+        print(
+            f"[red]Template [white]{template}[/white] not found. "
+            f"Available templates:[/red]"
+        )
+        print(utils.construct_table_from_templates(template_options.values()))
+        raise typer.Exit(1)
+
+    # Allow the user to template in this directory
+    path = path.resolve()
+    if path.name == name:
+        path = path.parent
+
+    destination = path / name
+
+    template_path = template_options[template][0]
+    template_metadata = template_options[template][1]
+
+    try:
+        templating.render_template(
+            project_name=name,
+            template=template,
+            destination=destination,
+            template_path=template_path,
+            exclude=template_metadata.metadata.templating_excludes,
+            prompt_if_duplicates=not force,
+        )
+    except templating.TemplatingException as e:
+        print(f"[red]Error when templating project:[/red] {e}")
+        raise typer.Exit(1)
+
+    print(f"Created [green]{template}[/green] project at [green]{destination}[/green]")
+
+
+@app.command()
+def deps(template: Optional[str] = typer.Argument(None)):
+    """Install dependencies for the specified template. If no template is specified,
+    install dependencies for all templates."""
+    try:
+        template_options = templating.get_template_options()
+    except templating.DuplicateTemplateError as e:
+        print("[red]Duplicate templates found:[/red]")
+        print(utils.construct_table_from_templates(e.duplicate_templates.values()))
+        print("[red]Please remove the duplicates and try again.[/red]")
+        raise typer.Exit(1)
+
+    if template is not None and template not in template_options:
+        print(
+            f"[red]Template [white]{template}[/white] not found. "
+            f"Available templates:[/red]"
+        )
+        print(utils.construct_table_from_templates(template_options.values()))
+        raise typer.Exit(1)
+
+    if template is None:
+        templates_with_dependencies = [
+            (template_name, toml_obj.metadata.template_requirements)
+            for template_name, (_, toml_obj) in template_options.items()
+        ]
+    else:
+        templates_with_dependencies = [
+            (template, template_options[template][1].metadata.template_requirements),
+        ]
+
+    to_install = [
+        (template_name, dependencies)
+        for template_name, dependencies in templates_with_dependencies
+        if dependencies
+    ]
+
+    if not to_install:
+        print("[green]No dependencies to install.[/green]")
+        raise typer.Exit(0)
+
+    for template_name, dependencies in to_install:
+        print(
+            f"[green]Installing dependencies for template "
+            f"[white]{template_name}[/white][/green]"
+        )
+        try:
+            utils.install_dependencies(dependencies)
+        except subprocess.CalledProcessError as e:
+            print(
+                f"[red]Error installing dependencies for template "
+                f"[white]{template_name}[/white]: {e}[/red]"
+            )
+
+    print("[green]Done.[/green]")
+
+
+@app.callback()
+def create_directories():
+    """Create directories used by iTmpl. This is called automatically when iTmpl is
+    run."""
+    global_vars.APP_DIR.mkdir(parents=True, exist_ok=True)
+    global_vars.TEMPLATES_DIR.mkdir(parents=True, exist_ok=True)
+    c = config.read_config()
+    c.extra_templates_dir.mkdir(parents=True, exist_ok=True)
+
+
+if __name__ == "__main__":
+    app()
---
+++ packages/itmpl/itmpl-0.1.9-py3-none-any.whl/itmpl/utils.py
@@ -0,0 +1,67 @@
+import importlib.util
+import subprocess
+import sys
+from datetime import datetime
+from pathlib import Path
+from types import ModuleType
+from typing import Iterable, List, Tuple
+
+from rich.table import Table
+
+from itmpl.metadata import ItmplToml
+
+
+def import_external_module(module_path: Path) -> ModuleType:
+    """Import a module from an external path."""
+    spec = importlib.util.spec_from_file_location("itmpl", module_path)
+
+    if spec is None:
+        raise ValueError(f"No module found at path: {module_path}")
+
+    module = importlib.util.module_from_spec(spec)
+
+    if spec.loader is None:
+        raise ValueError(f"No loader found for module: {module_path}")
+
+    try:
+        spec.loader.exec_module(module)
+    except FileNotFoundError:
+        raise ValueError(f"No module found at path: {module_path}")
+
+    return module
+
+
+def construct_table_from_templates(
+    templates: Iterable[Tuple[Path, ItmplToml]],
+) -> Table:
+    """Construct a Rich table from a list of templates."""
+    table = Table(show_header=True, header_style="bold")
+    table.add_column("Template", justify="left", no_wrap=True, header_style="blue")
+    table.add_column("Description")
+    table.add_column("Requirements")
+
+    for template, toml_obj in templates:
+        table.add_row(
+            template.name,
+            toml_obj.metadata.template_description,
+            ", ".join(toml_obj.metadata.template_requirements),
+        )
+
+    return table
+
+
+def install_dependencies(dependencies: List[str]):
+    """Install dependencies."""
+    subprocess.check_call(
+        [sys.executable, "-m", "pip", "install", *dependencies],
+    )
+
+
+def get_current_year() -> str:
+    """Get the current year."""
+    return str(datetime.now().year)
+
+
+def get_current_datetime() -> str:
+    """Get the current datetime."""
+    return str(datetime.now().isoformat(timespec="seconds"))
