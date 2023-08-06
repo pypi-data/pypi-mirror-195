648bdd977190068610a3f21118497ac2ce24ee2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:47:45 +0000

    Add package aeon/0.0.0/aeon-0.0.0.tar.gz

---
+++ packages/aeon/aeon-0.0.0.tar.gz/aeon-0.0.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='aeon',
+    version='0.0.0',
+    packages=[],
+    url='',
+    license='BSD 3-Clause License',
+    author='aeon developers',
+    author_email='',
+    description=''
+)
