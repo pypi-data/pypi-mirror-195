50cc2eb6c235ec9345d0076ac103a6dc819d310e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:12:28 +0000

    Add package sigils/0.2.1/sigils-0.2.1.tar.gz

---
+++ packages/sigils/sigils-0.2.1.tar.gz/sigils-0.2.1/sigils/sigils.py
@@ -0,0 +1,29 @@
+from .tools import splice
+from .contexts import local_context
+
+
+class Sigil(str):
+    """Encapsulate a string that can contain sigils.
+    When an instance of this class has its __str__ method called,
+    the text gets passed through resolve automatically.
+    """
+
+    def __init__(self, original: str, **kwargs):
+        self.original = original
+        self.kwargs = kwargs
+
+    def __str__(self):
+        """Resolve the sigil."""
+        return splice(self.original, **self.kwargs)
+    
+    def __repr__(self):
+        """Return a representation of the sigil."""
+        return self.original
+
+    def __call__(self, *args, **kwargs):
+        """Send all args and kwargs to context, then resolve."""
+        with local_context(*args, **kwargs):
+            return splice(self.original, **self.kwargs)
+
+
+__all__ = ["Sigil"]
