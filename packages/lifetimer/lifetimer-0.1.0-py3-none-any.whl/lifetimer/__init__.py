aa12798bf8074b7edab6d1d73488307e64566c33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:36:30 +0000

    Add package lifetimer/0.1.0/lifetimer-0.1.0-py3-none-any.whl

---
+++ packages/lifetimer/lifetimer-0.1.0-py3-none-any.whl/lifetimer/__init__.py
@@ -0,0 +1,4 @@
+from lifetimer.lifetimer import Lifetimer
+
+__all__ = ("Lifetimer",)
+__version__ = "0.1.0"
---
+++ packages/lifetimer/lifetimer-0.1.0-py3-none-any.whl/lifetimer/__main__.py
@@ -0,0 +1,7 @@
+import sys
+
+from .cli import execute_from_command_line
+
+
+if __name__ == "__main__":
+    sys.exit(execute_from_command_line())
