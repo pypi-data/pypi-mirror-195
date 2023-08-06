f6fcf8515639dc089fa8f1a1b060df560f66221d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:37 +0000

    Add package aishell/2023.3.3.post1/aishell-2023.3.3.post1-py3-none-any.whl

---
+++ packages/aishell/aishell-2023.3.3.post1-py3-none-any.whl/aishell/cli.py
@@ -0,0 +1,71 @@
+import os
+import sys
+import webbrowser
+
+import pyperclip
+import rich
+import typer
+from rich.console import Console
+from yt_dlp.cookies import SUPPORTED_BROWSERS
+
+from aishell.adapters.openai_cookie_adapter import OpenAICookieAdapter
+from aishell.exceptions import UnauthorizedAccessError
+from aishell.models.language_model import LanguageModel
+from aishell.query_clients import GPT3Client, OfficialChatGPTClient, QueryClient, ReverseEngineeredChatGPTClient
+
+cli_app = typer.Typer()
+
+
+def _open_chatgpt_browser():
+    CHATGPT_LOGIN_URL = 'https://chat.openai.com/auth/login?next=/chat'
+    webbrowser.open(CHATGPT_LOGIN_URL)
+
+
+def _ask_user_copy_session_token_to_clipboard(session_token: str) -> None:
+    copy_session_token = typer.confirm('Do you want to copy the session token to your clipboard?')
+    if copy_session_token:
+        pyperclip.copy(session_token)
+        rich.print(
+            'Session token copied to clipboard. [bold]`export CHATGPT_SESSION_TOKEN=<session_token>`[/bold] to set it.'
+        )
+
+
+@cli_app.command()
+def ask(question: str, language_model: LanguageModel = LanguageModel.REVERSE_ENGINEERED_CHATGPT):
+    query_client: QueryClient
+    if language_model == LanguageModel.GPT3:
+        query_client = GPT3Client()
+    elif language_model == LanguageModel.OFFICIAL_CHATGPT:
+        query_client = OfficialChatGPTClient()
+    elif language_model == LanguageModel.REVERSE_ENGINEERED_CHATGPT:
+        try:
+            query_client = ReverseEngineeredChatGPTClient()
+        except UnauthorizedAccessError:
+            print('You are not logged in to OpenAI, attempting to log you in...')
+            _open_chatgpt_browser()
+            BROWSER_NAME = typer.prompt(f'Which browser did you use to log in? [{SUPPORTED_BROWSERS}]')
+            adapter = OpenAICookieAdapter(BROWSER_NAME)
+            session_token = adapter.get_openai_session_token()
+            if session_token is not None:
+                os.environ['CHATGPT_SESSION_TOKEN'] = session_token
+                _ask_user_copy_session_token_to_clipboard(session_token)
+                ask(question, language_model)
+            else:
+                print('Failed to log in.')
+            sys.exit()
+
+    query_client.query(question)
+
+    console = Console()
+    with console.status(
+            f'''
+[green] AiShell is thinking of `{question}` ...[/green]
+
+[dim]AiShell is not responsible for any damage caused by the command executed by the user.[/dim]'''.strip(), ):
+        response = query_client.query(question)
+    console.print(f'AiShell: {response}\n')
+
+    will_execute = typer.confirm('Execute this command?')
+
+    if will_execute:
+        os.system(response)
