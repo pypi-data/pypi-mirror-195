65a8883f05dbd96c1200058c3e501efe3be2c1f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:38:05 +0000

    Add package sqltrack/0.1.2/sqltrack-0.1.2-py3-none-any.whl

---
+++ packages/sqltrack/sqltrack-0.1.2-py3-none-any.whl/sqltrack/commands/migrate.py
@@ -0,0 +1,82 @@
+from __future__ import annotations
+
+from typing import Iterable
+from pathlib import Path
+
+import psycopg as pg
+
+from ..client import Client
+from ..queries import first_values
+
+
+__all__ = [
+    "migrate"
+]
+
+
+def migrate(client: Client, paths: Iterable[str | Path]):
+    """
+    Execute SQL scripts to setup/migrate the database.
+    The included `base.sql` script is always executed first.
+    User-defined scripts are run in the order they are given.
+    A script is never run twice.
+    Whether a script has already been run before is determined by filename.
+    Thus `base.sql` cannot be used as filename for user-defined scripts.
+
+    Example script:
+        BEGIN;
+
+        ALTER TABLE metrics
+            ADD COLUMN train_start TIMESTAMP WITH TIME ZONE,
+            ADD COLUMN train_end TIMESTAMP WITH TIME ZONE,
+            ADD COLUMN train_loss FLOAT,
+            ADD COLUMN train_top1 FLOAT,
+            ADD COLUMN train_top5 FLOAT,
+            ADD COLUMN val_start TIMESTAMP WITH TIME ZONE,
+            ADD COLUMN val_end TIMESTAMP WITH TIME ZONE,
+            ADD COLUMN val_loss FLOAT,
+            ADD COLUMN val_top1 FLOAT,
+            ADD COLUMN val_top5 FLOAT,
+            ADD COLUMN test_start TIMESTAMP WITH TIME ZONE,
+            ADD COLUMN test_end TIMESTAMP WITH TIME ZONE,
+            ADD COLUMN test_loss FLOAT,
+            ADD COLUMN test_top1 FLOAT,
+            ADD COLUMN test_top5 FLOAT;
+
+        END;
+
+    Parameters:
+        client: Client to connect to the database
+        paths: Paths to SQL scripts; executed in the order that they are given
+    """
+    with client.connect() as conn:
+        with conn.cursor() as cursor:
+            # create the schema if it does not exist
+            if client.schema is not None:
+                schema = client.schema
+                cursor.execute(f"CREATE SCHEMA IF NOT EXISTS {schema};")
+                conn.commit()
+                print("Schema:", schema)
+            # try to get names of applied migration files
+            try:
+                applied_names = set(first_values(
+                    cursor, "SELECT name FROM applied_migrations;"))
+            except pg.ProgrammingError:
+                applied_names = set()
+                conn.rollback()
+            # base schema file is stored in the same folder as this module
+            base = Path(__file__).parent / "base.sql"
+            # execute scripts if they have not yet been applied
+            for path in (base,) + tuple(paths):
+                path = Path(path)
+                if str(path.name) in applied_names:
+                    print("(OK)", path.name)
+                else:
+                    with open(path, encoding='UTF-8') as fp:
+                        script = fp.read()
+                    cursor.execute(script)
+                    cursor.execute(
+                        "INSERT INTO applied_migrations (name) VALUES (%s)",
+                        (str(path.name),),
+                    )
+                    print("(NEW)", path.name)
---
+++ packages/sqltrack/sqltrack-0.1.2-py3-none-any.whl/sqltrack/util.py
@@ -0,0 +1,35 @@
+import itertools as it
+import os
+from configparser import ConfigParser
+from pathlib import Path
+
+
+def load_config(path=None):
+    """
+    Parse config file.
+    Parameters may be overridden by `SQLTRACK_DSN_<PARAM>` environment variables.
+    Path defaults to `./sqltrack.conf`.
+    """
+    path = Path(path if path is not None else "sqltrack.conf")
+    parser = ConfigParser()
+    # parse config file, if it exists
+    if path.exists():
+        with open(path, encoding='UTF-8') as fp:
+            parser.read_file(it.chain(("[DEFAULT]",), fp))
+    config = dict(parser["DEFAULT"])
+    # check environment for overrides
+    for k, v in os.environ.items():
+        if k.startswith("SQLTRACK_DSN_"):
+            _, _, name = k.partition("SQLTRACK_DSN_")
+            config[name.lower()] = v
+    return config
+
+
+def coalesce(*values):
+    """
+    Returns the first none-None value.
+    """
+    for v in values:
+        if v is not None:
+            return v
+    return None
