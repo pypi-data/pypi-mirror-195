55d1d2f1e74239563bfcd9072af0029e73c3ba3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:16:09 +0000

    Add package pycaracal/0.14.4/pycaracal-0.14.4.tar.gz

---
+++ packages/pycaracal/pycaracal-0.14.4.tar.gz/pycaracal-0.14.4/python/tests/test_experimental.py
@@ -0,0 +1,16 @@
+import logging
+
+from pycaracal import Probe, experimental, set_log_level, utilities
+
+
+def test_prober():
+    prober = experimental.Prober(
+        utilities.get_default_interface(), 100, 1024 * 1024, 1, True
+    )
+    probes = [
+        Probe("8.8.4.4", 24000, 33434, 32, "icmp"),
+        Probe("8.8.4.4", 24000, 33434, 32, "udp"),
+    ]
+    set_log_level(logging.DEBUG)
+    replies = prober.probe(probes, 1000)
+    assert isinstance(replies, list)
