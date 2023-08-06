7b6a345e5350d35d623e69074dbefdb094cb4099
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:17:34 +0000

    Add package govee-btled-h613b/0.0.4/govee_btled_h613b-0.0.4.tar.gz

---
+++ packages/govee-btled-h613b/govee_btled_h613b-0.0.4.tar.gz/govee_btled_h613b-0.0.4/src/govee_btled_H613B/models.py
@@ -0,0 +1,12 @@
+from __future__ import annotations
+
+from dataclasses import dataclass
+
+
+@dataclass(frozen=True)
+class GoveeState:
+
+    power: bool = False
+    rgb: tuple[int, int, int] = (0, 0, 0)
+    w: int = 0
+    brightness: int = 0
