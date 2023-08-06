9edb4e52d82c6d0464199f59158caa002cc033c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:12:10 +0000

    Add package flipwizard/1.0.0/flipwizard-1.0.0.tar.gz

---
+++ packages/flipwizard/flipwizard-1.0.0.tar.gz/flipwizard-1.0.0/setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+	name="flipwizard",
+	version = "1.0.0",
+	author = "zappy",
+	description = "An api wrapper for https://bloxflip.com",
+	packages=["flipwizard"]
+)
