c57e80421f0202ce852292ad46a8c42e9a21921d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:57:56 +0000

    Add package qthreading/0.0.1/qthreading-0.0.1.tar.gz

---
+++ packages/qthreading/qthreading-0.0.1.tar.gz/qthreading-0.0.1/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+with open('README.md') as readme_file:
+    README = readme_file.read()
+
+with open('HISTORY.md') as history_file:
+    HISTORY = history_file.read()
+
+setup_args = dict(
+    name='qthreading',
+    version='0.0.1',
+    description='balanced threadpool',
+    long_description_content_type="text/markdown",
+    long_description=README + '\n\n' + HISTORY,
+    license='MIT',
+    packages=find_packages(),
+    author='laghmari khalil',
+    author_email='laghmari.khalil@gmail.com',
+    keywords=['thread', 'pool', 'balance'],
+    #url='https://github.com/',
+    #download_url='https://pypi.org/project/qthreading/'
+)
+
+install_requires = [
+]
+
+if __name__ == '__main__':
+    setup(
+		**setup_args, 
+		install_requires=install_requires,
+		include_package_data = True
+	)
