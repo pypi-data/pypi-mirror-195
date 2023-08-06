34a90734ac988f45e764e8bbced029bb87c3c0b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:59:17 +0000

    Add package streamlit-datalist/0.0.5/streamlit_datalist-0.0.5.tar.gz

---
+++ packages/streamlit-datalist/streamlit_datalist-0.0.5.tar.gz/streamlit_datalist-0.0.5/streamlit_datalist/frontend/build/precache-manifest.b6b025c5479c67524b3ceb6dc4cd27c9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09187d3daf72b8c7ffced4da0260f63e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "08bafb0fdc02211d2ed4",
+    "url": "./static/js/2.2cdf07e4.chunk.js"
+  },
+  {
+    "revision": "3dc2da520b7611f998bec3f8b32ee24e",
+    "url": "./static/js/2.2cdf07e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d70e330cce6db2d54bcc",
+    "url": "./static/js/main.f77b83ac.chunk.js"
+  },
+  {
+    "revision": "b06f8eed806227215735",
+    "url": "./static/js/runtime-main.5ae4fbea.js"
+  }
+]);
