de34ec0cb40e31c2754f72485a488417e0ab3f2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:24 +0000

    Add package fflogsapi/0.5.0/fflogsapi-0.5.0-py3-none-any.whl

---
+++ packages/fflogsapi/fflogsapi-0.5.0-py3-none-any.whl/fflogsapi/data/__init__.py
@@ -0,0 +1,11 @@
+from .dataclasses import FFLogsAbility, FFLogsActor
+from .page import FFLogsPage, FFLogsPageIterator, FFLogsPaginationIterator
+
+__all__ = [
+    'FFLogsPage',
+    'FFLogsPageIterator',
+    'FFLogsPaginationIterator',
+
+    'FFLogsAbility',
+    'FFLogsActor',
+]
---
+++ packages/fflogsapi/fflogsapi-0.5.0-py3-none-any.whl/fflogsapi/reports/__init__.py
@@ -0,0 +1,7 @@
+from .fight import FFLogsFight
+from .report import FFLogsReport
+
+__all__ = [
+    'FFLogsReport',
+    'FFLogsFight',
+]
