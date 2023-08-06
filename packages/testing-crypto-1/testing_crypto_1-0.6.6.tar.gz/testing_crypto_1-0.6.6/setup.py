7faada065ac1553e3b974175ba0a81911bb1b4d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:14 +0000

    Add package testing-crypto-1/0.6.6/testing_crypto_1-0.6.6.tar.gz

---
+++ packages/testing-crypto-1/testing_crypto_1-0.6.6.tar.gz/testing_crypto_1-0.6.6/setup.py
@@ -0,0 +1,23 @@
+# __Author__ = "Pranav Chandran"
+# __Date__ = 05-03-2023
+# __Time__ = 12:26
+# __FileName__ = setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name='testing_crypto_1',
+    version='0.6.6',
+    description='Crypting code',
+    author='Pranav Chandran',
+    author_email='pranav.chandran@gmail.com',
+    packages=find_packages(),  # same as name if we are not using any subpackages
+    package_data={'testing_crypto_1': ['test1.py']},
+    install_requires=['pyarmor'],  # from pypi
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
+
