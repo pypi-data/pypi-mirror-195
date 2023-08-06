37bb8f03914c1fca86b100db4e749bed21d17203
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:47:47 +0000

    Add package incharge/1.0.3/incharge-1.0.3.tar.gz

---
+++ packages/incharge/incharge-1.0.3.tar.gz/incharge-1.0.3/README.md
@@ -0,0 +1,25 @@
+[![codecov](https://codecov.io/gh/tgriek/incharge/branch/main/graph/badge.svg?token=SZPKLIUKS2)](https://codecov.io/gh/tgriek/incharge)
+# Vattenfall InCharge
+Python implementation of the Vattenfall InCharge API
+
+## Usage
+
+```python
+from incharge.api import InCharge
+
+api = InCharge(<username>, <password>)
+
+#Get a list of stations
+stations = api.get_stations().json()
+print(stations)
+
+#Get total consumption data for one station 
+data = api.get_station_consumption(<station_name>).json()
+print(data[0]['total'])
+```
+## Description
+This package currently supports
+
+`api.get_stations()` Get list of stations.
+
+`api.get_station_consumption(station_name, since_date)` Get consumption of a single station based on its name.
---
+++ packages/incharge/incharge-1.0.3.tar.gz/incharge-1.0.3/incharge/api.py
@@ -0,0 +1,85 @@
+"""API for VattenFall InCharge integration."""
+
+import functools
+import requests
+
+from requests import Response
+from requests.auth import HTTPBasicAuth
+from incharge.const import (
+    API_ACCEPT_HEADER,
+    API_AUTH_PATH,
+    API_AUTH_SUB_KEY_HEADER,
+    API_BASE_URL,
+    API_ENDPOINT_SUB_KEY_HEADER,
+    API_GET_STATION_CONSUMPTION_PATH,
+    API_GET_STATIONS_PATH,
+    API_REFERER_HEADER,
+)
+
+
+class InCharge:
+    """Implementation of the InCharge API."""
+
+    def __init__(self, username: str, password: str):
+        """Init with username and password."""
+        self.username = username
+        self.password = password
+        self.jwt_token = ""
+
+    def authenticate(self) -> Response:
+        """Auth and retrieve JWT token."""
+        headers = {**API_AUTH_SUB_KEY_HEADER, **API_REFERER_HEADER}
+        response = requests.post(
+            timeout=10000,
+            url=API_BASE_URL + API_AUTH_PATH,
+            headers=headers,
+            auth=HTTPBasicAuth(self.username, self.password),
+        )
+        self.jwt_token = response.headers.get("Authorization")
+        return response
+
+    @staticmethod
+    def auth(func):
+        """Auth decorator"""
+
+        @functools.wraps(func)
+        def wrap(self, *args, **kwargs):
+            auth_response = self.authenticate()
+            if not self.jwt_token:
+                return auth_response
+            else:
+                return func(self, *args, **kwargs)
+
+        return wrap
+
+    @auth
+    def get_stations(self) -> Response:
+        """Get list of charging stations."""
+        headers = {
+            "Authorization": self.jwt_token,
+            **API_ENDPOINT_SUB_KEY_HEADER,
+            **API_REFERER_HEADER,
+        }
+        response = requests.get(
+            timeout=10000, url=API_BASE_URL + API_GET_STATIONS_PATH, headers=headers
+        )
+        return response
+
+    @auth
+    def get_station_consumption(
+        self, station_name: str, since_date: str = "2000-01-01T00%3A00%3A00.00Z"
+    ) -> Response:
+        """Get consumption data for one charging station."""
+        headers = {
+            "Authorization": self.jwt_token,
+            **API_ENDPOINT_SUB_KEY_HEADER,
+            **API_REFERER_HEADER,
+            **API_ACCEPT_HEADER,
+        }
+
+        response = requests.get(
+            timeout=10000,
+            url=f"{API_BASE_URL}{API_GET_STATION_CONSUMPTION_PATH}{station_name}?since={since_date}",
+            headers=headers,
+        )
+        return response
