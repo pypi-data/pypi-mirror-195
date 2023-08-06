f683d849f1041bd3c52cdb6fb6cb424a57682ebd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:46:05 +0000

    Add package zakpy/0.0.1/zakpy-0.0.1.tar.gz

---
+++ packages/zakpy/zakpy-0.0.1.tar.gz/zakpy-0.0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+setup(
+    name = 'zakpy',
+    version = '0.0.1',
+    description = 'Say Hi to new developer',
+    py_modules = ['zakpy'],
+    package_dir = {'': 'src'}
+)
