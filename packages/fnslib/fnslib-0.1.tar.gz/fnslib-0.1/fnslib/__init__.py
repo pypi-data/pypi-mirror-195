e335dcc37fc1cc3a93a2d82a94c875e29f36eafb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:09:15 +0000

    Add package fnslib/0.1/fnslib-0.1.tar.gz

---
+++ packages/fnslib/fnslib-0.1.tar.gz/fnslib-0.1/fnslib/__init__.py
@@ -0,0 +1,25 @@
+import openai
+fnsload = """ ╔═══╗╔═╗ ╔╗╔═══╗╔╗     ╔╗  
+ ║╔══╝║║╚╗║║║╔═╗║║║     ║║  
+ ║╚══╗║╔╗╚╝║║╚══╗║║   ╔╗║╚═╗
+ ║╔══╝║║╚╗║║╚══╗║║║ ╔╗╠╣║╔╗║
+╔╝╚╗  ║║ ║║║║╚═╝║║╚═╝║║║║╚╝║
+╚══╝  ╚╝ ╚═╝╚═══╝╚═══╝╚╝╚══╝
+                            
+                            
+"""
+def getgpt(tkn, req):
+	openai.api_key = tkn
+	resp = openai.Completion.create(
+		engine="text-davinci-002",
+		prompt=req,
+		max_tokens=1024,
+		n=1,
+		stop=None,
+		temperature=0.5,
+	).get("choices")[0].text
+	return resp
+
+def getpass(symb):
+	return 'Generating pass, please wait...'
+
