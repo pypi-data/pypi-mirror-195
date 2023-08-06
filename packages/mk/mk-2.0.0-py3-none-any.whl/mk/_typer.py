7a020b88c682f7b9c0d7dd84f4daefbf0bf5e99b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:20:49 +0000

    Add package mk/2.0.0/mk-2.0.0-py3-none-any.whl

---
+++ packages/mk/mk-2.0.0-py3-none-any.whl/mk/_typer.py
@@ -0,0 +1,49 @@
+import typer
+
+# from click_help_colors import HelpColorsCommand, HelpColorsGroup
+
+
+# class CustomHelpColorsGroup(HelpColorsGroup):
+#     def __init__(self, *args, **kwargs) -> None:
+#         super().__init__(*args, **kwargs)
+#         self.help_headers_color = "blue"
+#         self.help_options_color = "yellow"
+
+
+# class CustomHelpColorsCommand(HelpColorsCommand):
+#     def __init__(self, *args, **kwargs) -> None:
+#         super().__init__(*args, **kwargs)
+#         self.help_headers_color = "blue"
+#         self.help_options_color = "yellow"
+
+
+class CustomTyper(typer.Typer):
+    def __init__(
+        self,
+        *args,
+        cls=None,  # CustomHelpColorsGroup,
+        context_settings={
+            "help_option_names": ["-h", "--help"],
+        },
+        width: int = 80,
+        **kwargs,
+    ) -> None:
+        context_settings["max_content_width"] = width
+        super().__init__(
+            *args,
+            cls=cls,
+            context_settings=context_settings,
+            no_args_is_help=True,
+            **kwargs,
+        )
+
+    def command(
+        self,
+        *args,
+        cls=None,  # cls=CustomHelpColorsCommand,
+        context_settings={"help_option_names": ["-h", "--help"]},
+        **kwargs,
+    ):
+        return super().command(
+            *args, cls=cls, context_settings=context_settings, **kwargs
+        )
