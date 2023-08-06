37dd946ae3b86abe42a3dfa4f655fd104ea765e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:15:15 +0000

    Add package color-fade/0.0.0/color-fade-0.0.0.tar.gz

---
+++ packages/color-fade/color-fade-0.0.0.tar.gz/color-fade-0.0.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+setup(
+    name="color-fade",
+    author="FabianNWO",
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python'],
+    classifiers=[
+        "Operating System :: Microsoft :: Windows"
+    ]
+)
