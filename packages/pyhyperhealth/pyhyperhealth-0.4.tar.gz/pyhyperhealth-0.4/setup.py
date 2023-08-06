60b912c89060ed8d71b77aaf51e6f409465e21c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:14:49 +0000

    Add package pyhyperhealth/0.4/pyhyperhealth-0.4.tar.gz

---
+++ packages/pyhyperhealth/pyhyperhealth-0.4.tar.gz/pyhyperhealth-0.4/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='pyhyperhealth',
+    version='0.4',
+    packages=['pyhyperhealth'],
+    url='https://github.com/Dcode99/Blockchain-Healthcare-Project/',
+    license='Apache 2.0',
+    author='Dillon Tate',
+    author_email='dillon.tate@uky.edu',
+    description='Python Hyperledger Iroha Healthcare Permissions Library',
+    install_requires=['iroha',
+                     'flask',
+                     're',
+                     'binascii',
+                     'sys'],
+
+)
