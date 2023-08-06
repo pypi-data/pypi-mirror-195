fdcb60e7f25eba499ad6ca6e12ece78ae76f4393
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:14:16 +0000

    Add package novem/0.3.0/novem-0.3.0.tar.gz

---
+++ packages/novem/novem-0.3.0.tar.gz/novem-0.3.0/novem/vis/files.py
@@ -0,0 +1,37 @@
+from novem.exceptions import Novem404
+
+from ..api_ref import NovemAPI
+
+
+class NovemFiles(object):
+    """
+    Novem Files
+    """
+
+    # api: NovemAPI = None
+
+    def __init__(self, api: NovemAPI) -> None:
+        """ """
+        self.api: NovemAPI = api
+
+    def get(self, fn: str) -> str:
+        try:
+            ctnt = self.api.api_read(f"/files/{fn}")
+        except Novem404:
+            ctnt = ""
+
+        return ctnt
+
+    @property
+    def txt(self) -> str:
+        """
+        return novem text file
+        """
+        return self.get(f"{self.api._type}.txt")
+
+    @property
+    def ansi(self) -> str:
+        """
+        return novem ansi representation file
+        """
+        return self.get(f"{self.api._type}.ansi")
