5901a6a79ec197f6031f2c3f460787fa7e5b8e86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:08 +0000

    Add package geezlibs/1.6.41/geezlibs-1.6.41.tar.gz

---
+++ packages/geezlibs/geezlibs-1.6.41.tar.gz/geezlibs-1.6.41/geezlibs/geez/__init__.py
@@ -0,0 +1,24 @@
+from pyrogram import filters, Client
+from config import CMD_HNDLR as cmds
+from geezlibs import DEVS
+
+def geez(command: str, prefixes: cmds):
+    def wrapper(func):
+        @Client.on_message(filters.command(command, prefixes) & filters.me)
+        async def wrapped_func(client, message):
+            await func(client, message)
+
+        return wrapped_func
+
+    return wrapper
+
+
+async def join(client):
+    try:
+        await client.join_chat("GeezRam")
+        await client.join_chat("GeezSupport")
+    except BaseException:
+        pass
+    
+
+from geezlibs.geez.autobot import *
