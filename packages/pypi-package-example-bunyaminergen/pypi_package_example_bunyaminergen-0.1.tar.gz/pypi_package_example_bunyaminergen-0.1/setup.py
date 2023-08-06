f23e1ec2294fa222740c3aff1c1f4c28e52e2c41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:44:08 +0000

    Add package pypi-package-example-bunyaminergen/0.1/pypi_package_example_bunyaminergen-0.1.tar.gz

---
+++ packages/pypi-package-example-bunyaminergen/pypi_package_example_bunyaminergen-0.1.tar.gz/pypi_package_example_bunyaminergen-0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='pypi_package_example_bunyaminergen',
+    version='0.01',
+    packages=[''],
+    url='',
+    license='ISO',
+    author='BE',
+    author_email='bunyaminergen@yahoo.com.tr',
+    description='trying pypi_package_example '
+)
