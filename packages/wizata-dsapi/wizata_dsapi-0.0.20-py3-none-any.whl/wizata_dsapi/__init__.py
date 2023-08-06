7b306d62f8d1c75adeb2b2e9a0e8d9996ea6b485
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:44:45 +0000

    Add package wizata-dsapi/0.0.20/wizata_dsapi-0.0.20-py3-none-any.whl

---
+++ packages/wizata-dsapi/wizata_dsapi-0.0.20-py3-none-any.whl/wizata_dsapi/__init__.py
@@ -0,0 +1,6 @@
+from .plot import Plot
+from .mlmodel import MLModel
+from .request import Request
+from .execution import Execution
+from .dsapi_json_encoder import DSAPIEncoder
+from .wizata_dsapi_client import WizataDSAPIClient
