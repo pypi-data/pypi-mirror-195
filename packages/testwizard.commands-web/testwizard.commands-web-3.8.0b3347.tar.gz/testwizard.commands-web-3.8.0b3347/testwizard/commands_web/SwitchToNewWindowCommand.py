92066d1d1fba2b129d89aeb1eb8d3c6d26cc56ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:25:48 +0000

    Add package testwizard.commands-web/3.8.0b3347/testwizard.commands-web-3.8.0b3347.tar.gz

---
+++ packages/testwizard.commands-web/testwizard.commands-web-3.8.0b3347.tar.gz/testwizard.commands-web-3.8.0b3347/testwizard/commands_web/SwitchToNewWindowCommand.py
@@ -0,0 +1,19 @@
+from testwizard.commands_core import CommandBase
+from testwizard.commands_core.OkErrorCodeAndMessageResult import OkErrorCodeAndMessageResult
+
+
+class SwitchToNewWindowCommand(CommandBase):
+    def __init__(self, testObject):
+        CommandBase.__init__(self, testObject, "Selenium.SwitchToNewWindow")
+
+    def execute(self, windowName, url):
+        if windowName is None:
+            raise Exception("window name is required")
+
+        requestObj = [windowName]
+        if url is not None:
+            requestObj = [windowName, url]
+            
+        result = self.executeCommand(requestObj)
+
+        return OkErrorCodeAndMessageResult(result, "SwitchToNewWindow was successful", "SwitchToNewWindow failed")
