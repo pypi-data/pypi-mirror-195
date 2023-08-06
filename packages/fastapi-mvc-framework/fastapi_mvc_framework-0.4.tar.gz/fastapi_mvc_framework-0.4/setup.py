3c8d29b401dfa116b0d0cae63fd5e9ce1c2fb2ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:09:59 +0000

    Add package fastapi-mvc-framework/0.4/fastapi_mvc_framework-0.4.tar.gz

---
+++ packages/fastapi-mvc-framework/fastapi_mvc_framework-0.4.tar.gz/fastapi_mvc_framework-0.4/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+
+setup(
+    name='fastapi_mvc_framework',
+    version='0.4',
+    author='Bruce chou',
+    author_email='smjkzsl@gmail.com',
+    description='MVC framework implemented by fastapi',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+
+    url='https://github.com/smjkzsl/fastapi_framework',
+    packages=['fastapi_mvc_framework'],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
