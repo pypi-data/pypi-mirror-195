bfed9992266a28d22854d899742b14183be75c4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:14:56 +0000

    Add package pinecone-cli/0.4.0/pinecone-cli-0.4.0.tar.gz

---
+++ packages/pinecone-cli/pinecone-cli-0.4.0.tar.gz/pinecone-cli-0.4.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+    name='pinecone-cli',
+    description='pinecone-cli is a command-line client for interacting with the pinecone vector embedding database.',
+    version='0.4.0',
+    url='https://github.com/tullytim/pinecone-cli',
+    author='Tim Tully',
+    author_email='tim@menlovc.com',
+    license='MIT',
+    keywords='pinecone vector vectors embeddings database transformers models',
+    python_requires='>=3',
+    py_modules=['pinecli'],
+    install_requires=[
+        'click', 'pandas', 'numpy', 'openai', 'pinecone-client', 'pinecone-client[grpc]', 'matplotlib', \
+            'scikit-learn', 'beautifulsoup4', 'nltk', 'sklearn', 'rich', 'python-dotenv', 'openai', 'python-dotenv'
+    ],
+    entry_points={
+        'console_scripts': [
+            'pinecli = pinecli:cli',
+        ],
+    },
+)
