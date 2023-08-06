4d8635fa086347ed3a7c427d0c1f8c8abd83c880
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:11:20 +0000

    Add package login-gmail-selenium/1.0.7/login_gmail_selenium-1.0.7.tar.gz

---
+++ packages/login-gmail-selenium/login_gmail_selenium-1.0.7.tar.gz/login_gmail_selenium-1.0.7/test/test.py
@@ -0,0 +1,10 @@
+from login_gmail_selenium.util.profile import ChromeProfile
+import time
+
+
+if __name__ == '__main__':
+    profile = ChromeProfile('lrod44536@gmail.com', 'Pt6o4FWy8k', 'karasalab906008@hotmail.com')
+    driver = profile.retrieve_driver()
+    profile.start()
+    driver.get('https://www.google.com/')
+    time.sleep(100)
