3a1dbe00e68e76adefa456fefc9bca028802c97c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:31:35 +0000

    Add package me3cs/0.1/me3cs-0.1.tar.gz

---
+++ packages/me3cs/me3cs-0.1.tar.gz/me3cs-0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='me3cs',
+    version='0.1',
+    packages=['misc', 'metrics', 'metrics.regression', 'framework', 'framework.helper_classes', 'model_types',
+              'model_types.regression', 'model_types.decomposition', 'missing_data', 'preprocessing',
+              'cross_validation'],
+    url='',
+    license='',
+    author='Daniel Andreas Njust Høj',
+    author_email='d.njust.hoej@gmail.com',
+    description=''
+)
