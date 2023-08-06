175a316e4cade24d60cff004fe2bc20d8bf9807e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:38 +0000

    Add package ikieksfeg/0.0.2/ikieksfeg-0.0.2.tar.gz

---
+++ packages/ikieksfeg/ikieksfeg-0.0.2.tar.gz/ikieksfeg-0.0.2/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.2'
+DESCRIPTION = 'kekw'
+LONG_DESCRIPTION = 'kekwkekwkekwkekwkekwkekw'
+
+# Setting up
+setup(
+    name="ikieksfeg",
+    version=VERSION,
+    author="Jonas",
+    author_email="kekw@kekw.kekw",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python'],
+    classifiers=[
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
