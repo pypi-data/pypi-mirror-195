0db60e73f97f4bc8983cc7d286dceeca172096cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:14:09 +0000

    Add package ego4d/1.3.1/ego4d-1.3.1.tar.gz

---
+++ packages/ego4d/ego4d-1.3.1.tar.gz/ego4d-1.3.1/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# Copyright (c) Meta Platforms, Inc. and affiliates. All Rights Reserved.
+
+from setuptools import find_packages, setup
+
+setup(
+    name="ego4d",
+    version="1.3.1",
+    author="FAIR",
+    author_email="info@ego4d-data.org",
+    description="Ego4D Dataset CLI",
+    url="https://github.com/facebookresearch/Ego4d/",
+    install_requires=[
+        "boto3",
+        "tqdm",
+        "regex",
+    ],
+    tests_require=[
+        "pytest",
+        "moto",
+    ],
+    packages=find_packages(exclude=("tests", "tests.*")),
+    entry_points={
+        "console_scripts": [
+            "ego4d=ego4d.cli.cli:main",
+            "ego4d_internal = ego4d.internal.cli:main",
+        ],
+    },
+)
