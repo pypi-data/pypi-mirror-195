c3e343d9dce6103d060c8cb0af284f7de8748f35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:12:45 +0000

    Add package Mensajes-ALISrj/5.0/Mensajes_ALISrj-5.0-py3-none-any.whl

---
+++ packages/Mensajes-ALISrj/Mensajes_ALISrj-5.0-py3-none-any.whl/mensajes/adios/despedidas.py
@@ -0,0 +1,6 @@
+def despedir():
+    print("Adios me despedio desde despedidas.despedir()")
+    
+class Despedidad():
+    def __init__(self):
+        print("me despido desde Despedida.init()")
---
+++ packages/Mensajes-ALISrj/Mensajes_ALISrj-5.0-py3-none-any.whl/mensajes/hola/saludos.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+def saludar():
+    print("Hola te saludooo desde saludo.saludar()")
+    
+
+class Saludo():
+    
+    def __init__(self):
+        print("Hola te saludooo desde saludo.init()")
+        
+def prueba():
+    print("Saludar")
+    
+def generar_array(numero):
+    return np.arange(numero)
+    
+     
+if __name__ =="__main__":     # Esta linea nos dice si el archivo que lo ejecuta es igual al que lo contiene se ejecuten las demas lineas
+    
+    print(generar_array(5))
