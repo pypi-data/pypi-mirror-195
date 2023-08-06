8972a2a94e2fddfa23cd26a9f096a8c8ea5c794f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:10:22 +0000

    Add package bikash-calculator/0.3/bikash-calculator-0.3.tar.gz

---
+++ packages/bikash-calculator/bikash-calculator-0.3.tar.gz/bikash-calculator-0.3/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+from pathlib import Path
+
+
+setuptools.setup(
+    name='bikash-calculator',
+    version='0.3',
+    author='Bikash Chowdhury',
+    author_email= 'chowdhurybikash38@gmail.com',
+    description='This is very basic calculator',
+    long_description=Path('README.md').read_text(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/bikash829',
+    packages=setuptools.find_packages(),
+    classifiers=[
+    'Programming Language :: Python :: 3',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent',
+    ],
+)
