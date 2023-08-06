fae97bdac1f78873b2f95ac9a6f8d46232933c59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:39:38 +0000

    Add package miaou/0.2.0/miaou-0.2.0.tar.gz

---
+++ packages/miaou/miaou-0.2.0.tar.gz/miaou-0.2.0/test/test.py
@@ -0,0 +1,22 @@
+# -*- coding:utf-8 -*-
+#
+# author: philip1134
+# date: 2023-02-27
+#
+
+
+import miaou
+
+
+miaou.generate(
+    site_url="http://localhost:21250",
+    username="admin",
+    password="Lton2008@",
+    combined_print=True,
+    output_path=".",
+    # scanner="selenium",
+    scanner="api",
+)
+
+
+# end
