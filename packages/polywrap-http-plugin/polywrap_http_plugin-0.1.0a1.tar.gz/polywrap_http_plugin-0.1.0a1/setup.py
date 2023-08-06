272abbfb4b576cb5c622ca1e93cac2a01870ae2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:39 +0000

    Add package polywrap-http-plugin/0.1.0a1/polywrap_http_plugin-0.1.0a1.tar.gz

---
+++ packages/polywrap-http-plugin/polywrap_http_plugin-0.1.0a1.tar.gz/polywrap_http_plugin-0.1.0a1/setup.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['polywrap_http_plugin']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['httpx>=0.23.3,<0.24.0',
+ 'polywrap-client==0.1.0a11',
+ 'polywrap-plugin>=0.1.0a11,<0.2.0']
+
+setup_kwargs = {
+    'name': 'polywrap-http-plugin',
+    'version': '0.1.0a1',
+    'description': '',
+    'long_description': '# Polywrap HTTP Plugin',
+    'author': 'Niraj Kamdar',
+    'author_email': 'niraj@polywrap.io',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
