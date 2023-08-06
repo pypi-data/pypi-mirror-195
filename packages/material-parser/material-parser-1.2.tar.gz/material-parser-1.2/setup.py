8876cf655bdf81fa954a4a2cad7e6d5910f69a35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:33:49 +0000

    Add package material-parser/1.2/material-parser-1.2.tar.gz

---
+++ packages/material-parser/material-parser-1.2.tar.gz/material-parser-1.2/setup.py
@@ -0,0 +1,43 @@
+from codecs import open
+from os import path
+from setuptools import setup
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file.
+#with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+#    long_description = f.read()
+
+#import unittest
+
+
+#def my_test_suite():
+#    test_loader = unittest.TestLoader()
+#    test_suite = test_loader.discover('nerd/tests', pattern='test_*.py')
+#    return test_suite
+
+
+setup(
+    name='material-parser',
+    version='1.2',
+    description='Grobid superconductors tools material parser',
+    url='https://github.com/lfoppiano/grobid-superconductors-tools',
+    author='Luca Foppiano',
+    author_email='FOPPIANO.Luca@nims.go.jp',
+    license='Apache 2.0',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        # 'Topic :: Text processing',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 3.6',
+    ],
+    keywords=['text mining', 'machine learning'],
+    # install_requires=['pymatgen==2021.3.3'],
+    # package_dir={'commons': ''},
+    # packages={'commons'},
+    long_description="Material parser used in grobid-superconductors",
+    zip_safe=False#,
+    #test_suite='setup.my_test_suite'
+
+)
