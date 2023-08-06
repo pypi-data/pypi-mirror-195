621e2d08b9677cd2f3ff4ac927d3cd32af1f84c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:27 +0000

    Add package onsite/0.3.2/onsite-0.3.2.tar.gz

---
+++ packages/onsite/onsite-0.3.2.tar.gz/onsite-0.3.2/setup.py
@@ -0,0 +1,31 @@
+import setuptools 
+ 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+ 
+setuptools.setup(
+    name="onsite", 
+    version="0.3.2",
+    author="Zhou Huajun, Huang Yan",   
+    author_email="huangyan520@tongji.edu.cn",   
+    description="A tool package for Onsite",
+    long_description=long_description,    
+    long_description_content_type="text/markdown",
+    url="https://gitee.com/huangyansmile/onsite-public", 
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License"
+    ],
+    install_requires=[
+        'numpy',
+        'shapely',
+        'pandas',
+        'matplotlib',
+        'scipy',
+        'lxml',
+        'xgboost',
+        'sklearn'
+    ],
+    python_requires='>=3.6',    #对python的最低版本要求
+)
