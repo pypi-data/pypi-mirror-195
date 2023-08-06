95535015c6f37169c412abf44e83131f152149b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:13 +0000

    Add package apollinaire/1.2/apollinaire-1.2-py2.py3-none-any.whl

---
+++ packages/apollinaire/apollinaire-1.2-py2.py3-none-any.whl/apollinaire/__init__.py
@@ -0,0 +1,20 @@
+__author__ = 'Sylvain N. Breton'
+__uri__ = 'https://apollinaire.readthedocs.io'
+__license__ = 'CeCILL'
+__version__ = '1.2'
+__description__ = 'Module for helio- and asteroseismic data analysis'
+__email__ = 'sylvain.breton@inaf.it'
+
+import apollinaire.songlib
+
+import apollinaire.psd
+
+import apollinaire.processing
+
+import apollinaire.synthetic
+
+import apollinaire.peakbagging
+
+import apollinaire.timeseries
+
+import apollinaire.test
