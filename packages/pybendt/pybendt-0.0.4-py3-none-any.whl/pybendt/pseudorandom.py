a256fb3479755bf1f811e671f88abd7ddce0915b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:36:04 +0000

    Add package pybendt/0.0.4/pybendt-0.0.4-py3-none-any.whl

---
+++ packages/pybendt/pybendt-0.0.4-py3-none-any.whl/pybendt/pseudorandom.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+class Pseudorandom():
+	def __init__(self, seed = None, dimensions = 3):
+		if seed is None: self.seed = np.random.randint(2**32-1)
+		else: self.seed = seed
+		self.generator = np.random.RandomState(self.seed)
+		self.draw_count = 0
+		self.dimensions = dimensions
+	def draw(self):
+		self.draw_count += self.dimensions
+		return self.generator.standard_normal(self.dimensions)
