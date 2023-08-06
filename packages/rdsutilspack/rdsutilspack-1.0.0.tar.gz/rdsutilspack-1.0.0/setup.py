0a3c693be98ccfd11b41325e2b8702ff9226ab70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:59:14 +0000

    Add package rdsutilspack/1.0.0/rdsutilspack-1.0.0.tar.gz

---
+++ packages/rdsutilspack/rdsutilspack-1.0.0.tar.gz/rdsutilspack-1.0.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='rdsutilspack',
+    version='1.0.0',
+    description='My first Python package',
+    author='johnson',
+    author_email='johnssimon007@email.com',
+    packages=['rdsutilspack'],
+    install_requires=[
+        'requests',
+        'dnspython',
+    ],
+    entry_points={
+        'console_scripts': [
+            'rdsutilspack=rdsutilspack.__main__:main',
+        ],
+    },
+)
