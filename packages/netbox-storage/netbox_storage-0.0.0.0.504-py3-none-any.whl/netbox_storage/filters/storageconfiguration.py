854697718987edb3f0f6a4c93348f70b44fb7bc7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:17:02 +0000

    Add package netbox-storage/0.0.0.0.504/netbox_storage-0.0.0.0.504-py3-none-any.whl

---
+++ packages/netbox-storage/netbox_storage-0.0.0.0.504-py3-none-any.whl/netbox_storage/filters/storageconfiguration.py
@@ -0,0 +1,16 @@
+from django.db.models import Q
+
+from netbox.filtersets import NetBoxModelFilterSet
+
+from netbox_storage.models import StorageConfiguration
+
+
+class StorageConfigurationFilter(NetBoxModelFilterSet):
+    """Filter capabilities for LogicalVolume instances."""
+
+    class Meta:
+        model = StorageConfiguration
+        fields = [
+            "virtual_machine"
+        ]
+
