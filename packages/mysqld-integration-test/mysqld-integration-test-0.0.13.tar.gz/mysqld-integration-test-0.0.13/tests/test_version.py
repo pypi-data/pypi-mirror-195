7619182f005cbe225ba277f072b6cee2bc6436da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:07 +0000

    Add package mysqld-integration-test/0.0.13/mysqld-integration-test-0.0.13.tar.gz

---
+++ packages/mysqld-integration-test/mysqld-integration-test-0.0.13.tar.gz/mysqld-integration-test-0.0.13/tests/test_version.py
@@ -0,0 +1,9 @@
+from mysqld_integration_test.version import __version__
+
+
+def test_version_isnotnone():
+    assert __version__ is not None
+
+
+def test_version_semver():
+    assert len(__version__.split('.')) == 3
