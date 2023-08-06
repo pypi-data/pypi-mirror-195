f54e35d40e08c06b97ba903acb2bb87936ae1d83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:13:15 +0000

    Add package EchartsLib/0.1.0/EchartsLib-0.1.0-cp37-cp37m-win_amd64.whl

---
+++ packages/EchartsLib/EchartsLib-0.1.0-cp37-cp37m-win_amd64.whl/echartslib/plot/component/__init__.py
@@ -0,0 +1,16 @@
+from .title import Title
+from .style import TextStyle, SubTextStyle
+from .legend import Legend
+from .tooltip import Tooltip
+from .toolbox import Toolbox
+from .datazoom import DatazoomInside, DatazoomSlider
+from .grid import Grid, Grid3D
+from .figsize import Figsize
+from .brush import Brush
+
+__all__ = ["Title", "TextStyle", 'SubTextStyle', 'Legend', 'Grid', 'Grid3D', 'Figsize', 'Brush',
+          'Tooltip', 'Toolbox', 'DatazoomInside', 'DatazoomSlider']
+
+
+def get_all_supported_components():
+    return sorted(__all__)
