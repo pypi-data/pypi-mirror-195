08c5ee719c4da47b2f055e878cc4873933e55ba5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:30:32 +0000

    Add package deeper-rpg/0.1.9/deeper_rpg-0.1.9-cp38-cp38-macosx_10_9_x86_64.macosx_11_0_arm64.macosx_10_9_universal2.whl

---
+++ packages/deeper-rpg/deeper_rpg-0.1.9-cp38-cp38-macosx_10_9_x86_64.macosx_11_0_arm64.macosx_10_9_universal2.whl/deeper/levels/test_level.py
@@ -0,0 +1,32 @@
+import arcade
+
+from ..constants import *
+from ..level import Level
+from deeper import Block
+from deeper.components.sprite_vu import SpriteVu
+from deeper.catalog import Catalog
+
+class TestLevel(Level):
+    def __init__(self, timed=False):
+        super().__init__(timed)
+        self.create_grid()
+        self.create_layer('Floor')
+        self.create_layer('Default')
+
+    def create_grid(self):
+        catalog = Catalog.instance
+        blueprint = catalog.find('Cell')
+        size = glm.vec3(CELL_WIDTH, 0.01, 1)
+        layer = self.create_layer('Grid')
+        for ty in range(0, 16):
+            for tx in range(0, 16):
+                position = glm.vec3(tx * CELL_WIDTH, 0, ty)
+                # position = glm.vec3(tx * CELL_WIDTH - CELL_HALF_WIDTH, 0, ty - CELL_HALF_DEPTH)
+                # print("position: ", position)
+                block = Block(position, size)
+                vu = SpriteVu(
+                    arcade.Sprite(
+                        ':deeper:tiles/_Grid/GRID.png',
+                    )
+                )
+                self.create_entity(layer, blueprint, block, vu)
