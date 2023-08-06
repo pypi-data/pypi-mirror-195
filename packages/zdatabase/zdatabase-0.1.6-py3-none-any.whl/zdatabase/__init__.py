296ca02f0a757bb429ca0eabb835cbc94d17e6bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:09:27 +0000

    Add package zdatabase/0.1.6/zdatabase-0.1.6-py3-none-any.whl

---
+++ packages/zdatabase/zdatabase-0.1.6-py3-none-any.whl/zdatabase/__init__.py
@@ -0,0 +1,20 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData, create_engine
+
+
+class Database(SQLAlchemy):
+    def connect(self, config, db_type):
+        if db_type == 'postgre':
+            url = 'postgresql://{user}:{password}@{host}:{port}/{db}'.format(**config)
+        else:
+            url = 'mysql+pymysql://{user}:{password}@{host}:{port}/{db}?charset=utf8mb4'.format(**config)
+        self.eng = create_engine(url)
+        self.meta = MetaData(bind=self.engine)
+        return url
+
+    def mount(self, app):
+        super().init_app(app)
+
+
+db = Database()
+session = db.session
