e2ac5cc2e30110aa97d95426018530795ecda6b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:32:26 +0000

    Add package chatgpt-cli-tool/1.0.8/chatgpt-cli-tool-1.0.8.tar.gz

---
+++ packages/chatgpt-cli-tool/chatgpt-cli-tool-1.0.8.tar.gz/chatgpt-cli-tool-1.0.8/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+from setuptools import find_packages
+
+desc = "Command line interface tool for interacting with ChatGPT using your favorite terminal"
+
+version = "1.0.8"
+
+setup(name="chatgpt-cli-tool",
+      version=version,
+      author="luka",
+      author_email="lukamatosevic5@gmail.com",
+      url='https://github.com/lmatosevic/chatgpt-cli',
+      download_url=f'https://github.com/lmatosevic/chatgpt-cli/archive/refs/tags/{version}.tar.gz',
+      packages=find_packages(),
+      install_requires=["openai", "python-dotenv"],
+      python_requires='>=3.7.1',
+      entry_points={
+          'console_scripts': [
+              'chatgpt-cli = cli:main.main'
+          ]
+      },
+      keywords=['chatgpt', 'openapi', 'cli', 'chat'],
+      description=desc)
