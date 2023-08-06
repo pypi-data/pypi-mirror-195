6daf0fba753d72fa3aa6b1549113e12724d32e92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:10:06 +0000

    Add package bikash-calculator/0.2/bikash_calculator-0.2-py3-none-any.whl

---
+++ packages/bikash-calculator/bikash_calculator-0.2-py3-none-any.whl/bikash-calculator/calculator.py
@@ -0,0 +1,20 @@
+
+def add_number(a,b):
+    return a + b
+
+ 
+def sub_number(a,b):
+    return a - b
+
+     
+def mul_number(a,b):
+    return a * b
+
+     
+def div_number(a,b):
+    return a / b
+
+
+
+# from mycalculator import calculator
+# calculator.add_number(7,8)
