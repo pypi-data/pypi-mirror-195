e90891d66a85a7e07728b4bbbc2d005f62b18dda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:24:48 +0000

    Add package aib-test/0.2/aib-test-0.2.tar.gz

---
+++ packages/aib-test/aib-test-0.2.tar.gz/aib-test-0.2/aib/pipeline/__init__.py
@@ -0,0 +1,4 @@
+from pipeline.api import *
+from aib.pipeline.artifacts import *
+from pipeline.component import *
+from pipeline.pipeline import *
---
+++ packages/aib-test/aib-test-0.2.tar.gz/aib-test-0.2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='aib-test',
+    version='0.2',
+    description='',
+    long_description='',
+    author='',
+    author_email='',
+    url='',
+    license='MIT',
+    python_requires='>=3.10',
+    install_requires=[
+        "ray==2.2.0",
+        "scikit-learn==1.2.1",
+        "xgboost==1.7.3",
+        "mysql-connector-python==8.0.32",
+        "dill==0.3.6"
+    ],
+    packages=find_packages()
+)
