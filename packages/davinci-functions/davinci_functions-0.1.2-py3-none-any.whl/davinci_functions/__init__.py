42c3c596c48e05c15dbc189d8dfe9cff96e801fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:11:07 +0000

    Add package davinci-functions/0.1.2/davinci_functions-0.1.2-py3-none-any.whl

---
+++ packages/davinci-functions/davinci_functions-0.1.2-py3-none-any.whl/davinci_functions/__init__.py
@@ -0,0 +1,18 @@
+"""Package definition."""
+
+from __future__ import annotations
+
+try:
+    from davinci_functions import _version
+
+    __version__ = _version.__version__
+except Exception:
+    __version__ = ""
+
+from davinci_functions._judge import judge
+from davinci_functions._list import list
+
+__all__ = [
+    "judge",
+    "list",
+]
