d37f5b0a0ce20c26a544102e4888cfd936f1b288
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:23:19 +0000

    Add package auraxium/0.2.3/auraxium-0.2.3-py3-none-any.whl

---
+++ packages/auraxium/auraxium-0.2.3-py3-none-any.whl/auraxium/event/__init__.py
@@ -0,0 +1,38 @@
+"""Event definitions and utilities for the WebSocket event stream.
+
+This module contains the custom :class:`EventClient` that adds support
+for the real-time endpoint, defines the available event types, and
+provides a trigger system used to respond to in-game events.
+"""
+
+from ._client import EventClient
+from ._trigger import Trigger
+from ..models import (AchievementAdded, BattleRankUp, Death, Event,
+                      FacilityControl, GainExperience, ItemAdded,
+                      MetagameEvent, PlayerFacilityCapture,
+                      PlayerFacilityDefend, PlayerLogin, PlayerLogout,
+                      SkillAdded, VehicleDestroy, ContinentLock,
+                      ContinentUnlock)
+
+__all__ = [
+    'Event',
+    'EventClient',
+    'Trigger',
+
+    # Event subclasses
+    'AchievementAdded',
+    'BattleRankUp',
+    'ContinentLock',
+    'ContinentUnlock',
+    'Death',
+    'FacilityControl',
+    'GainExperience',
+    'ItemAdded',
+    'MetagameEvent',
+    'PlayerFacilityCapture',
+    'PlayerFacilityDefend',
+    'PlayerLogin',
+    'PlayerLogout',
+    'SkillAdded',
+    'VehicleDestroy'
+]
