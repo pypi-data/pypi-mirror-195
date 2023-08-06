bc53f19a82781c9b7afac63405ef2c9b9914428d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:27:47 +0000

    Add package llm-kira/0.7.4/llm_kira-0.7.4.tar.gz

---
+++ packages/llm-kira/llm_kira-0.7.4.tar.gz/llm_kira-0.7.4/src/llm_kira/client/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# @Time    : 12/6/22 12:02 PM
+# @FileName: chat.py
+# @Software: PyCharm
+# @Github    ：sudoskys
+
+from .llms import openai
+from .enhance import Support
+from .agent import Conversation, MemoryManager
+from .anchor import ChatBot
