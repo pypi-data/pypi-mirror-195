598fd6c54e01feebaf95a35f1dd6af369d0b6836
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:36:44 +0000

    Add package bcsfe-discord/2.7.12/bcsfe-discord-2.7.12.tar.gz

---
+++ packages/bcsfe-discord/bcsfe-discord-2.7.12.tar.gz/bcsfe-discord-2.7.12/src/BCSFE_Python_Discord/edits/gamototo/gamatoto_xp.py
@@ -0,0 +1,136 @@
+"""Handler for editing gamatoto xp"""
+from typing import Any, Optional
+
+from ... import helper, user_input_handler, item, game_data_getter
+
+
+def get_boundaries(is_jp: bool) -> Optional[list[int]]:
+    """Get the xp requirements for each level"""
+
+    file_data = game_data_getter.get_file_latest(
+        "DataLocal", "GamatotoExpedition.csv", is_jp
+    )
+    if file_data is None:
+        helper.error_text("Failed to get gamatoto xp requirements")
+        return None
+    boundaries = file_data.decode("utf-8").splitlines()
+    previous = 0
+    xp_requirements: list[int] = []
+    previous = 0
+    for line in boundaries:
+        requirement = int(line.split(",")[0])
+        if previous >= requirement:
+            break
+        xp_requirements.append(requirement)
+        previous = requirement
+    return xp_requirements
+
+
+def get_level_from_xp(gamatoto_xp: int, is_jp: bool) -> Optional[dict[str, Any]]:
+    """Get the level from the xp amount"""
+
+    xp_requirements = get_boundaries(is_jp)
+    if xp_requirements is None:
+        return None
+    level = 1
+    for requirement in xp_requirements:
+        if gamatoto_xp >= requirement:
+            level += 1
+    return {
+        "level": level,
+        "max_level": len(xp_requirements),
+        "max_xp": xp_requirements[-2],
+    }
+
+
+def get_xp_from_level(level: int, is_jp: bool) -> Optional[int]:
+    """Get the xp amount from the level"""
+
+    xp_requirements = get_boundaries(is_jp)
+    if xp_requirements is None:
+        return None
+    if level <= 1:
+        gamatoto_xp = 0
+    else:
+        gamatoto_xp = xp_requirements[level - 2]
+    return gamatoto_xp
+
+
+def edit_gamatoto_xp(save_stats: dict[str, Any]) -> dict[str, Any]:
+    """Handler for gamatoto xp"""
+
+    gamatoto_xp = save_stats["gamatoto_xp"]
+
+    data = get_level_from_xp(gamatoto_xp["Value"], helper.check_data_is_jp(save_stats))
+    if data is None:
+        return save_stats
+    level = data["level"]
+
+    helper.colored_text(f"Gamatoto xp: &{gamatoto_xp['Value']}&\nLevel: &{level}&")
+    raw = (
+        user_input_handler.colored_input(
+            "Do you want to edit raw xp(&1&) or the level(&2&)?:"
+        )
+        == "1"
+    )
+
+    if raw:
+        gam_xp = item.Item(
+            name="Gamatoto XP",
+            value=gamatoto_xp["Value"],
+            max_value=None,
+            edit_name="value",
+        )
+        gam_xp.edit()
+        gamatoto_xp["Value"] = gam_xp.value
+    else:
+        gam_level = item.Item(
+            name="Gamatoto Level",
+            value=level,
+            max_value=data["max_level"],
+            edit_name="level",
+        )
+        gam_level.edit()
+        gamatoto_xp["Value"] = get_xp_from_level(
+            int(gam_level.value), helper.check_data_is_jp(save_stats)
+        )
+
+    save_stats["gamatoto_xp"] = gamatoto_xp
+    return save_stats
+
+def edit_gamatoto_xp2(save_stats: dict[str, Any], level: int) -> dict[str, Any]:
+    """Handler for gamatoto xp"""
+
+    gamatoto_xp = save_stats["gamatoto_xp"]
+
+    data = get_level_from_xp(gamatoto_xp["Value"], helper.check_data_is_jp(save_stats))
+    if data is None:
+        return save_stats
+    level = data["level"]
+
+    helper.colored_text(f"Gamatoto xp: &{gamatoto_xp['Value']}&\nLevel: &{level}&")
+    raw = False
+
+    if raw:
+        gam_xp = item.Item(
+            name="Gamatoto XP",
+            value=gamatoto_xp["Value"],
+            max_value=None,
+            edit_name="value",
+        )
+        gam_xp.edit()
+        gamatoto_xp["Value"] = gam_xp.value
+    else:
+        gam_level = item.Item(
+            name="Gamatoto Level",
+            value=level,
+            max_value=data["max_level"],
+            edit_name="level",
+        )
+        gam_level.edit()
+        gamatoto_xp["Value"] = get_xp_from_level(
+            level, helper.check_data_is_jp(save_stats)
+        )
+
+    save_stats["gamatoto_xp"] = gamatoto_xp
+    return save_stats
---
+++ packages/bcsfe-discord/bcsfe-discord-2.7.12.tar.gz/bcsfe-discord-2.7.12/src/BCSFE_Python_Discord/user_input_handler.py
@@ -0,0 +1,234 @@
+"""Handler for user input"""
+
+from typing import Any, Tuple, Union
+
+from . import helper, config_manager
+
+
+def handle_all_at_once(
+    ids: list[int],
+    all_at_once: bool,
+    data: list[int],
+    names: list[Any],
+    item_name: str,
+    group_name: str,
+    explain_text: str = "",
+    value2: str = "",
+) -> list[int]:
+    """Handle all at once option"""
+
+    first = True
+    value = None
+    for item_id in ids:
+        if all_at_once and first:
+            value = int(value2)
+            first = False
+        elif not all_at_once:
+            value = helper.check_int(
+                value2
+            )
+        if value is None:
+            continue
+        data[item_id] = value
+    return data
+
+
+def create_all_list(
+    ids: list[str],
+    max_val: int,
+) -> dict[str, Any]:
+    """Creates a list with an all at once option"""
+
+    all_at_once = False
+    if f"{max_val}" in ids:
+        ids_s = list(range(1, max_val))
+        ids = [format(x, "02d") for x in ids_s]
+        all_at_once = True
+    return {"ids": ids, "at_once": all_at_once}
+
+
+def create_all_list_inc(ids: list[str], max_val: int) -> dict[str, Any]:
+    """Creates a list with an all at once option and include all"""
+
+    return create_all_list(ids, max_val)
+
+
+def create_all_list_not_inc(ids: list[str], max_val: int) -> list[str]:
+    """Creates a list with an all at once option and don't include all"""
+
+    return create_all_list(ids, max_val)["ids"]
+
+
+def get_range(
+    usr_input: str,
+    length: Union[int, None] = None,
+    min_val: int = 0,
+    all_ids: Union[list[int], None] = None,
+) -> list[int]:
+    """Get a range of numbers from user input"""
+
+    ids: list[int] = []
+    for item in usr_input.split(" "):
+        if item.lower() == "all":
+            if length is None and all_ids is None:
+                helper.colored_text("You can't use &all& here", helper.RED)
+                return []
+            if all_ids:
+                return all_ids
+            if length is not None:
+                return list(range(min_val, length))
+        if "-" in item:
+            start_s, end_s = item.split("-")
+            start = helper.check_int(start_s)
+            end = helper.check_int(end_s)
+            if start is None or end is None:
+                helper.colored_text(
+                    "Invalid input. Please enter a valid range of numbers separated by a dash.",
+                    helper.RED,
+                )
+                return ids
+            if start > end:
+                start, end = end, start
+            ids.extend(list(range(start, end + 1)))
+        else:
+            item_id = helper.check_int(item)
+            if item_id is None:
+                helper.colored_text(
+                    "Invalid input. Please enter a valid integer.", helper.RED
+                )
+                return ids
+            ids.append(item_id)
+    return ids
+
+
+def colored_input(dialog: str) -> str:
+    """Format dialog as a colored string"""
+
+    helper.colored_text(dialog, end="")
+    return input()
+
+
+def get_range_ids(group_name: str, length: int) -> list[int]:
+    """Get a range of ids from user input"""
+
+    ids = get_range(
+        colored_input(
+            f"Enter {group_name} ids(You can enter &all& to get all, a range e.g &1&-&50&, or ids separate by spaces e.g &5 4 7&):"
+        ),
+        length,
+    )
+    return ids
+
+
+def select_options(
+    options: list[str],
+    mode: str = "edit",
+    extra_data: Union[list[Any], None] = None,
+    offset: int = 0,
+) -> Tuple[list[int], bool]:
+    """Select an option or multiple options from a list"""
+
+    if len(options) == 1:
+        return [0], True
+
+    helper.colored_list(options, extra_data=extra_data, offset=offset)
+    total = len(options)
+    helper.colored_text(f"{total+1}. &Select all&")
+    ids_s = colored_input(
+        f"What do you want to {mode} (You can enter multiple values separated by spaces to {mode} multiple at once):"
+    ).split(" ")
+    individual = True
+    if str(total + 1) in ids_s:
+        ids = list(range(1, total + 1))
+        individual = False
+        ids_s = helper.int_to_str_ls(ids)
+
+    ids = helper.parse_int_list(ids_s, -1)
+    for item_id in ids:
+        if item_id < 0 or item_id > total - 1:
+            helper.colored_text(
+                f"Invalid input. Please enter a valid integer between 1 and {total+1}.",
+                helper.RED,
+            )
+            return select_options(options, mode, extra_data, offset)
+    return ids, individual
+
+
+def select_inc(
+    options: list[str],
+    mode: str = "edit",
+    extra_data: Union[list[Any], None] = None,
+    offset: int = 0,
+) -> Tuple[list[int], bool]:
+    """Select an option or multiple options from a list and include all"""
+
+    return select_options(options, mode, extra_data, offset)
+
+
+def select_not_inc(
+    options: list[str],
+    mode: str = "edit",
+    extra_data: Union[list[Any], None] = None,
+    offset: int = 0,
+) -> list[int]:
+    """Select an option or multiple options from a list and don't include all"""
+    return select_options(options, mode, extra_data, offset)[0]
+
+
+def select_single(
+    options: list[str],
+    mode: str = "edit",
+    title: str = "",
+    allow_text: bool = False,
+) -> int:
+    "Select a single option from a list"
+    if not options:
+        raise ValueError("No options to select from")
+    if len(options) == 1:
+        return 1
+    helper.colored_list(options)
+    if not title:
+        title = f"Select an option to {mode}:"
+    val = colored_input(title)
+    if allow_text:
+        if val in options:
+            return options.index(val) + 1
+    val = helper.check_int(val)
+    if val is None:
+        helper.colored_text("Invalid input. Please enter a valid integer.", helper.RED)
+        return select_single(options, mode, title, allow_text)
+    if val < 1 or val > len(options):
+        helper.colored_text(
+            f"Invalid input. Please enter an integer between 1 and {len(options)}.",
+            helper.RED,
+        )
+        return select_single(options, mode, title, allow_text)
+    return val
+
+
+def get_int(dialog: str) -> int:
+    """Get user input as an integer and keep asking until a valid integer is entered"""
+
+    helper.colored_text(dialog, end="")
+    while True:
+        try:
+            val = input()
+            val = val.strip(" ")
+            return int(val)
+        except ValueError:
+            helper.colored_text(
+                "Invalid input. Please enter a valid integer.", helper.RED
+            )
+
+
+def ask_if_individual(item_name: str) -> bool:
+    """Ask if the user wants to edit an individual item"""
+
+    is_individual = (
+        colored_input(
+            f"Do you want to edit {item_name} individually (&1&), or all at once (&2&):"
+        )
+        == "1"
+    )
+    print(is_individual)
+    return is_individual
