f79fec1851d2b47126e8714b8df98e87c7ad3c1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:13:56 +0000

    Add package sushipy/0.1.1/sushipy-0.1.1-py3-none-any.whl

---
+++ packages/sushipy/sushipy-0.1.1-py3-none-any.whl/sushipy/execute.py
@@ -0,0 +1,119 @@
+"""
+executes functions from another language
+"""
+
+# pylint: disable=invalid-name, redefined-outer-name, global-statement, missing-class-docstring
+
+import configparser
+import inspect
+import re
+import shlex
+import subprocess
+from dataclasses import dataclass
+from os import remove, system
+from os.path import isfile
+
+from .cache.main import Cache
+
+# pylint: disable=import-error
+if isfile("sushicache.py"):
+    import sushicache
+# pylint: enable=import-error
+
+
+config = configparser.ConfigParser()
+config.read("sushi.conf")
+
+main_config = config["main"]
+launch_config = config["launch"]
+
+TEMP_FILE = ""
+INIT_ARGS = ()
+
+
+@dataclass
+class TranslateData:
+    import_syntax: str
+    file_name: str
+    call_function: str
+
+
+class Execute:
+    """executes function"""
+
+    def translate(self, data: TranslateData):
+        """translates string (multiple replace)"""
+
+        translate_data = {
+            "$SUSHI_IMPORT": data.import_syntax.replace("[file-name]", data.file_name),
+            "$SUSHI_FUNCTION": data.call_function,
+            "$SUSHI_ARGS": INIT_ARGS,
+            "$SUSHI_SEMICOLON": ";",
+            "$SUSHI_NEWLINE": "\n",
+        }
+
+        global TEMP_FILE
+        for i, j in translate_data.items():
+            TEMP_FILE = TEMP_FILE.replace(i, j)
+
+    def __init__(self, *args, **kwargs) -> None:
+        global INIT_ARGS
+
+        # get from what function was this called
+        call_function = inspect.stack()[1].function
+
+        INIT_ARGS = re.sub("[()]", "", rf"{args}".replace(",", ""))
+        import_syntax = launch_config["import_syntax"]
+
+        file_name = main_config["lib_path"].split("/")[-1]
+        if main_config["lib_path"][-1] == "*":
+            # user selected multiple files
+            file_name = main_config["lib_path"].replace("*", kwargs.get("file"))
+            file_name = file_name.split("/")[-1]
+
+        global TEMP_FILE
+        TEMP_FILE = config["temp_file"]["temp_file"]
+
+        data = TranslateData(import_syntax, file_name, call_function)
+        self.translate(data)
+
+        self.function()
+
+    def function(self):
+        """runs function from another language"""
+
+        path = main_config["lib_path"].split("/")[0]
+
+        if sushicache.LAST_EXECUTED_CODE == TEMP_FILE:
+            system(f"./{path}/out")
+            return
+
+        # create temporary file
+        temp_extension = config["temp_file"]["extension"]
+
+        with open(
+            file=f"{path}/temp.{temp_extension}", mode="w", encoding="UTF-8"
+        ) as f:
+            f.write(TEMP_FILE)
+        f.close()
+
+        subprocess.call(
+            shlex.split(
+                launch_config["exec_command"].replace(
+                    "[file-name]", f"lib/temp.{temp_extension}"
+                )
+            ),
+            shell=False,
+        )
+
+        Cache.update(
+            Cache,
+            f"LAST_EXECUTED_CODE = {sushicache.LAST_EXECUTED_CODE}",
+            f'LAST_EXECUTED_CODE = """{TEMP_FILE}"""',
+        )
+
+        # remove temp file
+        remove(f"{path}/temp.{temp_extension}")
+        subprocess.call([f"./{path}/out"], shell=False)
+
+        return
