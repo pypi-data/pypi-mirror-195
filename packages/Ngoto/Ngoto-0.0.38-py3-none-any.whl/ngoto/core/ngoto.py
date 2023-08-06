bc527a3ce7af7aec2dced1380b9c7af1c7490742
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:32:54 +0000

    Add package Ngoto/0.0.38/Ngoto-0.0.38-py3-none-any.whl

---
+++ packages/Ngoto/Ngoto-0.0.38-py3-none-any.whl/ngoto/core/ngoto.py
@@ -0,0 +1,121 @@
+from ngoto.core.util.node import Node
+from ngoto.core.util.logging import Logging
+from ngoto.core.util.task_controller import TaskController
+from ngoto.core.util.interface import output, get_input
+from ngoto.core.decorators import Command, Plugin, Task
+from concurrent.futures import ThreadPoolExecutor
+from ngoto import cogs as inbuilt_cogs
+from sys import platform
+import os
+from time import sleep, time
+
+
+def get_object_from_method(method):
+    """ Returns object from method """
+    num_args = method.__code__.co_argcount
+    args = []
+    for _ in range(num_args):
+        args.append('')
+    return method(*args)
+
+
+class Ngoto:
+    """ Base ngoto class for implementations of ngoto """
+    curr_pos: Node = None  # current position in plugin tree
+    logger: Logging
+    os: str = None  # eg 'Linux', 'Windows', 'MacOS'
+    tasks = TaskController()
+    commands = []
+
+    def __init__(self):
+        if platform == "linux" or platform == "linux2":
+            self.os = "Linux"
+        elif platform == "darwin":
+            self.os = "MacOS"
+        elif platform == "win32":
+            self.os = "Windows"
+
+        self.logger = Logging()
+        self.curr_pos = Node('root')
+        cogs = [getattr(inbuilt_cogs, cog)() for cog in inbuilt_cogs.__all__]
+        self.load_cogs(cogs)
+
+    def add_plugin(self, plugin: Plugin, location: str) -> None:
+        """ Add plugin to the correct point in tree """
+        curr_node = self.curr_pos
+        for folder in location.split('/'):
+            if folder != '':
+                # if folder exists in tree
+                if curr_node.has_child(folder):
+                    curr_node = curr_node.get_child_from_name(folder)
+                else:  # if folder does not exist in tree
+                    new_node = Node(folder)
+                    curr_node.add_child(new_node)
+                    curr_node = new_node
+        curr_node.add_plugin(plugin)
+
+    def load_cogs(self, cog_classes: list):
+        """ Load cogs from list of classes """
+        for cog in cog_classes:
+            for method in dir(cog):
+                if method[0] != '_':
+                    method = getattr(cog, method)
+                    method_object = get_object_from_method(method)
+                    if isinstance(method_object, Command):
+                        self.commands.append(method_object)
+                    elif isinstance(method_object, Task):
+                        method_object.logger = self.logger
+                        self.tasks.add_task(method_object)
+                    elif isinstance(method_object, Plugin):
+                        self.add_plugin(method_object, method_object.folder)
+
+    def load_cogs_from_folder(self, folder) -> None:
+        """Load all commands in commands folders __init__ __all__ list """
+        cogs, files_paths = [], []
+        for c in os.listdir(folder):
+            if c.endswith('.py') and not c.startswith('__'):
+                files_paths.append(c)
+        for files_path in files_paths:
+            complete_path = folder + '/' + files_path
+            complete_path = complete_path.replace('/', '.')[:-3]
+            # load file and call setup function
+            module = __import__(complete_path, fromlist=['setup'])
+            cogs.append(module.setup())
+        self.load_cogs(cogs)
+
+    def run_command(self, command: str, options: list = []) -> bool:
+        if command.isdigit():
+            if (num := int(options[0])-1) < self.curr_pos.num_children:
+                command = 'openFolder'
+            elif num < self.curr_pos.num_children + self.curr_pos.num_plugins:
+                command = 'openPlugin'
+        for cmd in self.commands:
+            if command in cmd.aliases or command == cmd.name:
+                pos = cmd.func(self, self.curr_pos, options, self.logger)
+                if pos:
+                    self.curr_pos = pos
+                return True
+        return False
+
+    def clt(self):
+        """ CLT loop"""
+        option = get_input('\n[Ngoto] > ').split()
+        if not option:
+            pass
+        elif not self.run_command(option[0], option):
+            output("Unknown command")
+        self.clt()
+
+    def start(self) -> None:
+        """ Main loop """
+        self.run_command('clear')
+        self.run_command('options')
+        with ThreadPoolExecutor(max_workers=3) as executor:
+            clt_loop = executor.submit(self.clt)
+            while True:
+                curr_time = time()
+                self.tasks.check_available_tasks(executor, curr_time, self.os)
+                self.tasks.check_running_tasks(self.logger)
+                if clt_loop.done():
+                    break
+                sleep(1 - (time() - curr_time))
---
+++ packages/Ngoto/Ngoto-0.0.38-py3-none-any.whl/ngoto/core/util/interface.py
@@ -0,0 +1,127 @@
+# script contains interface functions
+from ngoto.core.util.rich.console import Console
+from ngoto.core.util.rich.table import Table  # used in this plugin
+from ngoto.core.util.rich.style import Style  # used in this plugin
+import os
+
+console: Console = Console()
+logo_style = Style(color="blue", blink=False, bold=True)
+exit_style = Style(color="blue", blink=False, bold=True)
+folder_style = Style(color="yellow", blink=False, bold=True)
+plugin_style = Style(color="magenta", blink=False, bold=True)
+input_style = Style(color="cyan", blink=False, bold=True)
+output_style = Style(color="blue", blink=False, bold=True)
+greyed_out_style = Style(color="grey30", blink=False, bold=False)
+
+title_style = Style(color="blue", blink=False, bold=True)
+border_style = Style(color="black", blink=False, bold=True)
+header_style = Style(color="black", blink=False, bold=True)
+
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+
+def output(output, style=Style()):
+    console.print(output, style=style, highlight=False)
+
+
+def get_input(text=''):
+    """ Input Method """
+    return console.input('[blue][bold]' + text + '[/bold][/blue]')
+
+
+def logo():
+    output('''
+ ██████   █████                    █████
+░░██████ ░░███                    ░░███
+ ░███░███ ░███   ███████  ██████  ███████    ██████
+ ░███░░███░███  ███░░███ ███░░███░░░███░    ███░░███
+ ░███ ░░██████ ░███ ░███░███ ░███  ░███    ░███ ░███
+ ░███  ░░█████ ░███ ░███░███ ░███  ░███ ███░███ ░███
+ █████  ░░█████░░███████░░██████   ░░█████ ░░██████
+░░░░░    ░░░░░  ░░░░░███ ░░░░░░     ░░░░░   ░░░░░░
+                ███ ░███
+               ░░██████
+''', logo_style)
+
+
+def show_options(curr_node):  # given Node in plugin
+    logo()
+    index = 1
+    for folder in curr_node.get_children():  # print folders
+        output(f'{index}. {folder.name}', style=folder_style)
+        index += 1
+    for plugin in curr_node.get_plugins():  # print plugins
+        output(f'{index}. {plugin.name}', style=plugin_style)
+        index += 1
+
+
+def show_commands(commands: list):
+    try:
+        table = Table(
+            title="Ngoto Commands",
+            title_style=title_style,
+            border_style=border_style)
+        table.add_column("Commands", style=output_style)
+        table.add_column("Description", style=output_style)
+        for command in commands:
+            actions = str(
+                command.aliases
+                ).replace("'", "").replace("[", "").replace("]", "")
+            table.add_row(command.name + ', ' + actions, command.desc)
+        table.add_row("t/task d {task_id}/all", "Disable a task")
+        table.add_row("t/task e {task_id}/all", "Enable a task")
+        table.add_row("t/task delay {task_id}/all {delay}", "Set a task delay")
+        output(table)
+    except Exception as e:
+        output(e)
+
+
+def show_tasks(tasks: list, thisOS: str):
+    table = Table(
+        title="Ngoto Tasks",
+        title_style=title_style,
+        border_style=border_style)
+    table.add_column("ID", style=output_style)
+    table.add_column("Delay", style=output_style)
+    table.add_column("Description", style=output_style)
+    table.add_column("Last Output", style=output_style)
+    table.add_column("Ran", style=output_style)
+    table.add_column("Enabled", style=output_style)
+    table.add_column("OS", style=output_style)
+
+    for task in tasks:
+        if thisOS in task.os:
+            table.add_row(
+                task.id,
+                str(task.delay),
+                task.desc,
+                task.last_output,
+                str(task.iteration),
+                str(task.active),
+                str(task.os))
+        else:
+            table.add_row(
+                task.id,
+                str(task.delay),
+                task.desc,
+                task.last_output,
+                str(task.iteration),
+                "False",
+                str(task.os),
+                style=greyed_out_style)
+    output(table)
+
+
+def clear_screen():
+    os.system('cls' if os.name in ('nt', 'dos') else 'clear')
+    return True
