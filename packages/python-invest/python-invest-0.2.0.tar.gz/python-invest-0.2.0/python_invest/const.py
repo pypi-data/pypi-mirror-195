22719927a45d8865ff5bce1374847bf494878860
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:12:48 +0000

    Add package python-invest/0.2.0/python-invest-0.2.0.tar.gz

---
+++ packages/python-invest/python-invest-0.2.0.tar.gz/python-invest-0.2.0/python_invest/const.py
@@ -0,0 +1,12 @@
+# API URL
+API_URL = 'http://api.scraperlink.com/investpy/?email={email}&{args}'
+
+# PRODUCTS
+PRODUCT_LIST = ['cryptos', 'stocks', 'funds']
+
+# Time Frame
+TIME_FRAME_LIST = ['daily', 'monthly', 'weekly']
+
+# Firestore API
+FB_BASE_URL = 'https://firestore.googleapis.com/v1/projects/python-invest-67b1c/databases/(default)/documents/'
+FB_CRYPTO_DOC_ID = 'crypto/IEVxWEmy32J3FDlvq9Cj'
