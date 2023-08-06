50060e4f2236a5125819fe2b4e687d210c0ab687
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:05 +0000

    Add package simple-dyn365/1.5/simple-dyn365-1.5.tar.gz

---
+++ packages/simple-dyn365/simple-dyn365-1.5.tar.gz/simple-dyn365-1.5/setup.py
@@ -0,0 +1,31 @@
+import textwrap
+from distutils.core import setup
+
+setup(
+  name = 'simple-dyn365',
+  packages = ['simple_dyn365'],
+  version = '1.5',
+  license='MIT',
+  description = 'A simple Dynamics 365 Web API implementation',
+  author = 'Adam Mahameed',
+  long_description=textwrap.dedent(open('README.rst', 'r').read()),
+  long_description_content_type='text/x-rst',
+  author_email = 'adam.mah315@gmail.com',
+  url = 'https://github.com/adam-mah/simple-dyn365',
+  download_url = 'https://github.com/adam-mah/simple-dyn365/archive/refs/tags/1.5.tar.gz',
+  keywords = ['simple', 'dyn365', 'dynamics', 'WebAPI'],
+  install_requires=['requests'],
+  classifiers=[
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'Intended Audience :: System Administrators',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+    'Programming Language :: Python :: Implementation :: PyPy'
+  ],
+)
