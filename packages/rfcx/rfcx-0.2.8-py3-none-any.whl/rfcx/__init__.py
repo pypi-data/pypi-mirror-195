b00b01a624508a01d722d6b0b32bb4b29d19d0f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:29:45 +0000

    Add package rfcx/0.2.8/rfcx-0.2.8-py3-none-any.whl

---
+++ packages/rfcx/rfcx-0.2.8-py3-none-any.whl/rfcx/__init__.py
@@ -0,0 +1,11 @@
+"""## Getting started
+
+1. Get the latest version from [Rainforest Connection's Github releases page](https://github.com/rfcx/rfcx-sdk-python/releases)
+
+2. Install with pip: `pip install rfcx-x.x.x-py3-none-any.whl`
+
+3. Use `import rfcx`
+"""
+
+from .client import Client
+name = "rfcx"
