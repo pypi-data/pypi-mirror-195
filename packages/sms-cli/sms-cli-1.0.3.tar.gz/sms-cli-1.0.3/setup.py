54a871318b6c32b1e9e1a8ca58fd6dad400cc2ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:29:58 +0000

    Add package sms-cli/1.0.3/sms-cli-1.0.3.tar.gz

---
+++ packages/sms-cli/sms-cli-1.0.3.tar.gz/sms-cli-1.0.3/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+from setuptools import find_packages
+
+desc = "Command line interface for sending AT (ATtention) commands via serial port to GSM shield module."
+
+version = "1.0.3"
+
+setup(name="sms-cli",
+      version=version,
+      author="luka",
+      author_email="lukamatosevic5@gmail.com",
+      url="https://github.com/lmatosevic/sms-cli",
+      download_url=f"https://github.com/lmatosevic/sms-cli/archive/refs/tags/{version}.tar.gz",
+      packages=find_packages(),
+      install_requires=["pyserial", "argparse"],
+      python_requires=">=3.6",
+      entry_points={
+          "console_scripts": [
+              "sms-cli = cli.main:main"
+          ]
+      },
+      keywords=["sms", "cli", "messaging", "gsm"],
+      description=desc)
